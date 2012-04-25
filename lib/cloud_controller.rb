4bf441f5bfe9576b4946695385b47fd962b348aa
Patrick Bozeman <pbozeman@vmware.com>
2012-04-25 12:59:14 -0700

    quote normalization.
    Change-Id: I70a7d9e9829cea81d67ddd0f7de87b3c6b54f3fe

--- lib/cloud_controller.rb
+++ lib/cloud_controller.rb
@@ -1,13 +1,13 @@
 # Copyright (c) 2009-2012 VMware, Inc.
 
-require 'sequel'
-require 'vcap/logging'
+require "sequel"
+require "vcap/logging"
 
 module VCAP
   module CloudController; end
 end
 
-require 'cloud_controller/db'
+require "cloud_controller/db"
 require "sequel_plugins/vcap_validations"
 require "sequel_plugins/vcap_serialization"
 require "sequel_plugins/vcap_normalization"
