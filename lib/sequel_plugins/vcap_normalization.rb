8decd3b6dbe3c1bc3a6627a7f6ecbca8bb19bc82
Patrick Bozeman <pbozeman@vmware.com>
2012-04-26 17:46:38 -0700

    vcap normalization for sequel models
    Change-Id: I8d15220ba924880d3122164a1851ac0b507eb639

---
+++ lib/sequel_plugins/vcap_normalization.rb
@@ -0,0 +1,31 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+module Sequel::Plugins::VcapNormalization
+  module InstanceMethods
+    # Strips attributes of the model if requested.
+    def []=(k, v)
+      v = strip_if_needed(k, v)
+      super(k, v)
+    end
+
+    private
+
+    def strip_if_needed(k, v)
+      strip_attrs = self.class.strip_attrs || {}
+      v = v.strip if strip_attrs.include?(k) and v.respond_to?(:strip)
+      v
+    end
+  end
+
+  module ClassMethods
+    # Specify the attributes to perform whitespace normaliation on
+    #
+    # @param [Array<Symbol>] List of attributes to include when performing
+    # whitespace normalization.
+    def strip_attributes(*attributes)
+      self.strip_attrs = attributes
+    end
+
+    attr_accessor :strip_attrs
+  end
+end
---
+++ spec/sequel_plugins/vcap_normalization_spec.rb
@@ -0,0 +1,41 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+require File.expand_path("../spec_helper", __FILE__)
+
+describe "Sequel::Plugins::VcapNormalization" do
+  before do
+    db = Sequel.sqlite
+
+    db.create_table :test do
+      primary_key :id
+
+      String :val1
+      String :val2
+      String :val3
+    end
+
+    @c = Class.new(Sequel::Model)
+    @c.plugin :vcap_normalization
+    @c.set_dataset(db[:test])
+    @m = @c.new
+  end
+
+  describe "#strip_attributes" do
+    it "should not cause anything to be normalized if not called" do
+      @m.val1 = "hi "
+      @m.val2 = " bye"
+      @m.val1.should == "hi "
+      @m.val2.should == " bye"
+    end
+
+    it "should only result in provided strings being normalized" do
+      @c.strip_attributes :val2, :val3
+      @m.val1 = "hi "
+      @m.val2 = " bye"
+      @m.val3 = " with spaces "
+      @m.val1.should == "hi "
+      @m.val2.should == "bye"
+      @m.val3.should == "with spaces"
+    end
+  end
+end
