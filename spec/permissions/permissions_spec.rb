8a81f9f84db665b6cdacb0fb98012fdeb2e70272
Patrick Bozeman <pbozeman@vmware.com>
2012-05-09 12:49:34 -0700

    basic permission implementation
    Change-Id: I0e98eec8146ec61b9119bb5f33ce8e5c3b8ab3cb

---
+++ spec/permissions/permissions_spec.rb
@@ -0,0 +1,24 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+require File.expand_path("../spec_helper", __FILE__)
+
+describe VCAP::CloudController::Permissions do
+  let(:obj) { VCAP::CloudController::Models::Organization.make }
+  let(:user) { VCAP::CloudController::Models::User.make }
+  let(:admin) { VCAP::CloudController::Models::User.make(:admin => true) }
+
+  describe "#permissions_for" do
+    it "should return [Permission::Anonymous] only for a nil user" do
+      Permissions.permissions_for(obj, nil).should == [Permissions::Anonymous]
+    end
+
+    it "should return [Permission::Authenticated] for a standard user" do
+      Permissions.permissions_for(obj, user).should == [Permissions::Authenticated]
+    end
+
+    it "should return [Permission::Authenticated, Permission::CFAdmin] for an admin" do
+      Permissions.permissions_for(obj, admin).should == [Permissions::Authenticated,
+                                                         Permissions::CFAdmin]
+    end
+  end
+end
