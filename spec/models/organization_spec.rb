6f71b0deee652f0c9177a188408a93937c2a0435
Patrick Bozeman <pbozeman@vmware.com>
2012-05-21 23:27:32 -0700

    BillingManager implementation
    Change-Id: Ifda2ecdb0890f8a1c16abaf2df2408b51143d83a

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -10,6 +10,7 @@ describe VCAP::CloudController::Models::Organization do
     :many_to_zero_or_more => {
       :users      => lambda { |org| VCAP::CloudController::Models::User.make },
       :managers   => lambda { |org| VCAP::CloudController::Models::User.make },
+      :billing_managers => lambda { |org| VCAP::CloudController::Models::User.make },
     },
     :one_to_zero_or_more => {
       :app_spaces => lambda { |org| VCAP::CloudController::Models::AppSpace.make }
