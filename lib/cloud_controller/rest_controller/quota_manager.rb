74cd61ca6c59fcfbb6ef8ee98ac3bf8342cff4cd
Patrick Bozeman <pbozeman@vmware.com>
2012-05-31 10:52:52 -0700

    in progress call to quota mgr
    Change-Id: Ie2c2594da9642fdf00881115dae60a2305af6481

--- lib/cloud_controller/rest_controller/quota_manager.rb
+++ lib/cloud_controller/rest_controller/quota_manager.rb
@@ -1,47 +1,69 @@
 # Copyright (c) 2009-2012 VMware, Inc.
 
+require "restclient"
+
 module VCAP::CloudController::RestController
   module QuotaManager
-    class BlindApproval
-      class ApprovedToken
-        def approved?
-          true
-        end
-
-        def declined?
-          false
-        end
-
-        def reason
-        end
-
-        def abandon(reason)
-        end
-
-        def commit
-        end
+    class BlindApprovalToken
+      def approved?
+        true
       end
 
-      def self.fetch_quota_token(request_body)
-        ApprovedToken.new
+      def declined?
+        false
+      end
+
+      def reason
+      end
+
+      def abandon(reason)
+      end
+
+      def commit
+      end
+    end
+
+    class BlindApproval
+      def self.fetch_quota_token(quota_request)
+        BlindApprovalToken.new
       end
     end
 
     class MoneyMakerApproval
-      # TODO
+      class << self
+        def fetch_quota_token(quota_request)
+          url     = quota_manager_url(quota_request[:path])
+          body    = Yajl::Encoder.encode(quota_request[:body])
+          headers = {
+            :content_type  => :json,
+            :accept        => :json,
+            :authorization => "testtoken"
+          }
+
+          response = RestClient.post(url, body, headers)
+        rescue Exception => e
+          BlindApprovalToken.new
+        end
+
+        def quota_manager_url(path)
+          "http://localhost:31004#{path}"
+        end
+      end
     end
 
     class << self
       def policy
-        @policy ||= BlindApproval
+        # @policy ||= BlindApproval
+        @policy ||= MoneyMakerApproval
       end
 
       def policy=(policy)
         @policy = policy
       end
 
-      def fetch_quota_token(request_body)
-        policy.fetch_quota_token(request_body)
+      def fetch_quota_token(quota_request)
+        p = quota_request ? policy : BlindApproval
+        p.fetch_quota_token(quota_request)
       end
     end
   end
