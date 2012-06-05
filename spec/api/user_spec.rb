0b549da907457a49c47a20e26449532ea9374e72
Patrick Bozeman <pbozeman@vmware.com>
2012-06-05 13:41:24 -0700

    validate export of attributes in api for read
    Change-Id: Iad33d42772ff28efcf1772892e8043f17613a1ce

--- spec/api/user_spec.rb
+++ spec/api/user_spec.rb
@@ -7,7 +7,6 @@ describe VCAP::CloudController::User do
   it_behaves_like "a CloudController API", {
     :path                 => "/v2/users",
     :model                => VCAP::CloudController::Models::User,
-    :basic_attributes     => :guid,
     :required_attributes  => :guid,
     :unique_attributes    => :guid,
     :many_to_many_collection_ids => {
