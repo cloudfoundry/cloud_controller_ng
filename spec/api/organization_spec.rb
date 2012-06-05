03c4b1959f8501e17edf97c4e9493aa7d7f138a1
Patrick Bozeman <pbozeman@vmware.com>
2012-06-05 12:49:32 -0700

    Put back spec that got commented out
    Change-Id: I7e07b08a18436d753a2c3c0b8127b2898161fac1

--- spec/api/organization_spec.rb
+++ spec/api/organization_spec.rb
@@ -5,21 +5,21 @@ require File.expand_path("../spec_helper", __FILE__)
 describe VCAP::CloudController::Organization do
   let(:org)   { VCAP::CloudController::Models::Organization.make }
 
-  # it_behaves_like "a CloudController API", {
-  #   :path                => "/v2/organizations",
-  #   :model               => VCAP::CloudController::Models::Organization,
-  #   :basic_attributes    => :name,
-  #   :required_attributes => :name,
-  #   :unique_attributes   => :name,
-  #   :many_to_many_collection_ids => {
-  #     :users    => lambda { |org| VCAP::CloudController::Models::User.make },
-  #     :managers => lambda { |org| VCAP::CloudController::Models::User.make },
-  #     :billing_managers => lambda { |org| VCAP::CloudController::Models::User.make }
-  #   },
-  #   :one_to_many_collection_ids  => {
-  #     :app_spaces => lambda { |org| VCAP::CloudController::Models::AppSpace.make }
-  #   }
-  # }
+  it_behaves_like "a CloudController API", {
+    :path                => "/v2/organizations",
+    :model               => VCAP::CloudController::Models::Organization,
+    :basic_attributes    => :name,
+    :required_attributes => :name,
+    :unique_attributes   => :name,
+    :many_to_many_collection_ids => {
+      :users    => lambda { |org| VCAP::CloudController::Models::User.make },
+      :managers => lambda { |org| VCAP::CloudController::Models::User.make },
+      :billing_managers => lambda { |org| VCAP::CloudController::Models::User.make }
+    },
+    :one_to_many_collection_ids  => {
+      :app_spaces => lambda { |org| VCAP::CloudController::Models::AppSpace.make }
+    }
+  }
 
   shared_examples "enumerate orgs ok" do |perm_name|
     describe "GET /v2/organizations" do
