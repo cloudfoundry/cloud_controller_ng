d82e8e4cef192db016f9c01f1d19843bc7ab90f2
Patrick Bozeman <pbozeman@gmail.com>
2012-06-27 12:55:54 -0700

    4 part service parms for quota call
    Change-Id: I898f77d21208934c98de115d2617192a3768cfbc

--- lib/cloud_controller/api/service_instance.rb
+++ lib/cloud_controller/api/service_instance.rb
@@ -17,6 +17,25 @@ module VCAP::CloudController
 
     query_parameters :app_space_guid, :service_plan_guid, :service_binding_guid
 
+    def create_quota_token_request(obj)
+      {
+        :path => obj.app_space.organization_guid,
+        :body => {
+          :op           => "post",
+          :user_id      => @user.guid,
+          :object       => "service",
+          :object_id    => obj.guid,
+          :app_space_id => obj.app_space.guid,
+          :object_name  => obj.name,
+          :plan_name    => obj.service_plan.name,
+          :service_label => obj.service_plan.service.label,
+          :service_provider => obj.service_plan.service.provider,
+          :service_version => obj.service_plan.service.version,
+          :audit_data   => obj.to_json
+        }
+      }
+    end
+
     def self.translate_validation_exception(e, attributes)
       app_space_and_name_errors = e.errors.on([:app_space_id, :name])
       if app_space_and_name_errors && app_space_and_name_errors.include?(:unique)
