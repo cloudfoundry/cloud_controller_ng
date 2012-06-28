87e80eeb359219225b518285b6e6a9bf3a16190b
Timothy Chen <chent@rbcon.com>
2012-06-28 14:32:13 -0700

    Fix app quota calls; change default plan name for now
    Change-Id: I9108a84695a821f445e3ba31929c35b068c14762

--- lib/cloud_controller/legacy_api/legacy_services.rb
+++ lib/cloud_controller/legacy_api/legacy_services.rb
@@ -14,7 +14,7 @@ module VCAP::CloudController
     SERVICE_TOKEN_KEY = "HTTP_X_VCAP_SERVICE_TOKEN"
     DEFAULT_PROVIDER = "core"
     LEGACY_API_USER_GUID = "legacy-api"
-    LEGACY_PLAN_OVERIDE = "free"
+    LEGACY_PLAN_OVERIDE = "P200"
 
     def initialize(config, logger, request, service_auth_token = nil)
       super(config, logger, request)
