bc6bf2d59177c6e612dbafb27a1ce3020a1ffced
Patrick Bozeman <pbozeman@gmail.com>
2012-07-16 16:17:17 -0700

    start of the work for converting to json_message
    this is an internal handoff to kowshik and should not be submitted
    
    Change-Id: I686b6ac042215eb1eb916df50597746927000d8f

--- lib/cloud_controller/api/service.rb
+++ lib/cloud_controller/api/service.rb
@@ -7,16 +7,17 @@ module VCAP::CloudController
       read Permissions::Authenticated
     end
 
+    # TODO: require the url to use HTTPS?
     define_attributes do
       attribute :label,          String
       attribute :provider,       String
-      attribute :url,            Message::HTTPS_URL
+      attribute :url,            Message::URL
       attribute :description,    String
       attribute :version,        String
-      attribute :info_url,       Message::URL
-      attribute :acls,           {"users" => [String], "wildcards" => [String]}
-      attribute :timeout,        Integer
-      attribute :active,         Message::Boolean
+      attribute :info_url,       Message::URL, :default => nil
+      attribute :acls,           {"users" => [String], "wildcards" => [String]}, :default => nil
+      attribute :timeout,        Integer, :default => nil
+      attribute :active,         Message::Boolean, :default => false
       to_many   :service_plans
     end
 
--- lib/cloud_controller/errors.rb
+++ lib/cloud_controller/errors.rb
@@ -13,6 +13,7 @@ module VCAP::CloudController::Errors
     # TODO: redo ranges.  10000 is for vcap base.
     # 20000 should be for cc base.
     ["QuotaDeclined",         HTTP::BAD_REQUEST, 200001, "Quota declined: %s"],
+    ["MessageParseError",     HTTP::BAD_REQUEST, 200002, "Request invalid due to parse error: %s"],
 
     ["UserInvalid",           HTTP::BAD_REQUEST, 20001, "The user info is invalid: %s"],
     ["UaaIdTaken",            HTTP::BAD_REQUEST, 20002, "The UAA ID is taken: %s"],
