415ee5cf1930fdd8b093186c4d648187b26a4309
Patrick Bozeman <pbozeman@gmail.com>
2012-08-06 13:49:19 -0700

    LegacyStaging.with_upload_handle
    Change-Id: I5dff209a6615f8f15f17ce00122c8f0e6bcb4465

--- lib/cloud_controller/legacy_api/legacy_staging.rb
+++ lib/cloud_controller/legacy_api/legacy_staging.rb
@@ -4,6 +4,66 @@ module VCAP::CloudController
   class LegacyStaging < LegacyApiBase
     include VCAP::CloudController::Errors
 
+    class DropletUploadHandle
+      attr_accessor :id, :upload_path
+
+      def initialize(id)
+        @id = id
+        @upload_path = nil
+      end
+    end
+
+    class << self
+      def configure(config)
+        @config = config
+      end
+
+      def download_app_uri(id)
+        staging_uri("/staging/app/#{id}")
+      end
+
+      def with_upload_handle(id)
+        handle = create_handle(id)
+        yield handle
+      ensure
+        destroy_handle handle
+      end
+
+      private
+
+      def upload_handles
+        @upload_handles ||= {}
+      end
+
+      def create_handle(id)
+        handle = DropletUploadHandle.new(id)
+        mutex.synchronize do
+          if upload_handles[id]
+            raise Errors::StagingError.new("staging already in progress for #{id}")
+          end
+          upload_handles[handle.id] = handle
+        end
+        handle
+      end
+
+      def destroy_handle(handle)
+        return unless handle
+        mutex.synchronize do
+          upload_handles.delete(handle.id)
+        end
+      end
+
+      MUTEX = Mutex.new
+      def mutex
+        MUTEX
+      end
+
+      def logger
+        @logger ||= Steno.logger("cc.legacy_staging")
+      end
+    end
+
+    # Handles an app download from a stager
     def download_app(id)
       app = Models::App.find(:guid => id)
       raise AppNotFound.new(id) if app.nil?
