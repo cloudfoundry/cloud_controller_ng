d7f660d9a99911c15b490178d1fc7da65e6a667b
Kowshik Prakasam <kprakasam@rbcon.com>
2012-08-31 16:07:33 -0700

    Implement V2 'instances' REST API endpoint.
    Change-Id: Ia91cb1a3e757702226ce52538d30a55527374729

---
+++ lib/cloud_controller/api/instances.rb
@@ -0,0 +1,22 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+module VCAP::CloudController
+  rest_controller :Instances do
+    disable_default_routes
+    path_base "apps"
+    model_class_name :App
+
+    permissions_required do
+      read Permissions::CFAdmin
+      read Permissions::SpaceDeveloper
+    end
+
+    def instances(id)
+      app = find_id_and_validate_access(:read, id)
+      instances = DeaClient.find_all_instances(app)
+      [HTTP::OK, Yajl::Encoder.encode(instances)]
+    end
+
+    get  "#{path_id}/instances", :instances
+  end
+end
