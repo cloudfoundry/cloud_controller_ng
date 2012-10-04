0c5a2b6f47071203d7edce2d8fd4004f5e20b123
Kowshik Prakasam <kprakasam@rbcon.com>
2012-10-04 11:57:57 -0700

    In files API, redirect to file uri v2.
    - Add/modify tests to suit this change.
    
    Change-Id: Ie9b3ddb18a5ff141faac8526de70f0f4ce39535d

--- spec/api/files_spec.rb
+++ spec/api/files_spec.rb
@@ -43,222 +43,221 @@ module VCAP::CloudController
           last_response.status.should == 400
         end
 
-        it "should return 400 when accessing of the file URL fails", :use_nginx => false do
-          instance_id = 5
+        context "dea returns file uri v1" do
+          it "should return 400 when accessing of the file URL fails", :use_nginx => false do
+            instance_id = 5
 
-          @app.state = "STARTED"
-          @app.instances = 10
-          @app.save
-          @app.refresh
+            @app.state = "STARTED"
+            @app.instances = 10
+            @app.save
+            @app.refresh
 
-          to_return = { :uri => "file_uri/",
-            :credentials => ["username", "password"],
-            :file_uri_v2 => false }
-          DeaClient.should_receive(:get_file_uri).with(@app, 5, nil).
-            and_return(to_return)
+            to_return = { :uri => "file_uri/",
+              :credentials => ["username", "password"],
+              :file_uri_v2 => false }
+            DeaClient.should_receive(:get_file_uri).with(@app, 5, nil).
+              and_return(to_return)
 
-          client = mock("http client")
-          HTTPClient.should_receive(:new).and_return(client)
-          client.should_receive(:set_auth).with(nil, "username", "password")
+            client = mock("http client")
+            HTTPClient.should_receive(:new).and_return(client)
+            client.should_receive(:set_auth).with(nil, "username", "password")
 
-          response = mock("http response")
-          client.should_receive(:get).with(
-            "file_uri/", :header => {}).and_return(response)
-          response.should_receive(:status).and_return(400)
+            response = mock("http response")
+            client.should_receive(:get).with(
+                                             "file_uri/", :header => {}).and_return(response)
+            response.should_receive(:status).and_return(400)
 
-          get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files",
-              {},
-              headers_for(@developer))
+            get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files",
+                {},
+                headers_for(@developer))
 
-          last_response.status.should == 400
-        end
+            last_response.status.should == 400
+          end
 
-        it "should return the expected files when path is specified", :use_nginx => false do
-          instance_id = 5
+          it "should return the expected files when path is specified", :use_nginx => false do
+            instance_id = 5
 
-          @app.state = "STARTED"
-          @app.instances = 10
-          @app.save
-          @app.refresh
+            @app.state = "STARTED"
+            @app.instances = 10
+            @app.save
+            @app.refresh
 
-          to_return = { :uri => "file_uri/path",
-            :credentials => ["username", "password"],
-            :file_uri_v2 => false }
-          DeaClient.should_receive(:get_file_uri).with(@app, 5, "path").
-            and_return(to_return)
+            to_return = { :uri => "file_uri/path",
+              :credentials => ["username", "password"],
+              :file_uri_v2 => false }
+            DeaClient.should_receive(:get_file_uri).with(@app, 5, "path").
+              and_return(to_return)
 
-          client = mock("http client")
-          HTTPClient.should_receive(:new).and_return(client)
-          client.should_receive(:set_auth).with(nil, "username", "password")
+            client = mock("http client")
+            HTTPClient.should_receive(:new).and_return(client)
+            client.should_receive(:set_auth).with(nil, "username", "password")
 
-          response = mock("http response")
-          client.should_receive(:get).with(
-            "file_uri/path", :header => {}).and_return(response)
-          response.should_receive(:status).at_least(:once).and_return(200)
-          response.should_receive(:body).and_return("files")
+            response = mock("http response")
+            client.should_receive(:get).with(
+                                             "file_uri/path", :header => {}).and_return(response)
+            response.should_receive(:status).at_least(:once).and_return(200)
+            response.should_receive(:body).and_return("files")
 
-          get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files/path",
-              {},
-              headers_for(@developer))
+            get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files/path",
+                {},
+                headers_for(@developer))
 
-          last_response.status.should == 200
-          last_response.body.should == "files"
-        end
+            last_response.status.should == 200
+            last_response.body.should == "files"
+          end
 
-        it "should return the expected files when no path is specified", :use_nginx => false do
-          instance_id = 5
+          it "should return the expected files when no path is specified", :use_nginx => false do
+            instance_id = 5
 
-          @app.state = "STARTED"
-          @app.instances = 10
-          @app.save
-          @app.refresh
+            @app.state = "STARTED"
+            @app.instances = 10
+            @app.save
+            @app.refresh
 
-          to_return = { :uri => "file_uri/",
-            :credentials => ["username", "password"],
-            :file_uri_v2 => false }
-          DeaClient.should_receive(:get_file_uri).with(@app, 5, nil).
-            and_return(to_return)
+            to_return = { :uri => "file_uri/",
+              :credentials => ["username", "password"],
+              :file_uri_v2 => false }
+            DeaClient.should_receive(:get_file_uri).with(@app, 5, nil).
+              and_return(to_return)
 
-          client = mock("http client")
-          HTTPClient.should_receive(:new).and_return(client)
-          client.should_receive(:set_auth).with(nil, "username", "password")
+            client = mock("http client")
+            HTTPClient.should_receive(:new).and_return(client)
+            client.should_receive(:set_auth).with(nil, "username", "password")
 
-          response = mock("http response")
-          client.should_receive(:get).with(
-            "file_uri/", :header => {}).and_return(response)
-          response.should_receive(:status).at_least(:once).and_return(200)
-          response.should_receive(:body).and_return("files")
+            response = mock("http response")
+            client.should_receive(:get).with(
+                                             "file_uri/", :header => {}).and_return(response)
+            response.should_receive(:status).at_least(:once).and_return(200)
+            response.should_receive(:body).and_return("files")
 
-          get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files",
-              {},
-              headers_for(@developer))
+            get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files",
+                {},
+                headers_for(@developer))
 
-          last_response.status.should == 200
-          last_response.body.should == "files"
-        end
+            last_response.status.should == 200
+            last_response.body.should == "files"
+          end
 
-        it "should forward the http range request", :use_nginx => false do
-          instance_id = 5
-          range = "bytes=100-200"
+          it "should forward the http range request", :use_nginx => false do
+            instance_id = 5
+            range = "bytes=100-200"
 
-          @app.state = "STARTED"
-          @app.instances = 10
-          @app.save
-          @app.refresh
+            @app.state = "STARTED"
+            @app.instances = 10
+            @app.save
+            @app.refresh
 
-          to_return = { :uri => "file_uri/",
-            :credentials => ["username", "password"],
-            :file_uri_v2 => false }
-          DeaClient.should_receive(:get_file_uri).with(@app, 5, nil).
-            and_return(to_return)
+            to_return = { :uri => "file_uri/",
+              :credentials => ["username", "password"],
+              :file_uri_v2 => false }
+            DeaClient.should_receive(:get_file_uri).with(@app, 5, nil).
+              and_return(to_return)
 
-          client = mock("http client")
-          HTTPClient.should_receive(:new).and_return(client)
-          client.should_receive(:set_auth).with(nil, "username", "password")
+            client = mock("http client")
+            HTTPClient.should_receive(:new).and_return(client)
+            client.should_receive(:set_auth).with(nil, "username", "password")
 
-          response = mock("http response")
-          client.should_receive(:get).with(
-            "file_uri/", :header => { "range" => range } ).and_return(response)
-          response.should_receive(:status).at_least(:once).and_return(206)
-          response.should_receive(:body).and_return("files")
+            response = mock("http response")
+            headers = { "range" => range }
+            client.should_receive(:get).with("file_uri/", :header => headers).
+              and_return(response)
 
-          get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files",
-              {},
-              headers_for(@developer).merge("HTTP_RANGE" => range))
+            response.should_receive(:status).at_least(:once).and_return(206)
+            response.should_receive(:body).and_return("files")
 
-          last_response.status.should == 206
-          last_response.body.should == "files"
-        end
+            get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files",
+                {},
+                headers_for(@developer).merge("HTTP_RANGE" => range))
 
-        it "should accept tail query parameter (non-nginx)", :use_nginx => false do
-          instance_id = 5
+            last_response.status.should == 206
+            last_response.body.should == "files"
+          end
 
-          @app.state = "STARTED"
-          @app.instances = 10
-          @app.save
-          @app.refresh
+          it "should accept tail query parameter (non-nginx)", :use_nginx => false do
+            instance_id = 5
 
-          to_return = { :uri => "file_uri",
-            :credentials => ["username", "password"],
-            :file_uri_v2 => false }
-          DeaClient.should_receive(:get_file_uri).with(@app, 5, "path").
-            and_return(to_return)
+            @app.state = "STARTED"
+            @app.instances = 10
+            @app.save
+            @app.refresh
 
-          client = mock("http client")
-          HTTPClient.should_receive(:new).and_return(client)
-          client.should_receive(:set_auth).with(nil, "username", "password")
+            to_return = { :uri => "file_uri",
+              :credentials => ["username", "password"],
+              :file_uri_v2 => false }
+            DeaClient.should_receive(:get_file_uri).with(@app, 5, "path").
+              and_return(to_return)
 
-          response = mock("http response")
-          client.should_receive(:get).with(
-            "file_uri&tail", :header => {}).and_return(response)
-          response.should_receive(:status).at_least(:once).and_return(200)
-          response.should_receive(:body).and_return("files")
+            client = mock("http client")
+            HTTPClient.should_receive(:new).and_return(client)
+            client.should_receive(:set_auth).with(nil, "username", "password")
 
-          get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files/path?tail",
-              {},
-              headers_for(@developer))
+            response = mock("http response")
+            client.should_receive(:get).with("file_uri&tail", :header => {}).
+              and_return(response)
 
-          last_response.status.should == 200
-          last_response.body.should == "files"
-        end
+            response.should_receive(:status).at_least(:once).and_return(200)
+            response.should_receive(:body).and_return("files")
 
-        it "should accept tail query parameter" do
-          instance_id = 5
+            get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files/path?tail",
+                {},
+                headers_for(@developer))
 
-          @app.state = "STARTED"
-          @app.instances = 10
-          @app.save
-          @app.refresh
+            last_response.status.should == 200
+            last_response.body.should == "files"
+          end
 
-          DeaClient.should_receive(:get_file_uri).with(@app, 5, "path").
-            and_return(
-              {
-                :uri => "http://1.2.3.4/foo/path",
-                :credentials => ["u", "p"],
-              }
-            )
+          it "should accept tail query parameter" do
+            instance_id = 5
 
-          get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files/path?tail",
-              {},
-              headers_for(@developer))
+            @app.state = "STARTED"
+            @app.instances = 10
+            @app.save
+            @app.refresh
 
-          last_response.status.should == 200
-          last_response.headers.should include(
-            {
+            to_return = {
+              :uri => "http://1.2.3.4/foo/path",
+              :credentials => ["u", "p"],
+            }
+            DeaClient.should_receive(:get_file_uri).with(@app, 5, "path").
+              and_return(to_return)
+
+            get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files/path?tail",
+                {},
+                headers_for(@developer))
+
+            expected = {
               "X-Accel-Redirect" => "/internal_redirect/http://1.2.3.4/foo/path&tail",
               # "dTpw" is ["u:p"].pack("m0")
               "X-Auth" => "Basic dTpw",
             }
-          )
+            last_response.status.should == 200
+            last_response.headers.should include(expected)
+          end
         end
 
-        it "should ignore absence of credentials in dea response", :use_nginx => false do
-          instance_id = 5
+        context "dea returns file uri v2" do
+          it "should issue redirect when file uri v2 is returned by the dea", :use_nginx => false do
+            instance_id = 5
+            range = "bytes=100-200"
 
-          @app.state = "STARTED"
-          @app.instances = 10
-          @app.save
-          @app.refresh
+            @app.state = "STARTED"
+            @app.instances = 10
+            @app.save
+            @app.refresh
 
-          to_return = { :uri => "file_uri/", :file_uri_v2 => true }
-          DeaClient.should_receive(:get_file_uri).with(@app, 5, nil).
-            and_return(to_return)
+            to_return = { :uri => "file_uri/", :file_uri_v2 => true }
+            DeaClient.should_receive(:get_file_uri).with(@app, 5, nil).
+              and_return(to_return)
 
-          client = mock("http client")
-          HTTPClient.should_receive(:new).and_return(client)
+            get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files",
+                {},
+                headers_for(@developer).merge("HTTP_RANGE" => range))
 
-          response = mock("http response")
-          client.should_receive(:get).with(
-            "file_uri/", :header => {}).and_return(response)
-          response.should_receive(:status).at_least(:once).and_return(200)
-          response.should_receive(:body).and_return("files")
-
-          get("/v2/apps/#{@app.guid}/instances/#{instance_id}/files",
-              {},
-              headers_for(@developer))
-
-          last_response.status.should == 200
-          last_response.body.should == "files"
+            last_response.status.should == 302
+            last_response.headers.should include "Location"
+            last_response.headers["Location"] == "file_uri/"
+            last_response.headers["Range"] == "bytes=100-200"
+          end
         end
 
         it "should return the staging task log if it exists" do
