3f8af3e1638480005d890292ac1ca1b5a66b7c1e
Patrick Bozeman <pbozeman@gmail.com>
2012-10-23 17:59:40 -0700

    fix domain permission check for system domains
    Change-Id: I9bfac7bed454feefdc20440cbccfaa7a18e368b2

--- lib/cloud_controller/permissions/org_permissions.rb
+++ lib/cloud_controller/permissions/org_permissions.rb
@@ -10,10 +10,15 @@ module VCAP::CloudController::Permissions
       if obj.kind_of?(Models::Organization)
         obj.send(relation).include?(user)
       elsif !obj.new?
+        if obj.respond_to?(:owning_organization)
+          return false unless obj.owning_organization
+        end
+
         if (obj.respond_to?(:organizations) &&
             obj.organizations_dataset.filter(relation => [user]).count >= 1)
           return true
         end
+
         if (obj.respond_to?(:organization) && obj.organization &&
             obj.organization.send("#{relation}_dataset")[user.id] != nil)
           return true
