d2e9c3e21b1e1168bbfd3ccfc27e05e70bb6e6a3
Patrick Bozeman <pbozeman@gmail.com>
2012-11-07 09:27:33 -0800

    ServiceDeleteEvent.create_from_service_instance
    Change-Id: Ic9358a7578609e2288766835bfa787c0ad899fc5

--- lib/cloud_controller/models/service_delete_event.rb
+++ lib/cloud_controller/models/service_delete_event.rb
@@ -24,5 +24,23 @@ module VCAP::CloudController::Models
     def event_type
       "service_delete"
     end
+
+    def self.create_from_service_instance(instance)
+      plan = instance.service_plan
+      svc = plan.service
+      space = instance.space
+      org = space.organization
+
+      return unless org.billing_enabled?
+      ServiceDeleteEvent.create(
+        :timestamp => Time.now,
+        :organization_guid => org.guid,
+        :organization_name => org.name,
+        :space_guid => space.guid,
+        :space_name => space.name,
+        :service_instance_guid => instance.guid,
+        :service_instance_name => instance.name,
+      )
+    end
   end
 end
--- spec/models/service_delete_event_spec.rb
+++ spec/models/service_delete_event_spec.rb
@@ -21,5 +21,29 @@ module VCAP::CloudController
       ],
       :disable_examples => :deserialization
     }
+
+    describe "create_from_service_instance" do
+      context "on an org without billing enabled" do
+        it "should do nothing" do
+          Models::ServiceDeleteEvent.should_not_receive(:create)
+          si = Models::ServiceInstance.make
+          org = si.space.organization
+          org.billing_enabled = false
+          org.save(:validate => false)
+          Models::ServiceDeleteEvent.create_from_service_instance(si)
+        end
+      end
+
+      context "on an org with billing enabled" do
+        it "should create an service delete event" do
+          Models::ServiceDeleteEvent.should_receive(:create)
+          si = Models::ServiceInstance.make
+          org = si.space.organization
+          org.billing_enabled = true
+          org.save(:validate => false)
+          Models::ServiceDeleteEvent.create_from_service_instance(si)
+        end
+      end
+    end
   end
 end
