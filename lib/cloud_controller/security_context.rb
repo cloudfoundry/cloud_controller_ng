128700475a70a66956c49e087db7f65ff9a419d9
Brad Midgley <bmidgley@rbcon.com>
2012-12-03 14:25:50 -0700

    use token's scope to determine admin privs in addition to db flag
    Change-Id: Ice1c2fd69a76b9fc611d3c4236f1e5fa0d553b0c

--- lib/cloud_controller/security_context.rb
+++ lib/cloud_controller/security_context.rb
@@ -17,7 +17,15 @@ module VCAP::CloudController
     end
 
     def self.current_user_is_admin?
+      return admin_flag? || admin_scope?
+    end
+
+    def self.admin_flag?
       return current_user && current_user.admin?
+    end
+
+    def self.admin_scope?
+      return token && token[:scope].include?('cloud_controller.admin')
     end
 
     def self.token
