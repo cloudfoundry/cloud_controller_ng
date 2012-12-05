ad6021f9235b7f2cc45a6a2408b6f1ddbcffcc1a
Brad Midgley <bmidgley@rbcon.com>
2012-12-05 11:30:09 -0800

    send public_key to token coder if present
    Change-Id: I7ab89461f13c4327e60ae7886e70f17869e5e2a8

--- lib/cloud_controller.rb
+++ lib/cloud_controller.rb
@@ -26,6 +26,7 @@ module VCAP::CloudController
 
     def initialize(config)
       @config = config
+      @validation_key = @config[:uaa][:validation_key]
       super()
     end
 
@@ -37,7 +38,7 @@ module VCAP::CloudController
       if auth_token && auth_token.upcase.start_with?("BEARER")
         token_coder = CF::UAA::TokenCoder.new(@config[:uaa][:resource_id],
                                               @config[:uaa][:symmetric_secret],
-                                              nil)
+                                              @validation_key)
         begin
           token_information = token_coder.decode(auth_token)
           logger.info("Token received from the UAA #{token_information.inspect}")
