7cf5ab7b41eedf04dec894bc861527c99185e116
Brad Midgley <bmidgley@rbcon.com>
2013-01-08 17:30:04 -0800

    Refactor CFAdmin to honor scoped admin
    Change-Id: I39091ce82fbf1bc60e09b0fd1ef8491e490dce28

--- lib/cloud_controller/permissions/anonymous.rb
+++ lib/cloud_controller/permissions/anonymous.rb
@@ -2,8 +2,8 @@
 
 module VCAP::CloudController::Permissions
   class Anonymous
-    def self.granted_to?(obj, user)
-      user.nil?
+    def self.granted_to?(obj, user, roles)
+      user.nil? && roles.none?
     end
 
     VCAP::CloudController::Permissions::register self
--- lib/cloud_controller/permissions/cf_admin.rb
+++ lib/cloud_controller/permissions/cf_admin.rb
@@ -2,8 +2,8 @@
 
 module VCAP::CloudController::Permissions
   class CFAdmin
-    def self.granted_to?(obj, user)
-      !user.nil? && user.admin?
+    def self.granted_to?(obj, user, roles)
+      (!user.nil? && user.admin?) || roles.admin?
     end
 
     VCAP::CloudController::Permissions::register self
