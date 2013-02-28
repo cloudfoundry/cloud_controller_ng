ab81c49ebac76b3155f0f50ffc5475173eaef60d
Dmitriy Kalinin and Jesse Zhang <pair+dmitriy+jesse@pivotallabs.com>
2013-02-28 15:07:12 -0800

    validate stacks config files
    Change-Id: Ie9345491d7ea22a80088bfa170bf8d6183749e6b

--- lib/cloud_controller/models/stack.rb
+++ lib/cloud_controller/models/stack.rb
@@ -17,12 +17,21 @@ module VCAP::CloudController::Models
 
     def self.populate_from_file(file_name)
       config_hash = YAML.load_file(file_name)
+      ConfigFileSchema.validate(config_hash)
+
       config_hash["stacks"].each do |stack_hash|
         populate_from_hash(stack_hash)
       end
     end
 
     private
+
+    ConfigFileSchema = Membrane::SchemaParser.parse {{
+      "stacks" => [{
+        "name" => String,
+        "description" => String,
+      }]
+    }}
 
     def self.populate_from_hash(hash)
       update_or_create(:name => hash["name"]) do |r|
--- spec/models/stack_spec.rb
+++ spec/models/stack_spec.rb
@@ -11,19 +11,39 @@ module VCAP::CloudController::Models
     }
 
     describe ".populate_from_file" do
-      before { reset_database }
-      let(:file) { File.expand_path("../../fixtures/config/stacks.yml", __FILE__) }
+      context "when stacks file is valid" do
+        before { reset_database }
+        let(:file) { File.expand_path("../../fixtures/config/stacks.yml", __FILE__) }
 
-      it "loads stacks" do
-        described_class.populate_from_file(file)
-        cider = described_class.find(:name => "cider")
-        cider.should be_valid
+        it "loads stacks" do
+          described_class.populate_from_file(file)
+          cider = described_class.find(:name => "cider")
+          cider.should be_valid
+        end
+
+        it "populates descriptions about loaded stacks" do
+          described_class.populate_from_file(file)
+          cider = described_class.find(:name => "cider")
+          cider.description.should == "cider-description"
+        end
       end
 
-      it "populates descriptions about loaded stacks" do
-        described_class.populate_from_file(file)
-        cider = described_class.find(:name => "cider")
-        cider.description.should == "cider-description"
+      context "when stacks file is invalid" do
+        let(:file) { File.expand_path("../../fixtures/config/invalid_stacks.yml", __FILE__) }
+
+        it "raises error" do
+          expect {
+            described_class.populate_from_file(file)
+          }.to raise_error(Membrane::SchemaValidationError, /name => Missing key/)
+        end
+      end
+
+      describe "config/stacks.yml" do
+        let(:file) { File.expand_path("../../../config/stacks.yml", __FILE__) }
+
+        it "can load" do
+          described_class.populate_from_file(file)
+        end
       end
     end
   end
