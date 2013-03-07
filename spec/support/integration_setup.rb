6fae43783e482b3c8a3c2f304af3f1c6dabb3f22
Dmitriy Kalinin and Mark Rushakoff <pair+dmitriy+mr@pivotallabs.com>
2013-03-07 15:54:13 -0800

    remove inccorect usage of check_process_alive?
    Change-Id: I7814f62e68d1c19c8213520935a3cf2ad0caf03a

--- spec/support/integration_setup.rb
+++ spec/support/integration_setup.rb
@@ -1,14 +1,14 @@
 module IntegrationSetup
   def start_nats(opts={})
     before(:all) { @nats_pid = run_cmd("nats-server -V -D", opts) }
-    before(:all) { check_process_alive!(:nats, @nats_pid, :sleep => 0.5) }
+    before(:all) { sleep(0.5) } # allow server to start up
     after(:all) { graceful_kill(:nats, @nats_pid) }
   end
 
   def start_cc(opts={})
     before(:all) { @cc_pid = run_cmd("bundle exec rake db:migrate && bin/cloud_controller config/cloud_controller.yml", opts) }
-    before(:all) { check_process_alive!(:cc, @cc_pid, :sleep => 10) }
-    after(:all) { graceful_kill(:dea, @cc_pid) }
+    before(:all) { sleep(10) } # allow server to start up
+    after(:all) { graceful_kill(:cc, @cc_pid) }
   end
 end
 
@@ -27,12 +27,6 @@ module IntegrationHelpers
         raise "`#{cmd}` exited with #{$?}" unless $?.success?
       end
     end
-  end
-
-  def check_process_alive!(name, pid, options={})
-    sleep(options[:sleep]) if options[:sleep]
-    raise "Process #{name} with pid #{pid} is not alive." \
-      unless process_alive?(pid)
   end
 
   def graceful_kill(name, pid)
