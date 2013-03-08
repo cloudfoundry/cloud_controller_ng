eb60c2fc207bc149c280708e392d2512a23fd2b3
Dmitriy Kalinin and Mark Rushakoff <pair+dmitriy+mr@pivotallabs.com>
2013-03-07 17:19:28 -0800

    fix random failure by stubbing MessageBus correctly
    Change-Id: Ic3f14e89767559d617d96501d3988e6dcdfb716e

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -2,6 +2,8 @@ require "spec_helper"
 
 describe VCAP::CloudController::Runner do
   describe "#run!" do
+    before { VCAP::CloudController::MessageBus.stub(:new => MockMessageBus.new({})) }
+
     subject do
       config_path = File.expand_path("../fixtures/config/minimal_config.yml", __FILE__)
       VCAP::CloudController::Runner.new(argv + ["-c", config_path]).tap do |r|
