e40a0591b7cca7969a18f0642db980be131e1f62
David Stevenson and Jesse Zhang <pair+stevend+jesse@pivotallabs.com>
2013-03-13 14:37:03 -0700

    A service doesn't have a crypted password

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -7,7 +7,6 @@ module VCAP::CloudController
     it_behaves_like "a CloudController model", {
       :required_attributes  => [:label, :provider, :url, :description, :version],
       :unique_attributes    => [:label, :provider],
-      :sensitive_attributes => :crypted_password,
       :stripped_string_attributes => [:label, :provider],
       :one_to_zero_or_more   => {
         :service_plans      => lambda { |_| Models::ServicePlan.make }
