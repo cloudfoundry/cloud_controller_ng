d0d509fa128f004d1e72a06667e1ba53b9fe2ad3
Andrew Liu and David Stevenson <pair+aliu+stevend@pivotallabs.com>
2013-03-18 10:55:02 -0700

    Fix migration that is broken in postgres only

--- db/migrations/20130314221003_add_extra_to_service_plans.rb
+++ db/migrations/20130314221003_add_extra_to_service_plans.rb
@@ -3,7 +3,7 @@
 Sequel.migration do
   change do
     alter_table :service_plans do
-      add_column :extra, String, :text => true, :size => :medium
+      add_column :extra, String, :text => true
     end
   end
 end
