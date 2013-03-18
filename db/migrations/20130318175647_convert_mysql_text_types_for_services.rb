46dee932b93f507ab95e0fa77740cda6d78297f9
Andrew Liu and David Stevenson <pair+aliu+stevend@pivotallabs.com>
2013-03-18 11:07:39 -0700

    Run custom SQL to convert text columns to mediumtext in mysql

---
+++ db/migrations/20130318175647_convert_mysql_text_types_for_services.rb
@@ -0,0 +1,18 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+Sequel.migration do
+  up do
+    # Sequel does not support a way to create a larger than 65k text column with mysql adapter
+    if self.class.name.match /mysql/i
+      run "ALTER TABLE `services` CHANGE COLUMN `extra` `extra` MEDIUMTEXT"
+      run "ALTER TABLE `service_plans` CHANGE COLUMN `extra` `extra` MEDIUMTEXT"
+    end
+  end
+
+  down do
+    if self.class.name.match /mysql/i
+      run "ALTER TABLE `services` CHANGE COLUMN `extra` `extra` TEXT"
+      run "ALTER TABLE `service_plans` CHANGE COLUMN `extra` `extra` TEXT"
+    end
+  end
+end
