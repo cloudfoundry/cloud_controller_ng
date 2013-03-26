11f03e9b0d4f993072609de24033c7aac652e519
Dmitriy Kalinin and Mark Rushakoff <pair+dmitriy+mr@pivotallabs.com>
2013-03-26 09:59:20 -0700

    remove fk constraints before dropping columns for mysql

--- db/migrations/20130323005501_remove_framework_and_runtime.rb
+++ db/migrations/20130323005501_remove_framework_and_runtime.rb
@@ -2,6 +2,16 @@
 
 Sequel.migration do
   change do
+    # Mysql requires us to remove foreign key constraint
+    # before dropping column (errno 150). Sequel also
+    # requires type=foreign_key to be present for such operations.
+    if self.class.name.match /mysql/i
+      alter_table :apps do
+        drop_constraint :fk_apps_framework_id, :type => :foreign_key
+        drop_constraint :fk_apps_runtime_id, :type => :foreign_key
+      end
+    end
+
     alter_table :apps do
       drop_column :framework_id
       drop_column :runtime_id
