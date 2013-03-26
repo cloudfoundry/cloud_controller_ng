42a6a554eeb8a59b5849ddbedf7d62022b83b35b
Dmitriy Kalinin and Mark Rushakoff <pair+dmitriy+mr@pivotallabs.com>
2013-03-26 09:53:47 -0700

    monkey patch sequel with class_evals instead of reopening classes

--- lib/vcap/sequel_case_insensitive_string_monkeypatch.rb
+++ lib/vcap/sequel_case_insensitive_string_monkeypatch.rb
@@ -1,5 +1,10 @@
 # Copyright (c) 2009-2012 VMware, Inc.
 
+require "sequel"
+require "sequel/adapters/sqlite"
+require "sequel/adapters/postgres"
+require "sequel/adapters/mysql2"
+
 # Add :case_insensitive as an option to the string type during migrations.
 # This results in case insensitive comparisions for indexing and querying, but
 # case preserving retrieval.
@@ -24,81 +29,71 @@
 # In the above migration, name will will have case insensitive comparisions,
 # but case preserving data, whereas base64_data will be case sensitive.
 
-module Sequel
-  module SQLite
-    class Database
-      def case_insensitive_string_column_type
-        "String"
+Sequel::SQLite::Database.class_eval do
+  def case_insensitive_string_column_type
+    "String"
+  end
+
+  def case_insensitive_string_column_opts
+    { :collate => :nocase }
+  end
+end
+
+Sequel::Postgres::Database.class_eval do
+  def case_insensitive_string_column_type
+    "CIText"
+  end
+
+  def case_insensitive_string_column_opts
+    {}
+  end
+end
+
+Sequel::Mysql2::Database.class_eval do
+  # Mysql is case insensitive by default
+  def case_insensitive_string_column_type
+    "VARCHAR(255)"
+  end
+
+  def case_insensitive_string_column_opts
+    { :collate => "latin1_general_ci" }
+  end
+end
+
+Sequel::Schema::Generator.class_eval do
+  def String(name, opts = {})
+    if opts[:case_insensitive]
+      unless @db.respond_to?(:case_insensitive_string_column_type)
+        raise Error, "DB adapater does not support case insensitive strings"
       end
 
-      def case_insensitive_string_column_opts
-        { :collate => :nocase }
-      end
+      column(
+        name,
+        @db.case_insensitive_string_column_type,
+        opts.merge(@db.case_insensitive_string_column_opts)
+      )
+    else
+      column(name, String, opts)
     end
   end
 end
 
-module Sequel
-  module Postgres
-    class Database
-      def case_insensitive_string_column_type
-        "CIText"
+Sequel::Schema::AlterTableGenerator.class_eval do
+  alias_method :set_column_type_original, :set_column_type
+
+  def set_column_type(name, type, opts = {})
+    if type.to_s == "String" && opts[:case_insensitive]
+      unless @db.respond_to?(:case_insensitive_string_column_type)
+        raise Error, "DB adapater does not support case insensitive strings"
       end
 
-      def case_insensitive_string_column_opts
-        {}
-      end
-    end
-  end
-end
-
-module Sequel
-  module Mysql2
-    class Database
-      # Mysql is case insensitive by default
-      def case_insensitive_string_column_type
-        "VARCHAR(255)"
-      end
-
-      def case_insensitive_string_column_opts
-        { :collate => "latin1_general_ci" }
-      end
-    end
-  end
-end
-
-module Sequel
-  module Schema
-    class Generator
-      def String(name, opts = {})
-        if opts[:case_insensitive]
-          unless @db.respond_to?(:case_insensitive_string_column_type)
-            raise Error, "DB adapater does not support case insensitive strings"
-          end
-
-          column(name, @db.case_insensitive_string_column_type,
-                 opts.merge(@db.case_insensitive_string_column_opts))
-        else
-          column(name, String, opts)
-        end
-      end
-    end
-
-    class AlterTableGenerator
-      alias_method :set_column_type_original, :set_column_type
-
-      def set_column_type(name, type, opts = {})
-        if type.to_s == "String" && opts[:case_insensitive]
-          unless @db.respond_to?(:case_insensitive_string_column_type)
-            raise Error, "DB adapater does not support case insensitive strings"
-          end
-          set_column_type_original(name,
-                                   @db.case_insensitive_string_column_type,
-                                   opts.merge(@db.case_insensitive_string_column_opts))
-        else
-          set_column_type_original(name, type, opts)
-        end
-      end
+      set_column_type_original(
+        name,
+        @db.case_insensitive_string_column_type,
+        opts.merge(@db.case_insensitive_string_column_opts)
+      )
+    else
+      set_column_type_original(name, type, opts)
     end
   end
 end
