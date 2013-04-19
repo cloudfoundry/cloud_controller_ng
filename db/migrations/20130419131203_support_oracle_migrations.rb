3150638c6c8cdb8ab55649e8ad3461574533c8db
Mike Youngstrom <youngm@gmail.com>
2013-04-19 03:30:45 -0600

    Only execute on databases other than oracle.  Since the Sequel Oracle adapter doesn't yet implement foreign_key_list

--- db/migrations/20130419131203_support_oracle_migrations.rb
+++ db/migrations/20130419131203_support_oracle_migrations.rb
@@ -2,19 +2,20 @@
 
 Sequel.migration do
   up do
-    schema(:billing_events).each { |column|
-      if column[0] == :timestamp
-        rename_column(:billing_events, :timestamp, :event_timestamp)
-      end
-    }
-    
-    VCAP::Migration.rename_foreign_key(self, :organizations, :fk_organizations_quota_definition_id, :fk_org_quota_definition_id)
-    VCAP::Migration.rename_foreign_key(self, :domains, :fk_domains_owning_organization_id, :fk_domains_owning_org_id)
-    VCAP::Migration.rename_foreign_key(self, :domains_organizations, :fk_domains_organizations_domain_id, :fk_domains_orgs_domain_id)
-    VCAP::Migration.rename_foreign_key(self, :domains_organizations, :fk_domains_organizations_organization_id, :fk_domains_orgs_org_id)
-    VCAP::Migration.rename_foreign_key(self, :service_instances, :service_instances_service_plan_id, :svc_instances_service_plan_id)
-    VCAP::Migration.rename_foreign_key(self, :service_bindings, :fk_service_bindings_service_instance_id, :fk_sb_service_instance_id)
-
+    if !self.class.name.match /oracle/i
+      schema(:billing_events).each { |column|
+        if column[0] == :timestamp
+          rename_column(:billing_events, :timestamp, :event_timestamp)
+        end
+      }
+      
+      VCAP::Migration.rename_foreign_key(self, :organizations, :fk_organizations_quota_definition_id, :fk_org_quota_definition_id)
+      VCAP::Migration.rename_foreign_key(self, :domains, :fk_domains_owning_organization_id, :fk_domains_owning_org_id)
+      VCAP::Migration.rename_foreign_key(self, :domains_organizations, :fk_domains_organizations_domain_id, :fk_domains_orgs_domain_id)
+      VCAP::Migration.rename_foreign_key(self, :domains_organizations, :fk_domains_organizations_organization_id, :fk_domains_orgs_org_id)
+      VCAP::Migration.rename_foreign_key(self, :service_instances, :service_instances_service_plan_id, :svc_instances_service_plan_id)
+      VCAP::Migration.rename_foreign_key(self, :service_bindings, :fk_service_bindings_service_instance_id, :fk_sb_service_instance_id)
+    end
   end
 
   down do
