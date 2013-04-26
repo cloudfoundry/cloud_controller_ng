5c99486ca5469a02e4e23e096b78ef8b6fe8a106
David Sabeti and Kowshik Prakasam <pair+david+kowshik@pivotallabs.com>
2013-04-26 11:33:23 -0700

    Fix crash_events migration

--- db/migrations/20130425230435_add_crash_events.rb
+++ db/migrations/20130425230435_add_crash_events.rb
@@ -5,13 +5,13 @@ Sequel.migration do
     create_table :crash_events do
       VCAP::Migration.common(self)
 
-      String :app_id, :null => false
+      Integer :app_id, :null => false
       String :instance_guid, :null => false
       Integer :instance_index, :null => false
       Integer :exit_status, :null => false
       String :exit_description
 
-      index [:app_id]
+      index :app_id
 
       foreign_key [:app_id], :apps, :name => :fk_crash_events_app_id
     end
