7d825cb93d0dcfb04dfcea17e8bf1e1c048e4386
Gregg Van Hove and Tony Hansmann <pair+gvanhove+tony@pivotallabs.com>
2013-05-09 10:45:38 -0700

    add the actual seeds

---
+++ lib/cloud_controller/seeds.rb
@@ -0,0 +1,40 @@
+module VCAP::CloudController
+  module Seeds
+    class << self
+      def write_seed_data(config)
+        create_seed_quota_definitions(config)
+        create_seed_stacks(config)
+        system_org = create_seed_organizations(config)
+        create_seed_domains(config, system_org)
+      end
+
+      def create_seed_quota_definitions(config)
+        config[:quota_definitions].each do |k, v|
+          Models::QuotaDefinition.update_or_create(:name => k.to_s) do |r|
+            r.update_from_hash(v)
+          end
+        end
+      end
+
+      def create_seed_stacks(config)
+        Models::Stack.populate
+      end
+
+      def create_seed_organizations(config)
+        quota_definition = Models::QuotaDefinition.find(:name => "paid")
+
+        unless quota_definition
+          raise ArgumentError, "Missing 'paid' quota definition in config file"
+        end
+
+        Models::Organization.find_or_create(:name => config[:system_domain_organization]) do |org|
+          org.quota_definition = quota_definition
+        end
+      end
+
+      def create_seed_domains(config, system_org)
+        Models::Domain.populate_from_config(config, system_org)
+      end
+    end
+  end
+end
