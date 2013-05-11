8a76b28e365c3cbd26508fad5a41c98525def37d
James Bayer and Tim Labeeuw <pair+james+tim@pivotallabs.com>
2013-05-10 17:58:19 -0700

    Updated to having all 3 Instant Client Libraries

--- spec/bin/install_oracle_travis.sh
+++ spec/bin/install_oracle_travis.sh
@@ -3,8 +3,13 @@
 echo "*** install oracle ***"
 sudo mkdir -p $ORACLE_HOME
 cd $ORACLE_BASE
-sudo wget $ORACLE_INSTANT_CLIENT_URL
-sudo tar -jxf $ORACLE_INSTANT_CLIENT_ZIP
+
+sudo wget $CLIENT_BASIC
+sudo wget $CLIENT_SDK
+sudo wget $CLIENT_SQLPLUS
+
+sudo find . -name "*.zip" -exec tar -zxf {} \;
+
 cd $ORACLE_HOME
 sudo ln -s libclntsh.so.11.1 libclntsh.so
 sudo ln -s libocci.so.11.1 libocci.so
