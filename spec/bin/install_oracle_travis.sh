c283705ed19a9aa8bf6c266fb69d2ce47ad60b6a
Jesse Zhang and Tim Labeeuw <pair+jesse+tim@pivotallabs.com>
2013-05-14 11:57:46 -0700

    Added AIO to the oracle build script

--- spec/bin/install_oracle_travis.sh
+++ spec/bin/install_oracle_travis.sh
@@ -5,6 +5,9 @@ echo "*** install oracle ***"
 sudo mkdir -p $ORACLE_HOME
 cd $ORACLE_BASE
 
+sudo apt-get update -qq
+sudo apt-get install -qq libaio1
+
 sudo wget -q $CLIENT_BASIC
 sudo wget -q $CLIENT_SDK
 sudo wget -q $CLIENT_SQLPLUS
