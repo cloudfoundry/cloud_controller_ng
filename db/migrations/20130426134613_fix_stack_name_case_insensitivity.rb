f63d4f8e72cca2e02cd66352f101ba387a88573d
Gregg Van Hove and Tony Hansmann <pair+gvanhove+thansmann@pivotallabs.com>
2013-05-16 17:04:43 -0700

    Revert "add migrations from oracle patch that ran successfully"
    This reverts commit 3c89e009bc8aca11b8e9b0f323378738a7daff15.

--- db/migrations/20130426134613_fix_stack_name_case_insensitivity.rb
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2009-2012 VMware, Inc.
-
-Sequel.migration do
-  change do
-    alter_table :stacks do
-      set_column_type :name, String, :null => false, :case_insensitive => true
-    end
-  end
-end
