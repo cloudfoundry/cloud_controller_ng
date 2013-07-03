f92387d6a2701e6ad5b833adab5e57915df3820d
David Julia and Matt Royal <pair+djulia+mroyal@pivotallabs.com>
2013-07-02 17:20:55 -0700

    Implement POST /v2/provided_service_instances
    [#50700489]

--- lib/cloud_controller/api/provided_service_instance.rb
+++ lib/cloud_controller/api/provided_service_instance.rb
@@ -1,9 +1,10 @@
 require 'cloud_controller/rest_controller'
 
 module VCAP::CloudController
-  class ProvidedServiceInstance < RestController::ModelController
+  rest_controller :ProvidedServiceInstance do
     permissions_required do
-      full Permissions::SpaceDeveloper
+      create Permissions::SpaceDeveloper
+      create Permissions::CFAdmin
     end
 
     define_attributes do
@@ -12,8 +13,14 @@ module VCAP::CloudController
 
       to_one :space
     end
-    define_messages
 
-    post '/v2/provided_service_instances', :create
+    def self.translate_validation_exception(e, attributes)
+      space_and_name_errors = e.errors.on([:space_id, :name])
+      if space_and_name_errors && space_and_name_errors.include?(:unique)
+        Errors::ServiceInstanceNameTaken.new(attributes["name"])
+      else
+        Errors::ServiceInstanceInvalid.new(e.errors.full_messages)
+      end
+    end
   end
 end
--- spec/api/provided_service_instance_spec.rb
+++ spec/api/provided_service_instance_spec.rb
@@ -1,18 +1,72 @@
 require_relative 'spec_helper'
 
-describe VCAP::CloudController::ProvidedServiceInstance do
-  describe "POST /v2/provided_service_instances" do
-    it "creates a provided service instance" do
-      space = VCAP::CloudController::Models::Space.make
-      developer = make_developer_for_space(space)
-      payload = {
-        'name' => 'provided',
-        'space_guid' => space.guid,
-        'credentials' => {'jz' => 'dj'},
-      }.to_json
-      expect { post "/v2/provided_service_instances", payload, headers_for(developer) }.to change {
-        VCAP::CloudController::Models::ProvidedServiceInstance.count
-      }.by 1
+module VCAP::CloudController
+  describe ProvidedServiceInstance do
+    include_examples "creating", path: "/v2/provided_service_instances",
+                     model: Models::ProvidedServiceInstance,
+                     required_attributes: %w(name space_guid credentials),
+                     unique_attributes: %w(space_guid name),
+                     extra_attributes: []
+
+    describe "Permissions" do
+      include_context "permissions"
+
+      before do
+        @obj_a = Models::ProvidedServiceInstance.make(:space => @space_a)
+        @obj_b = Models::ProvidedServiceInstance.make(:space => @space_b)
+      end
+
+      let(:creation_req_for_a) do
+        Yajl::Encoder.encode(
+          :name => Sham.name,
+          :space_guid => @space_a.guid,
+          :credentials => {"foopass" => "barpass"}
+        )
+      end
+      let(:update_req_for_a) {"{}"} # update is not implemented
+
+      def self.user_does_not_have_access(user_role, member_a_ivar, member_b_ivar)
+        describe user_role do
+          let(:member_a) { instance_variable_get(member_a_ivar) }
+          let(:member_b) { instance_variable_get(member_b_ivar) }
+
+          include_examples "permission checks", user_role,
+                           :model => Models::ProvidedServiceInstance,
+                           :path => "/v2/provided_service_instances",
+                           :enumerate => 0,
+                           :create => :not_allowed,
+                           :read => :not_allowed,
+                           :modify => :not_allowed,
+                           :delete => :not_allowed
+        end
+      end
+
+      describe "Org Level Permissions" do
+        user_does_not_have_access("OrgManager",     :@org_a_manager,         :@org_b_manager)
+        user_does_not_have_access("OrgUser",        :@org_a_member,          :@org_b_member)
+        user_does_not_have_access("BillingManager", :@org_a_billing_manager, :@org_b_billing_manager)
+        user_does_not_have_access("Auditor",        :@org_a_auditor,         :@org_b_auditor)
+      end
+
+      describe "App Space Level Permissions" do
+        user_does_not_have_access("SpaceManager", :@space_a_manager, :@space_b_manager)
+        user_does_not_have_access("SpaceAuditor", :@space_a_auditor, :@space_b_auditor)
+
+        describe "Developer" do
+          let(:member_a) { @space_a_developer }
+          let(:member_b) { @space_b_developer }
+
+          include_examples "permission checks", "Developer",
+                           :model => Models::ProvidedServiceInstance,
+                           :path => "/v2/provided_service_instances",
+                           :enumerate => 1,
+                           :create => :allowed,
+                           :read => :not_allowed,
+                           :modify => :not_allowed,
+                           :delete => :not_allowed
+        end
+      end
+
     end
   end
 end
