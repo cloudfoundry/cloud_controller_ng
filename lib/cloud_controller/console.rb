d4643887785c1d5a9a7841e42c55e4a4d85f004e
Alex Suraci and Phan Le <pair+alex+phan@pivotallabs.com>
2013-07-29 17:41:30 -0700

    Console using pry

--- lib/cloud_controller/console.rb
+++ lib/cloud_controller/console.rb
@@ -8,9 +8,14 @@ require "rubygems"
 require "bundler/setup"
 require "cloud_controller"
 require "irb/completion"
+require "pry"
 
 @config_file = File.expand_path("../../../config/cloud_controller.yml", __FILE__)
 @config = VCAP::CloudController::Config.from_file(@config_file)
 logger = Logger.new(STDOUT)
 
 VCAP::CloudController::DB.connect(logger, @config.fetch(:db).merge(log_level: :debug))
+
+module VCAP::CloudController
+  binding.pry
+end
