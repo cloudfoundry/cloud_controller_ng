284bf111dbdd012957b4363d2920a28d43dfd027
Jesse Zhang and Matthew Horan <pair+jz+mhoran@pivotallabs.com>
2013-08-23 10:15:55 -0700

    remove dead code

--- app/models/services/service_broker.rb
+++ app/models/services/service_broker.rb
@@ -5,10 +5,6 @@ module VCAP::CloudController::Models
     import_attributes :name, :broker_url, :token
     export_attributes :name, :broker_url
 
-    def self.user_visibility_filter(user)
-      user_visibility_filter_with_admin_override(id: []) # awful hack: non-admins see no records
-    end
-
     def validate
       validates_presence :name
       validates_presence :broker_url
