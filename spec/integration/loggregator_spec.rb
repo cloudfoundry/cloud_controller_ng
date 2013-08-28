67db3d80df64bf1876eec85917c4477eaa5807c6
Nick Wade and Johannes Tuchscherer <pairs+nwade+jtuchscherer@pivotallabs.com>
2013-08-28 11:03:23 -0600

    Adding loggregator tests

---
+++ spec/integration/loggregator_spec.rb
@@ -0,0 +1,66 @@
+require "spec_helper"
+require "securerandom"
+
+module VCAP::CloudController
+  describe "Cloud controller logs", :type => :integration do
+    before(:all) do
+      @loggregator_server = FakeLoggregatorServer.new(3456)
+      @loggregator_server.start
+
+      authed_headers = @authed_headers = {
+          "Authorization" => "bearer #{admin_token}",
+          "Accept" => "application/json",
+          "Content-Type" => "application/json"
+      }
+
+      start_cc(
+          debug: false,
+          config: "spec/fixtures/config/port_8181_config.yml"
+      )
+
+      org = make_post_request(
+          "/v2/organizations",
+          { "name" => "foo_org-#{SecureRandom.uuid}" }.to_json,
+          authed_headers
+      )
+      org_guid = org.json_body["metadata"]["guid"]
+
+      space = make_post_request(
+          "/v2/spaces",
+          { "name" => "foo_space",
+            "organization_guid" => org_guid
+          }.to_json,
+          authed_headers
+      )
+      @space_guid = space.json_body["metadata"]["guid"]
+
+      app = make_post_request(
+          "/v2/apps",
+          { "name" => "foo_app",
+            "space_guid" => @space_guid
+          }.to_json,
+          authed_headers
+      )
+
+      @app_id = app.json_body["metadata"]["guid"]
+    end
+
+    after(:all) do
+      stop_cc
+      stop_nats
+      @loggregator_server.stop(2)
+    end
+
+    it "to the loggregator" do
+      messages = @loggregator_server.messages
+
+      expect(messages.length).to eq 1
+      
+      message = messages[0]
+      expect(message.message).to eq "Created app with guid #{@app_id} with attributes {\"name\"=>\"foo_app\", \"space_guid\"=>\"#{@space_guid}\", \"production\"=>false, \"environment_json\"=>{}, \"memory\"=>256, \"instances\"=>1, \"disk_quota\"=>1024, \"state\"=>\"STOPPED\", \"console\"=>false}"
+      expect(message.app_id).to eq @app_id
+      expect(message.source_type).to eq LogMessage::SourceType::CLOUD_CONTROLLER
+      expect(message.message_type).to eq LogMessage::MessageType::OUT
+    end
+  end
+end
---
+++ spec/loggregator_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+module VCAP::CloudController
+  describe VCAP::CloudController::Loggregator do
+    describe "when no emitter is set" do
+      it "does not emit errors" do
+        LoggregatorEmitter::Emitter.any_instance.should_not_receive(:emit_error)
+        Loggregator.emit_error("app_id", "error message")
+      end
+      it "does not emit" do
+        LoggregatorEmitter::Emitter.any_instance.should_not_receive(:emit)
+        Loggregator.emit("app_id", "log message")
+      end
+
+    end
+    describe "when the emitter is set" do
+      it "emits errors to the loggregator" do
+        emitter = LoggregatorEmitter::Emitter.new("127.0.0.1:1234", LogMessage::SourceType::CLOUD_CONTROLLER)
+        emitter.should_receive(:emit_error).with("app_id", "error message")
+        Loggregator.emitter = emitter
+        Loggregator.emit_error("app_id", "error message")
+      end
+      it "emits to the loggregator" do
+        emitter = LoggregatorEmitter::Emitter.new("127.0.0.1:1234", LogMessage::SourceType::CLOUD_CONTROLLER)
+        emitter.should_receive(:emit).with("app_id", "log message")
+        Loggregator.emitter = emitter
+        Loggregator.emit("app_id", "log message")
+      end
+    end
+
+  end
+end
