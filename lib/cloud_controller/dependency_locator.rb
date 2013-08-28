434000a61c4a5bfb4d9b8ac73733cd5567a905cf
Maria Shaldibina and Mark Rushakoff <pair+mariash+mrushakoff@pivotallabs.com>
2013-08-28 12:22:15 -0700

    Prefer Hash#fetch in DependencyLocator

--- lib/cloud_controller/dependency_locator.rb
+++ lib/cloud_controller/dependency_locator.rb
@@ -17,21 +17,23 @@ module CloudController
     end
 
     def package_blob_store
-      package_cdn = Cdn.new(config[:packages][:cdn][:uri]) if config[:packages][:cdn]
+      packages = config.fetch(:packages)
+      package_cdn = Cdn.new(packages[:cdn].fetch(:uri, nil)) if packages.fetch(:cdn, nil)
 
       BlobStore.new(
-        config[:packages][:fog_connection],
-        config[:packages][:app_package_directory_key],
+        packages.fetch(:fog_connection),
+        packages.fetch(:app_package_directory_key),
         package_cdn
       )
     end
 
     def global_app_bits_cache
-      app_bit_cdn = Cdn.new(config[:resource_pool][:cdn][:uri]) if config[:resource_pool][:cdn]
+      resource_pool = config.fetch(:resource_pool)
+      app_bit_cdn = Cdn.new(resource_pool[:cdn].fetch(:uri, nil)) if resource_pool.fetch(:cdn, nil)
 
       BlobStore.new(
-        config[:resource_pool][:fog_connection],
-        config[:resource_pool][:resource_directory_key],
+        resource_pool.fetch(:fog_connection),
+        resource_pool.fetch(:resource_directory_key),
         app_bit_cdn
       )
     end
