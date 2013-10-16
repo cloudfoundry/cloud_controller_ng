ae4e2fe40113e2de109b449369c140fa6921f159
Phan Le and Ruth Byers <pair+phan+rbyers@pivotallabs.com>
2013-10-16 09:51:03 -0700

    Fixed the flakey test.

--- spec/varz_spec.rb
+++ spec/varz_spec.rb
@@ -4,12 +4,16 @@ require "cloud_controller/varz"
 
 module VCAP::CloudController
   describe Varz do
+
     it "should include the number of users in varz" do
+      # We have to use stubbing here because when we run in parallel mode,
+      # there might other tests running and create/delete users concurrently.
+      User.stub(:count).and_return(0)
       VCAP::Component.varz.synchronize do
         VCAP::Component.varz[:cc_user_count] = 0
       end
 
-      4.times { VCAP::CloudController::User.make }
+      User.stub(:count).and_return(4)
       Varz.bump_user_count
 
       VCAP::Component.varz.synchronize do
