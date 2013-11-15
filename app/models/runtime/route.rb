dc41ae498870f300bec521b062bc8c3e8a2e20ae
phanle <phan.anh.le@gmail.com>
2013-11-14 21:33:56 -0800

    Refactor to query commands and a maximum routes policy

--- app/models/runtime/route.rb
+++ app/models/runtime/route.rb
@@ -108,12 +108,37 @@ module VCAP::CloudController
       end
     end
 
+    class RoutesForOrganizationQuery
+      def initialize(organization)
+        @organization = organization
+      end
+
+      def count
+        dataset.count
+      end
+
+      private
+      def dataset
+        Route.dataset.join(:spaces, id: :space_id).
+            where(spaces__organization_id: @organization.id)
+      end
+    end
+
+    class MaximumRoutesPolicy
+      def initialize(organization)
+        @organization = organization
+      end
+
+      def can_add_route?(number_of_new_routes)
+        existing_total_routes = RoutesForOrganizationQuery.new(@organization).count
+        @organization.quota_definition.total_routes >= (existing_total_routes + number_of_new_routes)
+      end
+    end
+
     def validate_total_routes
       return unless new? && space
-      organization = space.organization
-      existing_total_routes = Route.dataset.join(:spaces, id: :space_id).where(spaces__organization_id: organization.id).count
 
-      if (existing_total_routes + 1) > organization.quota_definition.total_routes
+      unless MaximumRoutesPolicy.new(space.organization).can_add_route?(1)
         errors.add(:base, :total_routes_exceeded)
       end
     end
