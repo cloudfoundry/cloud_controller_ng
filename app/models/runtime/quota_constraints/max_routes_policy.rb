2e30e92fbb0ce5076c08741f353cdbec06abd1a1
Tim Labeeuw <tlabeeuw@pivotallabs.com>
2013-11-18 09:46:38 -0800

    Allow adding unlimited routes
    Signed-off-by: Tim Labeeuw <tlabeeuw@pivotallabs.com>

--- app/models/runtime/quota_constraints/max_routes_policy.rb
+++ app/models/runtime/quota_constraints/max_routes_policy.rb
@@ -4,6 +4,8 @@ class MaxRoutesPolicy
   end
 
   def allow_more_routes?(number_of_new_routes)
+    return true if @organization.quota_definition.total_routes == -1
+
     existing_total_routes = OrganizationRoutes.new(@organization).count
     @organization.quota_definition.total_routes >= (existing_total_routes + number_of_new_routes)
   end
--- spec/models/runtime/quota_contraints/max_routes_policy_spec.rb
+++ spec/models/runtime/quota_contraints/max_routes_policy_spec.rb
@@ -3,10 +3,8 @@ require "spec_helper"
 describe MaxRoutesPolicy do
   describe "#allow_more_routes?" do
     let(:policy) { MaxRoutesPolicy.new(organization) }
-    let(:organization) do
-      quota_definition = VCAP::CloudController::QuotaDefinition.make(total_routes: 4)
-      VCAP::CloudController::Organization.make(quota_definition: quota_definition)
-    end
+    let(:quota_definition) { VCAP::CloudController::QuotaDefinition.make(total_routes: 4) }
+    let(:organization) { VCAP::CloudController::Organization.make(quota_definition: quota_definition) }
     let!(:routes) do
       space = VCAP::CloudController::Space.make(organization: organization)
       2.times { VCAP::CloudController::Route.make(space: space) }
@@ -26,6 +24,12 @@ describe MaxRoutesPolicy do
 
     context "when requested less than the total allowed routes" do
       let(:requested_routes) { 1 }
+      it { should be_true }
+    end
+
+    context "when an unlimited amount routes are available" do
+      let(:quota_definition) { VCAP::CloudController::QuotaDefinition.make(total_routes: -1) }
+      let(:requested_routes) { 100_000_000 }
       it { should be_true }
     end
   end
