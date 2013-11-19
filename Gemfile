0b06e4c7642ebc839b10ba29025c17b7a6177fee
Tim Labeeuw <tlabeeuw@pivotallabs.com>
2013-11-19 15:46:01 -0800

    Small Refactors

--- Gemfile
+++ Gemfile
@@ -21,7 +21,6 @@ gem "cf-message-bus", git: "https://github.com/cloudfoundry/cf-message-bus.git"
 gem "vcap_common", git: "https://github.com/cloudfoundry/vcap-common.git"
 gem "allowy"
 gem "loggregator_emitter", "~> 2.0"
-# Point to our clone, pending a pull request merged to the original author
 gem "talentbox-delayed_job_sequel", git: "https://github.com/TalentBox/delayed_job_sequel", ref: "8725e1ee"
 gem "thin", "~> 1.5.1"
 
--- app/models/runtime/user.rb
+++ app/models/runtime/user.rb
@@ -2,41 +2,41 @@ module VCAP::CloudController
   class User < Sequel::Model
     no_auto_guid
 
-    many_to_many      :organizations
+    many_to_many :organizations
 
-    many_to_one       :default_space, :key => :default_space_id,
-                      :class => "VCAP::CloudController::Space"
+    many_to_one :default_space, key: :default_space_id,
+      class: "VCAP::CloudController::Space"
 
-    many_to_many      :managed_organizations,
-                      :class => "VCAP::CloudController::Organization",
-                      :join_table => "organizations_managers",
-                      :right_key => :organization_id, :reciprocal => :managers
+    many_to_many :managed_organizations,
+      class: "VCAP::CloudController::Organization",
+      join_table: "organizations_managers",
+      right_key: :organization_id, reciprocal: :managers
 
-    many_to_many      :billing_managed_organizations,
-                      :class => "VCAP::CloudController::Organization",
-                      :join_table => "organizations_billing_managers",
-                      :right_key => :organization_id,
-                      :reciprocal => :billing_managers
+    many_to_many :billing_managed_organizations,
+      class: "VCAP::CloudController::Organization",
+      join_table: "organizations_billing_managers",
+      right_key: :organization_id,
+      reciprocal: :billing_managers
 
-    many_to_many      :audited_organizations,
-                      :class => "VCAP::CloudController::Organization",
-                      :join_table => "organizations_auditors",
-                      :right_key => :organization_id, :reciprocal => :auditors
+    many_to_many :audited_organizations,
+      class: "VCAP::CloudController::Organization",
+      join_table: "organizations_auditors",
+      right_key: :organization_id, reciprocal: :auditors
 
-    many_to_many      :spaces,
-                      :class => "VCAP::CloudController::Space",
-                      :join_table => "spaces_developers",
-                      :right_key => :space_id, :reciprocal => :developers
+    many_to_many :spaces,
+      class: "VCAP::CloudController::Space",
+      join_table: "spaces_developers",
+      right_key: :space_id, reciprocal: :developers
 
-    many_to_many      :managed_spaces,
-                      :class => "VCAP::CloudController::Space",
-                      :join_table => "spaces_managers",
-                      :right_key => :space_id, :reciprocal => :managers
+    many_to_many :managed_spaces,
+      class: "VCAP::CloudController::Space",
+      join_table: "spaces_managers",
+      right_key: :space_id, reciprocal: :managers
 
-    many_to_many      :audited_spaces,
-                      :class => "VCAP::CloudController::Space",
-                      :join_table => "spaces_auditors",
-                      :right_key => :space_id, :reciprocal => :auditors
+    many_to_many :audited_spaces,
+      class: "VCAP::CloudController::Space",
+      join_table: "spaces_auditors",
+      right_key: :space_id, reciprocal: :auditors
 
     add_association_dependencies organizations: :nullify
     add_association_dependencies managed_organizations: :nullify
