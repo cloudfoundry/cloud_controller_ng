eaf4a2e82e22ab65ee6abde322e44d11b2c214d0
Eric Malm <emalm@pivotallabs.com>
2013-11-22 14:30:20 -0800

    Refactor specs
      * prefer let() to instance variables
      * cleanup naming
      * make DelayedJob expectations more readable
    
    [#60026682]
    
    Signed-off-by: Aram Price <aprice@pivotallabs.com>

--- spec/controllers/runtime/buildpacks_controller_spec.rb
+++ spec/controllers/runtime/buildpacks_controller_spec.rb
@@ -53,29 +53,29 @@ module VCAP::CloudController
       end
 
       context "GET" do
-        before { @test_buildpack = VCAP::CloudController::Buildpack.create_from_hash(name: "get_buildpack", key: "xyz", position: 0) }
+        let!(:buildpack) { VCAP::CloudController::Buildpack.create_from_hash(name: "get_buildpack", key: "xyz", position: 0) }
 
         describe "/v2/buildpacks/:guid" do
           it "lets you retrieve info for a specific buildpack" do
-            get "/v2/buildpacks/#{@test_buildpack[:guid]}", {}, headers_for(user)
+            get "/v2/buildpacks/#{buildpack[:guid]}", {}, headers_for(user)
             expect(last_response.status).to eq(200)
             entity = decoded_response['entity']
             metadata = decoded_response['metadata']
-            expect(metadata['guid']).to eq(@test_buildpack[:guid])
-            expect(entity['name']).to eq(@test_buildpack[:name])
+            expect(metadata['guid']).to eq(buildpack[:guid])
+            expect(entity['name']).to eq(buildpack[:name])
           end
         end
 
         describe "/v2/buildpacks?name" do
           it "lets you retrieve info for a specific buildpack" do
-            get "/v2/buildpacks?name=#{@test_buildpack[:name]}", {}, headers_for(user)
+            get "/v2/buildpacks?name=#{buildpack[:name]}", {}, headers_for(user)
             expect(last_response.status).to eq(200)
             expect(decoded_response['total_results']).to eq(1)
             resource = decoded_response['resources'][0]
             entity = resource['entity']
             metadata = resource['metadata']
-            expect(metadata['guid']).to eq(@test_buildpack[:guid])
-            expect(entity['name']).to eq(@test_buildpack[:name])
+            expect(metadata['guid']).to eq(buildpack[:guid])
+            expect(entity['name']).to eq(buildpack[:name])
           end
         end
 
@@ -84,40 +84,39 @@ module VCAP::CloudController
             get "/v2/buildpacks", {}, headers_for(user)
             expect(last_response.status).to eq(200)
             expect(decoded_response["total_results"]).to eq(1)
-            expect(decoded_response["resources"][0]["entity"]).to eq({
-              'name' => 'get_buildpack', 'position' => 0, 'enabled' => true})
+            expect(decoded_response["resources"][0]["entity"]).to eq({'name' => 'get_buildpack',
+                                                                      'position' => 0,
+                                                                      'enabled' => true})
           end
         end
       end
 
       context "UPDATE" do
-        before(:each) do
-          @orig_buildpack = VCAP::CloudController::Buildpack.create_from_hash(name: "original_buildpack", key: "xyz", position: 1)
-          @test_buildpack = VCAP::CloudController::Buildpack.create_from_hash(name: "update_buildpack", key: "xyz", position: 2)
-        end
+        let!(:buildpack1) { VCAP::CloudController::Buildpack.create_from_hash(name: "first_buildpack", key: "xyz", position: 1) }
+        let!(:buildpack2) { VCAP::CloudController::Buildpack.create_from_hash(name: "second_buildpack", key: "xyz", position: 2) }
 
         it "returns NOT AUTHORIZED (403) for non admins" do
-          put "/v2/buildpacks/#{@test_buildpack.guid}", {}, headers_for(user)
+          put "/v2/buildpacks/#{buildpack2.guid}", {}, headers_for(user)
           expect(last_response.status).to eq(403)
         end
 
         describe "/v2/buildpacks/:guid" do
           it "updates the position" do
             expect {
-              put "/v2/buildpacks/#{@test_buildpack.guid}", '{"position": 1}', admin_headers
+              put "/v2/buildpacks/#{buildpack2.guid}", '{"position": 1}', admin_headers
               expect(last_response.status).to eq(201)
             }.to change {
               Buildpack.order(:position).map { |bp| [bp.name, bp.position] }
             }.from(
-              [["original_buildpack", 1], ["update_buildpack", 2]]
-            ).to(
-              [["update_buildpack", 1], ["original_buildpack", 2]]
-            )
+                   [["first_buildpack", 1], ["second_buildpack", 2]]
+                 ).to(
+                   [["second_buildpack", 1], ["first_buildpack", 2]]
+                 )
           end
 
           it "updates current end to beyond end of list" do
             expect {
-              put "/v2/buildpacks/#{@test_buildpack.guid}", '{"position": 10}', admin_headers
+              put "/v2/buildpacks/#{buildpack2.guid}", '{"position": 10}', admin_headers
               expect(last_response.status).to eq(201)
             }.to_not change {
               Buildpack.order(:position).map { |bp| [bp.name, bp.position] }
@@ -126,20 +125,22 @@ module VCAP::CloudController
 
           it "updates current end position 0" do
             expect {
-              put "/v2/buildpacks/#{@test_buildpack.guid}", '{"position": 0}', admin_headers
+              put "/v2/buildpacks/#{buildpack2.guid}", '{"position": 0}', admin_headers
               expect(last_response.status).to eq(201)
             }.to change {
               Buildpack.order(:position).map { |bp| [bp.name, bp.position] }
             }.from(
-              [["original_buildpack", 1], ["update_buildpack", 2]]
-            ).to(
-              [["update_buildpack", 0], ["original_buildpack", 1]]
-            )
+                   [["first_buildpack", 1], ["second_buildpack", 2]]
+                 ).to(
+                   [["second_buildpack", 0], ["first_buildpack", 1]]
+                 )
           end
         end
       end
 
       context "DELETE" do
+        let(:buildpack) { VCAP::CloudController::Buildpack.make }
+
         it "returns NOT FOUND (404) if the buildpack does not exist" do
           delete "/v2/buildpacks/abcd", {}, admin_headers
           expect(last_response.status).to eq(404)
@@ -147,37 +148,34 @@ module VCAP::CloudController
 
         context "create a default buildpack" do
           it "returns NOT AUTHORIZED (403) for non admins" do
-            @test_buildpack = VCAP::CloudController::Buildpack.make
-            delete "/v2/buildpacks/#{@test_buildpack.guid}", {}, headers_for(user)
+            delete "/v2/buildpacks/#{buildpack.guid}", {}, headers_for(user)
             expect(last_response.status).to eq(403)
           end
 
           it "returns a NO CONTENT (204) if an admin deletes a build pack" do
-            @test_buildpack = VCAP::CloudController::Buildpack.make
-            delete "/v2/buildpacks/#{@test_buildpack.guid}", {}, admin_headers
+            delete "/v2/buildpacks/#{buildpack.guid}", {}, admin_headers
             expect(last_response.status).to eq(204)
           end
 
           it "destroys the buildpack key in the blobstore" do
             buildpack_blobstore = CloudController::DependencyLocator.instance.buildpack_blobstore
-            @test_buildpack = VCAP::CloudController::Buildpack.make
+            buildpack_blobstore.cp_to_blobstore(Tempfile.new(['FAKE_BUILDPACK', '.zip']), buildpack.key)
 
-            fake_buildpack_zip = Tempfile.new(['FAKE_BUILDPACK', '.zip'])
-            @test_buildpack.update_from_hash(key: File.new(fake_buildpack_zip).hexdigest)
-            buildpack_blobstore.cp_to_blobstore(fake_buildpack_zip, @test_buildpack.key)
-            expect(buildpack_blobstore.exists?(@test_buildpack.key)).to be_true
+            delete "/v2/buildpacks/#{buildpack.guid}", {}, admin_headers
 
-            delete "/v2/buildpacks/#{@test_buildpack.guid}", {}, admin_headers
-            expect(Buildpack.find(name: @test_buildpack.name)).to be_nil
-            expect(Delayed::Worker.new.work_off(1)).to eq([1,0])
-            expect(buildpack_blobstore.exists?(@test_buildpack.key)).to be_false
+            expect(Buildpack.find(name: buildpack.name)).to be_nil
+            expect { Delayed::Worker.new.work_off(1) }.to change {
+              buildpack_blobstore.exists?(buildpack.key)
+            }.from(true).to(false)
           end
 
           it "does not fail if no buildpack bits were ever uploaded" do
-            @test_buildpack = VCAP::CloudController::Buildpack.make(key: nil)
-            delete "/v2/buildpacks/#{@test_buildpack.guid}", {}, admin_headers
+            buildpack.update_from_hash(key: nil)
+            expect(buildpack.key).to be_nil
+
+            delete "/v2/buildpacks/#{buildpack.guid}", {}, admin_headers
             expect(last_response.status).to eql(204)
-            expect(Buildpack.find(name: @test_buildpack.name)).to be_nil
+            expect(Buildpack.find(name: buildpack.name)).to be_nil
           end
         end
       end
