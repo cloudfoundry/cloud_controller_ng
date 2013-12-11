a3c984110f3b5a085b43b8b58794f142d6916ebf
Lucas Marks <lmarks@pivotallabs.com>
2013-12-10 16:13:42 -0800

    Add API tests for Binding
    [#61759262]
    Signed-off-by: Jimmy Da <jda@pivotallabs.com>

--- spec/controllers/services/service_instances_controller/v2.0_spec.rb
+++ spec/controllers/services/service_instances_controller/v2.0_spec.rb
@@ -55,18 +55,20 @@ describe 'Service Broker API integration', type: :controller do
         end
 
         context 'when update-service-broker' do
+          let(:broker_guid) { @broker_info[0]}
+
           before(:all) do
-            @broker_guid, _ = setup_broker
+            @broker_info = setup_broker
           end
 
           after(:all) do
-            delete_broker(@broker_guid)
+            delete_broker(broker_guid)
           end
 
           before do
             stub_catalog_fetch
 
-            put("/v2/service_brokers/#{@broker_guid}",
+            put("/v2/service_brokers/#{broker_guid}",
               {}.to_json,
               json_headers(admin_headers))
           end
@@ -91,12 +93,16 @@ describe 'Service Broker API integration', type: :controller do
         }
       end
 
+      # TODO: cleanup access to broker_info, set instance vars in setup_broker? find a way to completely remove instance vars?
+      let(:broker_guid) { @broker_info[0]}
+      let(:plan_guid) { @broker_info[1]}
+
       before(:all) do
-        @broker_guid, @plan_guid = setup_broker
+        @broker_info = setup_broker
       end
 
       after(:all) do
-        delete_broker(@broker_guid)
+        delete_broker(broker_guid)
       end
 
       describe 'service provision request' do
@@ -110,7 +116,7 @@ describe 'Service Broker API integration', type: :controller do
             {
               name:              'test-service',
               space_guid:        space_guid,
-              service_plan_guid: @plan_guid
+              service_plan_guid: plan_guid
             }.to_json,
             json_headers(admin_headers))
         end
@@ -153,7 +159,7 @@ describe 'Service Broker API integration', type: :controller do
               to_return(status: 409, body: '{}')
 
             post('/v2/service_instances',
-              { name: 'test-service', space_guid: space_guid, service_plan_guid: @plan_guid }.to_json,
+              { name: 'test-service', space_guid: space_guid, service_plan_guid: plan_guid }.to_json,
               json_headers(admin_headers)
             )
           end
@@ -169,8 +175,104 @@ describe 'Service Broker API integration', type: :controller do
       end
     end
 
-    describe 'Binding'
-    describe 'Unbinding'
+    describe 'Binding' do
+      let(:binding_id_pattern) { '[[:alnum:]-]+$' }
+      let(:broker_response_status) { 200 }
+      let(:broker_response_body) do
+        {
+          credentials: {
+            uri:      "mysql://mysqluser:pass@mysqlhost:3306/dbname",
+            username: "mysqluser",
+            password: "pass",
+            host:     "mysqlhost",
+            port:     3306,
+            database: "dbname"
+          }
+        }.to_json
+      end
+      let(:application) { VCAP::CloudController::AppFactory.make(space: space) }
+      let(:app_guid) { application.guid }
+
+      let(:broker_guid) { broker_info[0]}
+      let(:plan_guid) { broker_info[1]}
+      let(:broker_info) { setup_broker}
+
+      after(:all) do
+        delete_broker(broker_guid)
+      end
+
+      describe 'service binding request' do
+        let(:request_from_cc_to_broker) do
+          {
+            plan_id: "plan1-guid-here",
+            service_id:"service-guid-here"
+          }
+        end
+
+        before do
+          @service_instance_guid = provision_service(plan_guid, space_guid)
+
+          stub_request(:put, %r(/v2/service_instances/#{@service_instance_guid}/service_bindings/#{binding_id_pattern})).
+            to_return(status: broker_response_status, body: broker_response_body)
+
+          post('/v2/service_bindings',
+            { app_guid: app_guid, service_instance_guid: @service_instance_guid }.to_json,
+            json_headers(admin_headers))
+        end
+
+        it 'uses the correct version header' do
+          request_has_version_header(:put, %r(/v2/service_instances/#{@service_instance_guid}/service_bindings/#{binding_id_pattern}))
+        end
+
+        it 'sends request with basic auth' do
+          a_request(:put, %r(http://username:password@broker-url/v2/service_instances/#{@service_instance_guid}/service_bindings/#{binding_id_pattern})).should have_been_made
+        end
+
+        it 'sends all required fields' do
+          a_request(:put, %r(broker-url/v2/service_instances/#{@service_instance_guid}/service_bindings/#{binding_id_pattern})).
+            with(body: hash_including(request_from_cc_to_broker)).
+            should have_been_made
+        end
+
+        it 'makes a request' do
+          a_request(:put, %r(/v2/service_instances/#{@service_instance_guid}/service_bindings/#{binding_id_pattern})).should have_been_made
+        end
+
+        context 'when broker responds with 200' do
+          let(:broker_response_status) { 200 }
+
+          it 'responds to user with 201' do
+            expect(last_response.status).to eq(201)
+          end
+        end
+
+        context 'when broker responds with 201' do
+          let(:broker_response_status) { 201 }
+
+          it 'responds to user with 201' do
+            expect(last_response.status).to eq(201)
+          end
+        end
+
+        context 'when broker responds with 409' do
+          let(:broker_response_status) { 409 }
+
+          it 'responds to user with 409' do
+            expect(last_response.status).to eq(409)
+          end
+        end
+
+        context 'when broker omits the - hash from its response' do
+          let(:broker_response_body) { {}.to_json }
+
+          it 'responds to user with 509' do
+            expect(last_response.status).to eq(201)
+          end
+        end
+      end
+    end
+
+    describe 'Unbinding' 
     describe 'Unprovisioning'
     describe 'Broker Errors'
     describe 'Orphans'
@@ -210,12 +312,12 @@ describe 'Service Broker API integration', type: :controller do
       post('/v2/service_brokers',
         { name: 'broker-name', broker_url: 'http://broker-url', auth_username: 'username', auth_password: 'password' }.to_json,
         json_headers(admin_headers))
-      response = JSON.parse(last_response.body)
+      response    = JSON.parse(last_response.body)
       broker_guid = response['metadata']['guid']
 
       get('/v2/services?inline-relations-depth=1', '{}', json_headers(admin_headers))
-      response = JSON.parse(last_response.body)
-      small_plan_guid = response['resources'].first['entity']['service_plans'].find {|plan| plan['entity']['name']=='small'}['metadata']['guid']
+      response        = JSON.parse(last_response.body)
+      small_plan_guid = response['resources'].first['entity']['service_plans'].find { |plan| plan['entity']['name']=='small' }['metadata']['guid']
 
       make_all_plans_public
 
@@ -225,8 +327,8 @@ describe 'Service Broker API integration', type: :controller do
     end
 
     def make_all_plans_public
-      response = get('/v2/service_plans', '{}', json_headers(admin_headers))
-      service_plan_guids = JSON.parse(response.body).fetch('resources').map {|plan| plan.fetch('metadata').fetch('guid')}
+      response           = get('/v2/service_plans', '{}', json_headers(admin_headers))
+      service_plan_guids = JSON.parse(response.body).fetch('resources').map { |plan| plan.fetch('metadata').fetch('guid') }
       service_plan_guids.each do |service_plan_guid|
         put("/v2/service_plans/#{service_plan_guid}", JSON.dump(public: true), json_headers(admin_headers))
       end
@@ -234,6 +336,26 @@ describe 'Service Broker API integration', type: :controller do
 
     def delete_broker(broker_guid)
       delete("/v2/service_brokers/#{broker_guid}", '{}', json_headers(admin_headers))
+    end
+
+    def provision_service(plan_guid, space_guid)
+      guid_pattern = '[[:alnum:]-]+'
+      broker_url = 'broker-url'
+      body = { dashboard_url: "https://your.service.com/dashboard" }.to_json
+      stub_request(:put, %r(#{broker_url}/v2/service_instances/#{guid_pattern})).
+        to_return(status: 201, body: "#{body}")
+
+      post('/v2/service_instances',
+        {
+          name:              'test-service',
+          space_guid:        space_guid,
+          service_plan_guid: plan_guid
+        }.to_json,
+        json_headers(admin_headers))
+
+      response = JSON.parse(last_response.body)
+      service_instance_guid = response['metadata']['guid']
+      service_instance_guid
     end
   end
 end
