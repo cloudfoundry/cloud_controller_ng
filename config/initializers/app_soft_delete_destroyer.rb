b90e96a26b8fdb0f8e1f24e24e226d01e7badf0d
Phan Le <ple@pivotallabs.com>
2013-12-19 11:53:41 -0800

    Ensure AppEvents are deleted before deleting their (soft deleted) apps
    Signed-off-by: Joseph Palermo <jpalermo@pivotallabs.com>

--- config/initializers/app_soft_delete_destroyer.rb
+++ config/initializers/app_soft_delete_destroyer.rb
@@ -1,5 +1,7 @@
 module CCInitializers
   def self.app_soft_delete_destroyer(_)
+    deleted_app_id_ref = VCAP::CloudController::App.where(not_deleted: nil).select(:id)
+    VCAP::CloudController::AppEvent.where(app_id: deleted_app_id_ref).delete
     VCAP::CloudController::App.where(not_deleted: nil).delete
   end
 end
