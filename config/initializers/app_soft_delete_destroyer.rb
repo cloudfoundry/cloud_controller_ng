2bef249a3c3cda2ffd519c4f5191fef8ee760b7f
Phan Le <ple@pivotallabs.com>
2013-12-20 10:09:09 -0800

    Change app routes cleaning up script
    Signed-off-by: Joseph Palermo <jpalermo@pivotallabs.com>

--- config/initializers/app_soft_delete_destroyer.rb
+++ config/initializers/app_soft_delete_destroyer.rb
@@ -1,9 +1,13 @@
 module CCInitializers
   def self.app_soft_delete_destroyer(_)
-    deleted_app_id_ref = VCAP::CloudController::App.where(not_deleted: nil).select(:id)
-    VCAP::CloudController::AppEvent.where(app_id: deleted_app_id_ref).delete
+    # We need to purge soft deleted apps. But we cannot do it in a migration because other
+    # running CC's might still be creating soft deleted apps while the migration is running
+    # This needs to persist until all CC's are no longer soft deleting apps which could
+    # be a while because of on premise installs.
     app_klass = VCAP::CloudController::App
-    app_klass.db.run("delete from apps_routes where apps_routes.app_id in (#{app_klass.where(not_deleted: nil).select(:id).sql})")
+    deleted_app_id_ref = app_klass.where(not_deleted: nil).select(:id)
+    VCAP::CloudController::AppEvent.where(app_id: deleted_app_id_ref).delete
+    Sequel::Model.db[:apps_routes].where(app_id: deleted_app_id_ref).delete
     app_klass.where(not_deleted: nil).delete
   end
 end
