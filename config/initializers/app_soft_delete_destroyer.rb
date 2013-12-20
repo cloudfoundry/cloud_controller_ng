6afe328d2b057f3ef569f395f292afd42806391d
Phan Le <ple@pivotallabs.com>
2013-12-19 18:00:04 -0800

    Deleted the route associations for soft deleted deleted apps.
    Signed-off-by: Joseph Palermo <jpalermo@pivotallabs.com>

--- config/initializers/app_soft_delete_destroyer.rb
+++ config/initializers/app_soft_delete_destroyer.rb
@@ -2,6 +2,8 @@ module CCInitializers
   def self.app_soft_delete_destroyer(_)
     deleted_app_id_ref = VCAP::CloudController::App.where(not_deleted: nil).select(:id)
     VCAP::CloudController::AppEvent.where(app_id: deleted_app_id_ref).delete
-    VCAP::CloudController::App.where(not_deleted: nil).delete
+    app_klass = VCAP::CloudController::App
+    app_klass.db.run("delete from apps_routes where apps_routes.app_id in (#{app_klass.where(not_deleted: nil).select(:id).sql})")
+    app_klass.where(not_deleted: nil).delete
   end
 end
