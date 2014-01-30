3bb78f9d2070370b962ebf288ee7fe05890463c6
Aram Price <aprice@pivotallabs.com>
2014-01-30 13:51:57 -0800

    Integration test only checks that /varz works
      * unit tests conver /varz data
    
    Signed-off-by: Matthew Kocher <mkocher@pivotallabs.com>

--- spec/integration/varz_spec.rb
+++ spec/integration/varz_spec.rb
@@ -33,45 +33,13 @@ describe 'Cloud Controller', :type => :integration do
     stop_nats
   end
 
-  it 'reports number of users' do
-    count = 0
-
-    make_get_request('/v2/users', auth_headers) #check it -- we hit the API to make the mysterious user with guid-1
-
-    sleep(1) #give varz a chance to populate
-
+  it 'responds to /varz with the expected keys' do
     make_get_request('/varz', varz_headers, 7800).tap do |response|
-      expect(JSON.parse(response.body)).to have_key('cc_user_count')
-      count = JSON.parse(response.body)['cc_user_count']
+      response_hash = JSON.parse(response.body)
+      expect(response_hash).to have_key('vcap_sinatra')
+      expect(response_hash['vcap_sinatra']).to have_key('requests')
+      expect(response_hash['vcap_sinatra']).to have_key('http_status')
+      expect(response_hash['vcap_sinatra']).to have_key('recent_errors')
     end
-
-    response = make_post_request('/v2/users', user_params.to_json, auth_headers)
-    expect(response.code).to eql('201')
-
-    sleep(1)
-
-    make_get_request('/varz', varz_headers, 7800).tap do |response|
-      expect(JSON.parse(response.body)['cc_user_count']).to eql(count + 1)
-    end
-  end
-
-  it 'reports the length of CC job queue' do
-    cc_job_queue_length = nil
-
-    response = make_post_request('/v2/users', user_params.to_json, auth_headers)
-    expect(response.code).to eql('201')
-
-    # Async deletion creates a delayed job
-    response = make_delete_request("/v2/users/#{user_guid}?async=true", auth_headers)
-    expect(response.code).to eql('202')
-
-    sleep 1
-
-    make_get_request('/varz', varz_headers, 7800).tap do |response|
-      expect(JSON.parse(response.body)).to have_key('cc_job_queue_length')
-      cc_job_queue_length = JSON.parse(response.body)['cc_job_queue_length']
-    end
-
-    expect(cc_job_queue_length['cc-generic']).to eq(1)
   end
 end
