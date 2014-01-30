ac73fcf6164ec71d97805d98e74fa03560f20730
Abhijit Hiremagalur <abhi@pivotallabs.com>
2014-01-29 16:10:50 -0800

    Introduce rack-timeout to interrupt long requests
    The timeout in this does not actually interrupt a thread that takes too long [#64688246]
    
    Signed-off-by: Eric Malm <emalm@pivotallabs.com>

--- lib/cloud_controller/rack_app_builder.rb
+++ lib/cloud_controller/rack_app_builder.rb
@@ -1,15 +1,19 @@
+require "rack/timeout"
+
 module VCAP::CloudController
   class RackAppBuilder
+    TIMEOUT = ::Rack::Timeout.timeout = 5.minutes
+
     def build(config)
       token_decoder = VCAP::UaaTokenDecoder.new(config[:uaa])
       Rack::Builder.new do
         use Rack::CommonLogger
+        use Rack::Timeout
 
         if config[:development_mode]
           require 'new_relic/rack/developer_mode'
           use NewRelic::Rack::DeveloperMode
         end
-
 
         map "/" do
           run Controller.new(config, token_decoder)
---
+++ spec/rack_app_builder_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+module VCAP::CloudController
+  describe RackAppBuilder do
+    subject(:builder) do
+      RackAppBuilder.new
+    end
+
+    describe "#build" do
+      before do
+        allow(Rack::CommonLogger).to receive(:new)
+        allow(Rack::Timeout).to receive(:new)
+      end
+
+      it "returns a Rack application" do
+        expect(builder.build(config)).to be_a(Rack::Builder)
+        expect(builder.build(config)).to respond_to(:call)
+      end
+
+      it "uses Rack::CommonLogger" do
+        builder.build(config).to_app
+
+        expect(Rack::CommonLogger).to have_received(:new)
+      end
+
+      it "uses Rack::Timeout to interrupt requests that take more than configured" do
+        builder.build(config).to_app
+
+        expect(Rack::Timeout).to have_received(:new)
+        expect(Rack::Timeout.timeout).to eq(RackAppBuilder::TIMEOUT)
+        expect(RackAppBuilder::TIMEOUT).to eq(5.minutes)
+      end
+    end
+  end
+end
