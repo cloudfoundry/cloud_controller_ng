9961f0d9720633dfc2f42b1bf370c7c8c14af51c
Andy Bravery <andrewjf_bravery@uk.ibm.com>
2014-02-12 14:14:37 +0000

    Add the service_broker_id to the attributes exposed on a service through the API

--- spec/controllers/services/services_controller_spec.rb
+++ spec/controllers/services/services_controller_spec.rb
@@ -112,7 +112,8 @@ module VCAP::CloudController
       let(:headers) { headers_for(user) }
 
       before do
-        @active = 3.times.map { Service.make(:active => true, :long_description => Sham.long_description).
+        @service_broker=ServiceBroker.make(name: 'FreeWidgets', broker_url: 'http://example.com/', auth_password: 'secret')
+        @active = 3.times.map { Service.make(:active => true, :long_description => Sham.long_description, :service_broker => @service_broker).
           tap{|svc| ServicePlan.make(:service => svc) } }
         @inactive = 2.times.map { Service.make(:active => false).tap{|svc| ServicePlan.make(:service => svc) } }
       end
@@ -178,6 +179,14 @@ module VCAP::CloudController
           get "/v2/services?q=active:f", {}, headers
           last_response.should be_ok
           decoded_guids.should =~ @inactive.map(&:guid)
+        end
+      end
+      
+      describe "get /v2/services?q=service_broker_guid:<guid>" do
+        it "can list services by broker" do
+          get "/v2/services?q=service_broker_guid:"+@service_broker.guid, {}, headers
+          last_response.should be_ok
+          decoded_guids.should =~ @active.map(&:guid)
         end
       end
     end
