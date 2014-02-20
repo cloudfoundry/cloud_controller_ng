0d725173c18ebcb03636476bd9dfda6536384cd0
Eric Malm <emalm@pivotallabs.com>
2014-02-20 09:48:14 -0800

    Create TimeoutJob wrapper for Delayed::Job
    [#62361748]
    
    Signed-off-by: Ruth Byers <rbyers@pivotallabs.com>

--- app/jobs/enqueuer.rb
+++ app/jobs/enqueuer.rb
@@ -5,5 +5,19 @@ module VCAP::CloudController
         Delayed::Job.enqueue(job, opts)
       end
     end
+
+    class TimeoutJob < Struct.new(:job)
+      def perform
+        Timeout.timeout max_run_time(job.job_name) do
+          job.perform
+        end
+      end
+
+      def max_run_time(job_name)
+        jobs_config = VCAP::CloudController::Config.config[:jobs]
+        job_config = jobs_config[job_name] || jobs_config[:global]
+        job_config[:timeout_in_seconds]
+      end
+    end
   end
 end
