351189528ed3a12e3f433279dae023b44a5e36ae
Tammer Saleh <tammer@pivotallabs.com>
2014-02-20 10:52:21 -0800

    IdempotentDirectory => CloudController::Blobstore::IdempotentDirectory
    Signed-off-by: Abhijit Hiremagalur <abhi@pivotallabs.com>

--- lib/cloud_controller/blobstore/idempotent_directory.rb
+++ lib/cloud_controller/blobstore/idempotent_directory.rb
@@ -1,9 +1,13 @@
-class IdempotentDirectory
-  def initialize(directory)
-    @directory = directory
-  end
+module CloudController
+  module Blobstore
+    class IdempotentDirectory
+      def initialize(directory)
+        @directory = directory
+      end
 
-  def get_or_create
-    @directory.get or @directory.create
+      def get_or_create
+        @directory.get or @directory.create
+      end
+    end
   end
 end
--- spec/blobstore/idempotent_directory_spec.rb
+++ spec/blobstore/idempotent_directory_spec.rb
@@ -1,34 +1,38 @@
 require "spec_helper"
 
-describe IdempotentDirectory do
-  let(:fog_directory) do
-    double("Fog::**::Directory")
-  end
-
-  let(:directory) do
-    double("Directory")
-  end
-
-  subject(:idempotent_directory) do
-    IdempotentDirectory.new(directory)
-  end
-
-  describe "#get_or_create" do
-    context "when the directory exists" do
-      it "should return the existing Fog directory" do
-        expect(directory).to receive(:get).and_return(fog_directory)
-        expect(directory).not_to receive(:create)
-
-        expect(idempotent_directory.get_or_create).to eq(fog_directory)
+module CloudController
+  module Blobstore
+    describe IdempotentDirectory do
+      let(:fog_directory) do
+        double("Fog::**::Directory")
       end
-    end
 
-    context "when the directory does not exist" do
-      it "should create and return a new Fog directory" do
-        expect(directory).to receive(:get).and_return(nil)
-        expect(directory).to receive(:create).and_return(fog_directory)
+      let(:directory) do
+        double("Directory")
+      end
 
-        expect(idempotent_directory.get_or_create).to eq(fog_directory)
+      subject(:idempotent_directory) do
+        IdempotentDirectory.new(directory)
+      end
+
+      describe "#get_or_create" do
+        context "when the directory exists" do
+          it "should return the existing Fog directory" do
+            expect(directory).to receive(:get).and_return(fog_directory)
+            expect(directory).not_to receive(:create)
+
+            expect(idempotent_directory.get_or_create).to eq(fog_directory)
+          end
+        end
+
+        context "when the directory does not exist" do
+          it "should create and return a new Fog directory" do
+            expect(directory).to receive(:get).and_return(nil)
+            expect(directory).to receive(:create).and_return(fog_directory)
+
+            expect(idempotent_directory.get_or_create).to eq(fog_directory)
+          end
+        end
       end
     end
   end
