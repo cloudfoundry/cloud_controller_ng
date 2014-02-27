316675a4d4b9066373350adc7755432471005bee
Tammer Saleh <tammer@pivotallabs.com>
2014-02-27 10:09:09 -0800

    Log errors from background jobs [#62361748]
    Signed-off-by: Ruth Byers <rbyers@pivotallabs.com>

--- spec/jobs/exception_catching_job_spec.rb
+++ spec/jobs/exception_catching_job_spec.rb
@@ -16,19 +16,46 @@ module VCAP::CloudController
       end
 
       context "#error(job, exception)" do
-        let(:job) { double("Job") }
-        let(:exception) { double("Exception", message: "ERROR") }
-        let(:exception_hash) { { } }
-        let(:error_presenter) { double("ErrorPresenter") }
+        let(:job) { double("Job").as_null_object }
+        let(:error_presenter) { double("ErrorPresenter", sanitized_hash: "sanitized exception hash").as_null_object }
+        let(:background_logger) { double("Steno").as_null_object }
+
+        before do
+          allow(Steno).to receive(:logger).and_return(background_logger)
+          allow(ErrorPresenter).to receive(:new).with("exception").and_return(error_presenter)
+          allow(error_presenter).to receive(:log_message).and_return("log message")
+        end
+
+        context "when the error is a client error" do
+          before do
+            allow(error_presenter).to receive(:client_error?).and_return(true)
+          end
+
+          it "logs the unsanitized information" do
+            expect(Steno).to receive(:logger).with("cc.background").and_return(background_logger)
+            expect(background_logger).to receive(:info).with("log message")
+            exception_catching_job.error(job, "exception")
+          end
+        end
+
+        context "when the error is a server error" do
+          before do
+            allow(error_presenter).to receive(:client_error?).and_return(false)
+          end
+
+          it "logs the unsanitized information as an error" do
+            expect(Steno).to receive(:logger).with("cc.background").and_return(background_logger)
+            expect(background_logger).to receive(:error).with("log message")
+            exception_catching_job.error(job, "exception")
+          end
+        end
 
         it "saves the exception on the job as cf_api_error" do
-          expect(ErrorPresenter).to receive(:new).with(exception).and_return(error_presenter)
-          expect(error_presenter).to receive(:sanitized_hash).and_return(exception_hash)
-          expect(YAML).to receive(:dump).with(exception_hash).and_return("marshaled hash")
+          expect(YAML).to receive(:dump).with("sanitized exception hash").and_return("marshaled hash")
           expect(job).to receive("cf_api_error=").with("marshaled hash")
           expect(job).to receive("save")
 
-          exception_catching_job.error(job, exception)
+          exception_catching_job.error(job, "exception")
         end
       end
     end
