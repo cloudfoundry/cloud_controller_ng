c314e250e3c0fdf33d54646325f9846d485a1fa8
Tammer Saleh <tammer@pivotallabs.com>
2014-03-10 10:56:26 -0700

    Group specs
    Signed-off-by: Abhijit Hiremagalur <abhi@pivotallabs.com>

--- spec/rest_controller/paginated_collection_renderer_spec.rb
+++ spec/rest_controller/paginated_collection_renderer_spec.rb
@@ -48,72 +48,76 @@ module VCAP::CloudController::RestController
         paginated_collection_renderer.render_json(CarsController, Car.dataset, "/v2/cars", opts, {})
       end
 
-      context 'when asked results_per_page is more than max results_per_page' do
-        let(:max_results_per_page) { 10 }
-        let(:results_per_page) { 11 }
+      context 'when results_per_page' do
+        context 'is more than max results_per_page' do
+          let(:max_results_per_page) { 10 }
+          let(:results_per_page) { 11 }
 
-        it 'raises ApiError error' do
-          expect { render_json_call }.to raise_error(VCAP::Errors::ApiError, /results_per_page/)
+          it 'raises ApiError error' do
+            expect { render_json_call }.to raise_error(VCAP::Errors::ApiError, /results_per_page/)
+          end
+        end
+
+        context 'equals to max results_per_page' do
+          let(:max_results_per_page) { 10 }
+          let(:results_per_page) { 10 }
+
+          it 'renders json response' do
+            expect(render_json_call).to be_instance_of(String)
+          end
+        end
+
+        context 'is less than max results_per_page' do
+          let(:max_results_per_page) { 10 }
+          let(:results_per_page) { 9 }
+
+          it 'renders json response' do
+            expect(render_json_call).to be_instance_of(String)
+          end
+        end
+
+        context 'was not specified' do
+          before do
+            Car.create(name: "car-1")
+            Car.create(name: "car-2")
+          end
+
+          let(:default_results_per_page) { 1 }
+
+          it 'renders limits number of results to default_results_per_page' do
+            expect(JSON.parse(render_json_call)["resources"].size).to eq(1)
+          end
         end
       end
 
-      context 'when asked results_per_page equals to max results_per_page' do
-        let(:max_results_per_page) { 10 }
-        let(:results_per_page) { 10 }
+      context 'when inline_relations_depth' do
+        context 'is more than max inline_relations_depth' do
+          let(:max_inline_relations_depth) { 10 }
+          let(:inline_relations_depth) { 11 }
 
-        it 'renders json response' do
-          expect(render_json_call).to be_instance_of(String)
-        end
-      end
-
-      context 'when asked results_per_page is less than max results_per_page' do
-        let(:max_results_per_page) { 10 }
-        let(:results_per_page) { 9 }
-
-        it 'renders json response' do
-          expect(render_json_call).to be_instance_of(String)
-        end
-      end
-
-      context 'when results_per_page were not specified' do
-        before do
-          Car.create(name: "car-1")
-          Car.create(name: "car-2")
+          it 'raises BadQueryParameter error' do
+            expect {
+              render_json_call
+            }.to raise_error(VCAP::Errors::ApiError, /inline_relations_depth/)
+          end
         end
 
-        let(:default_results_per_page) { 1 }
+        context 'is equal to max inline_relations_depth' do
+          let(:max_inline_relations_depth) { 10 }
+          let(:inline_relations_depth) { 10 }
 
-        it 'renders limits number of results to default_results_per_page' do
-          expect(JSON.parse(render_json_call)["resources"].size).to eq(1)
+          it 'renders json response' do
+            expect(render_json_call).to be_instance_of(String)
+          end
         end
-      end
 
-      context 'when asked inline_relations_depth is more than max inline_relations_depth' do
-        let(:max_inline_relations_depth) { 10 }
-        let(:inline_relations_depth) { 11 }
+        context 'is less than max inline_relations_depth' do
+          let(:max_inline_relations_depth) { 10 }
+          let(:inline_relations_depth) { 9 }
 
-        it 'raises BadQueryParameter error' do
-          expect {
-            render_json_call
-          }.to raise_error(VCAP::Errors::ApiError, /inline_relations_depth/)
-        end
-      end
-
-      context 'when asked inline_relations_depth equals to max inline_relations_depth' do
-        let(:max_inline_relations_depth) { 10 }
-        let(:inline_relations_depth) { 10 }
-
-        it 'renders json response' do
-          expect(render_json_call).to be_instance_of(String)
-        end
-      end
-
-      context 'when asked inline_relations_depth is less than max inline_relations_depth' do
-        let(:max_inline_relations_depth) { 10 }
-        let(:inline_relations_depth) { 9 }
-
-        it 'renders json response' do
-          expect(render_json_call).to be_instance_of(String)
+          it 'renders json response' do
+            expect(render_json_call).to be_instance_of(String)
+          end
         end
       end
     end
