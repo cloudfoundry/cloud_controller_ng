db5c00e1ef660ff3bd976d826bc9b5c16e378805
Karl Isenberg <kisenberg@pivotallabs.com>
2014-03-18 11:44:03 -0700

    Fix require paths
    They worked by coincidence because they resolved to /app/acc...
    
    [#67777650]
    
    Signed-off-by: Abhijit Hiremagalur <abhi@pivotallabs.com>

--- app/access/access.rb
+++ app/access/access.rb
@@ -1,4 +1,4 @@
-require File.expand_path("../../../app/access/base_access.rb", __FILE__)
-Dir[File.expand_path("../../../app/access/**/*.rb", __FILE__)].each do |file|
+require File.expand_path("base_access.rb", File.dirname(__FILE__))
+Dir[File.expand_path("**/*.rb", File.dirname(__FILE__))].each do |file|
   require file
 end
