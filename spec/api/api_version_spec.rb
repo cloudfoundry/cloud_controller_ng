30c074269141fc1061777975ed3860e87d455a46
Karl Isenberg <kisenberg@pivotallabs.com>
2014-03-18 16:11:10 -0700

    Updating the API checksum to reflect changes in previous commits.

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require "digest/sha1"
 
 describe "Stable API warning system", api_version_check: true do
-  API_FOLDER_CHECKSUM = "a21cc9d1457bbacdcf54bb098f7ff1bd54212853".freeze
+  API_FOLDER_CHECKSUM = "d28be0f4a3b60408d74556ef07a1a6c222e1ff4f".freeze
   it "tells the developer if the API specs change" do
     api_folder = File.expand_path("..", __FILE__)
     filenames = Dir.glob("#{api_folder}/**/*").reject {|filename| File.directory?(filename) || filename == __FILE__ }.sort
