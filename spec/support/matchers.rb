16b3a746076beb06b31c337a34d045f0436f7418
Phan Le <ple@pivotallabs.com>
2014-04-08 12:13:59 -0700

    Show specific error when match_app fails.
    Signed-off-by: Matthew Sykes <matthew.sykes@gmail.com>

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -16,18 +16,44 @@ RSpec::Matchers.define :be_a_deprecated_response do |_|
   end
 end
 
-RSpec::Matchers.define :match_app do |app|
-  match do |state|
-    state.state == app.state &&
-      state.instance_count == app.instances &&
-      state.memory_in_mb_per_instance == app.memory &&
-      state.app_guid == app.guid &&
-      state.app_name == app.name &&
-      state.org_guid == app.space.organization_guid &&
-      state.space_guid == app.space_guid &&
-      state.space_name == app.space.name &&
-      state.buildpack_guid == app.detected_buildpack_guid &&
-      state.buildpack_name == (app.custom_buildpack_url || app.detected_buildpack_name)
+RSpec::Matchers.define :match_app do |expected_app|
+  problems = []
+  match do |actual_event|
+    unless actual_event.state == expected_app.state
+      problems << "event.state: #{actual_event.state}, app.state: #{expected_app.state}"
+    end
+    unless actual_event.instance_count == expected_app.instances
+      problems << "event.instance_count: #{actual_event.instance_count}, app.instances: #{expected_app.instance_count}"
+    end
+    unless actual_event.memory_in_mb_per_instance == expected_app.memory
+      problems << "event.memory_in_mb_per_instance: #{actual_event.memory_in_mb_per_instance}, app.memory: #{expected_app.memory}"
+    end
+    unless actual_event.app_guid == expected_app.guid
+      problems << "event.app_guid: #{actual_event.app_guid}, app.guid: #{expected_app.guid}"
+    end
+    unless actual_event.app_name == expected_app.name
+      problems << "event.app_name: #{actual_event.app_name}, app.name: #{expected_app.name}"
+    end
+    unless actual_event.org_guid == expected_app.organization.guid
+      problems << "event.org_guid: #{actual_event.org_guid}, app.space.organization_guid: #{expected_app.organization.guid}"
+    end
+    unless actual_event.space_guid == expected_app.space_guid
+      problems << "event.space_name: #{actual_event.space_name}, app.space_guid: #{expected_app.space_guid}"
+    end
+    unless actual_event.space_name == expected_app.space.name
+      problems << "event.buildpack_guid: #{actual_event.buildpack_guid}, app.space.name: #{expected_app.space.name}"
+    end
+    unless actual_event.buildpack_guid == expected_app.detected_buildpack_guid
+      problems << "event.buildpack_guid: #{actual_event.buildpack_guid}, app.detected_buildpack_guid: #{expected_app.detected_buildpack_guid}"
+    end
+    unless actual_event.buildpack_name == (expected_app.custom_buildpack_url || expected_app.detected_buildpack_name)
+      problems << "event.buildpack_name: #{actual_event.buildpack_name}, app.buildpack_name: #{expected_app.custom_buildpack_url || expected_app.detected_buildpack_name}"
+    end
+    problems.empty?
+  end
+
+  failure_message_for_should do |actual_event|
+    "Expect event to match app, but did not. Problems were:\n" + problems.join("\n")
   end
 end
 
