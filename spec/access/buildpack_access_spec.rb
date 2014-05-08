3bd07d60ddbaf5d62a9a930799ba91bd0deeec99
David Sabeti and Derek Richard <drichard@pivotallabs.com>
2014-05-08 13:50:04 -0700

    Refactor and backfill some tests

--- spec/access/buildpack_access_spec.rb
+++ spec/access/buildpack_access_spec.rb
@@ -7,36 +7,29 @@ module VCAP::CloudController
     let(:roles) { double(:roles, :admin? => false, :none? => false, :present? => true) }
     let(:object) { VCAP::CloudController::Buildpack.make }
 
+    before do
+      token = {'scope' => scope }
+      VCAP::CloudController::SecurityContext.stub(:token).and_return(token)
+    end
+
     context 'for an admin' do
+      let(:scope) { 'cloud_controller.admin' }
       include_context :admin_setup
       it_behaves_like :full_access
       it { should allow_op_on_object :upload, object }
     end
 
     context 'for a logged in user' do
-      before do
-        token = {'scope' => 'cloud_controller.read cloud_controller.write'}
-        VCAP::CloudController::SecurityContext.stub(:token).and_return(token)
-      end
-
+      let(:scope) { 'cloud_controller.read cloud_controller.write' }
       it_behaves_like :read_only
       it { should_not allow_op_on_object :upload, object }
 
       context 'using a client without cloud_controller.read' do
-        before do
-          token = { 'scope' => ''}
-          VCAP::CloudController::SecurityContext.stub(:token).and_return(token)
-        end
+        let(:scope) { '' }
 
         it_behaves_like :no_access
+        it { should_not allow_op_on_object :upload, object }
       end
-    end
-
-    context 'a user that isnt logged in (defensive)' do
-      let(:user) { nil }
-      let(:roles) { double(:roles, :admin? => false, :none? => true, :present? => false) }
-      it_behaves_like :no_access
-      it { should_not allow_op_on_object :upload, object }
     end
   end
 end
