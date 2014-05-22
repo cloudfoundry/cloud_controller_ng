f00159a7628de2122e77e0bb78b535cac8d38a7f
James Myers <jmyers@pivotallabs.com>
2014-05-22 10:00:10 -0700

    API version bump after rebase

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require "digest/sha1"
 
 describe "Stable API warning system", api_version_check: true do
-  API_FOLDER_CHECKSUM = "9254984c59a827a36d76403b06eacb21b9095de8"
+  API_FOLDER_CHECKSUM = "3d243e5875969457346e945687ad44ec1cf659ca"
 
   it "double-checks the version" do
     expect(VCAP::CloudController::Constants::API_VERSION).to eq("2.5.0")
