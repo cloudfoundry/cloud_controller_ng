235b75d621c041eef01f1a269da06ea1babb7690
Abhijit Hiremagalur <abhi@pivotallabs.com>
2014-06-04 14:10:12 -0700

    Remove unused include of VCAP::CloudController in spec helper
    [#72354980]
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotallabs.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,7 +32,6 @@ RSpec.configure do |rspec_config|
   rspec_config.treat_symbols_as_metadata_keys_with_true_values = true
 
   rspec_config.include Rack::Test::Methods
-  rspec_config.include VCAP::CloudController
   rspec_config.include VCAP::CloudController::GlobalHelper
   rspec_config.include ModelCreation
   rspec_config.extend ModelCreation
