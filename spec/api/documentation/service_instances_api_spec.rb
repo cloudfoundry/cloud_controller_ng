02afeb9649e2f5976eb49fd323abc2d636d05a3e
David Sabeti and Derek Richard <dsabeti@pivotallabs.com>
2014-06-04 15:04:40 -0700

    Backfill docs for /v2/service_instances [finishes #62359354]

--- spec/api/documentation/service_instances_api_spec.rb
+++ spec/api/documentation/service_instances_api_spec.rb
@@ -2,12 +2,48 @@ require 'spec_helper'
 require 'rspec_api_documentation/dsl'
 
 resource "Service Instances", :type => :api do
+  authenticated_request
   let(:admin_auth_header) { headers_for(admin_user, :admin_scope => true)["HTTP_AUTHORIZATION"] }
   let!(:service_instance) { VCAP::CloudController::ManagedServiceInstance.make }
 
-  authenticated_request
+  describe 'Listing all service instances' do
+    standard_model_list :service_instance, VCAP::CloudController::ServiceInstancesController
+  end
 
-  standard_model_list :service_instance, VCAP::CloudController::ServiceInstancesController
+  post '/v2/service_instances/' do
+    field :name, 'A name for the service instance', required: true
+    field :service_plan_guid, 'The guid of the service plan to associate with the instance', required: true
+    field :space_guid, 'The guid of the space in which the instance will be created', required: true
+    field :gateway_data, 'Configuration information for the broker gateway in v1 services', required: false, deprecated: true
+
+    example 'Creating a service instance' do
+      space_guid = VCAP::CloudController::Space.make.guid
+      service_plan_guid = VCAP::CloudController::ServicePlan.make(public: true).guid
+      request_hash = {space_guid: space_guid, name: 'my-service-instance', service_plan_guid: service_plan_guid }
+      client.post '/v2/service_instances', Yajl::Encoder.encode(request_hash), headers
+      expect(status).to eq(201)
+
+      standard_entity_response parsed_response, :service_instance
+
+      #instance_guid = parsed_response['metadata']['guid']
+      #audited_event VCAP::CloudController::Event.find(:type => "audit.app.create", :actee => app_guid)
+    end
+  end
+
+  describe 'Deleting a service instance' do
+    field :guid, "The guid of the service instance", required: true
+    let(:guid) { VCAP::CloudController::ServiceInstance.make.guid }
+
+    standard_model_delete_without_async :service_instance
+  end
+
+  describe 'Getting a service instance' do
+    standard_list_parameters VCAP::CloudController::ServiceInstancesController
+    field :guid, "The guid of the service instance", required: true
+    let(:guid) { VCAP::CloudController::ServiceInstance.make.guid }
+
+    standard_model_get :service_instance
+  end
 
   get "/v2/service_instances/:guid/permissions" do
     field :guid, "The guid of the service instance", required: true, example_values: %w(6c4bd80f-4593-41d1-a2c9-b20cb65ec76e)
