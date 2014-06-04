2de58d5b1f6f69f5aa1b4f3756d18dc0eb1a54a0
Michael Fraenkel <fraenkel@us.ibm.com>
2014-06-04 15:50:18 -0400

    Shorten the foreign keys for App Sec Group Spaces
    [#71584680]
    
    Signed-off-by: sabha parameswaran <sparameswaran@gopivotal.com>

--- db/migrations/20140603184619_add_app_security_group_spaces_join_table.rb
+++ db/migrations/20140603184619_add_app_security_group_spaces_join_table.rb
@@ -2,10 +2,10 @@ Sequel.migration do
   change do
     create_table :app_security_groups_spaces do
       Integer :app_security_group_id, :null => false
-      foreign_key [:app_security_group_id], :app_security_groups, :name => :fk_spaces_asgs_app_security_group_id
+      foreign_key [:app_security_group_id], :app_security_groups, :name => :fk_app_security_group_id
 
       Integer :space_id, :null => false
-      foreign_key [:space_id], :spaces, :name => :fk_spaces_asgs_space_id
+      foreign_key [:space_id], :spaces, :name => :fk_space_id
 
       index [:app_security_group_id, :space_id], :unique => true, name: 'asgs_spaces_ids'
     end
