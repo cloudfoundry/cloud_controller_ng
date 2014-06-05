d144bcdc687a45f3992e477d87921deeb4247f2e
Zach Robinson <zrobinson@pivotallabs.com>
2014-06-05 10:19:34 -0700

    Refactor space summary controller.
    - move logic from controller into SpaceSummarizer object.
    - simplify tests
    
    Signed-off-by: Momchil Atanasov <m.momchil@gmail.com>

--- app/controllers/runtime/space_summaries_controller.rb
+++ app/controllers/runtime/space_summaries_controller.rb
@@ -5,41 +5,56 @@ module VCAP::CloudController
 
     get "#{path_guid}/summary", :summary
     def summary(guid)
-      space = find_guid_and_validate_access(:read, guid)
+      space      = find_guid_and_validate_access(:read, guid)
+      summarizer = SpaceSummarizer.new(space, instances_reporter)
 
-      apps = {}
-      space.apps.each do |app|
-        apps[app.guid] = app_summary(app)
-      end
-
-      services_summary = space.service_instances.map do |instance|
-        instance.as_summary_json
-      end
-
-      Yajl::Encoder.encode(
-        guid: space.guid,
-        name: space.name,
-        apps: apps.values,
-        services: services_summary,
-      )
+      Yajl::Encoder.encode(summarizer.space_summary, pretty: true)
     end
 
-    private
+    protected
+
+    attr_reader :instances_reporter
 
     def inject_dependencies(dependencies)
       super
       @instances_reporter = dependencies[:instances_reporter]
     end
+  end
 
-    def app_summary(app)
+  class SpaceSummarizer
+    attr_reader :space, :instances_reporter
+
+    def initialize(space, instances_reporter)
+      @instances_reporter = instances_reporter
+      @space              = space
+    end
+
+    def space_summary
       {
-        guid: app.guid,
-        urls: app.routes.map(&:fqdn),
-        routes: app.routes.map(&:as_summary_json),
-        service_count: app.service_bindings_dataset.count,
-        service_names: app.service_bindings_dataset.map(&:service_instance).map(&:name),
-        running_instances: @instances_reporter.number_of_starting_and_running_instances_for_app(app),
-      }.merge(app.to_hash)
+        guid:     space.guid,
+        name:     space.name,
+        apps:     app_summary,
+        services: services_summary,
+      }
+    end
+
+    def app_summary
+      space.apps.collect do |app|
+        {
+          guid:              app.guid,
+          urls:              app.routes.map(&:fqdn),
+          routes:            app.routes.map(&:as_summary_json),
+          service_count:     app.service_bindings_dataset.count,
+          service_names:     app.service_bindings_dataset.map(&:service_instance).map(&:name),
+          running_instances: instances_reporter.number_of_starting_and_running_instances_for_app(app),
+        }.merge(app.to_hash)
+      end
+    end
+
+    def services_summary
+     space.service_instances.map do |instance|
+        instance.as_summary_json
+      end
     end
   end
 end
--- spec/unit/controllers/runtime/space_summaries_controller_spec.rb
+++ spec/unit/controllers/runtime/space_summaries_controller_spec.rb
@@ -1,123 +1,178 @@
-require "spec_helper"
+require 'spec_helper'
 
 module VCAP::CloudController
-  describe VCAP::CloudController::SpaceSummariesController do
-    let(:mem_size) { 128 }
+  describe SpaceSummariesController do
     let(:space) { Space.make }
-    let(:routes) { 2.times.map { Route.make(:space => space) } }
-    let(:services) { 2.times.map { ManagedServiceInstance.make(:space => space) } }
 
-    let(:started_app) do
-      AppFactory.make(
-        space: space,
-        instances: 2,
-        memory: mem_size,
-        state: "STARTED",
-        package_hash: "abc",
-        package_state: "STAGED",
-      )
+    describe 'GET /v2/spaces/:id/summary' do
+      let(:summary_response) { { 'the' => 'output i want to see as json'} }
+      let(:space_summarizer) { double(:space_summarizer) }
+
+      before do
+        allow(SpaceSummarizer).to receive(:new).and_return space_summarizer
+        allow(space_summarizer).to receive(:space_summary).and_return(summary_response)
+      end
+
+      def make_request
+        get "/v2/spaces/#{space.guid}/summary", {}, admin_headers
+      end
+
+      it 'returns 200' do
+        response = make_request
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns json from SpaceSummarizer' do
+        response = make_request
+
+        expect { JSON.parse(response.body) }.to_not raise_error
+        response_body_as_hash = JSON.parse(response.body)
+        expect(response_body_as_hash).to eq(summary_response)
+      end
+
+      it 'properly constructs SpaceSummarizer' do
+        make_request
+
+        expect(SpaceSummarizer).to have_received(:new) do |requested_space, instance_reporter|
+          expect(requested_space.guid).to eq(space.guid)
+          expect(instance_reporter).to be_kind_of(VCAP::CloudController::InstancesReporter::LegacyInstancesReporter)
+        end
+      end
     end
+  end
 
-    let(:stopped_app) do
-      AppFactory.make(
-        :space => space,
-        :instances => 3,
-        :memory => mem_size,
-        :state => "STOPPED",
-      )
-    end
+  describe SpaceSummarizer do
+    let(:space) { Space.make }
+    let(:first_route) { Route.make(space: space) }
+    let(:second_route) { Route.make(space: space) }
+    let(:first_service) {  ManagedServiceInstance.make(space: space) }
+    let(:second_service) {  ManagedServiceInstance.make(space: space) }
+    let(:app) { AppFactory.make(space: space) }
 
-    let(:apps) { [started_app, stopped_app]}
+    let(:instances_reporter) { double(:instances_reporter) }
+    let(:running_instances) { 5 }
+
+    subject { described_class.new(space, instances_reporter) }
 
     before do
-      routes.each do |route|
-        started_app.add_route(route)
-        stopped_app.add_route(route)
+      app.add_route(first_route)
+      app.add_route(second_route)
+
+      ServiceBinding.make(app: app, service_instance: first_service)
+      ServiceBinding.make(app: app, service_instance: second_service)
+
+      allow(instances_reporter).to receive(:number_of_starting_and_running_instances_for_app).
+                                     and_return(running_instances)
+    end
+
+    describe '#app_summary' do
+      let(:app_summary) { subject.app_summary}
+
+      it 'is an array' do
+        expect(app_summary).to be_kind_of(Array)
       end
 
-      services.each do |service|
-        ServiceBinding.make(:app => started_app, :service_instance => service)
-        ServiceBinding.make(:app => stopped_app, :service_instance => service)
+      context 'when the space has multiple apps' do
+        before do
+          AppFactory.make(space: space)
+          AppFactory.make(space: space)
+        end
+
+        it 'has an equal number of summaries' do
+          expect(app_summary.length).to eq(3)
+        end
+      end
+
+      describe 'summary format' do
+        let(:summary) { app_summary.first }
+
+        it 'includes the app.to_hash' do
+          expect(summary).to include(app.to_hash)
+        end
+
+        it 'includes the app guid' do
+          expect(summary).to include(guid: app.guid)
+        end
+
+        it 'includes route summary information' do
+          expect(summary).to include(urls: [first_route.fqdn, second_route.fqdn])
+          expect(summary).to include(routes: [{
+                                                guid: first_route.guid,
+                                                host: first_route.host,
+                                                domain: {
+                                                  guid: first_route.domain.guid,
+                                                  name: first_route.domain.name
+                                                }
+                                              }, {
+                                                guid: second_route.guid,
+                                                host: second_route.host,
+                                                domain: {
+                                                  guid: second_route.domain.guid,
+                                                  name: second_route.domain.name
+                                                }
+                                              }]
+                             )
+        end
+
+        it 'includes service summary information' do
+          expect(summary).to include(service_count: 2)
+          expect(summary).to include(service_names: [first_service.name, second_service.name])
+        end
+
+        it 'includes instance summary information' do
+          expect(summary).to include(running_instances: running_instances)
+          expect(instances_reporter).to have_received(:number_of_starting_and_running_instances_for_app) do |requested_app|
+            expect(requested_app.guid).to eq(app.guid)
+          end
+        end
       end
     end
 
-    subject(:request) { get "/v2/spaces/#{space.guid}/summary", {}, admin_headers }
+    describe '#services_summary' do
+      let(:summary) { subject.services_summary }
 
-    describe "GET /v2/spaces/:id/summary" do
-      before do
-        instances_reporter = CloudController::DependencyLocator.instance.instances_reporter
-
-        allow(instances_reporter).to receive(:number_of_starting_and_running_instances_for_app).with(started_app).and_return(2)
-        allow(instances_reporter).to receive(:number_of_starting_and_running_instances_for_app).with(stopped_app).and_return(0)
+      it 'returns an array of summaries' do
+        expect(summary).to be_kind_of(Array)
+        expect(summary.length).to eq(2)
       end
 
-      its(:status) { should eq 200 }
+      it 'the summaries are instance.as_summary_json' do
+        expect(summary).to include({
+                                     'guid' => first_service.guid,
+                                     'name' => first_service.name,
+                                     'bound_app_count' => 1,
+                                     'dashboard_url' => first_service.dashboard_url,
+                                     'service_plan' => {
+                                       'guid' => first_service.service_plan.guid,
+                                       'name' => first_service.service_plan.name,
+                                       'service' => {
+                                         'guid' => first_service.service_plan.service.guid,
+                                         'label' => first_service.service_plan.service.label,
+                                         'provider' => first_service.service_plan.service.provider,
+                                         'version' => first_service.service_plan.service.version,
+                                       }
+                                     }
+                                   })
+      end
+    end
 
-      describe "the json" do
-        subject(:request) do
-          get "/v2/spaces/#{space.guid}/summary", {}, admin_headers
-          decoded_response
-        end
+    describe '#space_summary' do
+      let(:space_summary) { subject.space_summary }
 
-        its(["guid"]) { should eq space.guid }
-        its(["name"]) { should eq space.name }
-        its(["apps"]) { should have(apps.count).entries }
-        its(["services"]) { should have(services.count).entries }
+      it 'includes space guid' do
+        expect(space_summary[:guid]).to eq(space.guid)
+      end
 
-        it "returns the correct info for the apps" do
-          request["apps"].each do |app_resp|
-            app = apps.find { |a| a.guid == app_resp["guid"] }.reload
-            expected_running_instances = app.started? ? app.instances : 0
+      it 'includes space name' do
+        expect(space_summary[:name]).to eq(space.name)
+      end
 
-            expect(app_resp).to eq({
-              "guid" => app.guid,
-              "name" => app.name,
-              "urls" => routes.map(&:fqdn),
-              "routes" => [
-                {
-                  "guid" => routes[0].guid,
-                  "host" => routes[0].host,
-                  "domain" => {
-                    "guid" => routes[0].domain.guid,
-                    "name" => routes[0].domain.name
-                  }
-                }, {
-                "guid" => routes[1].guid,
-                "host" => routes[1].host,
-                "domain" => {
-                  "guid" => routes[1].domain.guid,
-                  "name" => routes[1].domain.name}
-              }
-              ],
-              "service_count" => services.count,
-              "service_names" => services.map(&:name),
-              "instances" => app.instances,
-              "running_instances" => expected_running_instances
-            }.merge(app.to_hash))
-          end
-        end
+      it 'includes apps summary' do
+        expect(space_summary[:apps]).to eq(subject.app_summary)
+      end
 
-        it "returns the correct info for a service" do
-          service_response = request["services"][0]
-          service = services.find { |service| service.guid == service_response["guid"] }
-
-          expect(service_response).to eq(
-            "guid" => service.guid,
-            "name" => service.name,
-            "bound_app_count" => apps.count,
-            "dashboard_url" => service.dashboard_url,
-            "service_plan" => {
-              "guid" => service.service_plan.guid,
-              "name" => service.service_plan.name,
-              "service" => {
-                "guid" => service.service_plan.service.guid,
-                "label" => service.service_plan.service.label,
-                "provider" => service.service_plan.service.provider,
-                "version" => service.service_plan.service.version,
-              }
-            }
-          )
-        end
+      it 'includes services summary' do
+        expect(space_summary[:services]).to eq(subject.services_summary)
       end
     end
   end
