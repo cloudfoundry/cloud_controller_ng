6bd3ea8848939a7fe07463c22e4ea8cf74944886
ericpromislow <ericp@activestate.com>
2014-06-13 17:43:51 -0700

    Update the unit test for semicolons in queries to use a sqlite db and full string matches.

--- spec/unit/lib/vcap/rest_api/query_semicolon_spec.rb
+++ spec/unit/lib/vcap/rest_api/query_semicolon_spec.rb
@@ -4,43 +4,44 @@ module VCAP::RestAPI
   describe VCAP::RestAPI::Query, non_transactional: true do
     include VCAP::RestAPI
 
-    class AuthorSemiColon < Sequel::Model
+    class Author < Sequel::Model
       one_to_many :books
     end
 
-    class BookSemiColon < Sequel::Model
-      many_to_one :author
+    class Book < Sequel::Model
+      many_to_one :authors
     end
 
     before do
-      db.create_table :author_semi_colons do
+      db = Sequel.sqlite(':memory:')
+      db.create_table :authors do
         primary_key :id
         String  :str_val
       end
 
-      db.create_table :book_semi_colons do
+      db.create_table :books do
         primary_key :id
         String  :str_val
         String  :month
-        foreign_key :author_id, :author_semi_colons
+        foreign_key :author_id, :authors
       end
 
-      AuthorSemiColon.set_dataset(db[:author_semi_colons])
-      BookSemiColon.set_dataset(db[:book_semi_colons])
+      Author.set_dataset(db[:authors])
+      Book.set_dataset(db[:books])
 
-      a = AuthorSemiColon.create(:str_val => "joe;semi")
-      a.add_book(BookSemiColon.create(:str_val => "two;;semis", :month => "Jan"))
-      a.add_book(BookSemiColon.create(:str_val => "three;;;semis and one;semi",
+      a = Author.create(:str_val => "joe;semi")
+      a.add_book(Book.create(:str_val => "two;;semis", :month => "Jan"))
+      a.add_book(Book.create(:str_val => "three;;;semis and one;semi",
                              :month => "Jan"))
-      a = AuthorSemiColon.create(:str_val => "joe/semi")
-      a.add_book(BookSemiColon.create(:str_val => "two;/semis", :month => "Jan"))
-      a.add_book(BookSemiColon.create(:str_val => "x;;semis and one;semi",
+      a = Author.create(:str_val => "joe/semi")
+      a.add_book(Book.create(:str_val => "two;/semis", :month => "Jan"))
+      a.add_book(Book.create(:str_val => "x;;semis and one;semi",
                              :month => "Jan"))
-      a.add_book(BookSemiColon.create(:str_val => "x;;/;;semis and one;semi",
+      a.add_book(Book.create(:str_val => "x;;/;;semis and one;semi",
                              :month => "Feb"))
-      a.add_book(BookSemiColon.create(:str_val => "x;;;;semis - don't match this",
+      a.add_book(Book.create(:str_val => "x;;;;semis - don't match this",
                              :month => "Feb"))
-      a.add_book(BookSemiColon.create(:str_val => "two;/semis", :month => "Feb"))
+      a.add_book(Book.create(:str_val => "two;/semis", :month => "Feb"))
       
       @queryable_attributes = Set.new(%w(str_val author_id book_id month))
     end
@@ -48,55 +49,46 @@ module VCAP::RestAPI
     describe "#filtered_dataset_from_query_params" do
       describe "shared prefix query" do
         it "should return all authors" do
-          q = "str_val:joe*"
-          ds = Query.filtered_dataset_from_query_params(AuthorSemiColon, AuthorSemiColon.dataset,
+          q = "str_val:joe;;semi"
+          ds = Query.filtered_dataset_from_query_params(Author, Author.dataset,
                                                         @queryable_attributes, :q => q)
-          ds.count.should == 2
+          ds.all.should == [Author[1]]
         end
       end
       
       describe "slash match 1" do
         it "should return the second author" do
-          q = "str_val:joe/s*"
-          ds = Query.filtered_dataset_from_query_params(AuthorSemiColon, AuthorSemiColon.dataset,
+          q = "str_val:joe/semi"
+          ds = Query.filtered_dataset_from_query_params(Author, Author.dataset,
                                                         @queryable_attributes, :q => q)
-          ds.all.should == [AuthorSemiColon[:str_val => "joe/semi"]]
-        end
-      end
-
-      describe "semicolon match 1" do
-        it "should return the first author" do
-          q = "str_val:joe;;s*"
-          ds = Query.filtered_dataset_from_query_params(AuthorSemiColon, AuthorSemiColon.dataset,
-                                                        @queryable_attributes, :q => q)
-          ds.all.should == [AuthorSemiColon[:str_val => "joe;semi"]]
+          ds.all.should == [Author[2]]
         end
       end
 
       describe "semicolon match on three;;;semis" do
         it "should return book 1-2" do
-          q = "str_val:three;;;;;;semis and one;;s*"
-          ds = Query.filtered_dataset_from_query_params(BookSemiColon, BookSemiColon.dataset,
+          q = "str_val:three;;;;;;semis and one;;semi"
+          ds = Query.filtered_dataset_from_query_params(Book, Book.dataset,
                                                         @queryable_attributes, :q => q)
-          ds.all.should == [BookSemiColon[2]]
+          ds.all.should == [Book[2]]
         end
       end
 
       describe "semicolon match on x;;/" do
         it "should return book 2-2" do
-          q = "str_val:x;;;;s*"
-          ds = Query.filtered_dataset_from_query_params(BookSemiColon, BookSemiColon.dataset,
+          q = "str_val:x;;;;semis and one;;semi"
+          ds = Query.filtered_dataset_from_query_params(Book, Book.dataset,
                                                         @queryable_attributes, :q => q)
-          ds.all.should == [BookSemiColon[4]]
+          ds.all.should == [Book[4]]
         end
       end
 
       describe "match two fields" do
-        it "should return book 2-4(6)" do
-          q = "str_val:two;;/s*;month:Feb"
-          ds = Query.filtered_dataset_from_query_params(BookSemiColon, BookSemiColon.dataset,
+        it "should return book 2-3(5)" do
+          q = "str_val:x;;;;/;;;;semis and one;;semi;month:Feb"
+          ds = Query.filtered_dataset_from_query_params(Book, Book.dataset,
                                                         @queryable_attributes, :q => q)
-          ds.all.should == [BookSemiColon[7]]
+          ds.all.should == [Book[5]]
         end
       end
 
