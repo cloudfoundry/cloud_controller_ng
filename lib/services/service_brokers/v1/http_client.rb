563ec8f5169e759293672b9ea2d3426555a50872
Noburou TANIGUCHI <dev@nota.m001.jp>
2014-06-17 06:38:09 +0900

    Complete deletion of v1 service when 404 is returned from v1 service gateway
    To avoid failure on deletion of v1 service binding / instance
    when v1 service gateway returns "404 Not Found".
    
    This problem is not limited on deletion of service binding / instance.
    The deletion of related app, space, and org also fails.
    
    So this commit modifies http client of v1 service gateway to ignore
    "404 Not Found" response for delete request, like client of v2 service
    ignores "410 Gone" response for delete request.

--- lib/services/service_brokers/v1/http_client.rb
+++ lib/services/service_brokers/v1/http_client.rb
@@ -77,6 +77,11 @@ module VCAP::Services
             return Yajl::Parser.parse(response.body)
           end
         else
+          if (req_class == "Delete") && ([404, 410].include?(response.code.to_i))
+            logger.warn("Already deleted: #{uri.to_s}")
+            return nil
+          end
+
           begin
             hash = Yajl::Parser.parse(response.body)
           rescue Yajl::ParseError
