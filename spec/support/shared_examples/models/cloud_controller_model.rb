a9301271f2a1313a21c9b453c3d066eacc75a860
Graeme Davison <gdavison@pivotallabs.com>
2014-06-25 09:39:14 -0700

    Remove shared model deletion and enumeration specs
    [#72664052]
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotallabs.com>

--- spec/support/shared_examples/models/cloud_controller_model.rb
+++ spec/support/shared_examples/models/cloud_controller_model.rb
@@ -4,13 +4,12 @@ shared_examples "a CloudController model" do |opts|
   # the later code is simplified if we can assume that these are always
   # arrays
   relation_types = RelationTypes.all
-  ([:sensitive_attributes, :extra_json_attributes, :disable_examples] +
+  ([:sensitive_attributes, :extra_json_attributes] +
    relation_types).each do |k|
      opts[k] ||= []
      opts[k] = Array[opts[k]] unless opts[k].respond_to?(:each)
    end
 
-  include_examples "model instance", opts
+  include_examples "creation with all required attributes"
   include_examples "model relationships", opts
-  include_examples "model enumeration", opts
 end
