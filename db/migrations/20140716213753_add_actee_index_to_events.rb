88cad1bb4c1dd55542bcc233ef595f1cc07c88e0
James Myers <jmyers@pivotallabs.com>
2014-07-16 16:55:05 -0700

    Add index for actee on the events table.
    * Truncate the events table to prevent deploy failures.
      Adding an index in a production like environment most
      likely will exceed the 10 minute limit. Also adding an index
      in mysql will lock the table, making adding an index on a large
      table extremely dangerous.
    
    [#69878326]

---
+++ db/migrations/20140716213753_add_actee_index_to_events.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  up do
+    self[:events].truncate
+
+    alter_table(:events) do
+      add_index :actee, :name => "events_actee_index"
+    end
+  end
+
+  down do
+    alter_table(:events) do
+      drop_index :actee, :name => "events_actee_index"
+    end
+  end
+end
