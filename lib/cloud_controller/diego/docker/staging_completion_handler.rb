06b33659e8af41e0a96d52b0d5be523070841a7e
Abhijit Hiremagalur <abhi@pivotallabs.com>
2014-08-11 15:58:42 -0700

    Introduce Docker staging completion handler
    - [#75217152]
    
    Signed-off-by: Peter Parente <pparent@us.ibm.com>

---
+++ lib/cloud_controller/diego/docker/staging_completion_handler.rb
@@ -0,0 +1,48 @@
+module VCAP::CloudController
+  module Diego
+    module Docker
+      class StagingCompletionHandler
+        def initialize(message_bus, messenger)
+          @message_bus = message_bus
+          @messenger = messenger
+        end
+
+        def subscribe!
+          @message_bus.subscribe("diego.docker.staging.finished", queue: "cc") do |payload|
+            app = App.find(guid: payload["app_id"])
+
+            if app.present?
+              if payload["task_id"] == app.staging_task_id
+                if payload["error"]
+                  app.mark_as_failed_to_stage # app.save is called in mark_as_failed_to_stage
+                  Loggregator.emit_error(app.guid, "Failed to stage Docker application: #{payload["error"]}")
+                else
+                  app.mark_as_staged
+                  app.save
+                  @messenger.send_desire_request(app)
+                end
+              else
+                logger.info(
+                  "diego.docker.staging.not-current",
+                  :response => payload,
+                  :current => app.staging_task_id,
+                )
+              end
+            else
+              logger.info(
+                "diego.docker.staging.unknown-app",
+                :response => payload,
+              )
+            end
+          end
+        end
+
+        private
+
+        def logger
+          @logger ||= Steno.logger("cc.docker.stager")
+        end
+      end
+    end
+  end
+end
---
+++ spec/unit/lib/cloud_controller/diego/docker/staging_completion_handler_spec.rb
@@ -0,0 +1,143 @@
+require "spec_helper"
+require "cloud_controller/diego/docker/staging_completion_handler"
+
+module VCAP::CloudController
+  module Diego
+    module Docker
+      describe StagingCompletionHandler do
+        let(:logger) do
+          instance_double(Steno::Logger, info: nil)
+        end
+
+        let(:payload) do
+          {}
+        end
+
+        let(:message_bus) do
+          message_bus = instance_double(CfMessageBus::MessageBus)
+          allow(message_bus).to receive(:subscribe).and_yield(payload)
+          message_bus
+        end
+
+        let(:messenger) do
+          instance_double(Diego::Messenger, send_desire_request: nil)
+        end
+
+        let(:app) do
+          AppFactory.make(staging_task_id: "fake-staging-task-id")
+        end
+
+        subject(:handler) do
+          StagingCompletionHandler.new(message_bus, messenger)
+        end
+
+        before do
+          allow(Steno).to receive(:logger).with("cc.docker.stager").and_return(logger)
+          allow(Loggregator).to receive(:emit_error)
+        end
+
+        it "subscribes to diego.docker.staging.finished responses" do
+          handler.subscribe!
+          expect(message_bus).to have_received(:subscribe).with("diego.docker.staging.finished", queue: "cc")
+        end
+
+        context "when it receives a success response" do
+          let(:payload) do
+            {
+              "app_id" => app.guid,
+              "task_id" => app.staging_task_id
+            }
+          end
+
+          it "marks the app as staged" do
+            expect {
+              handler.subscribe!
+            }.to change { app.reload.staged? }.to(true)
+          end
+
+          it "sends desires the app on Diego" do
+            handler.subscribe!
+            expect(messenger).to have_received(:send_desire_request).with(app)
+          end
+
+          context "when the app_id is invalid" do
+            let(:payload) do
+              {
+                "app_id" => "bad-app-id",
+              }
+            end
+
+            it "returns without sending a desire request for the app" do
+              handler.subscribe!
+
+              expect(messenger).not_to have_received(:send_desire_request)
+            end
+
+            it "logs info about an unknown app for the CF operator" do
+              handler.subscribe!
+
+              expect(logger).to have_received(:info).with(
+                "diego.docker.staging.unknown-app",
+                :response => payload
+              )
+            end
+
+          end
+
+          context "when the task_id is invalid" do
+            let(:payload) do
+              {
+                "app_id" => app.guid,
+                "task_id" => "bad-task-id"
+              }
+            end
+
+            it "returns without sending a desired request for the app" do
+              handler.subscribe!
+
+              expect(messenger).not_to have_received(:send_desire_request)
+            end
+
+            it "logs info about an invalid task id for the CF operator and returns" do
+              handler.subscribe!
+
+              expect(logger).to have_received(:info).with(
+                "diego.docker.staging.not-current",
+                :response => payload,
+                :current => app.staging_task_id
+              )
+            end
+          end
+        end
+
+        context "when it receives a failure response" do
+          let(:payload) do
+            {
+              "app_id" => app.guid,
+              "task_id" => app.staging_task_id,
+              "error" => "fake-error",
+            }
+          end
+
+          it "marks the app as failed to stage" do
+            expect {
+              handler.subscribe!
+            }.to change { app.reload.package_state }.to("FAILED")
+          end
+
+          it "logs an error for the CF user" do
+            handler.subscribe!
+
+            expect(Loggregator).to have_received(:emit_error).with(app.guid, /fake-error/)
+          end
+
+          it "returns without sending a desired request for the app" do
+            handler.subscribe!
+
+            expect(messenger).not_to have_received(:send_desire_request)
+          end
+        end
+      end
+    end
+  end
+end
