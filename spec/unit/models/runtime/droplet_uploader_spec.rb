594efae0634174a3ae13486dd4058758c8f6e21c
James Myers and Luan Santos <lsantos@pivotallabs.com>
2014-08-21 15:36:24 -0700

    Refactor DropletUploader spec
    [#76020130]

--- spec/unit/models/runtime/droplet_uploader_spec.rb
+++ spec/unit/models/runtime/droplet_uploader_spec.rb
@@ -3,7 +3,7 @@ require "tmpdir"
 
 describe CloudController::DropletUploader do
   let(:app) do
-    VCAP::CloudController::AppFactory.make(droplet_hash: "droplet_hash")
+    VCAP::CloudController::AppFactory.make(droplet_hash: nil)
   end
 
   let(:blobstore) do
@@ -12,69 +12,53 @@ describe CloudController::DropletUploader do
 
   subject { described_class.new(app, blobstore) }
 
-  describe "#execute" do
+  describe "#upload" do
     include TempFileCreator
 
-    it "add a new app droplet" do
-      old_size = app.droplets.size
-      expect { subject.upload(temp_file_with_content.path) }.to change {
-        app.droplet_hash
-      }
-      expect(app.droplets.size).to eql(old_size + 1)
-    end
+    context "when the upload to the blobstore suceeds" do
+      it "adds a new app droplet" do
+        expect(app.droplet_hash).to be_nil
 
-    it "does not create a new droplet if the upload fails" do
-      allow(blobstore).to receive(:cp_to_blobstore).and_raise "Upload failed"
-      expect {
         expect {
           subject.upload(temp_file_with_content.path)
-        }.to raise_error
-      }.not_to change {
-        app.reload.droplets.size
-      }
-    end
+        }.to change { app.droplets.size }.from(0).to(1)
 
-    it "deletes old droplets" do
-      droplets_to_keep = 2
-      expect(app.droplets).to have(1).items
-
-      Timecop.travel(Date.today + 2) do
-        file = temp_file_with_content("droplet version 2")
-        subject.upload(file.path)
-        expect(app.reload.droplets).to have(droplets_to_keep).items
+        expect(app.droplet_hash).to eq(app.droplets.last.droplet_hash)
       end
 
-      droplet_dest = Tempfile.new("downloaded_droplet")
-      app.current_droplet.download_to(droplet_dest.path)
-      expect(droplet_dest.read).to eql("droplet version 2")
+      it "deletes old droplets when there are more droplets than droplets_to_keep" do
+        droplets_to_keep = 1
+        expect(app.droplets.size).to eq(0)
 
-      Timecop.travel(Date.today + 3) do
-        subject.upload(temp_file_with_content("droplet version 3").path)
-        expect(app.reload.droplets).to have(droplets_to_keep).items
-      end
+        subject.upload(temp_file_with_content("droplet version 1").path, droplets_to_keep)
+        expect(app.reload.droplets.size).to eq(droplets_to_keep)
 
-      droplet_dest = Tempfile.new("downloaded_droplet")
-      app.current_droplet.download_to(droplet_dest.path)
-      expect(droplet_dest.read).to eql("droplet version 3")
-    end
-
-    it "deletes the number of old droplets specified in droplets_to_keep" do
-      droplets_to_keep = 1
-      expect(app.droplets).to have(1).items
-      old_droplet = app.droplets.first
-
-      Timecop.travel(Date.today + 2) do
-        subject.upload(temp_file_with_content("droplet version 2").path, droplets_to_keep)
-        expect(app.reload.droplets).to have(droplets_to_keep).items
-        expect(app.droplets).to_not include(old_droplet)
-      end
-
-      Timecop.travel(Date.today + 3) do
-        subject.upload(temp_file_with_content("droplet version 3").path, droplets_to_keep)
-        expect(app.reload.droplets).to have(droplets_to_keep).items
-        droplet_dest = Tempfile.new("")
+        droplet_dest = Tempfile.new("downloaded_droplet")
         app.current_droplet.download_to(droplet_dest.path)
-        expect(droplet_dest.read).to eql("droplet version 3")
+        expect(droplet_dest.read).to eql("droplet version 1")
+
+        subject.upload(temp_file_with_content("droplet version 2").path, droplets_to_keep)
+        expect(app.reload.droplets.size).to eq(droplets_to_keep)
+
+        droplet_dest = Tempfile.new("downloaded_droplet")
+        app.current_droplet.download_to(droplet_dest.path)
+        expect(droplet_dest.read).to eql("droplet version 2")
+      end
+    end
+
+    context "when the upload to the blobstore fails" do
+      before do
+        allow(blobstore).to receive(:cp_to_blobstore).and_raise "Upload failed"
+      end
+
+      it "does not create a new droplet" do
+        expect {
+          expect {
+            subject.upload(temp_file_with_content.path)
+          }.to raise_error
+        }.not_to change {
+          app.reload.droplets.size
+        }
       end
     end
   end
