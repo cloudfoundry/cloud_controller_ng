569461747f3a5c6a9e7d419dcf9f17dcfee7284e
Mike Dalessio <mdalessio@pivotal.io>
2014-08-31 18:29:10 -0400

    Created VCAP::CloudController::Encryptor::FieldEncryptor and specs
    - to standardize how we encrypt fields in ccdb
    - and to make it stupid simple for developers to encrypt fields
    
    Subsequent commits will use FieldEncryptor across all models.
    
    [#77942432]

--- lib/cloud_controller/encryptor.rb
+++ lib/cloud_controller/encryptor.rb
@@ -32,4 +32,56 @@ module VCAP::CloudController::Encryptor
       cipher.update(input).tap { |result| result << cipher.final }
     end
   end
+
+  module FieldEncryptor
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      def encrypt field_name, options={}
+        field_name = field_name.to_sym
+        salt_name = (options[:salt] || "#{field_name}_salt").to_sym
+        generate_salt_name = "generate_#{salt_name}".to_sym
+        storage_column = options[:column]
+
+        unless columns.include?(salt_name)
+          raise "Salt field `#{salt_name}` does not exist"
+        end
+
+        define_method generate_salt_name do
+          return unless send(salt_name).blank?
+          send "#{salt_name}=", VCAP::CloudController::Encryptor.generate_salt
+        end
+
+        if storage_column
+          define_method field_name do
+            send storage_column
+          end
+
+          define_method "#{field_name}=" do |value|
+            send "#{storage_column}=", value
+          end
+        end
+
+        define_method "#{field_name}_with_encryption" do
+          VCAP::CloudController::Encryptor.decrypt send("#{field_name}_without_encryption"), send(salt_name)
+        end
+        alias_method_chain field_name, "encryption"
+
+        define_method "#{field_name}_with_encryption=" do |value|
+          send generate_salt_name
+
+          encrypted_value =
+            if value.blank?
+              nil
+            else
+              VCAP::CloudController::Encryptor.encrypt(value, send(salt_name))
+            end
+
+          send "#{field_name}_without_encryption=", encrypted_value
+        end
+        alias_method_chain "#{field_name}=", "encryption"
+      end
+      private :encrypt
+    end
+  end
 end
--- spec/unit/lib/cloud_controller/encryptor_spec.rb
+++ spec/unit/lib/cloud_controller/encryptor_spec.rb
@@ -48,4 +48,158 @@ module VCAP::CloudController
       end
     end
   end
+
+  describe Encryptor::FieldEncryptor do
+    let(:klass) do
+      Class.new do
+        include VCAP::CloudController::Encryptor::FieldEncryptor
+      end
+    end
+
+    before do
+      allow(klass).to receive(:columns) { columns }
+      klass.send :attr_accessor, *columns # emulate Sequel super methods
+    end
+
+    describe "#encrypt" do
+      context "model does not have the salt column" do
+        let(:columns) { [:id, :name, :size] }
+
+        context "default name" do
+          it "raises an error" do
+            expect {
+              klass.send :encrypt, :name
+            }.to raise_error(RuntimeError, /name_salt/)
+          end
+        end
+
+        context "explicit name" do
+          it "raises an error" do
+            expect {
+              klass.send :encrypt, :name, salt: :foobar
+            }.to raise_error(RuntimeError, /foobar/)
+          end
+        end
+      end
+
+      context "model has the salt column" do
+        context "default name" do
+          let(:columns) { [:id, :name, :size, :name_salt] }
+
+          it "does not raise an error" do
+            expect {
+              klass.send :encrypt, :name
+            }.to_not raise_error
+          end
+
+          it "creates a salt generation method" do
+            klass.send :encrypt, :name
+            expect(klass.instance_methods).to include(:generate_name_salt)
+          end
+        end
+
+        context "explicit name" do
+          let(:columns) { [:id, :name, :size, :foobar] }
+
+          it "does not raise an error" do
+            expect {
+              klass.send :encrypt, :name, salt: :foobar
+            }.to_not raise_error
+          end
+
+          it "creates a salt generation method" do
+            klass.send :encrypt, :name, salt: :foobar
+            expect(klass.instance_methods).to include(:generate_foobar)
+          end
+        end
+      end
+    end
+
+    describe "field-specific methods" do
+      let(:columns) { [:sekret, :sekret_salt] }
+      let(:klass2) { Class.new klass }
+      let(:subject) { klass2.new }
+      let(:encryption_args) { {} }
+
+      before do
+        klass.class_eval do
+          def set_underlying_sekret value
+            @sekret = value
+          end
+          def get_underlying_sekret
+            @sekret
+          end
+        end
+        klass2.send :encrypt, :sekret, encryption_args
+      end
+
+      describe "salt generation method" do
+        context "salt is not set" do
+          it "updates the salt using Encryptor" do
+            [nil, ""].each do |unset_value|
+              expect(Encryptor).to receive(:generate_salt).and_return("new salt")
+              subject.sekret_salt = unset_value
+              subject.generate_sekret_salt
+              expect(subject.sekret_salt).to eq "new salt"
+            end
+          end
+        end
+
+        context "salt is set" do
+          it "does not update the salt" do
+            subject.sekret_salt = "old salt"
+            subject.generate_sekret_salt
+            expect(subject.sekret_salt).to eq "old salt"
+          end
+        end
+      end
+
+      describe "decryption" do
+        it "decrypts by passing the salt and the underlying value to Encryptor" do
+          subject.sekret_salt = "asdf"
+          subject.set_underlying_sekret "underlying"
+          expect(Encryptor).to receive(:decrypt).with("underlying", "asdf") { "unencrypted" }
+          expect(subject.sekret).to eq "unencrypted"
+        end
+      end
+
+      describe "encryption" do
+        it "calls the salt generation method" do
+          expect(subject).to receive(:generate_sekret_salt)
+          subject.sekret = "foobar"
+        end
+
+        context "blank value" do
+          it "stores a default nil value without trying to encrypt" do
+            expect(Encryptor).to_not receive(:encrypt)
+            [nil, ""].each do |blank_value|
+              subject.sekret = blank_value
+              expect(subject.get_underlying_sekret).to eq nil
+            end
+          end
+        end
+
+        context "non-blank value" do
+          it "encrypts by passing the value and salt to Encryptor" do
+            subject.sekret_salt = "asdf"
+            expect(Encryptor).to receive(:encrypt).with("unencrypted", "asdf") { "encrypted" }
+            subject.sekret = "unencrypted"
+            expect(subject.get_underlying_sekret).to eq "encrypted"
+          end
+        end
+      end
+
+      describe "alternative storage column is specified" do
+        let(:columns) { [:sekret, :sekret_salt, :encrypted_sekret] }
+        let(:encryption_args) { {column: :encrypted_sekret} }
+
+        it "stores the encrypted value in that column" do
+          expect(subject.encrypted_sekret).to eq nil
+          subject.sekret = "asdf"
+          expect(subject.encrypted_sekret).to_not eq nil
+          expect(subject.sekret).to eq "asdf"
+        end
+      end
+    end
+  end
 end
