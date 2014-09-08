991284711e3e5a1fcc02c0370b0868bd7ea2bb0c
Michael Fraenkel <fraenkel@us.ibm.com>
2014-09-08 05:17:53 -0700

    Handle query params properly
    * Handle both query params and posted params
    
    [#78365570]

--- spec/support/rack_test_monkey_patch.rb
+++ spec/support/rack_test_monkey_patch.rb
@@ -5,11 +5,20 @@ module Rack
 
       alias_method :orig_env_for, :env_for
       def env_for(path, env)
+        params = env[:params]
         new_env = orig_env_for(path, env)
-        uri = URI.parse(path)
-        if uri.query
-          new_env["QUERY_STRING"] = "#{uri.query}&#{new_env['QUERY_STRING']}"
+
+        if env[:method] == "GET"
+          # merge :params with the query string
+          uri = URI.parse(path)
+
+          if params
+            params = parse_nested_query(params) if params.is_a?(String)
+            uri.query = [uri.query, build_nested_query(params)].compact.join("&")
+          end
+          new_env["QUERY_STRING"] = uri.query
         end
+
         new_env
       end
     end
