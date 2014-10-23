9cb6979a308b2f705036e28f65f3020718d7f40c
Johannes Petzold <jpetzold@pivotal.io>
2014-10-23 13:26:48 -0600

    rename DIEGO_BETA -> DIEGO_STAGE_BETA [#80165358]
    Signed-off-by: Matthew Sykes <matthew.sykes@gmail.com>

--- app/models/runtime/app.rb
+++ app/models/runtime/app.rb
@@ -168,7 +168,7 @@ module VCAP::CloudController
     end
 
     def stage_with_diego?
-      run_with_diego? || !!(environment_json && environment_json["CF_DIEGO_BETA"] == "true")
+      run_with_diego? || !!(environment_json && environment_json["CF_DIEGO_STAGE_BETA"] == "true")
     end
 
     def version_needs_to_be_updated?
