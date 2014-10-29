28a39e1138177f4ba2adde514d0c21260d96bbd1
David Sabeti <dsabeti@pivotal.io>
2014-10-28 17:57:10 -0700

    Fix specs that broke as a result of API version bump

--- spec/unit/lib/services/service_brokers/v2/http_client_spec.rb
+++ spec/unit/lib/services/service_brokers/v2/http_client_spec.rb
@@ -204,7 +204,7 @@ module VCAP::Services::ServiceBrokers::V2
         make_request
         expect(a_request(http_method, full_url).
           with(:query => hash_including({})).
-          with(:headers => {'X-Broker-Api-Version' => '2.3'})).
+          with(:headers => {'X-Broker-Api-Version' => '2.4'})).
           to have_been_made
       end
 
