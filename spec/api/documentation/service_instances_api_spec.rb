be394efdec2978f0687589339eed8ba2997eb26f
Abhijit Hiremagalur and Rob Dimsdale <rdimsdale@pivotal.io>
2014-11-06 14:36:50 -0800

    Fixes to /v2/service_instances/:guid docs
    [#76633662]

--- spec/api/documentation/service_instances_api_spec.rb
+++ spec/api/documentation/service_instances_api_spec.rb
@@ -1,6 +1,6 @@
-require 'spec_helper'
-require 'rspec_api_documentation/dsl'
-require 'uri'
+require "spec_helper"
+require "rspec_api_documentation/dsl"
+require "uri"
 
 resource "Service Instances", :type => [:api, :legacy_api] do
   let(:admin_auth_header) { admin_headers["HTTP_AUTHORIZATION"] }
@@ -14,30 +14,30 @@ resource "Service Instances", :type => [:api, :legacy_api] do
     standard_model_get :managed_service_instance, path: :service_instance, nested_attributes: [:space, :service_plan]
     standard_model_delete_without_async :service_instance
 
-    post '/v2/service_instances/' do
-      field :name, 'A name for the service instance', required: true, example_values: ['my-service-instance']
-      field :service_plan_guid, 'The guid of the service plan to associate with the instance', required: true
-      field :space_guid, 'The guid of the space in which the instance will be created', required: true
-      field :gateway_data, 'Configuration information for the broker gateway in v1 services', required: false, deprecated: true
+    post "/v2/service_instances/" do
+      field :name, "A name for the service instance", required: true, example_values: ["my-service-instance"]
+      field :service_plan_guid, "The guid of the service plan to associate with the instance", required: true
+      field :space_guid, "The guid of the space in which the instance will be created", required: true
+      field :gateway_data, "Configuration information for the broker gateway in v1 services", required: false, deprecated: true
 
-      example 'Creating a Service Instance' do
+      example "Creating a Service Instance" do
         space_guid = VCAP::CloudController::Space.make.guid
         service_plan_guid = VCAP::CloudController::ServicePlan.make(public: true).guid
-        request_hash = {space_guid: space_guid, name: 'my-service-instance', service_plan_guid: service_plan_guid}
+        request_hash = {space_guid: space_guid, name: "my-service-instance", service_plan_guid: service_plan_guid}
 
-        client.post '/v2/service_instances', MultiJson.dump(request_hash, pretty: true), headers
+        client.post "/v2/service_instances", MultiJson.dump(request_hash, pretty: true), headers
         expect(status).to eq(201)
       end
     end
 
-    put '/v2/service_instances/:guid' do
+    put "/v2/service_instances/:guid" do
       let(:service_broker) { VCAP::CloudController::ServiceBroker.make }
       let(:service) { VCAP::CloudController::Service.make(service_broker: service_broker, plan_updateable: true) }
       let(:new_plan) { VCAP::CloudController::ServicePlan.make(service: service) }
       let(:old_plan) { VCAP::CloudController::ServicePlan.make(service: service) }
       let(:service_instance) { VCAP::CloudController::ManagedServiceInstance.make(service_plan: old_plan) }
 
-      field :service_plan_guid, 'The new plan guid for the service instance', required: true, example_values: ['6c4bd80f-4593-41d1-a2c9-b20cb65ec76e']
+      field :service_plan_guid, "The new plan guid for the service instance", required: false, example_values: ["6c4bd80f-4593-41d1-a2c9-b20cb65ec76e"]
 
       before do
         uri = URI(service_broker.broker_url)
@@ -47,7 +47,7 @@ resource "Service Instances", :type => [:api, :legacy_api] do
         stub_request(:patch, uri.to_s).to_return(:status => 200, :body => "", :headers => {})
       end
 
-      example 'Updating the service plan a service instance' do
+      example "Updating a service instance" do
         request_json = { service_plan_guid: new_plan.guid }.to_json
         client.put "/v2/service_instances/#{service_instance.guid}", request_json, headers
 
@@ -74,18 +74,18 @@ resource "Service Instances", :type => [:api, :legacy_api] do
       client.get "/v2/service_instances/#{service_instance.guid}/permissions", {}, headers
       expect(status).to eq(200)
 
-      expect(parsed_response).to eql({'manage' => true})
+      expect(parsed_response).to eql({"manage" => true})
     end
   end
 
-  put '/v2/service_plans/:service_plan_guid/service_instances' do
+  put "/v2/service_plans/:service_plan_guid/service_instances" do
     let(:new_plan) { VCAP::CloudController::ServicePlan.make }
     let(:old_plan) { service_instance.service_plan }
     let(:request_json) { {service_plan_guid: new_plan.guid}.to_json }
 
     field :service_plan_guid, "The guid of the plan to move the existing instances to", required: true, example_values: %w(6c4bd80f-4593-41d1-a2c9-b20cb65ec76e)
 
-    example 'Migrate Service Instances from one Service Plan to another Service Plan (experimental)' do
+    example "Migrate Service Instances from one Service Plan to another Service Plan (experimental)" do
       explanation <<-EOD
           Move all Service Instances for the service plan from the URL to the service plan in the request body
       EOD
@@ -93,7 +93,7 @@ resource "Service Instances", :type => [:api, :legacy_api] do
       client.put "/v2/service_plans/#{old_plan.guid}/service_instances", request_json, headers
 
       expect(status).to eq(200)
-      expect(parsed_response['changed_count']).to eq(1)
+      expect(parsed_response["changed_count"]).to eq(1)
     end
   end
 end
