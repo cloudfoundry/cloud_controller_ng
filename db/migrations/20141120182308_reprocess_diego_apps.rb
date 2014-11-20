28d7307a2fb048c0690ee91cee44bcbf77d7ddff
Matthew Sykes <matthew.sykes@gmail.com>
2014-11-20 14:24:25 -0500

    Fix up diego flag after env variable rename
    [#82870238]
    
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>

---
+++ db/migrations/20141120182308_reprocess_diego_apps.rb
@@ -0,0 +1,13 @@
+Sequel.migration do
+  up do
+    self[:apps].filter(:diego => true).each do |row|
+      self[:apps].filter(id: row[:id]).update(diego: diego?(row))
+    end
+  end
+end
+
+def diego?(row)
+  decrypted = VCAP::CloudController::Encryptor.decrypt(row[:encrypted_environment_json], row[:salt])
+  environment_json = JSON.parse(decrypted)
+  !!(environment_json["DIEGO_RUN_BETA"] == "true")
+end
