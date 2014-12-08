cloudfoundry/cloud_controller_ng

commit 3a0590b61f832787dc0eeb19c9407dfcde2a8cc7

Admin can see service instance audit events when creating, updating, and destroying service instances.
[#62604840]

app/controllers/services/service_instances_controller.rb
@@ -49,6 +49,15 @@ module VCAP::CloudController
       Errors::ApiError.new_from_details("ServiceInstanceNotFound", guid)
     end
 
+    def self.dependencies
+      [ :services_event_repository ]
+    end
+
+    def inject_dependencies(dependencies)
+      super
+      @services_event_repository = dependencies.fetch(:services_event_repository)
+    end
+
     def create
       json_msg = self.class::CreateMessage.decode(body)
 
@@ -90,24 +99,49 @@ module VCAP::CloudController
         raise e
       end
 
+      @services_event_repository.create_service_instance_event('audit.service_instance.create', service_instance, request_attrs)
+
       [ HTTP::CREATED,
         { "Location" => "#{self.class.path}/#{service_instance.guid}" },
         object_renderer.render_json(self.class, service_instance, @opts)
       ]
     end
 
-    def before_update(service_instance)
-      if @request_attrs["service_plan_guid"]
-        validate_access(:update, service_instance, request_attrs)
+    def update(guid)
+      json_msg = self.class::UpdateMessage.decode(body)
+      request_attrs = json_msg.extract(stringify_keys: true)
+      logger.debug "cc.update", guid: guid, attributes: request_attrs
+      raise InvalidRequest unless request_attrs
 
-        new_plan = ServicePlan.find(guid: @request_attrs["service_plan_guid"])
+      service_instance = find_guid(guid)
+      validate_access(:read_for_update, service_instance)
+      validate_access(:update, service_instance)
+
+      if request_attrs["service_plan_guid"]
         old_plan = service_instance.service_plan
-        if old_plan.service.plan_updateable
-          service_instance.client.update_service_plan(service_instance, new_plan)
-        else
+        unless old_plan.service.plan_updateable
           raise VCAP::Errors::ApiError.new_from_details("ServicePlanNotUpdateable")
         end
+
+        new_plan = ServicePlan.find(guid: request_attrs["service_plan_guid"])
+        service_instance.client.update_service_plan(service_instance, new_plan)
       end
+
+      if request_attrs['space_guid']
+        space = Space.find(guid: request_attrs['space_guid'])
+        unless space.developers.include?(VCAP::CloudController::SecurityContext.current_user)
+          raise VCAP::Errors::ApiError.new_from_details("NotAuthorized")
+        end
+      end
+
+      ServiceInstance.db.transaction do
+        service_instance.lock!
+        service_instance.update_from_hash(request_attrs)
+      end
+
+      @services_event_repository.create_service_instance_event('audit.service_instance.update', service_instance, request_attrs)
+
+      [HTTP::CREATED, {}, object_renderer.render_json(self.class, service_instance, @opts)]
     end
 
     class BulkUpdateMessage < VCAP::RestAPI::Message
@@ -162,7 +196,10 @@ module VCAP::CloudController
     end
 
     def delete(guid)
-      do_delete(find_guid_and_validate_access(:delete, guid, ServiceInstance))
+      model = ServiceInstance.find(guid: guid)
+      response = do_delete(find_guid_and_validate_access(:delete, guid, ServiceInstance))
+      @services_event_repository.create_service_instance_event('audit.service_instance.delete', model, {})
+      response
     end
 
     def get_filtered_dataset_for_enumeration(model, ds, qp, opts)

spec/unit/controllers/services/service_instances_controller_spec.rb
@@ -214,6 +214,29 @@ module VCAP::CloudController
           expect(instance.dashboard_url).to eq('the dashboard_url')
         end
 
+        it 'creates a service audit event for creating the service instance' do
+          instance = create_managed_service_instance(email: 'developer@example.com')
+
+          event = VCAP::CloudController::Event.first(type: 'audit.service_instance.create')
+          expect(event.type).to eq('audit.service_instance.create')
+          expect(event.actor_type).to eq('user')
+          expect(event.actor).to eq(developer.guid)
+          expect(event.actor_name).to eq('developer@example.com')
+          expect(event.timestamp).to be
+          expect(event.actee).to eq(instance.guid)
+          expect(event.actee_type).to eq('service_instance')
+          expect(event.actee_name).to eq(instance.name)
+          expect(event.space_guid).to eq(instance.space.guid)
+          expect(event.organization_guid).to eq(instance.space.organization.guid)
+          expect(event.metadata).to include({
+            'request' => {
+              'name' => instance.name,
+              'service_plan_guid' => instance.service_plan_guid,
+              'space_guid' => instance.space_guid,
+            }
+          })
+        end
+
         it 'creates a CREATED service usage event' do
           instance = nil
           expect {
@@ -315,7 +338,7 @@ module VCAP::CloudController
         context 'with naming collisions' do
 
           it 'does not allow duplicate managed service instances' do
-            instance = create_managed_service_instance
+            create_managed_service_instance
             expect(last_response.status).to eq(201)
 
             create_managed_service_instance
@@ -324,7 +347,7 @@ module VCAP::CloudController
           end
 
           it 'does not allow duplicate user provided service instances' do
-            instance = create_user_provided_service_instance
+            create_user_provided_service_instance
             expect(last_response.status).to eq(201)
 
             create_user_provided_service_instance
@@ -333,7 +356,7 @@ module VCAP::CloudController
           end
 
           it 'does not allow a user provided service instance with same name as managed service instance' do
-            instance = create_managed_service_instance
+            create_managed_service_instance
             expect(last_response.status).to eq(201)
 
             create_user_provided_service_instance
@@ -342,7 +365,7 @@ module VCAP::CloudController
           end
 
           it 'does not allow a managed service instance with same name as user provided service instance' do
-            instance = create_user_provided_service_instance
+            create_user_provided_service_instance
             expect(last_response.status).to eq(201)
 
             create_managed_service_instance
@@ -484,12 +507,33 @@ module VCAP::CloudController
           put "/v2/service_instances/#{service_instance.guid}", body, admin_headers
           expect(service_instance.reload.service_plan).to eq(new_service_plan)
         end
+
+        it 'creates a service audit event for updating the service instance' do
+          put "/v2/service_instances/#{service_instance.guid}", body, headers_for(admin_user, email: 'admin@example.com')
+
+          event = VCAP::CloudController::Event.first(type: 'audit.service_instance.update')
+          expect(event.type).to eq('audit.service_instance.update')
+          expect(event.actor_type).to eq('user')
+          expect(event.actor).to eq(admin_user.guid)
+          expect(event.actor_name).to eq('admin@example.com')
+          expect(event.timestamp).to be
+          expect(event.actee).to eq(service_instance.guid)
+          expect(event.actee_type).to eq('service_instance')
+          expect(event.actee_name).to eq(service_instance.name)
+          expect(event.space_guid).to eq(service_instance.space.guid)
+          expect(event.organization_guid).to eq(service_instance.space.organization.guid)
+          expect(event.metadata).to include({
+            'request' => {
+              'service_plan_guid' => new_service_plan.guid,
+            }
+          })
+        end
       end
 
       context 'When the broker did not declare support for plan upgrades' do
         let(:old_service_plan) { ServicePlan.make(:v2) }
 
-        it 'updates the service plan in the database' do
+        it 'does not update the service plan in the database' do
           put "/v2/service_instances/#{service_instance.guid}", body, admin_headers
           expect(service_instance.reload.service_plan).to eq(old_service_plan)
         end
@@ -645,6 +689,23 @@ module VCAP::CloudController
           }.to change(ServiceInstance, :count).by(-1)
           expect(last_response.status).to eq(204)
           expect(ServiceInstance.find(:guid => service_instance.guid)).to be_nil
+        end
+
+        it 'creates a service audit event for deleting the service instance' do
+          delete "/v2/service_instances/#{service_instance.guid}", {}, headers_for(admin_user, email: 'admin@example.com')
+
+          event = VCAP::CloudController::Event.first(type: 'audit.service_instance.delete')
+          expect(event.type).to eq('audit.service_instance.delete')
+          expect(event.actor_type).to eq('user')
+          expect(event.actor).to eq(admin_user.guid)
+          expect(event.actor_name).to eq('admin@example.com')
+          expect(event.timestamp).to be
+          expect(event.actee).to eq(service_instance.guid)
+          expect(event.actee_type).to eq('service_instance')
+          expect(event.actee_name).to eq(service_instance.name)
+          expect(event.space_guid).to eq(service_instance.space.guid)
+          expect(event.organization_guid).to eq(service_instance.space.organization.guid)
+          expect(event.metadata).to eq({'request' => {}})
         end
 
         context 'when the service broker returns a 409' do
@@ -875,13 +936,13 @@ module VCAP::CloudController
       end
     end
 
-    def create_managed_service_instance
+    def create_managed_service_instance(user_opts={})
       req = MultiJson.dump(
         :name => 'foo',
         :space_guid => space.guid,
         :service_plan_guid => plan.guid
       )
-      headers = json_headers(headers_for(developer))
+      headers = json_headers(headers_for(developer, user_opts))
 
       post "/v2/service_instances", req, headers
 

app/repositories/services/event_repository.rb
@@ -12,7 +12,7 @@ module VCAP::CloudController
             type: "service",
             name: service.label,
           }
-          with_audit_event(service, actee, &saveBlock)
+          with_audit_event(service, actee, :changes_from_catalog, &saveBlock)
         end
 
         def with_service_plan_event(plan, &saveBlock)
@@ -20,7 +20,18 @@ module VCAP::CloudController
             type: "service_plan",
             name: plan.name,
           }
-          with_audit_event(plan, actee, &saveBlock)
+          with_audit_event(plan, actee, :changes_from_catalog, &saveBlock)
+        end
+
+        def create_service_instance_event(type, service_instance, params)
+          actee = {
+            id: service_instance.guid,
+            type: 'service_instance',
+            name: service_instance.name,
+            space_guid: service_instance.space_guid,
+            organization_guid: service_instance.space.organization.guid,
+          }
+          create_event(type, actee, { request: params })
         end
 
         def create_delete_service_event(service, metadata={})
@@ -93,24 +104,26 @@ module VCAP::CloudController
           metadata
         end
 
-        def metadata_for_modified_model(model_instance)
+        def changes_for_modified_model(model_instance)
           changes = {}
           model_instance.to_hash.each do |key, value|
             if model_instance.new? || model_instance.modified?(key.to_sym)
               changes[key.to_s] = value
             end
           end
-
-          { changes_from_catalog: changes }
+          changes
         end
 
-        def with_audit_event(object, actee, &saveBlock)
+        def with_audit_event(object, actee, changes_key, &saveBlock)
           type = event_type(object, actee[:type])
-          metadata = metadata_for_modified_model(object)
-          saveBlock.call
+          metadata = {
+            changes_key => changes_for_modified_model(object)
+          }
+          result = saveBlock.call
 
           actee[:id] = object.guid
           create_event(type, actee, metadata)
+          result
         end
 
         def create_event(type, actee, metadata)
@@ -122,11 +135,11 @@ module VCAP::CloudController
             actor: user.guid,
             actor_name: @security_context.current_user_email,
             timestamp: Time.now,
-            actee: actee[:id],
-            actee_type: actee[:type],
-            actee_name: actee[:name],
-            space_guid: actee[:space_guid] || '',
-            organization_guid: actee[:organization_guid] || '',
+            actee: actee.fetch(:id),
+            actee_type: actee.fetch(:type),
+            actee_name: actee.fetch(:name),
+            space_guid: actee.fetch(:space_guid, ''),
+            organization_guid: actee.fetch(:organization_guid, ''),
             metadata: metadata,
           )
         end

spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require "digest/sha1"
 
 describe "Stable API warning system", api_version_check: true do
-  API_FOLDER_CHECKSUM = "8ecd8f79a90f20103f5a65975d73de6ed550f469"
+  API_FOLDER_CHECKSUM = "687d197eb4975c38e46cf71d68279db7e46bff40"
 
   it "double-checks the version" do
     expect(VCAP::CloudController::Constants::API_VERSION).to eq("2.18.0")

spec/api/documentation/events_api_spec.rb
@@ -459,5 +459,75 @@ resource "Events", :type => [:api, :legacy_api] do
                                :space_guid => '',
                                :metadata => {}
     end
+
+    example "List Service Instance Create Events" do
+      instance = VCAP::CloudController::ManagedServiceInstance.make
+      service_event_repository.create_service_instance_event('audit.service_instance.create', instance, {
+        'name' => instance.name,
+        'service_plan_guid' => instance.service_plan.guid,
+        'space_guid' => instance.space_guid,
+      })
+
+      client.get "/v2/events?q=type:audit.service_instance.create", {}, headers
+      expect(status).to eq(200)
+      standard_entity_response parsed_response["resources"][0], :event,
+                               :actor_type => "user",
+                               :actor => test_user.guid,
+                               :actor_name => test_user_email,
+                               :actee_type => "service_instance",
+                               :actee => instance.guid,
+                               :actee_name => instance.name,
+                               :space_guid => instance.space_guid,
+                               :metadata => {
+                                 'request' => {
+                                   'name' => instance.name,
+                                   'service_plan_guid' => instance.service_plan.guid,
+                                   'space_guid' => instance.space_guid,
+                                 }
+                               }
+
+    end
+
+    example "List Service Instance Update Events" do
+      instance = VCAP::CloudController::ManagedServiceInstance.make
+      service_event_repository.create_service_instance_event('audit.service_instance.update', instance, {
+        'service_plan_guid' => instance.service_plan.guid,
+      })
+
+      client.get "/v2/events?q=type:audit.service_instance.update", {}, headers
+      expect(status).to eq(200)
+      standard_entity_response parsed_response["resources"][0], :event,
+                               :actor_type => "user",
+                               :actor => test_user.guid,
+                               :actor_name => test_user_email,
+                               :actee_type => "service_instance",
+                               :actee => instance.guid,
+                               :actee_name => instance.name,
+                               :space_guid => instance.space_guid,
+                               :metadata => {
+                                 'request' => {
+                                   'service_plan_guid' => instance.service_plan.guid,
+                                 }
+                               }
+    end
+
+    example "List Service Instance Delete Events" do
+      instance = VCAP::CloudController::ManagedServiceInstance.make
+      service_event_repository.create_service_instance_event('audit.service_instance.delete', instance, {})
+
+      client.get "/v2/events?q=type:audit.service_instance.delete", {}, headers
+      expect(status).to eq(200)
+      standard_entity_response parsed_response["resources"][0], :event,
+                               :actor_type => "user",
+                               :actor => test_user.guid,
+                               :actor_name => test_user_email,
+                               :actee_type => "service_instance",
+                               :actee => instance.guid,
+                               :actee_name => instance.name,
+                               :space_guid => instance.space_guid,
+                               :metadata => {
+                                 'request' => {}
+                               }
+    end
   end
 end

commit 4a516186e3776cff0511bddc48ae0e6219501a5d

Return proper error codes for deleting service_instances
- 404 when instnace not found
- 403 when user not authorized to delete

This fixes a regression introduced in
3a0590b61f832787dc0eeb19c9407dfcde2a8cc7

app/controllers/services/service_instances_controller.rb
@@ -197,7 +197,7 @@ module VCAP::CloudController
     end
 
     def delete(guid)
-      service_instance = ServiceInstance.find(guid: guid)
+      service_instance = find_guid_and_validate_access(:delete, guid, ServiceInstance)
       raise_if_has_associations!(service_instance) if v2_api? && !recursive?
 
       deletion_job = Jobs::Runtime::ModelDeletion.new(ServiceInstance, guid)

spec/unit/controllers/services/service_instances_controller_spec.rb
@@ -731,6 +731,13 @@ module VCAP::CloudController
             expect(JSON.parse(last_response.body)['description']).to include 'service broker error'
           end
         end
+
+        context 'and the instance cannot be found' do
+          it 'returns a 404' do
+            delete "/v2/service_instances/non-existing-instance", {}, admin_headers
+            expect(last_response.status).to eq 404
+          end
+        end
       end
 
       context 'with a v1 service instance' do