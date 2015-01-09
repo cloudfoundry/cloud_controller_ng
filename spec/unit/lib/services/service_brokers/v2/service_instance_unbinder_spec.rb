5ac302d179f086055e502e06512bf20f6c32c60f
David Sabeti <dsabeti@pivotal.io>
2015-01-09 12:14:35 -0800

    Refactor ServiceInstanceUnbinder spec
    Tests the result, instead of making assertions about the nested jobs

--- spec/unit/lib/services/service_brokers/v2/service_instance_unbinder_spec.rb
+++ spec/unit/lib/services/service_brokers/v2/service_instance_unbinder_spec.rb
@@ -12,33 +12,47 @@ module VCAP::CloudController
       let(:name) { 'fake-name' }
 
       describe 'delayed_unbind' do
+        let(:mock_client) { double(:client, unbind: nil) }
+
         before do
           allow(Delayed::Job).to receive(:enqueue)
           allow(VCAP::Request).to receive(:current_id).and_return('current_thread_id')
+          allow(VCAP::Services::ServiceBrokers::V2::Client).to receive(:new).and_return(mock_client)
+        end
+
+        it 'enqueues a job with the correct queue and run_at time' do
+          ServiceInstanceUnbinder.delayed_unbind(client_attrs, binding)
+
+          expect(Delayed::Job).to have_received(:enqueue) do |job, opts|
+            expect(opts[:queue]).to eq 'cc-generic'
+            expect(opts[:run_at]).to be_within(0.01).of(Delayed::Job.db_time_now)
+          end
         end
 
         it 'enqueues a job that unbinds the instance' do
+          mock_binding = double(:binding,
+            guid: binding.guid,
+            app_guid: binding.app_guid,
+            service_instance_guid: binding.service_instance_guid)
+          allow(VCAP::CloudController::ServiceBinding).to receive(:new).and_return(mock_binding)
+
           ServiceInstanceUnbinder.delayed_unbind(client_attrs, binding)
 
           expect(Delayed::Job).to have_received(:enqueue) do |job, opts|
-            unbind_job = job.job.job
-            expect(unbind_job).to be_a VCAP::CloudController::Jobs::Services::ServiceInstanceUnbind
-            expect(unbind_job.name).to eq 'service-instance-unbind'
-            expect(unbind_job.client_attrs).to eq client_attrs
-            expect(unbind_job.binding_guid).to be(binding.guid)
-            expect(unbind_job.service_instance_guid).to be(binding.service_instance.guid)
-            expect(unbind_job.app_guid).to be(binding.app.guid)
+            job.perform
           end
+          expect(mock_client).to have_received(:unbind).with(mock_binding)
         end
 
         it 'creates the job in the same context as the original request' do
+          allow(VCAP::Request).to receive(:current_id=)
+
           ServiceInstanceUnbinder.delayed_unbind(client_attrs, binding)
 
           expect(Delayed::Job).to have_received(:enqueue) do |job, opts|
-            request_job = job.job
-            expect(request_job).to be_a VCAP::CloudController::Jobs::RequestJob
-            expect(request_job.request_id).to eq 'current_thread_id'
+            job.perform
           end
+          expect(VCAP::Request).to have_received(:current_id=).twice.with('current_thread_id')
         end
 
         it 'makes the job retryable' do
