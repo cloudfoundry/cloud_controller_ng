1a360b2c79e9576505049fe228bba6abcd6bfd52
David Sabeti and Whitney Schaefer <dsabeti@pivotal.io>
2015-01-12 09:49:11 -0800

    Add logging to deprovision job

--- app/jobs/services/service_instance_deprovision.rb
+++ app/jobs/services/service_instance_deprovision.rb
@@ -3,6 +3,9 @@ module VCAP::CloudController
     module Services
       class ServiceInstanceDeprovision < Struct.new(:name, :client_attrs, :service_instance_guid, :service_plan_guid)
         def perform
+          logger = Steno.logger('cc-background')
+          logger.info('There was an error during service instance provisioning. Attempting to delete potentially orphaned instance.')
+
           client = VCAP::Services::ServiceBrokers::V2::Client.new(client_attrs)
           service_plan = ServicePlan.first(guid: service_plan_guid)
           service_instance = ManagedServiceInstance.new(guid: service_instance_guid, service_plan: service_plan)
