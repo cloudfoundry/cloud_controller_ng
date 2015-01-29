f16f5f0260b9af75f03cb10438f746844835d576
Dan Lavine and Luan Santos <dlavine@us.ibm.com>
2015-01-29 15:19:26 -0800

    Revert change on user_visibility_filter for events
    - makes things slow
    - does not filter properly
    
    [#84973576]

--- app/models/runtime/event.rb
+++ app/models/runtime/event.rb
@@ -35,16 +35,12 @@ module VCAP::CloudController
       self.organization_guid = space.organization.guid
     end
 
-    def self.user_visible(user, admin_override=false)
-      if admin_override
-        dataset.filter(full_dataset_filter)
-      elsif user
-        where(space: user.spaces_dataset).
-          union(where(space: user.audited_spaces_dataset), from_self: false).
-          union(where(organization_guid: user.audited_organizations_dataset.map(&:guid)), from_self: false)
-      else
-        dataset.filter(unauthenticated_visibility_filter)
-      end
+    def self.user_visibility_filter(user)
+      Sequel.or([
+        [:space, user.audited_spaces_dataset],
+        [:space, user.spaces_dataset],
+        [:organization_guid, user.audited_organizations_dataset.map(&:guid)]
+      ])
     end
   end
 end
