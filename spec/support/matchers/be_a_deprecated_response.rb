2d0a322aafd19f066f2ed43546d3b1d63c99a2ed
Jeff Hui <jhui@pivotal.io>
2015-02-03 09:32:37 -0800

    Refactored be_a_deprecated_response to provide a better error message.
    Check the header's existance before unescaping it to show the existing
    headers instead of CGI.unescape raising an exception.

--- spec/support/matchers/be_a_deprecated_response.rb
+++ spec/support/matchers/be_a_deprecated_response.rb
@@ -1,7 +1,8 @@
 RSpec::Matchers.define :be_a_deprecated_response do |_|
   match do |actual|
-    unescaped_header = CGI.unescape(actual.headers['X-Cf-Warnings'])
-    unescaped_header == 'Endpoint deprecated'
+    escaped_header = actual.headers['X-Cf-Warnings']
+    unescaped_header = CGI.unescape(escaped_header || '')
+    escaped_header && unescaped_header == 'Endpoint deprecated'
   end
 
   failure_message do |actual|
