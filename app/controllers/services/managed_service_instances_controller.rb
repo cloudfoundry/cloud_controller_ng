ed1bc1e098d45f450f713243e2c7d7fc3d37365f
Jeff Hui <pair+jhui@pivotal.io>
2015-02-03 09:52:09 -0800

    Deprecated /managed_service_instances endpoints.
    No API docs to change since /managed_service_instances is not mentioned.
    
    [Finishes #55946682].

--- app/controllers/services/managed_service_instances_controller.rb
+++ app/controllers/services/managed_service_instances_controller.rb
@@ -4,6 +4,8 @@ module VCAP::CloudController
   class ManagedServiceInstancesController < RestController::ModelController
     allow_unauthenticated_access
 
+    deprecated_endpoint '/v2/managed_service_instance'
+
     get '/v2/managed_service_instances/:guid', :read
     def read(guid)
       redirect "v2/service_instances/#{guid}"
--- spec/unit/controllers/services/managed_service_instances_controller_spec.rb
+++ spec/unit/controllers/services/managed_service_instances_controller_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 
 module VCAP::CloudController
   describe ManagedServiceInstancesController, :services do
+    describe 'GET', '/v2/managed_service_instances' do
+      it 'should be deprecated' do
+        get '/v2/managed_service_instances'
+        expect(last_response).to be_a_deprecated_response
+      end
+    end
+
     describe 'GET', '/v2/managed_service_instances/:guid' do
+      it 'should be deprecated' do
+        get '/v2/managed_service_instances/abcd'
+        expect(last_response).to be_a_deprecated_response
+      end
+
       it 'redirects to /v2/service_instances/:guid' do
         get '/v2/managed_service_instances/abcd'
-        expect(last_response.status).to eq(302)
+        expect(last_response).to have_status_code(302)
       end
     end
   end
