7f2414edf53fdfad86060a6a4a207432f0d6b258
David Sabeti and Michael Maximilien <dsabeti@pivotal.io>
2015-02-05 16:39:56 -0800

    Split out broker responses into different classes

--- lib/services/service_brokers/v2/response_parser.rb
+++ lib/services/service_brokers/v2/response_parser.rb
@@ -14,117 +14,201 @@ module VCAP::Services
 
           case method
           when :put
-            return parse_put_response(code, uri, response)
+            return PutResponse.new(uri, response).handle(code)
           when :delete
-            return parse_delete_response(code, uri, response)
+            return DeleteResponse.new(uri, response).handle(code)
           when :get
-            return parse_get_response(code, uri, response)
+            return GetResponse.new(uri, response).handle(code)
           when :patch
-            return parse_patch_response(code, uri, response)
+            return PatchResponse.new(uri, response).handle(code)
           end
         end
 
         private
 
-        def parse_get_response(code, uri, response)
-          parsed_response = parse_response(uri, :get, response)
-          state = state_from_parsed_response(parsed_response)
-          case code
-          when 200
-            raise_if_malformed_response(uri, :get, response, parsed_response)
-            return parsed_response if recogized_operation_state?(state)
-            raise Errors::ServiceBrokerResponseMalformed.new(uri.to_s, :get, response)
-          else
-            raise Errors::ServiceBrokerBadResponse.new(uri.to_s, :get, response)
+        class BaseResponse
+          def initialize(uri, response)
+            @uri = uri
+            @response = response
+          end
+
+          def recogized_operation_state?(state)
+            ['succeeded', 'failed', 'in progress', nil].include?(@state)
+          end
+
+          def parse_response(uri, method, response)
+            begin
+              parsed_response = MultiJson.load(response.body)
+            rescue MultiJson::ParseError
+              logger.warn("MultiJson parse error `#{response.try(:body).inspect}'")
+            end
+
+            return nil unless parsed_response.is_a?(Hash)
+
+            parsed_response
+          end
+
+          def state_from_parsed_response(parsed_response)
+            parsed_response ||= {}
+            last_operation = parsed_response['last_operation'] || {}
+            last_operation['state']
+          end
+
+          def raise_if_malformed_response(uri, method, response, parsed_response)
+            unless parsed_response
+              raise Errors::ServiceBrokerResponseMalformed.new(uri, method, sanitize_response(response))
+            end
+          end
+
+          def handle_other_code(method)
+            raise Errors::ServiceBrokerBadResponse.new(@uri.to_s, method, @response)
+          end
+
+          def sanitize_response(response)
+            HttpResponse.new(
+              code: response.code,
+              message: response.message,
+              body: "\"#{response.body}\""
+            )
           end
         end
 
-        def parse_put_response(code, uri, response)
-          parsed_response = parse_response(uri, :put, response)
-          if request_for_bindings?(uri) && parsed_response
-            parsed_response.except!('last_operation')
-          end
-          state = state_from_parsed_response(parsed_response)
-
-          handle_put_non_success(code, uri, response, parsed_response)
-
-          case code
-          when 200
-            raise_if_malformed_response(uri, :put, response, parsed_response)
-            return parsed_response if recogized_operation_state?(state)
-          when 201
-            raise_if_malformed_response(uri, :put, response, parsed_response)
-            return parsed_response if ['succeeded', nil].include?(state)
-          when 202
-            raise_if_malformed_response(uri, :put, response, parsed_response)
-            raise Errors::ServiceBrokerBadResponse.new(uri.to_s, :put, response) if request_for_bindings?(uri)
-            return parsed_response if state == 'in progress'
+        class GetResponse < BaseResponse
+          def initialize(uri, response)
+            super(uri, response)
+            @parsed_response = parse_response(uri, :get, response)
+            @state = state_from_parsed_response(@parsed_response)
           end
 
-          raise Errors::ServiceBrokerResponseMalformed.new(uri.to_s, :put, response)
-        end
-
-        def handle_put_non_success(code, uri, response, parsed_response)
-          case code
-          when 200, 201, 202
-            return
-          when 409
-            raise Errors::ServiceBrokerConflict.new(uri.to_s, :put, response)
-          when 422
-            raise Errors::AsyncRequired.new(uri.to_s, :put, response) if is_async_required? parsed_response
+          def handle(code)
+            case code
+            when 200
+              handle_200
+            else
+              handle_other_code(:get)
+            end
           end
 
-          raise Errors::ServiceBrokerBadResponse.new(uri.to_s, :put, response)
-        end
-
-        def parse_patch_response(code, uri, response)
-          parsed_response = parse_response(uri, :patch, response)
-          case code
-          when 200, 202
-            raise_if_malformed_response(uri, :get, response, parsed_response)
-            return parsed_response
-          when 422
-            raise Errors::ServiceBrokerRequestRejected.new(uri.to_s, :patch, response)
-          else
-            raise Errors::ServiceBrokerBadResponse.new(uri.to_s, :patch, response)
+          def handle_200
+            raise_if_malformed_response(@uri, :get, @response, @parsed_response)
+            return @parsed_response if recogized_operation_state?(@state)
+            raise Errors::ServiceBrokerResponseMalformed.new(@uri.to_s, :get, @response)
           end
         end
 
-        def parse_delete_response(code, uri, response)
-          parsed_response = parse_response(uri, :delete, response)
-          case code
-          when 200
-            raise_if_malformed_response(uri, :get, response, parsed_response)
-            return parsed_response
-          when 410
-            logger.warn("Already deleted: #{uri}")
-            return nil
-          else
-            raise Errors::ServiceBrokerBadResponse.new(uri.to_s, :delete, response)
+        class PutResponse < BaseResponse
+          def initialize(uri, response)
+            super(uri, response)
+            @parsed_response = parse_response(@uri, :put, @response)
+            if request_for_bindings?(@uri) && @parsed_response
+              @parsed_response.except!('last_operation')
+            end
+            @state = state_from_parsed_response(@parsed_response)
+          end
+
+          def handle(code)
+            handled_response =
+            case code
+            when 200
+              handle_200
+            when 201
+              handle_201
+            when 202
+              handle_202
+            when 409
+              raise Errors::ServiceBrokerConflict.new(@uri.to_s, :put, @response)
+            when 422
+              raise Errors::AsyncRequired.new(@uri.to_s, :put, @response) if is_async_required? @parsed_response
+              raise Errors::ServiceBrokerBadResponse.new(@uri.to_s, :put, @response)
+            else
+              handle_other_code(:put)
+            end
+
+            raise Errors::ServiceBrokerResponseMalformed.new(@uri.to_s, :put, @response) unless handled_response
+            handled_response
+          end
+
+          def handle_200
+            raise_if_malformed_response(@uri, :put, @response, @parsed_response)
+            @parsed_response if recogized_operation_state?(@state)
+          end
+
+          def handle_201
+            raise_if_malformed_response(@uri, :put, @response, @parsed_response)
+            return @parsed_response if ['succeeded', nil].include?(@state)
+          end
+
+          def handle_202
+            raise_if_malformed_response(@uri, :put, @response, @parsed_response)
+            raise Errors::ServiceBrokerBadResponse.new(@uri.to_s, :put, @response) if request_for_bindings?(@uri)
+            @parsed_response if @state == 'in progress'
+          end
+
+          def request_for_bindings?(uri)
+            !!%r{/v2/service_instances/[[:alnum:]-]+/service_bindings}.match(uri.to_s)
+          end
+
+          def is_async_required?(parsed_response)
+            parsed_response.is_a?(Hash) && parsed_response['error'] == 'AsyncRequired'
           end
         end
 
-        def state_from_parsed_response(parsed_response)
-          parsed_response ||= {}
-          last_operation = parsed_response['last_operation'] || {}
-          last_operation['state']
-        end
-
-        def parse_response(uri, method, response)
-          begin
-            parsed_response = MultiJson.load(response.body)
-          rescue MultiJson::ParseError
-            logger.warn("MultiJson parse error `#{response.try(:body).inspect}'")
+        class PatchResponse < BaseResponse
+          def initialize(uri, response)
+            super(uri, response)
+            @parsed_response = parse_response(@uri, :patch, @response)
           end
 
-          return nil unless parsed_response.is_a?(Hash)
+          def handle(code)
+            case code
+            when 200, 202
+              handle_200_202
+            when 422
+              handle_422
+            else
+              raise Errors::ServiceBrokerBadResponse.new(@uri.to_s, :patch, @response)
+            end
+          end
 
-          parsed_response
+          def handle_200_202
+            raise_if_malformed_response(@uri, :patch, @response, @parsed_response)
+            @parsed_response
+          end
+
+          def handle_422
+            raise Errors::ServiceBrokerRequestRejected.new(@uri.to_s, :patch, @response)
+          end
         end
 
-        def raise_if_malformed_response(uri, method, response, parsed_response)
-          unless parsed_response
-            raise Errors::ServiceBrokerResponseMalformed.new(uri, method, sanitize_response(response))
+        class DeleteResponse < BaseResponse
+          def initialize(uri, response)
+            super(uri, response)
+            @parsed_response = parse_response(@uri, :delete, @response)
+          end
+
+          def handle(code)
+            case code
+            when 200
+              handle_200
+            when 410
+              handle_410
+            else
+              raise Errors::ServiceBrokerBadResponse.new(@uri.to_s, :delete, @response)
+            end
+          end
+
+          def handle_200
+            raise_if_malformed_response(@uri, :delete, @response, @parsed_response)
+            @parsed_response
+          end
+
+          def handle_410
+            logger.warn("Already deleted: #{@uri}")
+            nil
+          end
+
+          def logger
+            @logger ||= Steno.logger('cc.service_broker.v2.client')
           end
         end
 
@@ -143,32 +227,8 @@ module VCAP::Services
           end
         end
 
-        def is_async_required?(parsed_response)
-          parsed_response.is_a?(Hash) && parsed_response['error'] == 'AsyncRequired'
-        end
-
-        def request_for_bindings?(uri)
-          !!%r{/v2/service_instances/[[:alnum:]-]+/service_bindings}.match(uri.to_s)
-        end
-
-        def recogized_operation_state?(state)
-          ['succeeded', 'failed', 'in progress', nil].include?(state)
-        end
-
-        def sanitize_response(response)
-          HttpResponse.new(
-            code: response.code,
-            message: response.message,
-            body: "\"#{response.body}\""
-          )
-        end
-
         def uri_for(path)
           URI(@url + path)
-        end
-
-        def logger
-          @logger ||= Steno.logger('cc.service_broker.v2.client')
         end
       end
     end
