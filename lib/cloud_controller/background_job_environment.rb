83e83cbcbb36d4668ef8a9f317feb73ae5d6de2f
Luan Santos <lsantos@pivotal.io>
2015-02-10 09:41:11 -0800

    Setup logging properly on CC worker and clock jobs
    [#76069390]

--- lib/cloud_controller/background_job_environment.rb
+++ lib/cloud_controller/background_job_environment.rb
@@ -1,8 +1,10 @@
 class BackgroundJobEnvironment
   def initialize(config)
     @config = config
+    @log_counter = Steno::Sink::Counter.new
+
     VCAP::CloudController::StenoConfigurer.new(config[:logging]).configure do |steno_config_hash|
-      steno_config_hash[:sinks] = [Steno::Sink::IO.new(STDOUT)]
+      steno_config_hash[:sinks] << @log_counter
     end
   end
 
