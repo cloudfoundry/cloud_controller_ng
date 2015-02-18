3bcf13379a30929b40be0437b6747da7ce82d50d
Luan Santos and Zak Auerbach <lsantos@pivotal.io>
2015-02-18 10:49:04 -0800

    Ignore v3 directory on api_version_spec

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'digest/sha1'
 
 describe 'Stable API warning system', api_version_check: true do
-  API_FOLDER_CHECKSUM = 'ac120aa6d3f212488ba18272e7015afb33467519'
+  API_FOLDER_CHECKSUM = '15768b5211c4171afd28ddbb9ede55b0bad57694'
 
   it 'double-checks the version' do
     expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.22.0')
@@ -10,7 +10,7 @@ describe 'Stable API warning system', api_version_check: true do
 
   it 'tells the developer if the API specs change' do
     api_folder = File.expand_path('..', __FILE__)
-    filenames = Dir.glob("#{api_folder}/**/*").reject { |filename| File.directory?(filename) || filename == __FILE__ }.sort
+    filenames = Dir.glob("#{api_folder}/**/*").reject { |filename| File.directory?(filename) || filename == __FILE__ || filename.include?('v3') }.sort
 
     all_file_checksum = filenames.each_with_object('') do |filename, memo|
       memo << Digest::SHA1.file(filename).hexdigest
