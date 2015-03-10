a938932d5ddf3432c89e95d6ff0eefff9a808a8e
Serguei Filimonov and Zach Robinson <zrobinson@pivotal.io>
2015-03-10 15:36:59 -0700

    Move stack seeding from migration to configuration.
    [#89643092]

--- db/migrations/20130219194917_create_stacks_table.rb
+++ db/migrations/20130219194917_create_stacks_table.rb
@@ -12,24 +12,10 @@ Sequel.migration do
       index :name, unique: true
     end
 
-    # Create single stack to be able to
-    # set existing apps to use it; otherwise,
-    # we cannot add non_null constraint on apps.stack_id.
-    lucid64_stack_id = self[:stacks].insert(
-      guid: SecureRandom.uuid,
-      name: 'lucid64',
-      description: 'Ubuntu 10.04 on x86-64',
-      created_at: Sequel::CURRENT_TIMESTAMP,
-    )
-
     alter_table :apps do
       add_column :stack_id, Integer
       add_foreign_key [:stack_id], :stacks, name: :fk_apps_stack_id
     end
-
-    self[:apps].update(
-      stack_id: lucid64_stack_id,
-    )
 
     alter_table :apps do
       set_column_not_null :stack_id
