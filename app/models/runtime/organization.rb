98feb26cf84a865133bf47b433051ef350b35927
Christopher Piraino and David Sabeti <dsabeti@pivotal.io>
2015-03-11 11:24:01 -0700

    Remove extraneous association dependency between org and service instance

--- app/models/runtime/organization.rb
+++ app/models/runtime/organization.rb
@@ -69,7 +69,6 @@ module VCAP::CloudController
                 before_add: proc { |org, quota| quota.organization.id == org.id }
 
     add_association_dependencies(
-      service_instances: :destroy,
       owned_private_domains: :destroy,
       private_domains: :nullify,
       service_plan_visibilities: :destroy,
