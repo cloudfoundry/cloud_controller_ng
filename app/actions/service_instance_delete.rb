b065d5b9ffc940099805f9257f4d12a67d9d9446
David Sabeti and Greg Cobb <gcobb@pivotal.io>
2015-04-03 13:43:18 -0700

    Refactor ServiceInstanceDelete
    - Lock is responsible for tracking if it needs to be unlocked
    - Remove check for user provided service instances since they now behave
    like regular service instances
    
    [#90815812]

--- app/actions/service_instance_delete.rb
+++ app/actions/service_instance_delete.rb
@@ -10,62 +10,58 @@ module VCAP::CloudController
     end
 
     def delete(service_instance_dataset)
-      service_instance_dataset.each_with_object([]) do |service_instance, errs|
-        errs = delete_service_instance(service_instance, errs)
-        errs
+      service_instance_dataset.each_with_object([]) do |service_instance, errors_accumulator|
+        binding_errors = delete_service_bindings(service_instance)
+        errors_accumulator.concat binding_errors
+
+        if binding_errors.empty?
+          instance_errors = delete_service_instance(service_instance)
+          errors_accumulator.concat instance_errors
+        end
       end
     end
 
     private
 
-    def delete_service_instance(service_instance, errs)
-      if service_instance.user_provided_instance?
-        begin
-          service_instance.destroy
-          return errs
-        rescue => e
-          errs << e
-          return errs
+    def delete_service_instance(service_instance)
+      errors = []
+      lock = DeleterLock.new(service_instance)
+      lock.lock!
+
+      begin
+        attributes_to_update, poll_interval = service_instance.client.deprovision(
+          service_instance,
+          accepts_incomplete: @accepts_incomplete
+        )
+
+        if attributes_to_update[:last_operation][:state] == 'succeeded'
+          lock.unlock_and_destroy!
+        else
+          lock.enqueue_unlock!(attributes_to_update, build_fetch_job(poll_interval, service_instance))
         end
+      rescue => e
+        errors << e
+        lock.unlock_and_fail!
+      ensure
+        lock.unlock_and_fail! if lock.needs_unlock?
       end
 
-      errors = ServiceBindingDelete.new.delete(service_instance.service_bindings_dataset)
-      errs.concat(errors)
-      if errors.empty?
-        lock = DeleterLock.new(service_instance)
-        lock.lock!
+      errors
+    end
 
-        needs_unlock = true
-        begin
-          attributes_to_update, poll_interval = service_instance.client.deprovision(service_instance, accepts_incomplete: @accepts_incomplete)
-          if attributes_to_update[:last_operation][:state] == 'succeeded'
-            lock.unlock_and_destroy!
-            needs_unlock = false
-            return errs
-          end
+    def delete_service_bindings(service_instance)
+      ServiceBindingDelete.new.delete(service_instance.service_bindings_dataset)
+    end
 
-          service_instance.save_with_operation(attributes_to_update)
-
-          job = VCAP::CloudController::Jobs::Services::ServiceInstanceStateFetch.new(
-            'service-instance-state-fetch',
-            service_instance.client.attrs,
-            service_instance.guid,
-            @event_repository_opts,
-            {},
-            poll_interval,
-          )
-
-          lock.enqueue_unlock!(attributes_to_update, job)
-          needs_unlock = false
-        rescue => e
-          errs << e
-          lock.unlock_and_fail!
-          needs_unlock = false
-        ensure
-          lock.unlock_and_fail! if needs_unlock
-        end
-      end
-      errs
+    def build_fetch_job(poll_interval, service_instance)
+      VCAP::CloudController::Jobs::Services::ServiceInstanceStateFetch.new(
+        'service-instance-state-fetch',
+        service_instance.client.attrs,
+        service_instance.guid,
+        @event_repository_opts,
+        {},
+        poll_interval,
+      )
     end
   end
 end
