b75ecc17ddf712e5fc63524c51ee707881e00ad6
Greg Cobb <gcobb@pivotal.io>
2015-04-24 13:53:49 -0700

    Backfill tests for ServiceKeyCreate
    Signed-off-by: Raina Masand <rmasand@pivotal.io>

---
+++ spec/unit/actions/service_key_create_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+require 'actions/service_key_create'
+
+module VCAP::CloudController
+  describe ServiceKeyCreate do
+    let(:service_instance) { ManagedServiceInstance.make }
+    let(:service_binding_url_pattern) { %r{/v2/service_instances/#{service_instance.guid}/service_bindings/} }
+
+    describe 'creating a service key' do
+      let(:logger) { double(Steno::Logger) }
+      let(:key_attrs) do
+        {
+            'service_instance_guid' => service_instance.guid,
+        }
+      end
+
+      describe 'orphan mitigation situations' do
+        context 'when the broker returns an error on creation' do
+          before do
+            stub_bind(service_instance, status: 500)
+          end
+
+          it 'does not create a key' do
+            ServiceKeyCreate.new(logger).create(service_instance, key_attrs, {})
+
+            expect(ServiceKey.count).to eq 0
+          end
+        end
+
+        context 'when broker request is successful but the database fails to save the key (Hail Mary)' do
+          before do
+            stub_bind(service_instance)
+            stub_request(:delete, service_binding_url_pattern)
+
+            allow_any_instance_of(ServiceKey).to receive(:save).and_raise
+
+            allow(logger).to receive :error
+
+            ServiceKeyCreate.new(logger).create(service_instance, key_attrs, {})
+          end
+
+          it 'immediately attempts to unbind the service instance' do
+            expect(a_request(:put, service_binding_url_pattern)).to have_been_made.times(1)
+            expect(a_request(:delete, service_binding_url_pattern)).to have_been_made.times(1)
+          end
+
+          it 'does not try to enqueue an orphan mitigation job' do
+            orphan_mitigating_job = Delayed::Job.first
+            expect(orphan_mitigating_job).to be_nil
+          end
+
+          context 'when the orphan mitigation unbind fails' do
+            before do
+              stub_request(:delete, service_binding_url_pattern).
+                  to_return(status: 500, body: {}.to_json)
+            end
+
+            it 'logs that the unbind failed' do
+              expect(logger).to have_received(:error).with /Unable to delete orphaned key/
+            end
+          end
+        end
+      end
+    end
+  end
+end
