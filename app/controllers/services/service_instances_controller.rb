fe6b8565f0cd234864786ef051a60d3f73b6993f
David Sabeti and Matt Boedicker <dsabeti@pivotal.io>
2015-05-14 16:30:27 -0700

    Reorganization ServiceInstancesController

--- app/controllers/services/service_instances_controller.rb
+++ app/controllers/services/service_instances_controller.rb
@@ -52,10 +52,6 @@ module VCAP::CloudController
       Errors::ApiError.new_from_details('ServiceInstanceInvalid', e.errors.full_messages)
     end
 
-    def self.not_found_exception(guid)
-      Errors::ApiError.new_from_details('ServiceInstanceNotFound', guid)
-    end
-
     def self.dependencies
       [:services_event_repository]
     end
@@ -112,6 +108,47 @@ module VCAP::CloudController
       ]
     end
 
+    def read(guid)
+      logger.debug 'cc.read', model: :ServiceInstance, guid: guid
+
+      service_instance = find_guid_and_validate_access(:read, guid, ServiceInstance)
+      object_renderer.render_json(self.class, service_instance, @opts)
+    end
+
+    def delete(guid)
+      accepts_incomplete = convert_flag_to_bool(params['accepts_incomplete'])
+      async = convert_flag_to_bool(params['async'])
+
+      service_instance = find_guid(guid, ServiceInstance)
+
+      validate_access(:delete, service_instance)
+      association_not_empty!(:service_bindings) if has_bindings?(service_instance) && !recursive?
+      association_not_empty!(:service_keys) if has_keys?(service_instance) && !recursive?
+
+      deprovisioner = ServiceInstanceDeprovisioner.new(@services_event_repository, self, logger)
+      delete_job = deprovisioner.deprovision_service_instance(service_instance, accepts_incomplete, async)
+
+      if delete_job
+        [HTTP::ACCEPTED, JobPresenter.new(delete_job).to_json]
+      elsif service_instance.exists?
+        [HTTP::ACCEPTED, {}, object_renderer.render_json(self.class, service_instance.refresh, @opts)]
+      else
+        [HTTP::NO_CONTENT, nil]
+      end
+    end
+
+    get '/v2/service_instances/:guid/permissions', :permissions
+    def permissions(guid)
+      find_guid_and_validate_access(:read_permissions, guid, ServiceInstance)
+      [HTTP::OK, {}, JSON.generate({ manage: true })]
+    rescue Errors::ApiError => e
+      if e.name == 'NotAuthorized'
+        [HTTP::OK, {}, JSON.generate({ manage: false })]
+      else
+        raise e
+      end
+    end
+
     class BulkUpdateMessage < VCAP::RestAPI::Message
       required :service_plan_guid, String
     end
@@ -144,45 +181,8 @@ module VCAP::CloudController
       end
     end
 
-    def read(guid)
-      logger.debug 'cc.read', model: :ServiceInstance, guid: guid
-
-      service_instance = find_guid_and_validate_access(:read, guid, ServiceInstance)
-      object_renderer.render_json(self.class, service_instance, @opts)
-    end
-
-    get '/v2/service_instances/:guid/permissions', :permissions
-    def permissions(guid)
-      find_guid_and_validate_access(:read_permissions, guid, ServiceInstance)
-      [HTTP::OK, {}, JSON.generate({ manage: true })]
-    rescue Errors::ApiError => e
-      if e.name == 'NotAuthorized'
-        [HTTP::OK, {}, JSON.generate({ manage: false })]
-      else
-        raise e
-      end
-    end
-
-    def delete(guid)
-      accepts_incomplete = convert_flag_to_bool(params['accepts_incomplete'])
-      async = convert_flag_to_bool(params['async'])
-
-      service_instance = find_guid(guid, ServiceInstance)
-
-      validate_access(:delete, service_instance)
-      association_not_empty!(:service_bindings) if has_bindings?(service_instance) && !recursive?
-      association_not_empty!(:service_keys) if has_keys?(service_instance) && !recursive?
-
-      deprovisioner = ServiceInstanceDeprovisioner.new(@services_event_repository, self, logger)
-      delete_job = deprovisioner.deprovision_service_instance(service_instance, accepts_incomplete, async)
-
-      if delete_job
-        [HTTP::ACCEPTED, JobPresenter.new(delete_job).to_json]
-      elsif service_instance.exists?
-        [HTTP::ACCEPTED, {}, object_renderer.render_json(self.class, service_instance.refresh, @opts)]
-      else
-        [HTTP::NO_CONTENT, nil]
-      end
+    def self.not_found_exception(guid)
+      Errors::ApiError.new_from_details('ServiceInstanceNotFound', guid)
     end
 
     def get_filtered_dataset_for_enumeration(model, ds, qp, opts)
