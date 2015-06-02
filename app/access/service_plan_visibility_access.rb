9ce3dae9a8d1bf3d499e28bb9d3d1e913b4f602a
Greg Cobb and Raina Masand <gcobb@pivotal.io>
2015-06-02 15:47:57 -0700

    Service plan visibilities list requires admin access
    [#95215270]

--- app/access/service_plan_visibility_access.rb
+++ app/access/service_plan_visibility_access.rb
@@ -1,4 +1,7 @@
 module VCAP::CloudController
   class ServicePlanVisibilityAccess < BaseAccess
+    def index?(*_)
+      admin_user?
+    end
   end
 end
--- spec/unit/controllers/services/service_plan_visibilities_controller_spec.rb
+++ spec/unit/controllers/services/service_plan_visibilities_controller_spec.rb
@@ -26,7 +26,7 @@ module VCAP::CloudController
         @obj_b = ServicePlanVisibility.make
       end
 
-      def self.user_sees_empty_enumerate(user_role, member_a_ivar, member_b_ivar)
+      def self.user_sees_forbidden_enumerate(user_role, member_a_ivar, member_b_ivar)
         describe user_role do
           let(:member_a) { instance_variable_get(member_a_ivar) }
           let(:member_b) { instance_variable_get(member_b_ivar) }
@@ -34,17 +34,28 @@ module VCAP::CloudController
           include_examples 'permission enumeration', user_role,
                            name: 'service plan visibility',
                            path: '/v2/service_plan_visibilities',
-                           enumerate: 0
+                           enumerate: :not_allowed
         end
       end
 
-      user_sees_empty_enumerate('Developer',      :@space_a_developer,     :@space_b_developer)
-      user_sees_empty_enumerate('OrgManager',     :@org_a_manager,         :@org_b_manager)
-      user_sees_empty_enumerate('OrgUser',        :@org_a_member,          :@org_b_member)
-      user_sees_empty_enumerate('BillingManager', :@org_a_billing_manager, :@org_b_billing_manager)
-      user_sees_empty_enumerate('Auditor',        :@org_a_auditor,         :@org_b_auditor)
-      user_sees_empty_enumerate('SpaceManager',   :@space_a_manager,       :@space_b_manager)
-      user_sees_empty_enumerate('SpaceAuditor',   :@space_a_auditor,       :@space_b_auditor)
+      user_sees_forbidden_enumerate('Developer',      :@space_a_developer,     :@space_b_developer)
+      user_sees_forbidden_enumerate('OrgManager',     :@org_a_manager,         :@org_b_manager)
+      user_sees_forbidden_enumerate('OrgUser',        :@org_a_member,          :@org_b_member)
+      user_sees_forbidden_enumerate('BillingManager', :@org_a_billing_manager, :@org_b_billing_manager)
+      user_sees_forbidden_enumerate('Auditor',        :@org_a_auditor,         :@org_b_auditor)
+      user_sees_forbidden_enumerate('SpaceManager',   :@space_a_manager,       :@space_b_manager)
+      user_sees_forbidden_enumerate('SpaceAuditor',   :@space_a_auditor,       :@space_b_auditor)
+
+      describe 'admin' do
+        let(:member_a) { instance_variable_get(:@cf_admin) }
+        let(:member_b) { instance_variable_get(:@cf_admin) }
+
+        include_examples 'permission enumeration', 'Admin',
+                         name: 'service plan visibility',
+                         path: '/v2/service_plan_visibilities',
+                         enumerate: 2,
+                         permissions_overlap: true
+      end
     end
 
     describe 'Query Parameters' do
