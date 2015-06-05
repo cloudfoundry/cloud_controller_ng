181c184bd0e236f361a87f305ed0a7276149dcad
James Quintana and Zach Robinson <zrobinson@pivotal.io>
2015-06-05 14:57:57 -0700

    Handle nil blobstore key on droplet
    [#96312898]

--- app/actions/droplet_delete.rb
+++ app/actions/droplet_delete.rb
@@ -4,8 +4,11 @@ module VCAP::CloudController
       droplets = [droplets] unless droplets.is_a?(Array)
 
       droplets.each do |droplet|
-        blobstore_delete = Jobs::Runtime::BlobstoreDelete.new(droplet.blobstore_key, :droplet_blobstore, nil)
-        Jobs::Enqueuer.new(blobstore_delete, queue: 'cc-generic').enqueue
+        if droplet.blobstore_key
+          blobstore_delete = Jobs::Runtime::BlobstoreDelete.new(droplet.blobstore_key, :droplet_blobstore, nil)
+          Jobs::Enqueuer.new(blobstore_delete, queue: 'cc-generic').enqueue
+        end
+
         droplet.destroy
       end
     end
--- spec/unit/actions/droplet_delete_spec.rb
+++ spec/unit/actions/droplet_delete_spec.rb
@@ -6,29 +6,41 @@ module VCAP::CloudController
     subject(:droplet_delete) { DropletDelete.new }
 
     describe '#delete' do
-      context 'when the droplet exists' do
-        let!(:droplet) { DropletModel.make(droplet_hash: 'droplet_hash') }
+      let!(:droplet) { DropletModel.make(droplet_hash: 'droplet_hash') }
 
-        it 'deletes the droplet record' do
-          expect {
-            droplet_delete.delete([droplet])
-          }.to change { DropletModel.count }.by(-1)
-          expect { droplet.refresh }.to raise_error Sequel::Error, 'Record not found'
+      it 'deletes the droplet record' do
+        expect {
+          droplet_delete.delete([droplet])
+        }.to change { DropletModel.count }.by(-1)
+        expect { droplet.refresh }.to raise_error Sequel::Error, 'Record not found'
+      end
+
+      it 'schedules a job to the delete the blobstore item' do
+        expect {
+          droplet_delete.delete([droplet])
+        }.to change {
+               Delayed::Job.count
+             }.by(1)
+
+        job = Delayed::Job.last
+        expect(job.handler).to include('VCAP::CloudController::Jobs::Runtime::BlobstoreDelete')
+        expect(job.handler).to include("key: #{droplet.blobstore_key}")
+        expect(job.handler).to include('droplet_blobstore')
+        expect(job.queue).to eq('cc-generic')
+        expect(job.guid).not_to be_nil
+      end
+
+      context 'when the droplet does not have a blobstore key' do
+        before do
+          allow(droplet).to receive(:blobstore_key).and_return(nil)
         end
 
-        it 'schedules a job to the delete the blobstore item' do
+        it 'does not schedule a blobstore delete job' do
           expect {
             droplet_delete.delete([droplet])
-          }.to change {
+          }.not_to change {
             Delayed::Job.count
-          }.by(1)
-
-          job = Delayed::Job.last
-          expect(job.handler).to include('VCAP::CloudController::Jobs::Runtime::BlobstoreDelete')
-          expect(job.handler).to include("key: #{droplet.blobstore_key}")
-          expect(job.handler).to include('droplet_blobstore')
-          expect(job.queue).to eq('cc-generic')
-          expect(job.guid).not_to be_nil
+          }
         end
       end
     end
