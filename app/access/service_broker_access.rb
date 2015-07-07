f6d8a108351ff5cc2da0829670ac2a45f90077c7
Greg Cobb and Jeff Pak <gcobb@pivotal.io>
2015-07-07 14:17:07 -0700

    Allow users to index service brokers
    - All users may request an index of service brokers, but the results
      will be filtered outside of the access layer.
    
    [#97654336]

--- app/access/service_broker_access.rb
+++ app/access/service_broker_access.rb
@@ -1,9 +1,5 @@
 module VCAP::CloudController
   class ServiceBrokerAccess < BaseAccess
-    def index?(*_)
-      admin_user?
-    end
-
     def create?(service_broker, _=nil)
       return true if admin_user?
 
