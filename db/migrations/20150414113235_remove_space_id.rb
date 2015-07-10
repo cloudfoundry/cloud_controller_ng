1bed076811359b63206477c56a0edf9cf1ad7bea
Greg Cobb and James Quintana <gcobb@pivotal.io>
2015-07-10 10:02:28 -0700

    Remove backwards-incompatible migration
    - The migration removed a column. For deployments with multiple CC
      instances, the migrations run when the first instance is updated. The
      remaining instances running the old CC code do not appreciate the
      column disappearing which results in API outages until all the CCs
      have been updated.
    - Gutting this migration will result in a vestigial space_id column for
      deployments that have not migrated yet, but upgrades will be
      zero-downtime.
    
    [#98701792]

--- db/migrations/20150414113235_remove_space_id.rb
+++ db/migrations/20150414113235_remove_space_id.rb
@@ -1,15 +1,3 @@
 Sequel.migration do
-  up do
-    alter_table :events do
-      drop_foreign_key([:space_id])
-      drop_column :space_id
-    end
-  end
-
-  down do
-    alter_table :events do
-      add_column :space_id, Integer, null: true
-      add_foreign_key [:space_id], :spaces, name: :fk_no_cascade_events_space_id, on_delete: :set_null
-    end
-  end
+  # This migration was not backwards-compatible and caused API outages during upgrade.
 end
