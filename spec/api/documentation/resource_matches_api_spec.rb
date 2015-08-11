58f0e6941b25cefcec0067133c76e31728645dd0
Jonathan Berkhahn <jaberkha@us.ibm.com>
2015-08-10 17:27:49 -0700

    Add a description to the Resource Match API page.
    [#100654666]
    
    Signed-off-by: James Quintana <jquintana@pivotal.io>

--- spec/api/documentation/resource_matches_api_spec.rb
+++ spec/api/documentation/resource_matches_api_spec.rb
@@ -9,6 +9,10 @@ resource 'Resource Match', type: [:api, :legacy_api] do
 
   put '/v2/resource_match' do
     example 'List all matching resources' do
+      explanation 'This endpoint matches given resource SHA/filesize pairs against the Cloud Controller cache,
+        and reports the subset which describes already existing files.
+        This is usually used to avoid uploading duplicate files when
+        pushing an app which has only been partially changed.'
       @resource_pool.add_directory(@tmpdir)
       resources = [@descriptors.first] + [@dummy_descriptor]
       encoded_resources = MultiJson.dump(resources, pretty: true)
