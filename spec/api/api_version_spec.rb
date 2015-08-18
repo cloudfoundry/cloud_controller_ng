7fdb742f2f43329d3f11ebbf834b7d1bf03e926b
Greg Cobb <gcobb@pivotal.io>
2015-08-18 11:28:08 -0700

    Bump CC API version
    [#101471540]
    
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -5,7 +5,7 @@ describe 'Stable API warning system', api_version_check: true do
   API_FOLDER_CHECKSUM = '33a30ed29da8de2a49da905f4f28332fa76e543a'
 
   it 'double-checks the version' do
-    expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.34.0')
+    expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.35.0')
   end
 
   it 'tells the developer if the API specs change' do
