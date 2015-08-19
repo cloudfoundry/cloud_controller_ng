373804ba29eb7be84f76bed67a85f22ba83c778e
Raina Masand <rmasand@pivotal.io>
2015-08-19 14:15:02 -0700

    Revert "Add tests for async provision"
    This reverts commit 77a310feed2cd805ae397332f4bb5849cab06839.
    
    Signed-off-by: Ben Watts <bwatts@pivotal.io>

--- spec/unit/actions/services/service_instance_create_spec.rb
+++ spec/unit/actions/services/service_instance_create_spec.rb
@@ -137,66 +137,6 @@ module VCAP::CloudController
           create_action.create(request_attrs, true)
           expect(event_repository).not_to have_received(:record_service_instance_event)
         end
-
-        context 'when the service instance create returns dashboard client credentials' do
-          let(:body) do
-            {
-              dashboard_url: 'http://example-dashboard.com/9189kdfsk0vfnku',
-              dashboard_client: {
-                id: 'client-id-1',
-                secret: 'secret-1',
-                redirect_uri: 'https://dashboard.service.com'
-              }
-            }.to_json
-          end
-          let(:client_manager) { instance_double(VCAP::Services::SSO::DashboardClientManager) }
-
-          before do
-            stub_provision(service_plan.service.service_broker, body: body)
-            allow(client_manager).to receive(:add_client_for_instance)
-            allow(VCAP::Services::SSO::DashboardClientManager).to receive(:new).and_return(client_manager)
-          end
-
-          context 'the dashboard_url is missing' do
-            let(:mock_orphan_mitigator) { double(:mock_orphan_mitigator, attempt_deprovision_instance: nil) }
-            let(:body) do
-              {
-                dashboard_client: {
-                  'space_guid' => space.guid,
-                  'service_plan_guid' => service_plan.guid,
-                  'name' => 'my-instance'
-                }
-              }.to_json
-            end
-
-            before do
-              allow(SynchronousOrphanMitigate).to receive(:new).and_return(mock_orphan_mitigator)
-              allow(logger).to receive(:error)
-            end
-
-            it 'attempts synchronous orphan mitigation' do
-              expect {
-                create_action.create(request_attrs, false)
-              }.to raise_error
-              expect(mock_orphan_mitigator).to have_received(:attempt_deprovision_instance)
-            end
-          end
-
-          it 'creates a new UAA dashboard client' do
-            create_action.create(request_attrs, false)
-
-            expect(VCAP::Services::SSO::DashboardClientManager).to have_received(:new).with(
-              anything,
-              event_repository,
-              VCAP::CloudController::ServiceInstanceDashboardClient
-            )
-            expect(client_manager).to have_received(:add_client_for_instance).with(hash_including({
-              'id' => 'client-id-1',
-              'secret' => 'secret-1',
-              'redirect_uri' => 'https://dashboard.service.com'
-            }))
-          end
-        end
       end
 
       context 'when the instance fails to save to the db' do
