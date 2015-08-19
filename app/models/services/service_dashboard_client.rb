eb387cdc0497f3cc2198935b80aab7e3cd84eb93
Ben Watts <bwatts@pivotal.io>
2015-08-19 16:33:39 -0700

    Revert "Create ServiceInstanceDashboardClient class"
    This reverts commit 13ea062a5d54ee270d1703f7a7ad028bd2eb9a45.
    
    Signed-off-by: Raina Masand <rmasand@pivotal.io>

--- app/models/services/service_dashboard_client.rb
+++ app/models/services/service_dashboard_client.rb
@@ -1,94 +1,44 @@
 module VCAP::CloudController
-  module DashboardClientValidation
-    def validate
-      validates_presence :uaa_id
-      validates_unique :uaa_id
-    end
-  end
+  class ServiceDashboardClient < Sequel::Model
+    many_to_one :service_broker
 
-  module DashboardClient
-    def find_client_by_uaa_id(uaa_id)
-      where(uaa_id: uaa_id).first
+    def self.find_clients_claimed_by_broker(broker)
+      where(service_broker_id: broker.id)
     end
 
-    def release_client(uaa_id)
-      where(uaa_id: uaa_id).delete
-    end
-
-    def claim_client(uaa_id, resource)
-      return if client_claimed?(uaa_id, resource)
+    def self.claim_client_for_broker(uaa_id, claiming_broker)
+      return if client_claimed_by_broker?(uaa_id, claiming_broker)
 
       unclaimed_client = unclaimed_client_with_uaa_id(uaa_id)
 
       if unclaimed_client.nil?
-        create_resource(resource, uaa_id)
+        create(uaa_id: uaa_id, service_broker: claiming_broker)
       else
-        update_resource(resource, unclaimed_client)
+        unclaimed_client.update(service_broker_id: claiming_broker.id)
       end
 
       nil
     end
 
-    def client_claimed?(uaa_id, resource)
-      get_client(resource.id, uaa_id).any?
+    def self.client_claimed_by_broker?(uaa_id, broker)
+      where(service_broker_id: broker.id, uaa_id: uaa_id).any?
     end
 
-    def unclaimed_client_with_uaa_id(uaa_id)
-      get_client(nil, uaa_id).first
+    def self.remove_claim_on_client(uaa_id)
+      where(uaa_id: uaa_id).delete
     end
-  end
 
-  class ServiceDashboardClient < Sequel::Model
-    many_to_one :service_broker
-
-    include DashboardClientValidation
-    extend DashboardClient
-
-    class << self
-      def find_claimed_client(broker)
-        where(service_broker_id: broker.id)
-      end
-
-      private
-
-      def get_client(broker_id, uaa_id)
-        where(service_broker_id: broker_id, uaa_id: uaa_id)
-      end
-
-      def update_resource(broker, unclaimed_client)
-        unclaimed_client.update(service_broker_id: broker.id)
-      end
-
-      def create_resource(broker, uaa_id)
-        create(uaa_id: uaa_id, service_broker: broker)
-      end
+    def self.find_client_by_uaa_id(uaa_id)
+      where(uaa_id: uaa_id).first
     end
-  end
 
-  class ServiceInstanceDashboardClient < Sequel::Model
-    many_to_one :managed_service_instance
+    def validate
+      validates_presence :uaa_id
+      validates_unique :uaa_id
+    end
 
-    include DashboardClientValidation
-    extend DashboardClient
-
-    class << self
-      def find_claimed_client(instance)
-        where(managed_service_instance_id: instance.id)
-      end
-
-      private
-
-      def get_client(instance_id, uaa_id)
-        where(managed_service_instance_id: instance_id, uaa_id: uaa_id)
-      end
-
-      def update_resource(instance, unclaimed_client)
-        unclaimed_client.update(managed_service_instance_id: instance.id)
-      end
-
-      def create_resource(instance, uaa_id)
-        create(uaa_id: uaa_id, managed_service_instance: instance)
-      end
+    def self.unclaimed_client_with_uaa_id(uaa_id)
+      where(service_broker_id: nil, uaa_id: uaa_id).first
     end
   end
 end
