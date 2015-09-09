fe97132193d28efa5aa1fb7443f593ffa5c5ff97
Dan Wendorf <dwendorf@pivotal.io>
2015-09-09 10:19:36 -0700

    Change API Checksum
    Signed-off-by: Ben Watts <bwatts@pivotal.io>

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'vcap/digester'
 
 describe 'Stable API warning system', api_version_check: true do
-  API_FOLDER_CHECKSUM = 'b3f94f4e5017492c73b454cd2797bfdd4ffaa706'
+  API_FOLDER_CHECKSUM = 'de3700d82e0d2cae88dea1a61b2571c5354a9b1a'
 
   it 'double-checks the version' do
     expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.36.0')
