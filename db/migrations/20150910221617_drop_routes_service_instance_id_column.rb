96151a637b3390ef09e184925a3d2d066c3ee41d
Greg Cobb <gcobb@pivotal.io>
2015-09-15 15:47:09 -0700

    Change migration to be backwards compatible
    Inline relations depth exercises associations despite our desires to the
    contrary. This means we can not remove the column until the next CF
    release at the soonest, so there is no application code that will touch
    the relation.
    
    This change drops the foreign key constraint, but not the column.
    
    Signed-off-by: Utako Ueda <uueda@pivotal.io>

--- db/migrations/20150910221617_drop_routes_service_instance_id_column.rb
+++ db/migrations/20150910221617_drop_routes_service_instance_id_column.rb
@@ -1,13 +1,16 @@
+# This migration was not backwards-compatible and caused API outages during upgrade.
+# It has been altered from its original form.
+
 Sequel.migration do
   up do
     alter_table :routes do
-      drop_foreign_key :service_instance_id
+      drop_foreign_key [:service_instance_id]
     end
   end
 
   down do
     alter_table :routes do
-      add_foreign_key :service_instance_id, :service_instances
+      add_foreign_key [:service_instance_id], :service_instances
     end
   end
 end
