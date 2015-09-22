da49fc8349861e419be01a3403c4edbddda0f930
Greg Cobb <gcobb@pivotal.io>
2015-09-22 15:37:31 -0700

    Make api spec pass
    Signed-off-by: Raina Masand <rmasand@pivotal.io>

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'vcap/digester'
 
 describe 'Stable API warning system', api_version_check: true do
-  API_FOLDER_CHECKSUM = 'fd2b89e5bf6516da0eee86ec4950a89808b6b248'
+  API_FOLDER_CHECKSUM = '9a953235134b6bfd7473430d3b52ac041045cf11'
 
   it 'double-checks the version' do
     expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.37.0')
