7207d6a6b8e7b1534115ef2fc6ec697929dc73bd
Jeff Pak <jeffrey.pak@emc.com>
2015-09-23 13:45:27 -0700

    Add more helpful error messages to our be_a_response_like matcher.
    [#100608644]
    Signed-off-by: Ben Watts <bwatts@pivotal.io>
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- spec/support/matchers/be_a_valid_response.rb
+++ spec/support/matchers/be_a_valid_response.rb
@@ -1,4 +1,10 @@
 RSpec::Matchers.define :be_a_response_like do |expected|
+
+  define_method :bad_key! do |key|
+    @problem_keys ||= []
+    @problem_keys << key
+  end
+
   match do |actual|
     expected.each do |expected_key, expected_value|
       if expected_value.is_a?(Array)
@@ -6,6 +12,7 @@ RSpec::Matchers.define :be_a_response_like do |expected|
           expect(actual[expected_key][index]).to be_a_response_like(nested_expected_value)
         end
       else
+        bad_key!(expected_key) if actual[expected_key] != expected_value
         expect(actual[expected_key]).to match(expected_value)
       end
     end
@@ -17,8 +24,22 @@ RSpec::Matchers.define :be_a_response_like do |expected|
           expect(expected[actual_key][index]).to be_a_response_like(nested_actual_value)
         end
       else
+        bad_key!(actual_key) if expected[actual_key] != actual_value
         expect(expected[actual_key]).to match(actual_value)
       end
     end
   end
+
+  diffable
+
+  failure_message do |actual|
+    bad_keys_info = (!!@problem_keys ? "Bad keys: #{@problem_keys}" : "")
+
+    <<-HEREDOC
+      expected: #{expected}
+      got: #{actual}
+      #{bad_keys_info}
+    HEREDOC
+  end
+
 end
