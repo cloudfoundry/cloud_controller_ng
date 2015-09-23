668e9184479d46c16e005ac60483e3b40dfd75a2
Kris Hicks <khicks@pivotal.io>
2015-09-22 17:53:15 -0700

    Allow modifying ssh-enabled apps always
    In the case that an app is pushed with ssh enabled, and later ssh is
    disabled either globally or for the space that the app was pushed to,
    allow restarting, scaling, and pushing the same app. Only when flipping
    ssh off and on at the app level should an error occur in this scenario
    (when trying to switch it back on).
    
    Additionally, the error message now properly reflects at which level ssh
    was disabled (global, space).
    
    Some tests in apps_controller_spec were deleted as they were testing at
    the same granularity as the tests in enable_ssh_policy_spec.
    
    [#103911138]
    
    Signed-off-by: Andrew Edgar <aedgar@ca.ibm.com>

--- app/models/runtime/constraints/enable_ssh_policy.rb
+++ app/models/runtime/constraints/enable_ssh_policy.rb
@@ -1,16 +1,20 @@
 class EnableSshPolicy
   def initialize(app)
-    @errors = app.errors
     @app = app
+    @errors = app.errors
   end
 
   def validate
-    return unless @app.enable_ssh
-    global_allow_ssh = VCAP::CloudController::Config.config[:allow_app_ssh_access]
-    ssh_allowed = global_allow_ssh && @app.space.allow_ssh
+    if @app.modified?(:enable_ssh) && @app.enable_ssh
+      global_allow_ssh = VCAP::CloudController::Config.config[:allow_app_ssh_access]
 
-    unless ssh_allowed
-      @errors.add(:enable_ssh, 'enable_ssh must be false due to global allow_ssh setting')
+      if !global_allow_ssh
+        @errors.add(:enable_ssh, 'must be false due to ssh being disabled globally')
+      end
+
+      if !@app.space.allow_ssh
+        @errors.add(:enable_ssh, 'must be false due to ssh being disabled on space')
+      end
     end
   end
 end
--- spec/unit/models/runtime/constraints/enable_ssh_policy_spec.rb
+++ spec/unit/models/runtime/constraints/enable_ssh_policy_spec.rb
@@ -1,38 +1,81 @@
 require 'spec_helper'
 
 describe EnableSshPolicy do
-  let(:space) { VCAP::CloudController::Space.make(allow_ssh: true) }
+  let(:space_allows_ssh) { true }
+  let(:ssh_allowed_globally) { true }
+  let(:space) { VCAP::CloudController::Space.make(allow_ssh: space_allows_ssh) }
   let(:app) { VCAP::CloudController::AppFactory.make(space: space) }
-  let(:error_name) { 'enable_ssh must be false due to global allow_ssh setting' }
+  let(:ssh_disabled_on_space) { 'must be false due to ssh being disabled on space' }
+  let(:ssh_disabled_globally) { 'must be false due to ssh being disabled globally' }
   subject(:validator) { EnableSshPolicy.new(app) }
 
   before do
     allow(VCAP::CloudController::Config.config).to receive(:[]).with(anything).and_call_original
-    allow(VCAP::CloudController::Config.config).to receive(:[]).with(:allow_app_ssh_access).and_return(true)
+    allow(VCAP::CloudController::Config.config).to receive(:[]).with(:allow_app_ssh_access).and_return(ssh_allowed_globally)
   end
 
-  it 'is valid when enable_ssh is false' do
-    app.enable_ssh = false
-    expect(validator).to validate_without_error(app)
+  context 'when enable_ssh is false' do
+    before { app.enable_ssh = false }
+
+    it 'is valid' do
+      expect(validator).to validate_without_error(app)
+    end
   end
 
-  it 'is valid when ssh is enabled globally and allowed on the space' do
-    app.enable_ssh = false
-    expect(validator).to validate_without_error(app)
-  end
+  context 'when enable_ssh is true' do
+    before { app.enable_ssh = true }
 
-  it 'is invalid when ssh is disabled globally' do
-    allow(VCAP::CloudController::Config.config).to receive(:[]).with(:allow_app_ssh_access).and_return(false)
-    app.enable_ssh = true
+    context 'when ssh is enabled globally' do
+      let(:ssh_allowed_globally) { true }
 
-    expect(validator).to validate_with_error(app, :enable_ssh, error_name)
-  end
+      context 'when ssh is disabled on the space' do
+        let(:space_allows_ssh) { false }
 
-  it 'is invalid when space does not allow ssh access' do
-    space.allow_ssh = false
-    space.save
-    app.enable_ssh = true
+        it 'is invalid' do
+          expect(validator).to validate_with_error(app, :enable_ssh, ssh_disabled_on_space)
+        end
+      end
 
-    expect(validator).to validate_with_error(app, :enable_ssh, error_name)
+      context 'when ssh is enabled on the space' do
+        let(:space_allows_ssh) { true }
+
+        it 'is valid' do
+          expect(validator).to validate_without_error(app)
+        end
+
+        context 'when ssh is later disabled on the space' do
+          before do
+            space.allow_ssh = false
+            space.save
+          end
+
+          it 'is valid' do
+            expect(validator).to validate_without_error(app)
+          end
+
+          context 'when ssh_enabled is then flipped on the app' do
+            before do
+              app.enable_ssh = false
+              app.save
+              expect(validator).to validate_without_error(app)
+
+              app.enable_ssh = true
+            end
+
+            it 'is invalid' do
+              expect(validator).to validate_with_error(app, :enable_ssh, ssh_disabled_on_space)
+            end
+          end
+        end
+      end
+    end
+
+    context 'when ssh is disabled globally' do
+      let(:ssh_allowed_globally) { false }
+
+      it 'is invalid' do
+        expect(validator).to validate_with_error(app, :enable_ssh, ssh_disabled_globally)
+      end
+    end
   end
 end
