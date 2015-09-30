8ebe9d9c3030f13aeffa5a48dad9eabba5082556
Greg Cobb <gcobb@pivotal.io>
2015-09-30 12:02:33 -0700

    Refactor app model visibility
    - SQL query for space guids is the same, just uses more concise Sequel
    
    [#97661020]
    
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>
    Signed-off-by: Greg Cobb <gcobb@pivotal.io>

--- app/models/v3/persistence/app_model.rb
+++ app/models/v3/persistence/app_model.rb
@@ -24,20 +24,29 @@ module VCAP::CloudController
       validate_droplet_is_staged
     end
 
-    def self.user_visible(user)
-      dataset.where(user_visibility_filter(user))
-    end
+    class << self
+      def user_visible(user)
+        dataset.where(user_visibility_filter(user))
+      end
 
-    def self.user_visibility_filter(user)
-      {
-        space_guid: Space.dataset.join_table(:inner, :spaces_developers, space_id: :id, user_id: user.id).select(:spaces__guid).union(
-            Space.dataset.join_table(:inner, :spaces_managers, space_id: :id, user_id: user.id).select(:spaces__guid)
-          ).union(
-            Space.dataset.join_table(:inner, :spaces_auditors, space_id: :id, user_id: user.id).select(:spaces__guid)
-          ).union(
-            Space.dataset.join_table(:inner, :organizations_managers, organization_id: :organization_id, user_id: user.id).select(:spaces__guid)
+      def user_visibility_filter(user)
+        {
+          space_guid: space_guids_where_visible(user)
+        }
+      end
+
+      private
+
+      def space_guids_where_visible(user)
+        Space.join(:spaces_developers, space_id: :id, user_id: user.id).select(:spaces__guid)
+        .union(
+          Space.join(:spaces_managers, space_id: :id, user_id: user.id).select(:spaces__guid)
+        ).union(
+          Space.join(:spaces_auditors, space_id: :id, user_id: user.id).select(:spaces__guid)
+        ).union(
+          Space.join(:organizations_managers, organization_id: :organization_id, user_id: user.id).select(:spaces__guid)
         ).select(:space_guid)
-      }
+      end
     end
 
     private
