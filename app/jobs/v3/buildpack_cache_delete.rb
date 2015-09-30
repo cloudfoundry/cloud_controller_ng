96c2fe3c9fbd2fb9ae01bac1ea69339e0eb5881e
Zach Robinson <zrobinson@pivotal.io>
2015-09-29 17:30:30 -0700

    delete all buildpack caches for a v3 app when app is deleted
    [#95583828]
    
    Signed-off-by: Swetha Repakula <swetharepakula@pivotal.io>
    Signed-off-by: Zach Robinson <zrobinson@pivotal.io>

---
+++ app/jobs/v3/buildpack_cache_delete.rb
@@ -0,0 +1,29 @@
+module VCAP::CloudController
+  module Jobs
+    module V3
+      class BuildpackCacheDelete < VCAP::CloudController::Jobs::CCJob
+        attr_accessor :app_guid
+
+        def initialize(app_guid)
+          @app_guid = app_guid
+        end
+
+        def perform
+          logger = Steno.logger('cc.background')
+          logger.info("Attempting delete of all blobs matching key '#{app_guid}' from blobstore buildpack_cache_blobstore")
+
+          blobstore = CloudController::DependencyLocator.instance.public_send(:buildpack_cache_blobstore)
+          blobstore.delete_matching_key(/#{@app_guid}/)
+        end
+
+        def job_name_in_configuration
+          :buildpack_cache_delete
+        end
+
+        def max_attempts
+          3
+        end
+      end
+    end
+  end
+end
