a8de4956144bf5d15768f216e399d2659079f9de
Jonathan Berkhahn <jaberkha@us.ibm.com>
2015-10-02 11:30:14 -0700

    Bump CC aapi minor version to 2.38
    [#104696720]
    
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- lib/cloud_controller/constants.rb
+++ lib/cloud_controller/constants.rb
@@ -1,5 +1,5 @@
 module VCAP::CloudController
   class Constants
-    API_VERSION = '2.37.0'.freeze
+    API_VERSION = '2.38.0'.freeze
   end
 end
--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -5,7 +5,7 @@ describe 'Stable API warning system', api_version_check: true do
   API_FOLDER_CHECKSUM = '6f0af85015e8d00ef6dbef8f1d99d393676aeed9'
 
   it 'double-checks the version' do
-    expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.37.0')
+    expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.38.0')
   end
 
   it 'tells the developer if the API specs change' do
