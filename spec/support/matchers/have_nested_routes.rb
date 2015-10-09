9961e4ecfa2873de47745215ba12c13ca76553e7
Michael Smykowski <msmykowski@pivotal.io>
2015-10-09 14:06:30 -0700

    Fix nested route matcher
    - Matcher now checks for actual routes not in expected routes as well as
      expected routes not in actual routes
    
    Signed-off-by: Utako Ueda <uueda@pivotal.io>
    Signed-off-by: Michael Smykowski <msmykowski@pivotal.io>

--- spec/support/matchers/have_nested_routes.rb
+++ spec/support/matchers/have_nested_routes.rb
@@ -1,27 +1,32 @@
 RSpec::Matchers.define :have_nested_routes do |association_routes, options={}|
   errors = []
-
   match do |controller|
-    to_manies = controller.send(:to_many_relationships)
+    actual_to_manies = controller.send(:to_many_relationships)
+    expected_to_manies = association_routes
 
-    to_manies.each do |association_name, attr|
-      expected_routes = Array(association_routes[association_name])
+    actual_to_manies.each do |association_name, attr|
+      expected_actions_for_association = Array(expected_to_manies[association_name])
 
-      extra_routes   = Array(attr.route_for) - expected_routes
-      missing_routes = expected_routes - Array(attr.route_for)
+      actual_actions = Array(attr.route_for)
+      extra_actions   = actual_actions - expected_actions_for_association
+      missing_actions = expected_actions_for_association - actual_actions
 
-      error_root_path = "#{controller.path_guid}/#{association_name}"
-      error_full_path = "#{error_root_path}/:guid"
+      handle_errors(extra_actions, missing_actions, controller, association_name, errors)
+    end
 
-      extra_routes.each do |route|
-        error_path = route == :get ? error_root_path : error_full_path
-        should_not_exist_error(error_path, errors)
+    expected_to_manies.each do |association_name, expected_actions|
+      missing_route = association_name unless actual_to_manies.include?(association_name)
+
+      if missing_route
+        route_should_exist_error(missing_route, errors)
+      else
+        actual_actions = Array(actual_to_manies[association_name].route_for)
+        extra_actions   = expected_actions - actual_actions
+        missing_actions = actual_actions - expected_actions
+
+        handle_errors(missing_actions, extra_actions, controller, association_name, errors)
       end
 
-      missing_routes.each do |route|
-        error_path = route == :get ? error_root_path : error_full_path
-        should_exist_error(error_path, errors)
-      end
     end
 
     errors.length == 0
@@ -31,11 +36,34 @@ RSpec::Matchers.define :have_nested_routes do |association_routes, options={}|
     errors.join("\n")
   end
 
-  def should_exist_error(path, errors)
+  private
+
+  def handle_errors(actions_that_should_not_exist, actions_that_should_exist, controller, association_name, errors)
+    error_root_path = "#{controller.path_guid}/#{association_name}"
+    error_full_path = "#{error_root_path}/:guid"
+
+    actions_that_should_not_exist.each do |action|
+      error_path = action == :get ? error_root_path : error_full_path
+      action_should_not_exist_error(error_path, errors)
+    end
+
+    actions_that_should_exist.each do |action|
+      error_path = action == :get ? error_root_path : error_full_path
+      action_should_exist_error(error_path, errors)
+    end
+
+    errors
+  end
+
+  def route_should_exist_error(route, errors)
+    errors << "expected #{route} to exist"
+  end
+
+  def action_should_exist_error(path, errors)
     errors << "expected #{path} to exist"
   end
 
-  def should_not_exist_error(path, errors)
+  def action_should_not_exist_error(path, errors)
     errors << "expected #{path} not to exist"
   end
 end
