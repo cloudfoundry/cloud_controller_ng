f52d18369bd8fdfc86ecde4691f95919bb8458b6
Chris Piraino <cpiraino@pivotal.io>
2015-10-12 09:18:13 -0700

    Correct api version SHA
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'vcap/digester'
 
 describe 'Stable API warning system', api_version_check: true do
-  API_FOLDER_CHECKSUM = '42081385fe93cfeb395613183aa7f45be4225ff8'
+  API_FOLDER_CHECKSUM = 'd21a601b2f0ceabe23fe9bef021a7e830434ec85'
 
   it 'double-checks the version' do
     expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.40.0')
