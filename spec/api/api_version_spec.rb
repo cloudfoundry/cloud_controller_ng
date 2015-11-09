04565d697522f298d192ae33034dfe2ef5a1aec0
Kira Combs <kcombs@pivotal.io>
2015-11-09 10:29:46 -0800

    Bump to version 2.43.0
    [#107463634]
    
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -5,7 +5,7 @@ describe 'Stable API warning system', api_version_check: true do
   API_FOLDER_CHECKSUM = 'a7ea8c1392e7e17427307f0f8535259b59886e5a'
 
   it 'double-checks the version' do
-    expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.42.0')
+    expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.43.0')
   end
 
   it 'tells the developer if the API specs change' do
