50c1360342c044c2514e3a2fb8781f17be7c92d2
Swetha Repakula <srepaku@us.ibm.com>
2015-11-11 16:27:48 -0800

    allow console to properly load
    Signed-off-by: Zach Robinson <zrobinson@pivotal.io>

--- lib/cloud_controller/controllers.rb
+++ lib/cloud_controller/controllers.rb
@@ -1,4 +1,5 @@
 require 'cloud_controller/rest_controller'
+require 'controllers/v3/application_controller'
 
 Dir[File.expand_path('../../../app/controllers/**/*.rb', __FILE__)].each do |file|
   require file
