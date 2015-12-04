3866897942da69737c48372304ffb3fe9a637ed5
Swetha Repakula <srepaku@us.ibm.com>
2015-12-04 14:24:32 -0800

    Bump CC API minor version to 2.45.0
    [#109503490]

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -5,7 +5,7 @@ describe 'Stable API warning system', api_version_check: true do
   API_FOLDER_CHECKSUM = '238c693bec6b1edbcbfe6cc55a43e5c20dc9c40d'
 
   it 'double-checks the version' do
-    expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.44.0')
+    expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.45.0')
   end
 
   it 'tells the developer if the API specs change' do
