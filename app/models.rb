70e52dc6b5f62a3bb4cb978b9378787004428d99
Iryna Shustava <ishustava@pivotal.io>
2016-01-12 15:45:23 -0800

    Revert "Add route_mapping model"
    This reverts commit e68983876d51567aec81e841a6cd7a2bff3e1e80.
    
    Signed-off-by: Chris Piraino <cpiraino@pivotal.io>

--- app/models.rb
+++ app/models.rb
@@ -42,7 +42,6 @@ require 'models/runtime/space_quota_definition'
 require 'models/runtime/stack'
 require 'models/runtime/user'
 require 'models/runtime/locking'
-require 'models/runtime/route_mapping'
 
 require 'models/services/service'
 require 'models/services/service_binding'
