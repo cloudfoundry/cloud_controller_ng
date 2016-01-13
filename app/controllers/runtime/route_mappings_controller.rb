cbfc45b1682f2b3b6f28940d4bb76e03d3d3af6d
Atul Kshirsagar <atul.kshirsagar@gmail.com>
2016-01-13 10:34:22 -0800

    Revert "Revert "Add validations when creating route mappings""
    This reverts commit 4997438cea711e8a19b8045d9aa936f898ff7574.

--- app/controllers/runtime/route_mappings_controller.rb
+++ app/controllers/runtime/route_mappings_controller.rb
@@ -1,12 +1,15 @@
 module VCAP::CloudController
   class RouteMappingsController < RestController::ModelController
+    class ValidationError < StandardError
+    end
+    class RouteMappingTaken < ValidationError
+    end
+
     define_attributes do
       to_one :app, exclude_in: [:update]
       to_one :route, exclude_in: [:update]
       attribute :app_port, Integer, default: nil
     end
-
-    post path, :create
 
     def self.translate_validation_exception(e, attributes)
       port_errors = e.errors.on(:app_port)
@@ -17,6 +20,40 @@ module VCAP::CloudController
       end
     end
 
+    def before_create
+      super
+      app_guid = request_attrs['app_guid']
+      route_guid = request_attrs['route_guid']
+      app_port = get_app_port(app_guid)
+      validate_route_mapping(app_guid, app_port, route_guid)
+    rescue RouteMappingsController::ValidationError => e
+      raise Errors::ApiError.new_from_details(e.class.name.demodulize, e.message)
+    end
+
+    def get_app_port(app_guid)
+      app_port = request_attrs['app_port']
+      if app_port.blank?
+        app = App.find(guid: app_guid)
+        if !app.nil?
+          return app.ports[0] unless app.ports.blank?
+        end
+      end
+
+      app_port
+    end
+
+    def validate_route_mapping(app_guid, app_port, route_guid)
+      mappings = RouteMapping.dataset.select_all(RouteMapping.table_name).
+        join(App.table_name, id: :app_id).
+        join(Route.table_name, id: :route_mappings__route_id).
+        where(:"#{RouteMapping.table_name}__app_port" => request_attrs['app_port'],
+              :"#{App.table_name}__guid" => request_attrs['app_guid'],
+              :"#{Route.table_name}__guid" => request_attrs['route_guid'])
+      raise RouteMappingTaken.new("Route #{route_guid} mapped to app #{app_guid} with
+                                   port #{app_port}") unless mappings.count == 0
+    end
+
     define_messages
+    define_routes
   end
 end
--- spec/unit/controllers/runtime/route_mappings_controller_spec.rb
+++ spec/unit/controllers/runtime/route_mappings_controller_spec.rb
@@ -3,10 +3,6 @@ require 'spec_helper'
 module VCAP::CloudController
   describe VCAP::CloudController::RouteMappingsController do
     describe 'Route Mappings' do
-      describe 'Permissions' do
-        it 'does permission things'
-      end
-
       describe 'POST /v2/route_mappings' do
         let(:route) { Route.make }
         let(:app_obj) { AppFactory.make(space: space) }
@@ -47,20 +43,88 @@ module VCAP::CloudController
 
         context 'when the app is a diego app' do
           let(:app_obj) { AppFactory.make(space: space, diego: true, ports: [8080, 9090]) }
+          let(:body) do
+            {
+              app_guid: app_obj.guid,
+              route_guid: route.guid
+            }.to_json
+          end
 
           context 'and no app port is specified' do
-            let(:body) do
-              {
-                app_guid: app_obj.guid,
-                route_guid: route.guid
-              }.to_json
-            end
-
             it 'uses the first port in the list of app ports' do
               post '/v2/route_mappings', body, headers_for(developer)
 
               expect(last_response).to have_status_code(201)
               expect(decoded_response['entity']['app_port']).to eq(8080)
+            end
+
+            context 'when another mapping with the same port already exists' do
+              it 'does not create another route mapping' do
+                post '/v2/route_mappings', body, headers_for(developer)
+                expect(last_response).to have_status_code(201)
+                expect(decoded_response['entity']['app_port']).to eq(8080)
+
+                post '/v2/route_mappings', body, headers_for(developer)
+                expect(last_response).to have_status_code(400)
+                expect(decoded_response['code']).to eq(210006)
+              end
+            end
+          end
+
+          context 'and there is another app already bound to the specified route' do
+            let(:route_2) { Route.make(space: space) }
+            let(:body_2) do
+              {
+                app_guid: app_obj.guid,
+                route_guid: route_2.guid
+              }.to_json
+            end
+
+            before do
+              post '/v2/route_mappings', body_2, headers_for(developer)
+              expect(last_response).to have_status_code(201)
+              expect(decoded_response['entity']['app_port']).to eq(8080)
+            end
+
+            it 'still makes a route mapping from the app to the route' do
+              post '/v2/route_mappings', body, headers_for(developer)
+              expect(last_response).to have_status_code(201)
+              expect(decoded_response['entity']['app_port']).to eq(8080)
+            end
+          end
+
+          context 'and the app is bound to another route' do
+            let(:app_obj_2) { AppFactory.make(space: space, diego: true, ports: [9090]) }
+            let(:body) do
+              {
+                app_guid: app_obj.guid,
+                route_guid: route.guid,
+                app_port: 9090
+              }.to_json
+            end
+            let(:body_2) do
+              {
+                app_guid: app_obj_2.guid,
+                route_guid: route.guid,
+                app_port: 9090
+              }.to_json
+            end
+
+            before do
+              post '/v2/route_mappings', body_2, headers_for(developer)
+              expect(last_response).to have_status_code(201)
+              expect(decoded_response['entity']['app_port']).to eq(9090)
+            end
+
+            it 'still makes a route mapping from the app to the route' do
+              post '/v2/route_mappings', body, headers_for(developer)
+              expect(last_response).to have_status_code(201)
+            end
+
+            it 'makes the route mapping even if the port number is the same' do
+              post '/v2/route_mappings', body, headers_for(developer)
+              expect(last_response).to have_status_code(201)
+              expect(decoded_response['entity']['app_port']).to eq(9090)
             end
           end
 
@@ -95,6 +159,53 @@ module VCAP::CloudController
 
               expect(last_response).to have_status_code(201)
               expect(decoded_response['entity']['app_port']).to eq(9090)
+            end
+
+            context 'when the same route mapping with the same port is specified' do
+              it 'does not create another route mapping' do
+                post '/v2/route_mappings', body, headers_for(developer)
+                expect(last_response).to have_status_code(201)
+                expect(decoded_response['entity']['app_port']).to eq(9090)
+
+                post '/v2/route_mappings', body, headers_for(developer)
+                expect(last_response).to have_status_code(400)
+                expect(decoded_response['code']).to eq(210006)
+              end
+            end
+
+            context 'when the same route mapping with the different port is specified' do
+              it 'creates another route mapping' do
+                post '/v2/route_mappings', body, headers_for(developer)
+                expect(last_response).to have_status_code(201)
+                expect(decoded_response['entity']['app_port']).to eq(9090)
+
+                body = {
+                    app_guid: app_obj.guid,
+                    route_guid: route.guid,
+                    app_port: 8080
+                  }.to_json
+                post '/v2/route_mappings', body, headers_for(developer)
+                expect(last_response).to have_status_code(201)
+                expect(decoded_response['entity']['app_port']).to eq(8080)
+              end
+            end
+          end
+
+          context 'and developer of different space is specified' do
+            let(:space1) { Space.make }
+            let(:developer) { make_developer_for_space(space1) }
+            let(:body) do
+              {
+                app_guid: app_obj.guid,
+                route_guid: route.guid,
+                app_port: 9090
+              }.to_json
+            end
+
+            it 'gets unauhtorized error' do
+              post '/v2/route_mappings', body, headers_for(developer)
+
+              expect(last_response).to have_status_code(403)
             end
           end
         end
