577f1a751736ad838bd3971825adcd291906aa02
Iryna Shustava <ishustava@pivotal.io>
2016-01-12 17:22:55 -0800

    Fix rubocop issues and api version spec
    Signed-off-by: Chris Piraino <cpiraino@pivotal.io>

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'vcap/digester'
 
 describe 'Stable API warning system', api_version_check: true do
-  API_FOLDER_CHECKSUM = 'c02ab486334be7a43334df83585823c01982ab0c'
+  API_FOLDER_CHECKSUM = '5f93b7f3df4bfdea2214562e2b9a72091a1bf248'
 
   it 'double-checks the version' do
     expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.47.0')
