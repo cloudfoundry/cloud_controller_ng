60c74bcfbf7e64b3c5ad093ea3a57c33f46cd921
Dan Wendorf <dwendorf@pivotal.io>
2016-01-28 10:56:59 -0800

    Bump API version to 2.49.0
    [finishes #112560755]

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -5,7 +5,7 @@ describe 'Stable API warning system', api_version_check: true do
   API_FOLDER_CHECKSUM = '055da58b6c04fb042774ac89190ceaf79d7d44a8'
 
   it 'double-checks the version' do
-    expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.48.0')
+    expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.49.0')
   end
 
   it 'tells the developer if the API specs change' do
