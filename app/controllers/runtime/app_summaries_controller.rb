799445a5db01f6ff5262b11ff101911f31b7982a
Frank Kotsianas <fkotsianas@pivotal.io>
2016-02-01 16:32:47 -0800

    stringify keys so that they merge safely in app_summaries_controller
    - previously some keys could be duplicated due to string != symbol
    
    Issue #524
    [#112428859]
    
    Signed-off-by: Zach Robinson <zrobinson@pivotal.io>

--- app/controllers/runtime/app_summaries_controller.rb
+++ app/controllers/runtime/app_summaries_controller.rb
@@ -12,13 +12,12 @@ module VCAP::CloudController
       app = find_guid_and_validate_access(:read, guid)
 
       app_info = {
-        guid: app.guid,
-        name: app.name,
-        routes: app.routes.map(&:as_summary_json),
-        running_instances: instances_reporters.number_of_starting_and_running_instances_for_app(app),
-        services: app.service_bindings.map { |service_binding| service_binding.service_instance.as_summary_json },
-        available_domains:
-          (app.space.organization.private_domains + SharedDomain.all).map(&:as_summary_json)
+        'guid'              => app.guid,
+        'name'              => app.name,
+        'routes'            => app.routes.map(&:as_summary_json),
+        'running_instances' => instances_reporters.number_of_starting_and_running_instances_for_app(app),
+        'services'          => app.service_bindings.map { |service_binding| service_binding.service_instance.as_summary_json },
+        'available_domains' => (app.space.organization.private_domains + SharedDomain.all).map(&:as_summary_json)
       }.merge(app.to_hash)
 
       MultiJson.dump(app_info)
