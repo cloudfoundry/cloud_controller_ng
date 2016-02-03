7c116aacf9ebac7c460c94ff6385aec332f1ace8
Zach Robinson <zrobinson@pivotal.io>
2016-02-03 10:58:17 -0800

    task request specs strictly check json response body
    - also use developer user instead of admin
    
    [#113006967]

--- spec/request/tasks_spec.rb
+++ spec/request/tasks_spec.rb
@@ -7,25 +7,27 @@ describe 'Tasks' do
   include ControllerHelpers
 
   def app
-    test_config = TestConfig.config
+    test_config     = TestConfig.config
     request_metrics = VCAP::CloudController::Metrics::RequestMetrics.new
     VCAP::CloudController::RackAppBuilder.new.build test_config, request_metrics
   end
 
   let(:space) { VCAP::CloudController::Space.make }
-  let!(:org) { space.organization }
-  let!(:user) { VCAP::CloudController::User.make }
+  let(:user) { VCAP::CloudController::User.make }
   let(:app_model) { VCAP::CloudController::AppModel.make(space_guid: space.guid) }
-  let!(:droplet) do
+  let(:droplet) do
     VCAP::CloudController::DropletModel.make(
-      app_guid: app_model.guid,
-      state: VCAP::CloudController::DropletModel::STAGED_STATE,
+      app_guid:     app_model.guid,
+      state:        VCAP::CloudController::DropletModel::STAGED_STATE,
       droplet_hash: 'droplet-hash'
     )
   end
+  let(:developer_headers) { headers_for(user) }
 
   before do
-    allow(ApplicationController).to receive(:configuration).and_return(TestConfig.config)
+    space.organization.add_user user
+    space.add_developer user
+
     stub_request(:post, 'http://nsync.service.cf.internal:8787/v1/tasks').to_return(status: 202)
 
     VCAP::CloudController::FeatureFlag.make(name: 'task_creation', enabled: true, error_message: nil)
@@ -41,62 +43,129 @@ describe 'Tasks' do
         command: 'be rake && true',
         environment_variables: {
           unicorn: 'magic'
-        }
+        },
+        memory_in_mb: 1234,
       }
-      post "/v3/apps/#{app_model.guid}/tasks", body, admin_headers
 
-      expect(last_response.status).to eq(202)
-      parsed_body = JSON.load(last_response.body)
+      post "/v3/apps/#{app_model.guid}/tasks", body, developer_headers
+
       guid = VCAP::CloudController::TaskModel.last.guid
 
-      expect(parsed_body['guid']).to eq(guid)
-      expect(parsed_body['name']).to eq('best task ever')
-      expect(parsed_body['command']).to eq('be rake && true')
-      expect(parsed_body['state']).to eq('RUNNING')
-      expect(parsed_body['memory_in_mb']).to eq(1024)
-      expect(parsed_body['result']).to eq({ 'failure_reason' => nil })
-      expect(parsed_body['environment_variables']).to eq({ 'unicorn' => 'magic' })
+      expected_response = {
+        'guid'                  => guid,
+        'name'                  => 'best task ever',
+        'command'               => 'be rake && true',
+        'state'                 => 'RUNNING',
+        'memory_in_mb'          => 1234,
+        'environment_variables' => { 'unicorn' => 'magic' },
+        'result'                => {
+          'failure_reason' => nil
+        },
+        'links'                 => {
+          'self'    => {
+            'href' => "/v3/tasks/#{guid}"
+          },
+          'app'     => {
+            'href' => "/v3/apps/#{app_model.guid}"
+          },
+          'droplet' => {
+            'href' => "/v3/droplets/#{droplet.guid}"
+          }
+        }
+      }
 
-      expect(parsed_body['links']['self']).to eq({ 'href' => "/v3/tasks/#{guid}" })
-      expect(parsed_body['links']['app']).to eq({ 'href' => "/v3/apps/#{app_model.guid}" })
-      expect(parsed_body['links']['droplet']).to eq({ 'href' => "/v3/droplets/#{droplet.guid}" })
+      parsed_response = JSON.load(last_response.body)
+
+      expect(last_response.status).to eq(202)
+      expect(parsed_response).to be_a_response_like(expected_response)
     end
   end
 
   describe 'GET /v3/tasks/:guid' do
     it 'returns a json representation of the task with the requested guid' do
-      task = VCAP::CloudController::TaskModel.make name: 'task', command: 'echo task', environment_variables: { unicorn: 'magic' }, app_guid: app_model.guid
+      task = VCAP::CloudController::TaskModel.make(
+        name: 'task',
+        command: 'echo task',
+        app_guid: app_model.guid,
+        droplet: app_model.droplet,
+        environment_variables: { unicorn: 'magic' },
+        memory_in_mb: 5,
+      )
       task_guid = task.guid
 
-      get "/v3/tasks/#{task_guid}", {}, admin_headers
+      get "/v3/tasks/#{task_guid}", nil, developer_headers
+
+      expected_response = {
+        'guid'                  => task_guid,
+        'name'                  => 'task',
+        'command'               => 'echo task',
+        'state'                 => 'RUNNING',
+        'memory_in_mb'          => 5,
+        'environment_variables' => { 'unicorn' => 'magic' },
+        'result'                => {
+          'failure_reason' => nil
+        },
+        'links'                 => {
+          'self'    => {
+            'href' => "/v3/tasks/#{task_guid}"
+          },
+          'app'     => {
+            'href' => "/v3/apps/#{app_model.guid}"
+          },
+          'droplet' => {
+            'href' => "/v3/droplets/#{app_model.droplet.guid}"
+          }
+        }
+      }
+
+      parsed_response = JSON.load(last_response.body)
 
       expect(last_response.status).to eq(200)
-      parsed_body = JSON.load(last_response.body)
-      expect(parsed_body['guid']).to eq(task_guid)
-      expect(parsed_body['name']).to eq('task')
-      expect(parsed_body['command']).to eq('echo task')
-      expect(parsed_body['state']).to eq('RUNNING')
-      expect(parsed_body['result']).to eq({ 'failure_reason' => nil })
-      expect(parsed_body['environment_variables']).to eq({ 'unicorn' => 'magic' })
+      expect(parsed_response).to be_a_response_like(expected_response)
     end
   end
 
   describe 'GET /v3/apps/:guid/tasks/:guid' do
     it 'returns a json representation of the task with the requested guid' do
-      app_guid = app_model.guid
-      task = VCAP::CloudController::TaskModel.make name: 'task', command: 'echo task', environment_variables: { unicorn: 'magic' }, app_guid: app_guid
-      task_guid = task.guid
+      task = VCAP::CloudController::TaskModel.make(
+        name:         'task',
+        command:      'echo task',
+        app:          app_model,
+        droplet:      app_model.droplet,
+        environment_variables: { unicorn: 'magic' },
+        memory_in_mb: 5,
+      )
+      guid = task.guid
 
-      get "/v3/apps/#{app_guid}/tasks/#{task_guid}", {}, admin_headers
+      get "/v3/apps/#{app_model.guid}/tasks/#{guid}", nil, developer_headers
+
+      expected_response = {
+        'guid'                  => guid,
+        'name'                  => 'task',
+        'command'               => 'echo task',
+        'state'                 => 'RUNNING',
+        'memory_in_mb'          => 5,
+        'environment_variables' => { 'unicorn' => 'magic' },
+        'result'                => {
+          'failure_reason' => nil
+        },
+        'links'                 => {
+          'self'    => {
+            'href' => "/v3/tasks/#{guid}"
+          },
+          'app'     => {
+            'href' => "/v3/apps/#{app_model.guid}"
+          },
+          'droplet' => {
+            'href' => "/v3/droplets/#{app_model.droplet.guid}"
+          }
+        }
+      }
+
+      parsed_response = JSON.load(last_response.body)
 
       expect(last_response.status).to eq(200)
-      parsed_body = JSON.load(last_response.body)
-      expect(parsed_body['guid']).to eq(task_guid)
-      expect(parsed_body['name']).to eq('task')
-      expect(parsed_body['command']).to eq('echo task')
-      expect(parsed_body['state']).to eq('RUNNING')
-      expect(parsed_body['result']).to eq({ 'failure_reason' => nil })
-      expect(parsed_body['environment_variables']).to eq({ 'unicorn' => 'magic' })
+      expect(parsed_response).to be_a_response_like(expected_response)
     end
   end
 end
