788d64fe0bddf714b3d6297ff4e5bd3764b68b46
Urvashi Reddy <ureddy@pivotal.io>
2016-02-11 10:02:48 -0800

    Rename boring test variable to something specific
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- spec/unit/access/service_instance_access_spec.rb
+++ spec/unit/access/service_instance_access_spec.rb
@@ -10,7 +10,7 @@ module VCAP::CloudController
     let(:space) { VCAP::CloudController::Space.make(organization: org) }
     let(:service) { VCAP::CloudController::Service.make }
     let(:service_plan) { VCAP::CloudController::ServicePlan.make(service: service) }
-    let(:object) { VCAP::CloudController::ManagedServiceInstance.make(service_plan: service_plan, space: space) }
+    let(:service_instance) { VCAP::CloudController::ManagedServiceInstance.make(service_plan: service_plan, space: space) }
 
     before do
       SecurityContext.set(user, token)
@@ -22,25 +22,27 @@ module VCAP::CloudController
 
     context 'admin' do
       include_context :admin_setup
-      it_behaves_like :full_access
+      it_behaves_like :full_access do
+        let(:object) { service_instance }
+      end
 
       context 'managed service instance' do
         it 'does not delegate to the ManagedServiceInstanceAccess' do
-          expect_any_instance_of(ManagedServiceInstanceAccess).not_to receive(:allowed?).with(object)
-          subject.create?(object)
-          subject.read_for_update?(object)
-          subject.update?(object)
+          expect_any_instance_of(ManagedServiceInstanceAccess).not_to receive(:allowed?).with(service_instance)
+          subject.create?(service_instance)
+          subject.read_for_update?(service_instance)
+          subject.update?(service_instance)
         end
       end
 
       context 'user provided service instance' do
-        let(:object) { VCAP::CloudController::UserProvidedServiceInstance.make(space: space) }
+        let(:service_instance) { VCAP::CloudController::UserProvidedServiceInstance.make(space: space) }
 
         it 'does not delegate to the UserProvidedServiceInstanceAccess' do
-          expect_any_instance_of(UserProvidedServiceInstanceAccess).not_to receive(:allowed?).with(object)
-          subject.create?(object)
-          subject.read_for_update?(object)
-          subject.update?(object)
+          expect_any_instance_of(UserProvidedServiceInstanceAccess).not_to receive(:allowed?).with(service_instance)
+          subject.create?(service_instance)
+          subject.read_for_update?(service_instance)
+          subject.update?(service_instance)
         end
       end
 
@@ -49,11 +51,13 @@ module VCAP::CloudController
           FeatureFlag.make(name: 'service_instance_creation', enabled: false, error_message: nil)
         end
 
-        it_behaves_like :full_access
+        it_behaves_like :full_access do
+          let(:object) { service_instance }
+        end
       end
 
       it 'returns true for purge' do
-        expect(subject).to allow_op_on_object(:purge, object)
+        expect(subject).to allow_op_on_object(:purge, service_instance)
       end
     end
 
@@ -62,19 +66,23 @@ module VCAP::CloudController
         org.add_user(user)
         space.add_developer(user)
       end
-      it_behaves_like :full_access
+      it_behaves_like :full_access do
+        let(:object) { service_instance }
+      end
 
       context 'when the organization is suspended' do
-        before { allow(object).to receive(:in_suspended_org?).and_return(true) }
-        it_behaves_like :read_only_access
+        before { allow(service_instance).to receive(:in_suspended_org?).and_return(true) }
+        it_behaves_like :read_only_access do
+          let(:object) { service_instance }
+        end
       end
 
       it 'allows the user to read the permissions of the service instance' do
-        expect(subject).to allow_op_on_object(:read_permissions, object)
+        expect(subject).to allow_op_on_object(:read_permissions, service_instance)
       end
 
       it 'returns false for purge' do
-        expect(subject).not_to allow_op_on_object(:purge, object)
+        expect(subject).not_to allow_op_on_object(:purge, service_instance)
       end
 
       context 'when the service_instance_creation feature flag is not set' do
@@ -83,7 +91,7 @@ module VCAP::CloudController
         end
 
         it 'allows all operations except create' do
-          expect { subject.create?(object) }.to raise_error(VCAP::Errors::ApiError, /service_instance_creation/)
+          expect { subject.create?(service_instance) }.to raise_error(VCAP::Errors::ApiError, /service_instance_creation/)
         end
       end
     end
@@ -94,53 +102,61 @@ module VCAP::CloudController
         space.add_auditor(user)
       end
 
-      it_behaves_like :read_only_access
+      it_behaves_like :read_only_access do
+        let(:object) { service_instance }
+      end
 
       it 'does not allow the user to read the permissions of the service instance' do
-        expect(subject).to_not allow_op_on_object(:read_permissions, object)
+        expect(subject).to_not allow_op_on_object(:read_permissions, service_instance)
       end
 
       it 'returns false for purge' do
-        expect(subject).not_to allow_op_on_object(:purge, object)
+        expect(subject).not_to allow_op_on_object(:purge, service_instance)
       end
     end
 
     context 'organization manager (defensive)' do
       before { org.add_manager(user) }
-      it_behaves_like :read_only_access
+      it_behaves_like :read_only_access do
+        let(:object) { service_instance }
+      end
 
       it 'does not allow the user to read the permissions of the service instance' do
-        expect(subject).to_not allow_op_on_object(:read_permissions, object)
+        expect(subject).to_not allow_op_on_object(:read_permissions, service_instance)
       end
 
       it 'returns false for purge' do
-        expect(subject).not_to allow_op_on_object(:purge, object)
+        expect(subject).not_to allow_op_on_object(:purge, service_instance)
       end
     end
 
     context 'organization billing manager (defensive)' do
       before { org.add_billing_manager(user) }
-      it_behaves_like :no_access
+      it_behaves_like :no_access do
+        let(:object) { service_instance }
+      end
 
       it 'does not allow the user to read the permissions of the service instance' do
-        expect(subject).to_not allow_op_on_object(:read_permissions, object)
+        expect(subject).to_not allow_op_on_object(:read_permissions, service_instance)
       end
 
       it 'returns false for purge' do
-        expect(subject).not_to allow_op_on_object(:purge, object)
+        expect(subject).not_to allow_op_on_object(:purge, service_instance)
       end
     end
 
     context 'organization auditor (defensive)' do
       before { org.add_auditor(user) }
-      it_behaves_like :no_access
+      it_behaves_like :no_access do
+        let(:object) { service_instance }
+      end
 
       it 'does not allow the user to read the permissions of the service instance' do
-        expect(subject).to_not allow_op_on_object(:read_permissions, object)
+        expect(subject).to_not allow_op_on_object(:read_permissions, service_instance)
       end
 
       it 'returns false for purge' do
-        expect(subject).not_to allow_op_on_object(:purge, object)
+        expect(subject).not_to allow_op_on_object(:purge, service_instance)
       end
     end
 
@@ -150,27 +166,31 @@ module VCAP::CloudController
         space.add_manager(user)
       end
 
-      it_behaves_like :read_only_access
+      it_behaves_like :read_only_access do
+        let(:object) { service_instance }
+      end
 
       it 'does not allow the user to read the permissions of the service instance' do
-        expect(subject).to_not allow_op_on_object(:read_permissions, object)
+        expect(subject).to_not allow_op_on_object(:read_permissions, service_instance)
       end
 
       it 'returns false for purge' do
-        expect(subject).not_to allow_op_on_object(:purge, object)
+        expect(subject).not_to allow_op_on_object(:purge, service_instance)
       end
     end
 
     context 'organization user (defensive)' do
       before { org.add_user(user) }
-      it_behaves_like :no_access
+      it_behaves_like :no_access do
+        let(:object) { service_instance }
+      end
 
       it 'does not allow the user to read the permissions of the service instance' do
-        expect(subject).to_not allow_op_on_object(:read_permissions, object)
+        expect(subject).to_not allow_op_on_object(:read_permissions, service_instance)
       end
 
       it 'returns false for purge' do
-        expect(subject).not_to allow_op_on_object(:purge, object)
+        expect(subject).not_to allow_op_on_object(:purge, service_instance)
       end
     end
 
@@ -180,14 +200,16 @@ module VCAP::CloudController
         different_organization.add_user(user)
       end
 
-      it_behaves_like :no_access
+      it_behaves_like :no_access do
+        let(:object) { service_instance }
+      end
 
       it 'does not allow the user to read the permissions of the service instance' do
-        expect(subject).to_not allow_op_on_object(:read_permissions, object)
+        expect(subject).to_not allow_op_on_object(:read_permissions, service_instance)
       end
 
       it 'returns false for purge' do
-        expect(subject).not_to allow_op_on_object(:purge, object)
+        expect(subject).not_to allow_op_on_object(:purge, service_instance)
       end
     end
 
@@ -197,10 +219,12 @@ module VCAP::CloudController
         different_organization.add_manager(user)
       end
 
-      it_behaves_like :no_access
+      it_behaves_like :no_access do
+        let(:object) { service_instance }
+      end
 
       it 'returns false for purge' do
-        expect(subject).not_to allow_op_on_object(:purge, object)
+        expect(subject).not_to allow_op_on_object(:purge, service_instance)
       end
     end
 
@@ -208,14 +232,16 @@ module VCAP::CloudController
       let(:user) { nil }
       let(:token) { { 'scope' => [] } }
 
-      it_behaves_like :no_access
+      it_behaves_like :no_access do
+        let(:object) { service_instance }
+      end
 
       it 'does not allow the user to read the permissions of the service instance' do
-        expect(subject).to_not allow_op_on_object(:read_permissions, object)
+        expect(subject).to_not allow_op_on_object(:read_permissions, service_instance)
       end
 
       it 'returns false for purge' do
-        expect(subject).not_to allow_op_on_object(:purge, object)
+        expect(subject).not_to allow_op_on_object(:purge, service_instance)
       end
     end
 
@@ -234,43 +260,49 @@ module VCAP::CloudController
 
       context 'with only cloud_controller.read scope' do
         let(:scope) { ['cloud_controller.read'] }
-        it_behaves_like :read_only_access
-        it { is_expected.to allow_op_on_object(:read_permissions, object) }
+        it_behaves_like :read_only_access do
+          let(:object) { service_instance }
+        end
+        it { is_expected.to allow_op_on_object(:read_permissions, service_instance) }
       end
 
       context 'with only cloud_controller_service_permissions.read scope' do
         let(:scope) { ['cloud_controller_service_permissions.read'] }
 
         it 'allows the user to read the permissions of the service instance' do
-          expect(subject).to allow_op_on_object(:read_permissions, object)
+          expect(subject).to allow_op_on_object(:read_permissions, service_instance)
         end
 
         # All other actions are disallowed
-        it_behaves_like :no_access
+        it_behaves_like :no_access do
+          let(:object) { service_instance }
+        end
       end
 
       context 'with no cloud_controller scopes' do
         let(:scope) { [] }
 
-        it_behaves_like :no_access
-        it { is_expected.not_to allow_op_on_object(:read_permissions, object) }
+        it_behaves_like :no_access do
+          let(:object) { service_instance }
+        end
+        it { is_expected.not_to allow_op_on_object(:read_permissions, service_instance) }
       end
     end
 
     describe '#allowed?' do
       context 'managed service instance' do
         it 'delegates to the ManagedServiceInstanceAccess' do
-          expect_any_instance_of(ManagedServiceInstanceAccess).to receive(:allowed?).with(object)
-          subject.allowed?(object)
+          expect_any_instance_of(ManagedServiceInstanceAccess).to receive(:allowed?).with(service_instance)
+          subject.allowed?(service_instance)
         end
       end
 
       context 'user provided service instance' do
-        let(:object) { VCAP::CloudController::UserProvidedServiceInstance.make(space: space) }
+        let(:service_instance) { VCAP::CloudController::UserProvidedServiceInstance.make(space: space) }
 
         it 'delegates to the UserProvidedServiceInstanceAccess' do
-          expect_any_instance_of(UserProvidedServiceInstanceAccess).to receive(:allowed?).with(object)
-          subject.allowed?(object)
+          expect_any_instance_of(UserProvidedServiceInstanceAccess).to receive(:allowed?).with(service_instance)
+          subject.allowed?(service_instance)
         end
       end
     end
