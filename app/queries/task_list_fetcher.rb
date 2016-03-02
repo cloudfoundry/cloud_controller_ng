5ca3af83be67d62ee1643ac8adf8e6fb513078a3
Kara Felix <kfelix@us.ibm.com>
2016-03-01 18:06:14 -0800

    Implement filtering on /v3/tasks and /v3/apps/:app_guid/tasks
    - code badly needs a refactor
    - /v3/apps/:app_guid/tasks still allows filtering by app_guids,
      space_guids, and organization_guids; it should not
    
    [#111472892]

--- app/queries/task_list_fetcher.rb
+++ app/queries/task_list_fetcher.rb
@@ -1,24 +1,56 @@
 module VCAP::CloudController
   class TaskListFetcher
-    def fetch(pagination_options, space_guids, app_guid)
+    def fetch(pagination_options, message, space_guids, app_guid)
+      # TODO: have dataset filter on space_guids
       dataset = TaskModel.select_all(:tasks)
-      filter(pagination_options, dataset, space_guids, app_guid)
+      filter(pagination_options, message, dataset, space_guids, app_guid)
     end
 
     private
 
-    def filter(pagination_options, dataset, space_guids, app_guid)
-      if space_guids || app_guid
-        dataset = dataset.join(AppModel.table_name, id: :app_id)
-      end
+    def filter(pagination_options, message, dataset, space_guids, app_guid)
+      app_dataset = nil
 
       if space_guids
-        dataset = dataset.where(:"#{AppModel.table_name}__space_guid" => space_guids)
+        app_dataset ||= AppModel.select(:id)
+        app_dataset = app_dataset.where(space_guid: space_guids)
       end
 
       if app_guid
-        dataset = dataset.where(:"#{AppModel.table_name}__guid" => app_guid)
+        app_dataset ||= AppModel.select(:id)
+        app_dataset = app_dataset.where(guid: app_guid)
       end
+
+      if message.requested?(:names)
+        dataset = dataset.where(name: message.names)
+      end
+
+      if message.requested?(:states)
+        dataset = dataset.where(state: message.states)
+      end
+
+      if message.requested?(:guids)
+        dataset = dataset.where(guid: message.guids)
+      end
+
+      if message.requested?(:app_guids)
+        app_dataset ||= AppModel.select(:id)
+        app_dataset = app_dataset.where(guid: message.app_guids)
+      end
+
+      if message.requested?(:space_guids)
+        app_dataset ||= AppModel.select(:id)
+        app_dataset = app_dataset.where(space_guid: message.space_guids)
+      end
+
+      if message.requested?(:organization_guids)
+        org_dataset = Organization.select(:id).where(guid: message.organization_guids)
+        space_dataset = Space.select(:guid).where(organization_id: org_dataset)
+        app_dataset ||= AppModel.select(:id)
+        app_dataset = app_dataset.where(space_guid: space_dataset)
+      end
+
+      dataset = dataset.where(app: app_dataset) if app_dataset
 
       SequelPaginator.new.get_page(dataset, pagination_options)
     end
