cd4f3dc5df0fdd59b4263385112e3598c52fe363
Kara Felix <kfelix@us.ibm.com>
2016-03-01 18:49:38 -0800

    Revert commit 5ca3af8 because it wasn't ready to be let into the wild

--- app/messages/tasks_list_message.rb
+++ app/messages/tasks_list_message.rb
@@ -2,18 +2,12 @@ require 'messages/base_message'
 
 module VCAP::CloudController
   class TasksListMessage < BaseMessage
-    ALLOWED_KEYS = [:names, :states, :guids, :app_guids, :organization_guids, :space_guids, :page, :per_page].freeze
+    ALLOWED_KEYS = [:page, :per_page].freeze
 
     attr_accessor(*ALLOWED_KEYS)
 
     validates_with NoAdditionalParamsValidator
 
-    validates :names, array: true, allow_nil: true
-    validates :states, array: true, allow_nil: true
-    validates :guids, array: true, allow_nil: true
-    validates :app_guids, array: true, allow_nil: true
-    validates :organization_guids, array: true, allow_nil: true
-    validates :space_guids, array: true, allow_nil: true
     validates_numericality_of :page, greater_than: 0, allow_nil: true, only_integer: true
     validates_numericality_of :per_page, greater_than: 0, allow_nil: true, only_integer: true
 
@@ -27,12 +21,6 @@ module VCAP::CloudController
 
     def self.from_params(params)
       opts = params.dup
-      to_array!(opts, 'names')
-      to_array!(opts, 'states')
-      to_array!(opts, 'guids')
-      to_array!(opts, 'app_guids')
-      to_array!(opts, 'organization_guids')
-      to_array!(opts, 'space_guids')
       new(opts.symbolize_keys)
     end
 
--- spec/unit/queries/task_list_fetcher_spec.rb
+++ spec/unit/queries/task_list_fetcher_spec.rb
@@ -10,38 +10,27 @@ module VCAP::CloudController
     let!(:task2_in_space1) { TaskModel.make(app_guid: app_in_space1.guid) }
     let!(:task_for_app2) { TaskModel.make(app_guid: app2_in_space1.guid) }
 
-    let(:space2) { Space.make }
-    let(:app_in_space2) { AppModel.make(space_guid: space2.guid) }
+    let(:app_in_space2) { AppModel.make }
     let!(:task_in_space2) { TaskModel.make(app_guid: app_in_space2.guid) }
-    let!(:failed_task_in_space2) { TaskModel.make(app_guid: app_in_space2.guid, state: TaskModel::FAILED_STATE) }
-
-    let(:org2) { Organization.make }
-    let(:space_in_org2) { Space.make(organization_guid: org2.guid) }
-    let(:app_in_org2) { AppModel.make(space_guid: space_in_org2.guid) }
-    let!(:task_in_org2) { TaskModel.make(app_guid: app_in_org2.guid) }
 
     let(:pagination_options) { PaginationOptions.new({}) }
-    let(:message) { TasksListMessage.new(filters) }
-    let(:filters) { {} }
     subject(:fetcher) { described_class.new }
 
-    results = nil
-
-    describe '#fetch' do
+    describe '#fetch_all' do
       it 'returns a PaginatedResult' do
-        results = fetcher.fetch(pagination_options, message, nil, nil)
+        results = fetcher.fetch(pagination_options, nil, nil)
         expect(results).to be_a(PaginatedResult)
       end
 
       it 'returns all of the tasks' do
-        results = fetcher.fetch(pagination_options, message, nil, nil).records
+        results = fetcher.fetch(pagination_options, nil, nil).records
 
-        expect(results).to match_array([task_in_space1, task_for_app2, task2_in_space1, task_in_space2, failed_task_in_space2, task_in_org2])
+        expect(results).to match_array([task_in_space1, task_for_app2, task2_in_space1, task_in_space2])
       end
 
       describe 'filtering by space' do
         it 'only returns tasks in those spaces' do
-          results = fetcher.fetch(pagination_options, message, [space1.guid], nil).records
+          results = fetcher.fetch(pagination_options, [space1.guid], nil).records
 
           expect(results).to match_array([task_in_space1, task2_in_space1, task_for_app2])
         end
@@ -49,70 +38,16 @@ module VCAP::CloudController
 
       describe 'filtering by app' do
         it 'only returns tasks for that app' do
-          results = fetcher.fetch(pagination_options, message, nil, app_in_space1.guid).records
+          results = fetcher.fetch(pagination_options, nil, app_in_space1.guid).records
 
           expect(results).to match_array([task_in_space1, task2_in_space1])
         end
       end
 
       it 'can filter by spaces and app' do
-        results = fetcher.fetch(pagination_options, message, [space1.guid], app_in_space1.guid).records
+        results = fetcher.fetch(pagination_options, [space1.guid], app_in_space1.guid).records
 
         expect(results).to match_array([task_in_space1, task2_in_space1])
-      end
-
-      describe 'filtering on message' do
-        before do
-          results = fetcher.fetch(pagination_options, message, nil, nil).records
-        end
-
-        context 'when task names are provided' do
-          let(:filters) { { names: [task_in_space1.name, task_in_space2.name] } }
-
-          it 'returns the correct set of tasks' do
-            expect(results).to match_array([task_in_space1, task_in_space2])
-          end
-        end
-
-        context 'when task states are provided' do
-          let(:filters) { { states: ['FAILED'] } }
-
-          it 'returns the correct set of tasks' do
-            expect(results).to match_array([failed_task_in_space2])
-          end
-        end
-
-        context 'when task guids are provided' do
-          let(:filters) { { guids: [task_in_space1.guid, task_in_space2.guid] } }
-
-          it 'returns the correct set of tasks' do
-            expect(results).to match_array([task_in_space1, task_in_space2])
-          end
-        end
-
-        context 'when app guids are provided' do
-          let(:filters) { { app_guids: [app2_in_space1.guid, app_in_space2.guid] } }
-
-          it 'returns the correct set of tasks' do
-            expect(results).to match_array([task_for_app2, failed_task_in_space2, task_in_space2])
-          end
-        end
-
-        context 'when space guids are provided' do
-          let(:filters) { { space_guids: [space2.guid] } }
-
-          it 'returns the correct set of tasks' do
-            expect(results).to match_array([failed_task_in_space2, task_in_space2])
-          end
-        end
-
-        context 'when org guids are provided' do
-          let(:filters) { { organization_guids: [org2.guid] } }
-
-          it 'returns the correct set of tasks' do
-            expect(results).to match_array([task_in_org2])
-          end
-        end
       end
     end
   end
