b2b3c1b7a57e308bf8138e54a5f8ca486857606a
Rizwan Reza <rreza@pivotal.io>
2016-03-02 16:45:24 -0800

    backfill task index request specs
    Signed-off-by: Zach Robinson <zrobinson@pivotal.io>

--- spec/request/tasks_spec.rb
+++ spec/request/tasks_spec.rb
@@ -36,49 +36,97 @@ describe 'Tasks' do
     app_model.save
   end
 
-  describe 'POST /v3/apps/:guid/tasks' do
-    it 'creates a task for an app with an assigned current droplet' do
-      body = {
-        name:                  'best task ever',
-        command:               'be rake && true',
+  describe 'GET /v3/tasks' do
+    it 'returns a paginated list of tasks' do
+      task1 = VCAP::CloudController::TaskModel.make(
+        name:                  'task one',
+        command:               'echo task',
+        app_guid:              app_model.guid,
+        droplet:               app_model.droplet,
         environment_variables: { unicorn: 'magic' },
-        memory_in_mb:          1234,
-      }
+        memory_in_mb:          5,
+      )
+      task2 = VCAP::CloudController::TaskModel.make(
+        name:         'task two',
+        command:      'echo task',
+        app_guid:     app_model.guid,
+        droplet:      app_model.droplet,
+        memory_in_mb: 100,
+      )
+      VCAP::CloudController::TaskModel.make(
+        app_guid: app_model.guid,
+        droplet:  app_model.droplet,
+      )
 
-      post "/v3/apps/#{app_model.guid}/tasks", body, developer_headers
+      get '/v3/tasks?per_page=2', nil, developer_headers
+
+      expected_response =
+        {
+          'pagination' => {
+            'total_results' => 3,
+            'first'         => { 'href' => '/v3/tasks?page=1&per_page=2' },
+            'last'          => { 'href' => '/v3/tasks?page=2&per_page=2' },
+            'next'          => { 'href' => '/v3/tasks?page=2&per_page=2' },
+            'previous'      => nil,
+          },
+          'resources' => [
+            {
+              'guid'                  => task1.guid,
+              'name'                  => 'task one',
+              'command'               => 'echo task',
+              'state'                 => 'RUNNING',
+              'memory_in_mb'          => 5,
+              'environment_variables' => { 'unicorn' => 'magic' },
+              'result'                => {
+                'failure_reason' => nil
+              },
+              'droplet_guid'          => task1.droplet.guid,
+              'created_at'            => iso8601,
+              'updated_at'            => nil,
+              'links'                 => {
+                'self' => {
+                  'href' => "/v3/tasks/#{task1.guid}"
+                },
+                'app' => {
+                  'href' => "/v3/apps/#{app_model.guid}"
+                },
+                'droplet' => {
+                  'href' => "/v3/droplets/#{app_model.droplet.guid}"
+                }
+              }
+            },
+            {
+              'guid'                  => task2.guid,
+              'name'                  => 'task two',
+              'command'               => 'echo task',
+              'state'                 => 'RUNNING',
+              'memory_in_mb'          => 100,
+              'environment_variables' => {},
+              'result'                => {
+                'failure_reason' => nil
+              },
+              'droplet_guid'          => task2.droplet.guid,
+              'created_at'            => iso8601,
+              'updated_at'            => nil,
+              'links'                 => {
+                'self' => {
+                  'href' => "/v3/tasks/#{task2.guid}"
+                },
+                'app' => {
+                  'href' => "/v3/apps/#{app_model.guid}"
+                },
+                'droplet' => {
+                  'href' => "/v3/droplets/#{app_model.droplet.guid}"
+                }
+              }
+            }
+          ]
+        }
 
       parsed_response = MultiJson.load(last_response.body)
-      guid            = parsed_response['guid']
 
-      expected_response = {
-        'guid'                  => guid,
-        'name'                  => 'best task ever',
-        'command'               => 'be rake && true',
-        'state'                 => 'RUNNING',
-        'memory_in_mb'          => 1234,
-        'environment_variables' => { 'unicorn' => 'magic' },
-        'result'                => {
-          'failure_reason' => nil
-        },
-        'droplet_guid'          => droplet.guid,
-        'created_at'            => iso8601,
-        'updated_at'            => iso8601,
-        'links'                 => {
-          'self' => {
-            'href' => "/v3/tasks/#{guid}"
-          },
-          'app' => {
-            'href' => "/v3/apps/#{app_model.guid}"
-          },
-          'droplet' => {
-            'href' => "/v3/droplets/#{droplet.guid}"
-          }
-        }
-      }
-
-      expect(last_response.status).to eq(202)
+      expect(last_response.status).to eq(200)
       expect(parsed_response).to be_a_response_like(expected_response)
-      expect(VCAP::CloudController::TaskModel.find(guid: guid)).to be_present
     end
   end
 
@@ -129,6 +177,166 @@ describe 'Tasks' do
     end
   end
 
+  describe 'PUT /v3/tasks/:guid/cancel' do
+    it 'returns a json representation of the task with the requested guid' do
+      task      = VCAP::CloudController::TaskModel.make name: 'task', command: 'echo task', environment_variables: { unicorn: 'magic' }, app_guid: app_model.guid
+      task_guid = task.guid
+
+      stub_request(:delete, "http://nsync.service.cf.internal:8787/v1/tasks/#{task_guid}").to_return(status: 202)
+
+      put "/v3/tasks/#{task_guid}/cancel", {}, admin_headers
+
+      expect(last_response.status).to eq(202)
+      parsed_body = JSON.load(last_response.body)
+      expect(parsed_body['guid']).to eq(task_guid)
+      expect(parsed_body['name']).to eq('task')
+      expect(parsed_body['command']).to eq('echo task')
+      expect(parsed_body['state']).to eq('CANCELING')
+      expect(parsed_body['result']).to eq({ 'failure_reason' => nil })
+      expect(parsed_body['environment_variables']).to eq({ 'unicorn' => 'magic' })
+    end
+  end
+
+  describe 'GET /v3/apps/:guid/tasks' do
+    it 'returns a paginated list of tasks' do
+      task1 = VCAP::CloudController::TaskModel.make(
+        name:                  'task one',
+        command:               'echo task',
+        app_guid:              app_model.guid,
+        droplet:               app_model.droplet,
+        environment_variables: { unicorn: 'magic' },
+        memory_in_mb:          5,
+      )
+      task2 = VCAP::CloudController::TaskModel.make(
+        name:         'task two',
+        command:      'echo task',
+        app_guid:     app_model.guid,
+        droplet:      app_model.droplet,
+        memory_in_mb: 100,
+      )
+      VCAP::CloudController::TaskModel.make(
+        app_guid: app_model.guid,
+        droplet:  app_model.droplet,
+      )
+
+      get "/v3/apps/#{app_model.guid}/tasks?per_page=2", nil, developer_headers
+
+      expected_response =
+        {
+          'pagination' => {
+            'total_results' => 3,
+            'first'         => { 'href' => "/v3/apps/#{app_model.guid}/tasks?page=1&per_page=2" },
+            'last'          => { 'href' => "/v3/apps/#{app_model.guid}/tasks?page=2&per_page=2" },
+            'next'          => { 'href' => "/v3/apps/#{app_model.guid}/tasks?page=2&per_page=2" },
+            'previous'      => nil,
+          },
+          'resources' => [
+            {
+              'guid'                  => task1.guid,
+              'name'                  => 'task one',
+              'command'               => 'echo task',
+              'state'                 => 'RUNNING',
+              'memory_in_mb'          => 5,
+              'environment_variables' => { 'unicorn' => 'magic' },
+              'result'                => {
+                'failure_reason' => nil
+              },
+              'droplet_guid'          => task1.droplet.guid,
+              'created_at'            => iso8601,
+              'updated_at'            => nil,
+              'links'                 => {
+                'self' => {
+                  'href' => "/v3/tasks/#{task1.guid}"
+                },
+                'app' => {
+                  'href' => "/v3/apps/#{app_model.guid}"
+                },
+                'droplet' => {
+                  'href' => "/v3/droplets/#{app_model.droplet.guid}"
+                }
+              }
+            },
+            {
+              'guid'                  => task2.guid,
+              'name'                  => 'task two',
+              'command'               => 'echo task',
+              'state'                 => 'RUNNING',
+              'memory_in_mb'          => 100,
+              'environment_variables' => {},
+              'result'                => {
+                'failure_reason' => nil
+              },
+              'droplet_guid'          => task2.droplet.guid,
+              'created_at'            => iso8601,
+              'updated_at'            => nil,
+              'links'                 => {
+                'self' => {
+                  'href' => "/v3/tasks/#{task2.guid}"
+                },
+                'app' => {
+                  'href' => "/v3/apps/#{app_model.guid}"
+                },
+                'droplet' => {
+                  'href' => "/v3/droplets/#{app_model.droplet.guid}"
+                }
+              }
+            }
+          ]
+        }
+
+      parsed_response = MultiJson.load(last_response.body)
+
+      expect(last_response.status).to eq(200)
+      expect(parsed_response).to be_a_response_like(expected_response)
+    end
+  end
+
+  describe 'POST /v3/apps/:guid/tasks' do
+    it 'creates a task for an app with an assigned current droplet' do
+      body = {
+        name:                  'best task ever',
+        command:               'be rake && true',
+        environment_variables: { unicorn: 'magic' },
+        memory_in_mb:          1234,
+      }
+
+      post "/v3/apps/#{app_model.guid}/tasks", body, developer_headers
+
+      parsed_response = MultiJson.load(last_response.body)
+      guid            = parsed_response['guid']
+
+      expected_response = {
+        'guid'                  => guid,
+        'name'                  => 'best task ever',
+        'command'               => 'be rake && true',
+        'state'                 => 'RUNNING',
+        'memory_in_mb'          => 1234,
+        'environment_variables' => { 'unicorn' => 'magic' },
+        'result'                => {
+          'failure_reason' => nil
+        },
+        'droplet_guid'          => droplet.guid,
+        'created_at'            => iso8601,
+        'updated_at'            => iso8601,
+        'links'                 => {
+          'self' => {
+            'href' => "/v3/tasks/#{guid}"
+          },
+          'app' => {
+            'href' => "/v3/apps/#{app_model.guid}"
+          },
+          'droplet' => {
+            'href' => "/v3/droplets/#{droplet.guid}"
+          }
+        }
+      }
+
+      expect(last_response.status).to eq(202)
+      expect(parsed_response).to be_a_response_like(expected_response)
+      expect(VCAP::CloudController::TaskModel.find(guid: guid)).to be_present
+    end
+  end
+
   describe 'GET /v3/apps/:guid/tasks/:guid' do
     it 'returns a json representation of the task with the requested guid' do
       task = VCAP::CloudController::TaskModel.make(
@@ -176,30 +384,10 @@ describe 'Tasks' do
     end
   end
 
-  describe 'PUT /v3/tasks/:guid/cancel' do
-    it 'returns a json representation of the task with the requested guid' do
-      task = VCAP::CloudController::TaskModel.make name: 'task', command: 'echo task', environment_variables: { unicorn: 'magic' }, app_guid: app_model.guid
-      task_guid = task.guid
-
-      stub_request(:delete, "http://nsync.service.cf.internal:8787/v1/tasks/#{task_guid}").to_return(status: 202)
-
-      put "/v3/tasks/#{task_guid}/cancel", {}, admin_headers
-
-      expect(last_response.status).to eq(202)
-      parsed_body = JSON.load(last_response.body)
-      expect(parsed_body['guid']).to eq(task_guid)
-      expect(parsed_body['name']).to eq('task')
-      expect(parsed_body['command']).to eq('echo task')
-      expect(parsed_body['state']).to eq('CANCELING')
-      expect(parsed_body['result']).to eq({ 'failure_reason' => nil })
-      expect(parsed_body['environment_variables']).to eq({ 'unicorn' => 'magic' })
-    end
-  end
-
   describe 'PUT /v3/apps/:guid/tasks/:guid/cancel' do
     it 'returns a json representation of the task with the requested guid' do
-      app_guid = app_model.guid
-      task = VCAP::CloudController::TaskModel.make name: 'task', command: 'echo task', environment_variables: { unicorn: 'magic' }, app_guid: app_guid
+      app_guid  = app_model.guid
+      task      = VCAP::CloudController::TaskModel.make name: 'task', command: 'echo task', environment_variables: { unicorn: 'magic' }, app_guid: app_guid
       task_guid = task.guid
 
       stub_request(:delete, "http://nsync.service.cf.internal:8787/v1/tasks/#{task_guid}").to_return(status: 202)
