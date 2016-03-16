2d3182363493d7e3cfe1c658ee5215b910ff31cc
Zach Robinson <zrobinson@pivotal.io>
2016-03-16 15:15:37 -0700

    scripts/which-cf.sh works with capi-release
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- scripts/which-cf.sh
+++ scripts/which-cf.sh
@@ -9,17 +9,22 @@ if [ $# == 0 ]; then
 fi
 
 CF_RELEASE_DIR=${CF_RELEASE_DIR:-~/workspace/cf-release}
+CAPI_RELEASE_DIR="${CF_RELEASE_DIR}/src/capi-release"
 CC_DIR="$(cd "$(dirname "$0")" && pwd)"
 SEARCH_SHA=$1
 
 declare -a branches=("origin/master" "origin/release-candidate" "origin/acceptance-deployed" "origin/runtime-passed" "origin/runtime-deployed" "origin/develop")
 
 function update_repos {
-  pushd $CF_RELEASE_DIR > /dev/null
+  pushd ${CF_RELEASE_DIR} > /dev/null
     git fetch
   popd > /dev/null
 
-  pushd $CC_DIR > /dev/null
+  pushd ${CAPI_RELEASE_DIR} > /dev/null
+    git fetch
+  popd > /dev/null
+
+  pushd ${CC_DIR} > /dev/null
     git fetch
   popd > /dev/null
 }
@@ -27,26 +32,35 @@ function update_repos {
 function main {
   update_repos
 
+  local capi_release_sha
+  local cc_sha
+  local exists
+  local result
+
   for branch in "${branches[@]}"
   do
-    pushd $CF_RELEASE_DIR > /dev/null
-      cc_sha=$(git ls-tree $branch src/cloud_controller_ng | grep -E -o --color=never "[0-9a-f]{40}")
+    pushd ${CF_RELEASE_DIR} > /dev/null
+      capi_release_sha=$(git ls-tree ${branch} src/capi-release | grep -E -o --color=never "[0-9a-f]{40}")
     popd > /dev/null
 
-    pushd $CC_DIR > /dev/null
+    pushd ${CAPI_RELEASE_DIR} > /dev/null
+      cc_sha=$(git ls-tree ${capi_release_sha} src/cloud_controller_ng | grep -E -o --color=never "[0-9a-f]{40}")
+    popd > /dev/null
+
+    pushd ${CC_DIR} > /dev/null
       set +e
-      git merge-base --is-ancestor $SEARCH_SHA $cc_sha
+      git merge-base --is-ancestor ${SEARCH_SHA} ${cc_sha}
       exists=$?
       set -e
     popd > /dev/null
 
-    if [[ $exists -eq 0 ]]; then
+    if [[ ${exists} -eq 0 ]]; then
       result="$(tput setaf 2)$(tput bold)found$(tput sgr0)"
     else
       result="$(tput setaf 3)$(tput bold)not found$(tput sgr0)"
     fi
 
-    printf "%-40s %s\n" "$(tput setaf 1)$branch:$(tput sgr0)" "$result"
+    printf "%-40s %s\n" "$(tput setaf 1)${branch}:$(tput sgr0)" "${result}"
   done
 }
 
