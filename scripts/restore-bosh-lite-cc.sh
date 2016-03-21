71796f22db79790e4315fc0f353e9c585e6edd79
Simon Leung <leungs@us.ibm.com>
2016-03-21 16:55:45 -0700

    finish up scripting for local cc on bosh-lite
    - use port 9022 so we have to change less on route registration
    - add blobstore domain to /etc/hosts
    - add script to start up local cc
    [#116049399]
    
    Signed-off-by: Zach Robinson <zrobinson@pivotal.io>

--- scripts/restore-bosh-lite-cc.sh
+++ scripts/restore-bosh-lite-cc.sh
@@ -2,7 +2,12 @@
 
 bosh ssh api_z1 0 <<'ENDSSH'
   sudo sed -i -- 's/192\.168\.50\.1/10\.244\.0\.138/g' '/var/vcap/jobs/route_registrar/config/registrar_settings.yml' && \
-  sudo sed -i -- 's/8181/9022/g' '/var/vcap/jobs/route_registrar/config/registrar_settings.yml' && \
   sudo /var/vcap/bosh/bin/monit start cloud_controller_ng && \
   sudo /var/vcap/bosh/bin/monit restart route_registrar
 ENDSSH
+
+line_number=$(cat /etc/hosts | grep -n "blobstore.service.cf.internal" | cut -d : -f 1)
+
+if [[ -n "${line_number}" ]]; then
+  sed "${line_number}d" /etc/hosts | sudo tee /etc/hosts > /dev/null
+fi
--- scripts/setup-local-cc.sh
+++ scripts/setup-local-cc.sh
@@ -2,7 +2,10 @@
 
 bosh ssh api_z1 0 <<'ENDSSH'
   sudo sed -i -- 's/10\.244\.0\.138/192\.168\.50\.1/g' '/var/vcap/jobs/route_registrar/config/registrar_settings.yml' && \
-  sudo sed -i -- 's/9022/8181/g' '/var/vcap/jobs/route_registrar/config/registrar_settings.yml' && \
   sudo /var/vcap/bosh/bin/monit stop cloud_controller_ng && \
   sudo /var/vcap/bosh/bin/monit restart route_registrar
 ENDSSH
+
+if ! cat /etc/hosts | grep "blobstore.service.cf.internal" > /dev/null; then
+  echo "10.244.0.130 blobstore.service.cf.internal" | sudo tee -a /etc/hosts > /dev/null
+fi
