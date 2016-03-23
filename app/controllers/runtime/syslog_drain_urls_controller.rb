b75a3fe123514131fbd1ea8e7ef7e09c8d9adeb5
Zach Robinson <zrobinson@pivotal.io>
2016-03-23 13:54:22 -0700

    refactor syslog drain query
    - use an aggregate function on the databse so we do not have to make n+1 queries
    - choose aggregate function based on database type
    
    [#113028583]
    
    Signed-off-by: Kara Felix <kfelix@us.ibm.com>

--- app/controllers/runtime/syslog_drain_urls_controller.rb
+++ app/controllers/runtime/syslog_drain_urls_controller.rb
@@ -1,8 +1,5 @@
 module VCAP::CloudController
   class SyslogDrainUrlsController < RestController::BaseController
-    V2_APP = 'v2'.freeze
-    V3_APP = 'v3'.freeze
-
     # Endpoint does its own basic auth
     allow_unauthenticated_access
 
@@ -13,21 +10,19 @@ module VCAP::CloudController
 
     get '/v2/syslog_drain_urls', :list
     def list
-      next_page_token = nil
-
-      guids_of_apps_with_drains =
+      guid_to_drain_maps =
         v2_apps_with_syslog_drains_dataset.
         union(v3_apps_with_syslog_drains_dataset, from_self: false, all: true).
         order(:guid).
         limit(batch_size).
         offset(last_id).
-        to_hash_groups(:app_version, :guid)
+        all
 
-      apps = v2_apps_from_guids(guids_of_apps_with_drains[V2_APP]) + v3_apps_from_guids(guids_of_apps_with_drains[V3_APP])
-
+      next_page_token = nil
       drain_urls = {}
-      apps.each do |app|
-        drain_urls[app.guid] = app.service_bindings.map(&:syslog_drain_url)
+
+      guid_to_drain_maps.each do |guid_and_drains|
+        drain_urls[guid_and_drains[:guid]] = guid_and_drains[:syslog_drain_urls].split(',')
         next_page_token = last_id + batch_size
       end
 
@@ -41,10 +36,10 @@ module VCAP::CloudController
         join(ServiceBinding.table_name, app_id: :id).
         where('syslog_drain_url IS NOT NULL').
         where("syslog_drain_url != ''").
-        distinct("#{App.table_name}__guid".to_sym).
+        group("#{App.table_name}__guid".to_sym).
         select(
           "#{App.table_name}__guid".to_sym,
-          Sequel.cast(V2_APP, String).as(:app_version)
+          aggregate_function("#{ServiceBinding.table_name}__syslog_drain_url".to_sym).as(:syslog_drain_urls)
         )
     end
 
@@ -53,21 +48,21 @@ module VCAP::CloudController
         join(ServiceBindingModel.table_name, app_id: :id).
         where('syslog_drain_url IS NOT NULL').
         where("syslog_drain_url != ''").
-        distinct("#{AppModel.table_name}__guid".to_sym).
+        group("#{AppModel.table_name}__guid".to_sym).
         select(
           "#{AppModel.table_name}__guid".to_sym,
-          Sequel.cast(V3_APP, String).as(:app_version)
+          aggregate_function("#{ServiceBindingModel.table_name}__syslog_drain_url".to_sym).as(:syslog_drain_urls)
         )
     end
 
-    def v2_apps_from_guids(guids)
-      App.where(guid: guids).
-        eager(service_bindings: proc { |ds| ds.where('syslog_drain_url IS NOT NULL').where("syslog_drain_url != ''") }).all
-    end
-
-    def v3_apps_from_guids(guids)
-      AppModel.where(guid: guids).
-        eager(service_bindings: proc { |ds| ds.where('syslog_drain_url IS NOT NULL').where("syslog_drain_url != ''") }).all
+    def aggregate_function(column)
+      if App.db.database_type == :postgres
+        Sequel.function(:string_agg, column, ',')
+      elsif App.db.database_type == :mysql
+        Sequel.function(:group_concat, column)
+      else
+        raise 'Unknown database type'
+      end
     end
 
     def last_id
