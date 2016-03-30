d5083e535f501cf324d037eca6128d0f4bdb51a2
Simon Leung <contact@simonleung.info>
2016-03-29 23:04:23 -0700

    Move v3/environment to new vcap_application builder
    [#113941537]

--- lib/cloud_controller/diego/v3/environment.rb
+++ lib/cloud_controller/diego/v3/environment.rb
@@ -13,6 +13,16 @@ module VCAP::CloudController
           app_env = @app.environment_variables || {}
           additional_variables ||= {}
 
+          vars_builder = VCAP::VarsBuilder.new(
+            @app,
+            memory_limit: @task.memory_in_mb,
+            disk_limit: default_disk_limit,
+            space: @space,
+            file_descriptors: Config.config[:instance_file_descriptor_limit] || 16384,
+            version: SecureRandom.uuid
+          )
+          vcap_application = vars_builder.vcap_application
+
           @initial_env.
             merge(app_env).
             merge(additional_variables.try(:stringify_keys)).
@@ -24,29 +34,6 @@ module VCAP::CloudController
         end
 
         private
-
-        def vcap_application
-          version = SecureRandom.uuid
-          uris = @app.routes.map(&:fqdn)
-
-          {
-            'limits' => {
-              'mem'  => @task.memory_in_mb,
-              'disk' => default_disk_limit,
-              'fds'  => Config.config[:instance_file_descriptor_limit] || 16384,
-            },
-            'application_id'      => @app.guid,
-            'application_version' => version,
-            'application_name'    => @app.name,
-            'application_uris'    => uris,
-            'version'             => version,
-            'name'                => @app.name,
-            'space_name'          => @space.name,
-            'space_id'            => @space.guid,
-            'uris'                => uris,
-            'users'               => nil
-          }
-        end
 
         def default_disk_limit
           Config.config[:default_app_disk_in_mb]
