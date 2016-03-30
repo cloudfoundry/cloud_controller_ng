8a49cbc430fd0cb90fada2c6e6a8089579ee1486
Simon Leung <contact@simonleung.info>
2016-03-29 23:01:01 -0700

    Move staging_environment_builder to new vcap_application builder
    [#113941537]

--- lib/cloud_controller/backends/staging_environment_builder.rb
+++ lib/cloud_controller/backends/staging_environment_builder.rb
@@ -1,3 +1,5 @@
+require_relative '../../vcap/vars_builder'
+
 module VCAP::CloudController
   class StagingEnvironmentBuilder
     def build(app, space, lifecycle, memory_limit, disk_limit, vars_from_message=nil)
@@ -5,40 +7,26 @@ module VCAP::CloudController
       vars_from_message ||= {}
       staging_var_group = EnvironmentVariableGroup.staging.environment_json
 
+      vars_builder = VCAP::VarsBuilder.new(
+        app,
+        memory_limit: memory_limit,
+        disk_limit: disk_limit,
+        space: space,
+        file_descriptors: Config.config[:instance_file_descriptor_limit] || 16384,
+        version: SecureRandom.uuid
+      )
+      vcap_application = vars_builder.vcap_application
+
       staging_var_group.
         merge(app_env).
         merge(vars_from_message.try(:stringify_keys)).
         merge(lifecycle.staging_environment_variables).
         merge(
           {
-            'VCAP_APPLICATION' => vcap_application(app, space, memory_limit, disk_limit),
+            'VCAP_APPLICATION' => vcap_application,
             'MEMORY_LIMIT'     => "#{memory_limit}m"
           }).
         merge(SystemEnvPresenter.new(app.service_bindings).system_env.stringify_keys)
-    end
-
-    private
-
-    def vcap_application(app, space, memory_limit, disk_limit)
-      version = SecureRandom.uuid
-      uris    = app.routes.map(&:fqdn)
-      {
-        'limits' => {
-          'mem'  => memory_limit,
-          'disk' => disk_limit,
-          'fds'  => Config.config[:instance_file_descriptor_limit] || 16384,
-        },
-        'application_id'      => app.guid,
-        'application_version' => version,
-        'application_name'    => app.name,
-        'application_uris'    => uris,
-        'version'             => version,
-        'name'                => app.name,
-        'space_name'          => space.name,
-        'space_id'            => space.guid,
-        'uris'                => uris,
-        'users'               => nil
-      }
     end
   end
 end
