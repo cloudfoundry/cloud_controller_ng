06ec4c93c791ea014dea2ca1b625d443bbf021d7
Kara Felix <kfelix@us.ibm.com>
2016-04-05 14:02:27 -0700

    Add default to space quota migration
    - addresses #570
    
    [#116542649]

--- db/migrations/20160221153023_add_total_service_keys_to_space_quota_definitions.rb
+++ db/migrations/20160221153023_add_total_service_keys_to_space_quota_definitions.rb
@@ -1,5 +1,5 @@
 Sequel.migration do
   change do
-    add_column :space_quota_definitions, :total_service_keys, Integer, null: false
+    add_column :space_quota_definitions, :total_service_keys, Integer, null: false, default: -1
   end
 end
