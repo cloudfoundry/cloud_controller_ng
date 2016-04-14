394e066bc01175a5b913b6bd8a56edf5453a975c
Dan Wendorf <dwendorf@pivotal.io>
2016-04-14 15:50:37 -0700

    Update app ssh controller spec to clarify user roles
    Signed-off-by: Eric Saxby <esaxby@pivotal.io>

--- spec/unit/controllers/runtime/apps_ssh_controller_spec.rb
+++ spec/unit/controllers/runtime/apps_ssh_controller_spec.rb
@@ -11,34 +11,17 @@ module VCAP::CloudController
     let(:space) { app_model.space }
 
     before do
-      space.organization.add_user(user)
-      space.add_developer(user)
-
-      set_current_user(user)
       allow(VCAP::CloudController::Config.config).to receive(:[]).with(anything).and_call_original
       allow(VCAP::CloudController::Config.config).to receive(:[]).with(:allow_app_ssh_access).and_return true
     end
 
     describe 'GET /internal/apps/:guid/ssh_access/:index' do
-      it 'returns a 200 and ProcessGuid' do
-        get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
-        expect(last_response.status).to eq(200)
-        expected_process_guid = VCAP::CloudController::Diego::ProcessGuid.from_app(app_model)
-        expect(decoded_response['process_guid']).to eq(expected_process_guid)
-      end
-
-      it 'creates an audit event recording this ssh access' do
-        expect {
-          get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
-        }.to change { Event.count }.by(1)
-        event = Event.last
-        expect(event.type).to eq('audit.app.ssh-authorized')
-        expect(event.actor).to eq(user.guid)
-        expect(event.metadata).to eq({ 'index' => instance_index })
-      end
-
-      context 'as an admin user' do
-        before { set_current_user(user, admin: true) }
+      context 'as a space developer' do
+        before do
+          space.organization.add_user(user)
+          space.add_developer(user)
+          set_current_user(user)
+        end
 
         it 'returns a 200 and ProcessGuid' do
           get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
@@ -54,6 +37,134 @@ module VCAP::CloudController
           event = Event.last
           expect(event.type).to eq('audit.app.ssh-authorized')
           expect(event.actor).to eq(user.guid)
+          expect(event.metadata).to eq({'index' => instance_index})
+        end
+
+        context 'when the app is not diego app' do
+          let(:diego) { false }
+
+          it 'returns a 400' do
+            get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
+            expect(last_response.status).to eq(400)
+          end
+
+          it 'creates an audit event recording this ssh failure' do
+            expect {
+              get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
+            }.to change { Event.count }.by(1)
+            event = Event.last
+            expect(event.type).to eq('audit.app.ssh-unauthorized')
+            expect(event.actor).to eq(user.guid)
+            expect(event.metadata).to eq({ 'index' => instance_index })
+          end
+        end
+
+        context 'when the app is does not allow ssh access' do
+          let(:enable_ssh) { false }
+
+          it 'returns a 400' do
+            get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
+            expect(last_response.status).to eq(400)
+          end
+
+          it 'creates an audit event recording this ssh failure' do
+            expect {
+              get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
+            }.to change { Event.count }.by(1)
+            event = Event.last
+            expect(event.type).to eq('audit.app.ssh-unauthorized')
+            expect(event.actor).to eq(user.guid)
+            expect(event.metadata).to eq({ 'index' => instance_index })
+          end
+        end
+
+        context 'when the app does not exists' do
+          context 'and the user has a valid auth token' do
+            it 'returns a 404' do
+              get '/internal/apps/does-not-exist/ssh_access/32914083940812934'
+              expect(last_response.status).to eq(404)
+            end
+          end
+
+          context 'and the user does not have a valid auth token' do
+            let(:user_with_invalid_token) { nil }
+
+            before { set_current_user(user_with_invalid_token) }
+
+            it 'returns a 401' do
+              expect {
+                get '/internal/apps/non-existant/ssh_access/324342'
+                expect(last_response.status).to eq(401)
+              }.not_to change { Event.count }
+            end
+          end
+        end
+
+        context 'when the space allow_ssh is set to false' do
+          before do
+            space.allow_ssh = false
+            space.save
+          end
+
+          it 'returns a 400' do
+            get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
+            expect(last_response.status).to eq(400)
+          end
+
+          it 'creates an audit event recording this ssh failure' do
+            expect {
+              get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
+            }.to change { Event.count }.by(1)
+            event = Event.last
+            expect(event.type).to eq('audit.app.ssh-unauthorized')
+            expect(event.actor).to eq(user.guid)
+            expect(event.metadata).to eq({ 'index' => instance_index })
+          end
+        end
+
+        context 'when the global allow_app_ssh_access is set to false' do
+          before do
+            allow(VCAP::CloudController::Config.config).to receive(:[]).with(:allow_app_ssh_access).and_return false
+          end
+
+          it 'returns a 400' do
+            get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
+            expect(last_response.status).to eq(400)
+          end
+
+          it 'creates an audit event recording this ssh failure' do
+            expect {
+              get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
+            }.to change { Event.count }.by(1)
+            event = Event.last
+            expect(event.type).to eq('audit.app.ssh-unauthorized')
+            expect(event.actor).to eq(user.guid)
+            expect(event.metadata).to eq({ 'index' => instance_index })
+          end
+        end
+      end
+
+      context 'as an admin user' do
+        let(:admin) { User.make }
+        before do
+          space.organization.add_user(admin)
+          set_current_user(admin, {admin: true})
+        end
+
+        it 'returns a 200 and ProcessGuid' do
+          get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
+          expect(last_response.status).to eq(200)
+          expected_process_guid = VCAP::CloudController::Diego::ProcessGuid.from_app(app_model)
+          expect(decoded_response['process_guid']).to eq(expected_process_guid)
+        end
+
+        it 'creates an audit event recording this ssh access' do
+          expect {
+            get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
+          }.to change { Event.count }.by(1)
+          event = Event.last
+          expect(event.type).to eq('audit.app.ssh-authorized')
+          expect(event.actor).to eq(admin.guid)
           expect(event.metadata).to eq({ 'index' => instance_index })
         end
       end
@@ -70,66 +181,6 @@ module VCAP::CloudController
         it 'returns a 403' do
           get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
           expect(last_response.status).to eq(403)
-        end
-      end
-
-      context 'when the app is not diego app' do
-        let(:diego) { false }
-
-        it 'returns a 400' do
-          get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
-          expect(last_response.status).to eq(400)
-        end
-
-        it 'creates an audit event recording this ssh failure' do
-          expect {
-            get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
-          }.to change { Event.count }.by(1)
-          event = Event.last
-          expect(event.type).to eq('audit.app.ssh-unauthorized')
-          expect(event.actor).to eq(user.guid)
-          expect(event.metadata).to eq({ 'index' => instance_index })
-        end
-      end
-
-      context 'when the app is does not allow ssh access' do
-        let(:enable_ssh) { false }
-
-        it 'returns a 400' do
-          get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
-          expect(last_response.status).to eq(400)
-        end
-
-        it 'creates an audit event recording this ssh failure' do
-          expect {
-            get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
-          }.to change { Event.count }.by(1)
-          event = Event.last
-          expect(event.type).to eq('audit.app.ssh-unauthorized')
-          expect(event.actor).to eq(user.guid)
-          expect(event.metadata).to eq({ 'index' => instance_index })
-        end
-      end
-
-      context 'when the app does not exists' do
-        context 'and the user has a valid auth token' do
-          it 'returns a 404' do
-            get '/internal/apps/does-not-exist/ssh_access/32914083940812934'
-            expect(last_response.status).to eq(404)
-          end
-        end
-
-        context 'and the user does not have a valid auth token' do
-          let(:user_with_invalid_token) { nil }
-
-          before { set_current_user(user_with_invalid_token) }
-
-          it 'returns a 401' do
-            expect {
-              get '/internal/apps/non-existant/ssh_access/324342'
-              expect(last_response.status).to eq(401)
-            }.not_to change { Event.count }
-          end
         end
       end
 
@@ -173,52 +224,15 @@ module VCAP::CloudController
           expect(event.metadata).to eq({ 'index' => instance_index })
         end
       end
-
-      context 'when the space allow_ssh is set to false' do
-        before do
-          space.allow_ssh = false
-          space.save
-        end
-
-        it 'returns a 400' do
-          get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
-          expect(last_response.status).to eq(400)
-        end
-
-        it 'creates an audit event recording this ssh failure' do
-          expect {
-            get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
-          }.to change { Event.count }.by(1)
-          event = Event.last
-          expect(event.type).to eq('audit.app.ssh-unauthorized')
-          expect(event.actor).to eq(user.guid)
-          expect(event.metadata).to eq({ 'index' => instance_index })
-        end
-      end
-
-      context 'when the global allow_app_ssh_access is set to false' do
-        before do
-          allow(VCAP::CloudController::Config.config).to receive(:[]).with(:allow_app_ssh_access).and_return false
-        end
-
-        it 'returns a 400' do
-          get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
-          expect(last_response.status).to eq(400)
-        end
-
-        it 'creates an audit event recording this ssh failure' do
-          expect {
-            get "/internal/apps/#{app_model.guid}/ssh_access/#{instance_index}"
-          }.to change { Event.count }.by(1)
-          event = Event.last
-          expect(event.type).to eq('audit.app.ssh-unauthorized')
-          expect(event.actor).to eq(user.guid)
-          expect(event.metadata).to eq({ 'index' => instance_index })
-        end
-      end
     end
 
     describe 'GET /internal/apps/:guid/ssh_access' do
+      before do
+        space.organization.add_user(user)
+        space.add_developer(user)
+        set_current_user(user)
+      end
+
       context 'when the user can access the app' do
         it 'creates an audit event recording this ssh access with an unknown index' do
           expect {
