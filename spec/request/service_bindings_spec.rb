033b389bf192672ac42a1ea048f6919e0398616a
Utako Ueda <uueda@pivotal.io>
2016-04-20 14:43:11 -0700

    made service_bindings_spec consistent with request specs
    [#113589723]
    
    Signed-off-by: Zach Robinson <zrobinson@pivotal.io>

--- spec/request/service_bindings_spec.rb
+++ spec/request/service_bindings_spec.rb
@@ -1,203 +1,190 @@
 require 'spec_helper'
 
 describe 'v3 service bindings' do
-  let(:user) { VCAP::CloudController::User.make }
+  let(:app_model) { VCAP::CloudController::AppModel.make }
+  let(:space) { app_model.space }
+  let(:user) { make_developer_for_space(space) }
+  let(:user_headers) { headers_for(user) }
 
-  let(:user_headers) do
-    json_headers(headers_for(user))
-  end
+  describe 'POST /v3/service_bindings' do
+    context 'managed service instance' do
+      let(:service_instance) { VCAP::CloudController::ManagedServiceInstance.make(space: space) }
 
-  let(:admin) do
-    json_headers(admin_headers)
-  end
+      before do
+        allow(VCAP::Services::ServiceBrokers::V2::Client).to receive(:new) do |*args, **kwargs, &block|
+          fb                  = FakeServiceBrokerV2Client.new(*args, **kwargs, &block)
+          fb.credentials      = { 'username' => 'managed_username' }
+          fb.syslog_drain_url = 'syslog://mydrain.example.com'
+          fb
+        end
+      end
 
-  let(:space_guid) do
-    post(
-      '/v2/organizations',
-      { 'name': 'development' }.to_json,
-      admin_headers
-    )
-    organization_guid = MultiJson.load(last_response.body)['metadata']['guid']
+      it 'creates a service binding' do
+        request_body = {
+          type:          'app',
+          data:          { parameters: { potato: 'tomato' } },
+          relationships: {
+            app:              { guid: app_model.guid },
+            service_instance: { guid: service_instance.guid },
+          }
+        }.to_json
 
-    put(
-      "/v2/organizations/#{organization_guid}/users/#{user.guid}",
-      nil,
-      admin_headers
-    )
+        post '/v3/service_bindings', request_body, json_headers(user_headers)
 
-    put(
-      "/v2/organizations/#{organization_guid}/managers/#{user.guid}",
-      nil,
-      admin_headers
-    )
+        parsed_response = MultiJson.load(last_response.body)
+        guid            = parsed_response['guid']
 
-    post(
-      '/v2/spaces',
-      {
-        name:              'development',
-        organization_guid: organization_guid
-      }.to_json,
-      user_headers
-    )
-    space_guid = MultiJson.load(last_response.body)['metadata']['guid']
-
-    put(
-      "/v2/spaces/#{space_guid}/developers/#{user.guid}",
-      nil,
-      user_headers
-    )
-
-    space_guid
-  end
-
-  let(:app_guid) do
-    post(
-      '/v3/apps',
-      {
-        name:          'my_app',
-        relationships: {
-          space: { guid: space_guid }
+        expected_response = {
+          'guid'       => guid,
+          'type'       => 'app',
+          'data'       => {
+            'credentials' => {
+              'username' => 'managed_username'
+            },
+            'syslog_drain_url' => 'syslog://mydrain.example.com'
+          },
+          'created_at' => iso8601,
+          'updated_at' => nil,
+          'links'      => {
+            'self'             => {
+              'href' => "/v3/service_bindings/#{guid}"
+            },
+            'service_instance' => {
+              'href' => "/v2/service_instances/#{service_instance.guid}"
+            },
+            'app' => {
+              'href' => "/v3/apps/#{app_model.guid}"
+            }
+          }
         }
-      }.to_json,
-      user_headers
-    )
 
-    MultiJson.load(last_response.body)['guid']
-  end
+        expect(last_response.status).to eq(201)
+        expect(parsed_response).to be_a_response_like(expected_response)
+        expect(VCAP::CloudController::ServiceBindingModel.find(guid: guid)).to be_present
 
-  describe 'when the service is managed' do
-    let(:service_instance_guid) do
-      post(
-        '/v2/service_brokers',
-        {
-          name:          'cool-runnings',
-          broker_url:    'https://example.com/foo/bar',
-          auth_username: 'utako',
-          auth_password: 'green'
-        }.to_json,
-        admin
-      )
-
-      get('/v2/service_plans', nil, admin_headers)
-      service_plan_guid = MultiJson.load(last_response.body)['resources'][0]['metadata']['guid']
-
-      put(
-        "/v2/service_plans/#{service_plan_guid}",
-        { 'public': true }.to_json,
-        admin
-      )
-
-      post(
-        '/v2/service_instances',
-        {
-          name:              'my-service-instance',
-          service_plan_guid: service_plan_guid,
-          space_guid:        space_guid
-        }.to_json,
-        user_headers
-      )
-
-      MultiJson.load(last_response.body)['metadata']['guid']
-    end
-
-    before do
-      allow(VCAP::Services::ServiceBrokers::V2::Client).to receive(:new) do |*args, **kwargs, &block|
-        fb                  = FakeServiceBrokerV2Client.new(*args, **kwargs, &block)
-        fb.credentials      = { 'username' => 'managed_username' }
-        fb.syslog_drain_url = 'syslog://mydrain.example.com'
-        fb
+        event = VCAP::CloudController::Event.last
+        expect(event.values).to match(
+          hash_including({
+            type:              'audit.service_binding.create',
+            actee:             guid,
+            actee_type:        'v3-service-binding',
+            actee_name:        '',
+            actor:             user.guid,
+            actor_type:        'user',
+            space_guid:        space.guid,
+            organization_guid: space.organization.guid
+          })
+        )
+        expect(event.metadata).to eq({
+          'request' => {
+            'type'          => 'app',
+            'relationships' => {
+              'app' => {
+                'guid' => app_model.guid,
+              },
+              'service_instance' => {
+                'guid' => service_instance.guid,
+              },
+            },
+            'data' => 'PRIVATE DATA HIDDEN'
+          }
+        })
       end
     end
 
-    it 'can be created' do
-      post(
-        '/v3/service_bindings',
-        {
+    context 'user provided service instance' do
+      let(:service_instance) do
+        VCAP::CloudController::UserProvidedServiceInstance.make(
+          space:            space,
+          credentials:      { 'username': 'user_provided_username' },
+          syslog_drain_url: 'syslog://drain.url.com'
+        )
+      end
+
+      it 'creates a service binding' do
+        request_body = {
           type:          'app',
           relationships: {
-            app:              { guid: app_guid },
-            service_instance: { guid: service_instance_guid },
-          },
-          data:          { parameters: { potato: 'tomato' } }
-        }.to_json,
-        user_headers
-      )
-
-      parsed_response = MultiJson.load(last_response.body)
-      guid            = parsed_response['guid']
-
-      expected_response = {
-        'guid'       => guid,
-        'type'       => 'app',
-        'data'       => {
-          'credentials' => {
-            'username' => 'managed_username'
-          },
-          'syslog_drain_url' => 'syslog://mydrain.example.com'
-        },
-        'created_at' => iso8601,
-        'updated_at' => nil,
-        'links'      => {
-          'self'             => {
-            'href' => "/v3/service_bindings/#{guid}"
-          },
-          'service_instance' => {
-            'href' => "/v2/service_instances/#{service_instance_guid}"
-          },
-          'app' => {
-            'href' => "/v3/apps/#{app_guid}"
+            app:              { guid: app_model.guid },
+            service_instance: { guid: service_instance.guid },
           }
-        }
-      }
+        }.to_json
 
-      expect(last_response.status).to eq(201)
-      expect(parsed_response).to be_a_response_like(expected_response)
-      expect(VCAP::CloudController::ServiceBindingModel.find(guid: guid)).to be_present
+        post '/v3/service_bindings', request_body, json_headers(user_headers)
 
-      space = VCAP::CloudController::Space.find(guid: space_guid)
+        parsed_response = MultiJson.load(last_response.body)
+        guid            = parsed_response['guid']
 
-      event = VCAP::CloudController::Event.last
-      expect(event.values).to match(
-        hash_including({
-          type:              'audit.service_binding.create',
-          actee:             guid,
-          actee_type:        'v3-service-binding',
-          actee_name:        '',
-          actor:             user.guid,
-          actor_type:        'user',
-          space_guid:        space.guid,
-          organization_guid: space.organization.guid
-        })
-      )
-      expect(event.metadata).to eq({
-        'request' => {
-          'type'          => 'app',
-          'relationships' => {
-            'app' => {
-              'guid' => app_guid,
+        expected_response = {
+          'guid'       => guid,
+          'type'       => 'app',
+          'data'       => {
+            'credentials' => {
+              'username' => 'user_provided_username'
+            },
+            'syslog_drain_url' => 'syslog://drain.url.com'
+          },
+          'created_at' => iso8601,
+          'updated_at' => nil,
+          'links'      => {
+            'self'             => {
+              'href' => "/v3/service_bindings/#{guid}"
             },
             'service_instance' => {
-              'guid' => service_instance_guid,
+              'href' => "/v2/service_instances/#{service_instance.guid}"
             },
-          },
-          'data' => 'PRIVATE DATA HIDDEN'
+            'app' => {
+              'href' => "/v3/apps/#{app_model.guid}"
+            }
+          }
         }
-      })
 
-      get(
-        "/v3/service_bindings/#{guid}",
-        nil,
-        user_headers
-      )
+        parsed_response = MultiJson.load(last_response.body)
 
-      expect(last_response.status).to eq(200)
-      expect(parsed_response).to be_a_response_like(expected_response)
+        expect(last_response.status).to eq(201)
+        expect(parsed_response).to be_a_response_like(expected_response)
+        expect(VCAP::CloudController::ServiceBindingModel.find(guid: guid)).to be_present
+      end
+    end
+  end
+
+  describe 'DELETE /v3/service_bindings/:guid' do
+    let(:service_binding) { VCAP::CloudController::ServiceBindingModel.make(service_instance: service_instance) }
+
+    context 'managed service instance' do
+      let(:service_instance) { VCAP::CloudController::ManagedServiceInstance.make(space: space) }
+
+      before do
+        allow(VCAP::Services::ServiceBrokers::V2::Client).to receive(:new) do |*args, **kwargs, &block|
+          FakeServiceBrokerV2Client.new(*args, **kwargs, &block)
+        end
+      end
+
+      it 'deletes the service binding and returns a 204' do
+        delete "/v3/service_bindings/#{service_binding.guid}", nil, user_headers
+
+        expect(last_response.status).to eq(204)
+        expect(service_binding.exists?).to be_falsey
+
+        event = VCAP::CloudController::Event.last
+        expect(event.values).to match(
+          hash_including({
+            type:              'audit.service_binding.delete',
+            actee:             service_binding.guid,
+            actee_type:        'v3-service-binding',
+            actee_name:        '',
+            actor:             user.guid,
+            actor_type:        'user',
+            space_guid:        space.guid,
+            organization_guid: space.organization.guid,
+            metadata:          '{}'
+          })
+        )
+      end
     end
 
-    describe 'DELETE /v3/service_bindings/:guid' do
-      let(:space) { VCAP::CloudController::Space.find(guid: space_guid) }
-      let(:service_instance) { VCAP::CloudController::ManagedServiceInstance.make(space: space) }
-      let!(:service_binding) { VCAP::CloudController::ServiceBindingModel.make(service_instance: service_instance) }
+    context 'user provided service instance' do
+      let(:service_instance) { VCAP::CloudController::UserProvidedServiceInstance.make(space: space) }
 
       it 'deletes the service binding and returns a 204' do
         delete "/v3/service_bindings/#{service_binding.guid}", nil, user_headers
@@ -223,109 +210,136 @@ describe 'v3 service bindings' do
     end
   end
 
-  describe 'when the service is user provided' do
-    let(:service_instance_guid) do
-      post(
-        '/v2/user_provided_service_instances',
-        {
-          name:             'test_ups',
-          space_guid:       space_guid,
-          credentials:      {
-            'username': 'user_provided_username'
-          },
-          syslog_drain_url: 'syslog://drain.url.com'
-
-        }.to_json,
-        user_headers
+  describe 'GET /v3/service_bindings/:guid' do
+    let(:service_instance) { VCAP::CloudController::ManagedServiceInstance.make(space: space) }
+    let(:service_binding) do
+      VCAP::CloudController::ServiceBindingModel.make(
+        service_instance: service_instance,
+        app:              app_model,
+        credentials:      { 'username' => 'managed_username' },
+        syslog_drain_url: 'syslog://mydrain.example.com',
       )
-
-      MultiJson.load(last_response.body)['metadata']['guid']
     end
 
-    it 'can be created' do
-      post(
-        '/v3/service_bindings',
-        {
-          type:          'app',
-          relationships: {
-            app:              { guid: app_guid },
-            service_instance: { guid: service_instance_guid },
-          }
-        }.to_json,
-        user_headers
-      )
+    it 'returns a service_binding' do
+      get "/v3/service_bindings/#{service_binding.guid}", nil, user_headers
 
       parsed_response = MultiJson.load(last_response.body)
-      guid            = parsed_response['guid']
 
       expected_response = {
-        'guid'       => guid,
+        'guid'       => service_binding.guid,
         'type'       => 'app',
         'data'       => {
           'credentials' => {
-            'username' => 'user_provided_username'
+            'username' => 'managed_username'
           },
-          'syslog_drain_url' => 'syslog://drain.url.com'
+          'syslog_drain_url' => 'syslog://mydrain.example.com'
         },
         'created_at' => iso8601,
         'updated_at' => nil,
         'links'      => {
           'self'             => {
-            'href' => "/v3/service_bindings/#{guid}"
+            'href' => "/v3/service_bindings/#{service_binding.guid}"
           },
           'service_instance' => {
-            'href' => "/v2/service_instances/#{service_instance_guid}"
+            'href' => "/v2/service_instances/#{service_instance.guid}"
           },
           'app' => {
-            'href' => "/v3/apps/#{app_guid}"
+            'href' => "/v3/apps/#{app_model.guid}"
           }
         }
       }
 
-      parsed_response = MultiJson.load(last_response.body)
-
-      expect(last_response.status).to eq(201)
+      expect(last_response.status).to eq(200)
       expect(parsed_response).to be_a_response_like(expected_response)
-      expect(VCAP::CloudController::ServiceBindingModel.find(guid: guid)).to be_present
+    end
+  end
 
-      get(
-        "/v3/service_bindings/#{guid}",
-        nil,
-        user_headers
+  describe 'GET /v3/service_bindings' do
+    let(:service_instance1) { VCAP::CloudController::ManagedServiceInstance.make(space: space) }
+    let(:service_instance2) { VCAP::CloudController::ManagedServiceInstance.make(space: space) }
+    let(:service_instance3) { VCAP::CloudController::ManagedServiceInstance.make(space: space) }
+
+    it 'returns a paginated list of service_bindings' do
+      service_binding1 = VCAP::CloudController::ServiceBindingModel.make(
+        service_instance: service_instance1,
+        app:              app_model,
+        credentials:      { 'binding1' => 'shtuff' },
+        syslog_drain_url: 'syslog://binding1.example.com',
       )
+      service_binding2 = VCAP::CloudController::ServiceBindingModel.make(
+        service_instance: service_instance2,
+        app:              app_model,
+        credentials:      { 'binding2' => 'things' },
+        syslog_drain_url: 'syslog://binding2.example.com',
+      )
+      VCAP::CloudController::ServiceBindingModel.make(service_instance: service_instance3, app: app_model)
+      VCAP::CloudController::ServiceBindingModel.make
+
+      get '/v3/service_bindings?per_page=2', nil, user_headers
+
+      expected_response = {
+        'pagination' => {
+          'total_results' => 3,
+          'first'         => { 'href' => '/v3/service_bindings?page=1&per_page=2' },
+          'last'          => { 'href' => '/v3/service_bindings?page=2&per_page=2' },
+          'next'          => { 'href' => '/v3/service_bindings?page=2&per_page=2' },
+          'previous'      => nil,
+        },
+        'resources' => [
+          {
+            'guid'       => service_binding1.guid,
+            'type'       => 'app',
+            'data'       => {
+              'credentials' => {
+                'binding1' => 'shtuff'
+              },
+              'syslog_drain_url' => 'syslog://binding1.example.com'
+            },
+            'created_at' => iso8601,
+            'updated_at' => nil,
+            'links'      => {
+              'self'             => {
+                'href' => "/v3/service_bindings/#{service_binding1.guid}"
+              },
+              'service_instance' => {
+                'href' => "/v2/service_instances/#{service_instance1.guid}"
+              },
+              'app' => {
+                'href' => "/v3/apps/#{app_model.guid}"
+              }
+            }
+          },
+          {
+            'guid'       => service_binding2.guid,
+            'type'       => 'app',
+            'data'       => {
+              'credentials' => {
+                'binding2' => 'things'
+              },
+              'syslog_drain_url' => 'syslog://binding2.example.com'
+            },
+            'created_at' => iso8601,
+            'updated_at' => nil,
+            'links'      => {
+              'self'             => {
+                'href' => "/v3/service_bindings/#{service_binding2.guid}"
+              },
+              'service_instance' => {
+                'href' => "/v2/service_instances/#{service_instance2.guid}"
+              },
+              'app' => {
+                'href' => "/v3/apps/#{app_model.guid}"
+              }
+            }
+          }
+        ]
+      }
 
       parsed_response = MultiJson.load(last_response.body)
 
       expect(last_response.status).to eq(200)
       expect(parsed_response).to be_a_response_like(expected_response)
-    end
-
-    describe 'DELETE /v3/service_bindings/:guid' do
-      let(:service_binding_guid) do
-        post(
-          '/v3/service_bindings',
-          {
-            type:          'app',
-            relationships: {
-              app:              { guid: app_guid },
-              service_instance: { guid: service_instance_guid },
-            }
-          }.to_json,
-          user_headers
-        )
-
-        MultiJson.load(last_response.body)['guid']
-      end
-
-      it 'deletes the service binding and returns a 204' do
-        delete "/v3/service_bindings/#{service_binding_guid}", {}, admin_headers
-
-        expect(last_response.status).to eq(204)
-
-        get "/v3/service_bindings/#{service_binding_guid}", {}, admin_headers
-
-        expect(last_response.status).to eq(404)
-      end
     end
   end
 end
