bec8904c917f695f4eb52c3116bd47c9c8f49920
Simon Leung <leungs@us.ibm.com>
2016-04-19 18:17:36 -0700

    Droplet delete and download event
    [#101936042]

--- app/repositories/runtime/droplet_event_repository.rb
+++ app/repositories/runtime/droplet_event_repository.rb
@@ -5,7 +5,10 @@ module VCAP::CloudController
         def self.record_dropet_create_by_staging(droplet, actor, actor_name, request_attrs, v3_app_name, space_guid, org_guid)
           Loggregator.emit(droplet.app_guid, "Creating droplet for app with guid #{droplet.app_guid}")
 
-          metadata = { request: request_attrs }
+          metadata = {
+            droplet_guid: droplet.guid,
+            request: request_attrs
+          }
 
           Event.create(
             type: 'audit.app.droplet.create',
@@ -16,9 +19,49 @@ module VCAP::CloudController
             actee_type: 'v3-app',
             actee_name: v3_app_name,
             timestamp: Sequel::CURRENT_TIMESTAMP,
+            metadata: metadata,
             space_guid: space_guid,
-            organization_guid: org_guid,
-            metadata: metadata
+            organization_guid: org_guid
+          )
+        end
+
+        def self.record_dropet_delete(droplet, actor_guid, actor_name, v3_app_name, space_guid, org_guid)
+          Loggregator.emit(droplet.app_guid, "Deleting droplet for app with guid #{droplet.app_guid}")
+
+          metadata = { droplet_guid: droplet.guid }
+
+          Event.create(
+            type: 'audit.app.droplet.delete',
+            actor: actor_guid,
+            actor_type: 'user',
+            actor_name: actor_name,
+            actee: droplet.app_guid,
+            actee_type: 'v3-app',
+            actee_name: v3_app_name,
+            timestamp: Sequel::CURRENT_TIMESTAMP,
+            metadata: metadata,
+            space_guid: space_guid,
+            organization_guid: org_guid
+          )
+        end
+
+        def self.record_dropet_download(droplet, actor, actor_name, v3_app_name, space_guid, org_guid)
+          Loggregator.emit(droplet.app_guid, "Downloading droplet for app with guid #{droplet.app_guid}")
+
+          metadata = { droplet_guid: droplet.guid }
+
+          Event.create(
+            type: 'audit.app.droplet.download',
+            actor: actor.guid,
+            actor_type: 'user',
+            actor_name: actor_name,
+            actee: droplet.app_guid,
+            actee_type: 'v3-app',
+            actee_name: v3_app_name,
+            timestamp: Sequel::CURRENT_TIMESTAMP,
+            metadata: metadata,
+            space_guid: space_guid,
+            organization_guid: org_guid
           )
         end
       end
--- spec/unit/repositories/runtime/droplet_event_repository_spec.rb
+++ spec/unit/repositories/runtime/droplet_event_repository_spec.rb
@@ -4,20 +4,20 @@ require 'repositories/runtime/droplet_event_repository'
 module VCAP::CloudController
   module Repositories::Runtime
     describe DropletEventRepository do
-      describe '#record_dropet_create_by_staging' do
-        let(:app) { AppModel.make }
-        let(:user) { User.make }
-        let(:package) { PackageModel.make(app_guid: app.guid) }
-        let(:droplet) { DropletModel.make(app_guid: app.guid) }
-        let(:email) { 'user-email' }
-        let(:request_attrs) do
-          {
-            'app_guid' => 'app-guid',
-            'type'     => 'docker',
-            'url'      => 'dockerurl.example.com'
-          }
-        end
+      let(:app) { AppModel.make }
+      let(:user) { User.make }
+      let(:package) { PackageModel.make(app_guid: app.guid) }
+      let(:droplet) { DropletModel.make(app_guid: app.guid) }
+      let(:email) { 'user-email' }
+      let(:request_attrs) do
+        {
+          'app_guid' => 'app-guid',
+          'type'     => 'docker',
+          'url'      => 'dockerurl.example.com'
+        }
+      end
 
+      describe '#record_dropet_create_by_staging' do
         it 'creates a new audit.app.droplet.create event' do
           event = DropletEventRepository.record_dropet_create_by_staging(droplet, user, email, request_attrs, app.name, package.space.guid, package.space.organization.guid)
           event.reload
@@ -34,8 +34,55 @@ module VCAP::CloudController
 
           expect(event.space_guid).to eq(app.space.guid)
 
+          droplet_guid = event.metadata.fetch('droplet_guid')
+          expect(droplet_guid).to eq(droplet.guid)
+
           request = event.metadata.fetch('request')
           expect(request).to eq(request_attrs)
+        end
+      end
+
+      describe '#record_dropet_delete' do
+        it 'creates a new audit.app.droplet.delete event' do
+          event = DropletEventRepository.record_dropet_delete(droplet, user.guid, email, app.name, package.space.guid, package.space.organization.guid)
+          event.reload
+
+          expect(event.type).to eq('audit.app.droplet.delete')
+
+          expect(event.actor).to eq(user.guid)
+          expect(event.actor_type).to eq('user')
+          expect(event.actor_name).to eq(email)
+
+          expect(event.actee).to eq(droplet.app_guid)
+          expect(event.actee_type).to eq('v3-app')
+          expect(event.actee_name).to eq(app.name)
+
+          expect(event.space_guid).to eq(app.space.guid)
+
+          droplet_guid = event.metadata.fetch('droplet_guid')
+          expect(droplet_guid).to eq(droplet.guid)
+        end
+      end
+
+      describe '#record_dropet_download' do
+        it 'creates a new audit.app.droplet.download event' do
+          event = DropletEventRepository.record_dropet_download(droplet, user, email, app.name, package.space.guid, package.space.organization.guid)
+          event.reload
+
+          expect(event.type).to eq('audit.app.droplet.download')
+
+          expect(event.actor).to eq(user.guid)
+          expect(event.actor_type).to eq('user')
+          expect(event.actor_name).to eq(email)
+
+          expect(event.actee).to eq(droplet.app_guid)
+          expect(event.actee_type).to eq('v3-app')
+          expect(event.actee_name).to eq(app.name)
+
+          expect(event.space_guid).to eq(app.space.guid)
+
+          droplet_guid = event.metadata.fetch('droplet_guid')
+          expect(droplet_guid).to eq(droplet.guid)
         end
       end
     end
