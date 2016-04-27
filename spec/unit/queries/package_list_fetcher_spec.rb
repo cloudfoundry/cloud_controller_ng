530f35de5ce13e0c03f8855a1df2bfecd16c2168
Simon Leung <leungs@us.ibm.com>
2016-04-27 16:17:44 -0700

    Refactor package_list_fetcher_spec
    [#101792648]
    
    Signed-off-by: Urvashi Reddy <ureddy@pivotal.io>

--- spec/unit/queries/package_list_fetcher_spec.rb
+++ spec/unit/queries/package_list_fetcher_spec.rb
@@ -3,197 +3,147 @@ require 'queries/package_list_fetcher'
 
 module VCAP::CloudController
   describe PackageListFetcher do
+
+    let(:space1) { Space.make }
+    let(:space2) { Space.make }
+    let(:app_in_space1) { AppModel.make(space_guid: space1.guid) }
+    let(:app2_in_space1) { AppModel.make(space_guid: space1.guid) }
+    let(:app3_in_space2) { AppModel.make(space_guid: space2.guid) }
+
+    let!(:package_in_space1) { PackageModel.make(app_guid: app_in_space1.guid, type: PackageModel::BITS_TYPE, state: PackageModel::FAILED_STATE) }
+    let!(:package2_in_space1) { PackageModel.make(app_guid: app_in_space1.guid, type: PackageModel::DOCKER_TYPE, state: PackageModel::READY_STATE) }
+
+    let!(:package_for_app2) { PackageModel.make(app_guid: app2_in_space1.guid, type: PackageModel::DOCKER_TYPE, state: PackageModel::CREATED_STATE) }
+    let!(:package_for_app3) { PackageModel.make(app_guid: app3_in_space2.guid, type: PackageModel::BITS_TYPE) }
+
     subject(:fetcher) { described_class.new }
     let(:message) { PackagesListMessage.new(filters) }
+
     let(:filters) { {} }
 
-    describe '#fetch_all' do
-      let(:app) { AppModel.make }
+    results = nil
 
-      it 'returns a PaginatedResult' do
+    describe '#fetch_all' do
+      before do
         results = fetcher.fetch_all(message: message)
+      end
+      it 'returns a PaginatedResult' do
         expect(results).to be_a(PaginatedResult)
       end
 
       it 'returns all of the packages' do
-        package1 = PackageModel.make
-        package2 = PackageModel.make
-        package3 = PackageModel.make
-
-        results = fetcher.fetch_all(message: message).records
-        expect(results).to match_array([package1, package2, package3])
+        expect(results.records).to match_array([package_in_space1, package2_in_space1, package_for_app2, package_for_app3])
       end
 
-      context 'filtering types' do
-        let(:filters) { { types: [PackageModel::BITS_TYPE] } }
-        let!(:package1) { PackageModel.make(app_guid: app.guid, type: PackageModel::BITS_TYPE) }
-        let!(:package2) { PackageModel.make(app_guid: app.guid, type: PackageModel::BITS_TYPE) }
+      describe 'filtering on messages' do
+        context 'filtering types' do
+          let(:filters) { { types: [PackageModel::BITS_TYPE] } }
 
-        before { PackageModel.make(app_guid: app.guid, type: PackageModel::DOCKER_TYPE) }
-
-        it 'returns all of the packages with the requested types' do
-          results = fetcher.fetch_all(message: message)
-          expect(results.records).to match_array([package1, package2])
+          it 'returns all of the packages with the requested types' do
+            expect(results.records).to match_array([package_in_space1, package_for_app3])
+          end
         end
-      end
 
-      context 'filtering states' do
-        let(:filters) { { states: [PackageModel::CREATED_STATE, PackageModel::READY_STATE] } }
-        let!(:package1) { PackageModel.make(app_guid: app.guid, state: PackageModel::CREATED_STATE) }
-        let!(:package2) { PackageModel.make(app_guid: app.guid, state: PackageModel::READY_STATE) }
+        context 'filtering states' do
+          let(:filters) { { states: [PackageModel::READY_STATE, PackageModel::FAILED_STATE] } }
 
-        before { PackageModel.make(app_guid: app.guid, state: PackageModel::FAILED_STATE) }
-
-        it 'returns all of the packages with the requested states' do
-          results = fetcher.fetch_all(message: message)
-          expect(results.records).to match_array([package1, package2])
+          it 'returns all of the packages with the requested states' do
+            expect(results.records).to match_array([package_in_space1, package2_in_space1])
+          end
         end
-      end
 
-      context 'filtering app guids' do
-        let(:app2) { AppModel.make }
-        let!(:package1) { PackageModel.make(app_guid: app.guid) }
-        let!(:package2) { PackageModel.make(app_guid: app2.guid) }
-        let(:filters) { { app_guids: [app.guid, app2.guid] } }
+        context 'filtering app guids' do
+          let(:filters) { { app_guids: [app_in_space1.guid, app3_in_space2.guid] } }
 
-        before { PackageModel.make }
-
-        it 'returns all the packages associated with the requested app guid' do
-          results = fetcher.fetch_all(message: message)
-          expect(results.records).to match_array([package1, package2])
+          it 'returns all the packages associated with the requested app guid' do
+            expect(results.records).to match_array([package_in_space1, package2_in_space1, package_for_app3])
+          end
         end
       end
     end
 
     describe '#fetch_for_spaces' do
-      let(:app) { AppModel.make }
-
+      before do
+        results = fetcher.fetch_for_spaces(message: message, space_guids: [space1.guid])
+      end
       it 'returns a PaginatedResult' do
-        results = fetcher.fetch_for_spaces(message: message, space_guids: [])
         expect(results).to be_a(PaginatedResult)
       end
 
       it 'returns only the packages in spaces requested' do
-        space1        = Space.make
-        app_in_space1 = AppModel.make(space: space1)
-        package1_in_space1 = PackageModel.make(app_guid: app_in_space1.guid)
-        package2_in_space1 = PackageModel.make(app_guid: app_in_space1.guid)
-
-        space2        = Space.make
-        app_in_space2 = AppModel.make(space: space2)
-        package1_in_space2 = PackageModel.make(app_guid: app_in_space2.guid)
-
-        PackageModel.make
-
-        results = fetcher.fetch_for_spaces(message: message, space_guids: [space1.guid, space2.guid]).records
-
-        expect(results).to match_array([package1_in_space1, package2_in_space1, package1_in_space2])
+        expect(results.records).to match_array([package_in_space1, package2_in_space1, package_for_app2])
       end
 
-      context 'filtering types' do
-        let(:filters) { { types: [PackageModel::BITS_TYPE] } }
-        let!(:package1) { PackageModel.make(app_guid: app.guid, type: PackageModel::BITS_TYPE) }
-        let!(:package2) { PackageModel.make(app_guid: app.guid, type: PackageModel::BITS_TYPE) }
+      describe 'filtering on messages' do
 
-        before do
-          PackageModel.make(app_guid: app.guid, type: PackageModel::DOCKER_TYPE)
-          PackageModel.make(type: PackageModel::BITS_TYPE)
+        context 'filtering types' do
+          let(:filters) { { types: [PackageModel::BITS_TYPE] } }
+
+          it 'returns all of the packages with the requested types' do
+            expect(results.records).to match_array([package_in_space1])
+          end
         end
 
-        it 'returns all of the packages with the requested types' do
-          results = fetcher.fetch_for_spaces(message: message, space_guids: [app.space_guid])
-          expect(results.records).to match_array([package1, package2])
-        end
-      end
+        context 'filtering states' do
+          let(:filters) { { states: [PackageModel::CREATED_STATE, PackageModel::READY_STATE] } }
 
-      context 'filtering states' do
-        let(:filters) { { states: [PackageModel::CREATED_STATE, PackageModel::READY_STATE] } }
-        let!(:package1) { PackageModel.make(app_guid: app.guid, state: PackageModel::CREATED_STATE) }
-        let!(:package2) { PackageModel.make(app_guid: app.guid, state: PackageModel::READY_STATE) }
-
-        before do
-          PackageModel.make(app_guid: app.guid, state: PackageModel::FAILED_STATE)
-          PackageModel.make(state: PackageModel::READY_STATE)
-          PackageModel.make
+          it 'returns all of the packages with the requested states' do
+            expect(results.records).to match_array([package2_in_space1, package_for_app2])
+          end
         end
 
-        it 'returns all of the packages with the requested states' do
-          results = fetcher.fetch_for_spaces(message: message, space_guids: [app.space_guid])
-          expect(results.records).to match_array([package1, package2])
-        end
-      end
+        context 'filtering app guids' do
+          let(:filters) { { app_guids: [app_in_space1.guid] } }
 
-      context 'filtering app guids' do
-        let(:app2) { AppModel.make }
-        let!(:package1) { PackageModel.make(app_guid: app.guid) }
-        let!(:package2) { PackageModel.make(app_guid: app2.guid) }
-        let(:filters) { { app_guids: [app.guid, app2.guid] } }
-
-        before { PackageModel.make }
-
-        it 'returns all the packages associated with the requested app guid' do
-          results = fetcher.fetch_for_spaces(message: message, space_guids: [app.space_guid, app2.space_guid])
-          expect(results.records).to match_array([package1, package2])
+          it 'returns all the packages associated with the requested app guid' do
+            expect(results.records).to match_array([package_in_space1, package2_in_space1])
+          end
         end
       end
     end
 
     describe '#fetch_for_app' do
-      let(:app) { AppModel.make }
-      let(:filters) { { app_guid: app.guid } }
+      returned_app = nil
+
+      before do
+        returned_app, results = fetcher.fetch_for_app(message: message)
+      end
+      let(:filters) { { app_guid: app_in_space1.guid } }
 
       it 'returns a PaginatedResult and the app' do
-        returned_app, results = fetcher.fetch_for_app(message: message)
         expect(results).to be_a(PaginatedResult)
-        expect(returned_app.guid).to eq(app.guid)
+        expect(returned_app.guid).to eq(app_in_space1.guid)
       end
 
       it 'returns only the packages for the app requested' do
-        package1 = PackageModel.make(app_guid: app.guid)
-        package2 = PackageModel.make(app_guid: app.guid)
-        PackageModel.make
-        PackageModel.make
-
-        _app, results = fetcher.fetch_for_app(message: message)
-
-        expect(results.records).to match_array([package1, package2])
+        expect(results.records).to match_array([package_in_space1, package2_in_space1])
       end
 
-      context 'filtering types' do
-        let(:filters) { { types: [PackageModel::BITS_TYPE], app_guid: app.guid } }
+      describe 'filtering on messages' do
 
-        it 'returns all of the packages with the requested types' do
-          package1 = PackageModel.make(app_guid: app.guid, type: PackageModel::BITS_TYPE)
-          package2 = PackageModel.make(app_guid: app.guid, type: PackageModel::BITS_TYPE)
-          PackageModel.make(app_guid: app.guid, type: PackageModel::DOCKER_TYPE)
-          PackageModel.make(type: PackageModel::BITS_TYPE)
+        context 'filtering types' do
+          let(:filters) { { types: [PackageModel::BITS_TYPE], app_guid: app_in_space1.guid } }
 
-          results = fetcher.fetch_for_app(message: message)
-          expect(results.last.records).to match_array([package1, package2])
+          it 'returns all of the packages with the requested types' do
+            puts results.inspect
+            expect(results.records).to match_array([package_in_space1])
+          end
         end
-      end
 
-      context 'filtering states' do
-        let(:filters) { { states: [PackageModel::CREATED_STATE, PackageModel::READY_STATE], app_guid: app.guid } }
+        context 'filtering states' do
+          let(:filters) { { states: [PackageModel::CREATED_STATE, PackageModel::READY_STATE], app_guid: app_in_space1.guid } }
 
-        it 'returns all of the packages with the requested states' do
-          package1 = PackageModel.make(app_guid: app.guid, state: PackageModel::CREATED_STATE)
-          package2 = PackageModel.make(app_guid: app.guid, state: PackageModel::READY_STATE)
-          PackageModel.make(app_guid: app.guid, state: PackageModel::FAILED_STATE)
-          PackageModel.make(state: PackageModel::READY_STATE)
-          PackageModel.make
-
-          results = fetcher.fetch_for_app(message: message)
-          expect(results.last.records).to match_array([package1, package2])
+          it 'returns all of the packages with the requested states' do
+            expect(results.records).to match_array([package2_in_space1])
+          end
         end
-      end
 
-      context 'when the app does not exist' do
-        let(:filters) { { app_guid: 'not a real guid' } }
-        it 'returns nil' do
-          returned_app, results = fetcher.fetch_for_app(message: message)
-          expect(results).to be_nil
-          expect(returned_app).to be_nil
+        context 'when the app does not exist' do
+          let(:filters) { { app_guid: 'not a real guid' } }
+          it 'returns nil' do
+            expect(results).to be_nil
+            expect(returned_app).to be_nil
+          end
         end
       end
     end
