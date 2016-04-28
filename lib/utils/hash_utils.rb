405ffe2057104cbc3374681325fdca2266e899d0
Eric Saxby <esaxby@pivotal.io>
2016-04-28 15:22:26 -0700

    Add comment explaining HashUtils.dig vs Hash#dig
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- lib/utils/hash_utils.rb
+++ lib/utils/hash_utils.rb
@@ -1,4 +1,14 @@
 module HashUtils
+  # #dig is a more-permissive alternative to Hash#dig
+  #
+  # HashUtils.dig(nil, 5) does not raise
+  # nil.dig(5) raises
+  #
+  # HashUtils.dig('foo', :foo) does not raise
+  # 'foo'.dig(:foo) raises
+  #
+  # HashUtils.dig({foo: 5}, :foo, :bar) does not raise
+  # {foo: 5}.dig(:foo, :bar) raises
   def self.dig(hash, *path)
     path.inject(hash) do |location, key|
       location.is_a?(Hash) ? location[key] : nil
