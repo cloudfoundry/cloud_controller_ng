a72b219db3f92af5b1e543222e288be76eff27e2
Eric Saxby <esaxby@pivotal.io>
2016-04-29 11:29:43 -0700

    Fix rubocop warning
    Signed-off-by: Amelia Downs <adowns@pivotal.io>

--- spec/unit/controllers/v3/apps_controller_spec.rb
+++ spec/unit/controllers/v3/apps_controller_spec.rb
@@ -1629,9 +1629,9 @@ describe AppsV3Controller, type: :controller do
       before do
         allow(membership).to receive(:has_any_roles?).with(
           [VCAP::CloudController::Membership::SPACE_DEVELOPER,
-            VCAP::CloudController::Membership::SPACE_MANAGER,
-            VCAP::CloudController::Membership::SPACE_AUDITOR,
-            VCAP::CloudController::Membership::ORG_MANAGER], space.guid, org.guid).
+           VCAP::CloudController::Membership::SPACE_MANAGER,
+           VCAP::CloudController::Membership::SPACE_AUDITOR,
+           VCAP::CloudController::Membership::ORG_MANAGER], space.guid, org.guid).
           and_return(true)
         allow(membership).to receive(:has_any_roles?).with(
           [VCAP::CloudController::Membership::SPACE_DEVELOPER], space.guid).
