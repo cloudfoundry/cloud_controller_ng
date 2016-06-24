81a9ec31deff067c703c65ea63ae661a3216ce8d
Utako Ueda <uueda@pivotal.io>
2016-06-24 16:48:13 -0700

    linters are real
    Signed-off-by: Urvashi Reddy <ureddy@pivotal.io>

--- spec/acceptance/broker_api_compatibility/broker_api_v2.8_spec.rb
+++ spec/acceptance/broker_api_compatibility/broker_api_v2.8_spec.rb
@@ -3,20 +3,20 @@ require 'spec_helper'
 RSpec.describe 'Service Broker API integration' do
   describe 'v2.8' do
     include VCAP::CloudController::BrokerApiHelper
-      let(:route) { VCAP::CloudController::Route.make(space: @space) }
-      let(:catalog) { default_catalog(requires: ['route_forwarding']) }
-      let(:service_broker_bind_request) { %r{.*/v2/service_instances/#{@service_instance_guid}/service_bindings/#{guid_pattern}} }
+    let(:route) { VCAP::CloudController::Route.make(space: @space) }
+    let(:catalog) { default_catalog(requires: ['route_forwarding']) }
+    let(:service_broker_bind_request) { %r{.*/v2/service_instances/#{@service_instance_guid}/service_bindings/#{guid_pattern}} }
 
-      before do
-        setup_cc
-        setup_broker(catalog)
-        create_app
-        provision_service
-      end
+    before do
+      setup_cc
+      setup_broker(catalog)
+      create_app
+      provision_service
+    end
 
-      after do
-        delete_broker
-      end
+    after do
+      delete_broker
+    end
 
     describe 'route forwarding for service instances' do
       context 'when the broker does not return a route service url' do
@@ -38,7 +38,7 @@ RSpec.describe 'Service Broker API integration' do
 
         before do
           stub_request(:put, service_broker_bind_request).
-            to_return(status: 201, body: {'route_service_url' => 'https://neopets.com'}.to_json)
+            to_return(status: 201, body: { 'route_service_url' => 'https://neopets.com' }.to_json)
         end
 
         it 'cc proxies the bind request' do
