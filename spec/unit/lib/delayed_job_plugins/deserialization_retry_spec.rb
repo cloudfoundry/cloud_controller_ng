37174b6d2a6994610234661f15c09a2bb3b92454
Eric Saxby <esaxby@pivotal.io>
2016-07-01 09:34:14 -0700

    Allow test to be within slightly more seconds

--- spec/unit/lib/delayed_job_plugins/deserialization_retry_spec.rb
+++ spec/unit/lib/delayed_job_plugins/deserialization_retry_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe DeserializationRetry do
       expect(job.locked_by).to be_nil
       expect(job.locked_at).to be_nil
 
-      expect(job.run_at).to be_within(1.second).of Delayed::Job.db_time_now + 5.minutes
+      expect(job.run_at).to be_within(2.seconds).of Delayed::Job.db_time_now + 5.minutes
       expect(job.attempts).to eq(1)
     end
 
