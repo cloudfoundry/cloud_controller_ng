3f718fb9301196d99c80fe290017f1b71191cb4c
Utako Ueda <uueda@pivotal.io>
2016-07-06 12:00:30 -0700

    Conditionally remove space_id from events table
    [#124291721]
    
    Signed-off-by: Eric Saxby <esaxby@pivotal.io>

--- db/migrations/20160621182906_remove_space_id_from_events.rb
+++ db/migrations/20160621182906_remove_space_id_from_events.rb
@@ -1,14 +1,20 @@
-# Reverted this migration 12e3e8a08591fe5bd3e29505c51a2124500cc73f
-# Originally this migration was to remove the space_id column but there was a previous migration
-# in CF 213 that did the same thing incorrectly, which was altered in CF 214. When this migration file
-# was written, it conflicted with the environments that had deployed CF 213 at some point in time. We reverted
-# this migration pending a future conditional migration. This is here as a stub for environments that had
-# run this migration file before reverting.
 Sequel.migration do
   up do
+    if database_type == :postgres
+      run <<-SQL
+        ALTER TABLE events DROP COLUMN IF EXISTS space_id;
+      SQL
+    else
+      if dataset.db['select * from information_schema.columns where table_name = \'events\' and column_name = \'space_id\''].any?
+        alter_table(:events) do
+          drop_column(:space_id)
+        end
+      end
+    end
   end
 
   down do
+    raise Sequel::Error.new('This migration cannot be reversed.')
   end
 end
 
