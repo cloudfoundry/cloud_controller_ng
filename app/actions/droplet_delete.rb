33ca8faaa2ace6991abfa25064bcabaff21e48f3
Zach Robinson <zrobinson@pivotal.io>
2016-08-17 15:30:16 -0700

    Delete blobs when expiring packages and droplets
    [#127118541]
    
    Signed-off-by: Urvashi Reddy <ureddy@pivotal.io>

--- app/actions/droplet_delete.rb
+++ app/actions/droplet_delete.rb
@@ -13,7 +13,7 @@ module VCAP::CloudController
 
       droplets.each do |droplet|
         if droplet.blobstore_key
-          blobstore_delete = Jobs::Runtime::BlobstoreDelete.new(droplet.blobstore_key, :droplet_blobstore, nil)
+          blobstore_delete = Jobs::Runtime::BlobstoreDelete.new(droplet.blobstore_key, :droplet_blobstore)
           Jobs::Enqueuer.new(blobstore_delete, queue: 'cc-generic').enqueue
         end
 
---
+++ app/jobs/runtime/delete_expired_droplet_blob.rb
@@ -0,0 +1,34 @@
+module VCAP::CloudController
+  module Jobs
+    module Runtime
+      class DeleteExpiredDropletBlob < VCAP::CloudController::Jobs::CCJob
+        attr_reader :droplet_guid
+
+        def initialize(droplet_guid)
+          @droplet_guid = droplet_guid
+        end
+
+        def perform
+          logger.info("Deleting expired droplet blob for droplet: #{droplet_guid}")
+
+          droplet = DropletModel.find(guid: droplet_guid)
+          return unless droplet
+          BlobstoreDelete.new(droplet.blobstore_key, :droplet_blobstore).perform
+          droplet.update(droplet_hash: nil)
+        end
+
+        def job_name_in_configuration
+          :delete_expired_droplet_blob
+        end
+
+        def max_attempts
+          1
+        end
+
+        def logger
+          @logger ||= Steno.logger('cc.background')
+        end
+      end
+    end
+  end
+end
