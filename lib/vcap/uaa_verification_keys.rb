3214e82a590ded2175552798683c99fd2f56abf5
Urvashi Reddy <ureddy@pivotal.io>
2016-09-15 10:04:14 -0700

    Add logging for retries on fetching uaa keys
    [#129583539]
    
    Signed-off-by: Tim Hausler <thausler@pivotal.io>

--- lib/vcap/uaa_verification_keys.rb
+++ lib/vcap/uaa_verification_keys.rb
@@ -18,7 +18,10 @@ module VCAP
     def update_keys(last_fetched_keys)
       validation_hash = fetch_from_uaa
 
-      raise VCAP::CloudController::UaaUnavailable if !(validation_hash.present? || last_fetched_keys)
+      if !(validation_hash.present? || last_fetched_keys)
+        logger.error('Fetching uaa verification keys failed')
+        raise VCAP::CloudController::UaaUnavailable
+      end
 
       if !validation_hash.present? && last_fetched_keys
         last_fetched_keys
@@ -40,13 +43,13 @@ module VCAP
 
     def fetch_from_uaa
       retries = 3
-      validation_hash = {}
+      validation_hash = @info.validation_keys_hash
 
       while retries > 0 && !validation_hash.present?
-        validation_hash = @info.validation_keys_hash
+        logger.debug("fetch-verficiation-keys-retry", remaining_retries: retries)
         retries -= 1
+        validation_hash = @info.validation_keys_hash
       end
-
       validation_hash
     end
 
@@ -54,6 +57,10 @@ module VCAP
 
     def invalid_keys?
       return true unless Time.now - @uaa_keys[:requested_time] < 30
+    end
+
+    def logger
+      @logger ||= Steno.logger('cc.uaa_verification_keys')
     end
   end
 end
