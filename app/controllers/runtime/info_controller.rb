10ed0b9fa1a7098c96907f329006fd54ac083a56
Warren Fernandes <warren.f.fernandes@gmail.com>
2016-09-29 22:10:02 -0600

    Removes more references for logging_endpoint
    [#129384577]

--- app/controllers/runtime/info_controller.rb
+++ app/controllers/runtime/info_controller.rb
@@ -28,10 +28,6 @@ module VCAP::CloudController
         info[:routing_endpoint] = @config[:routing_api][:url]
       end
 
-      if @config[:loggregator] && @config[:loggregator][:url]
-        info[:logging_endpoint] = @config[:loggregator][:url]
-      end
-
       if @config[:doppler][:enabled]
         info[:doppler_logging_endpoint] = @config[:doppler][:url]
       end
