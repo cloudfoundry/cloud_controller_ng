f78b38a5e07cb66b697e17bd9d5eb28f22585699
Ferran Rodenas <frodenas@gmail.com>
2016-11-19 18:18:00 +0100

    Allow to specify message bus options

--- lib/cloud_controller/background_job_environment.rb
+++ lib/cloud_controller/background_job_environment.rb
@@ -14,9 +14,7 @@ class BackgroundJobEnvironment
 
     Thread.new do
       EM.run do
-        message_bus = MessageBus::Configurer.new(
-          servers: @config[:message_bus_servers],
-          logger: Steno.logger('cc.message_bus')).go
+        message_bus = MessageBus::Configurer.new({ logger: Steno.logger('cc.message_bus') }.merge(@config[:message_bus])).go
 
         # The AppObserver need no knowledge of the DEA or stager pools
         # so we are passing in no-op objects for these arguments
--- spec/unit/lib/background_job_environment_spec.rb
+++ spec/unit/lib/background_job_environment_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe BackgroundJobEnvironment do
   let(:bg_config) { { db: 'cc-db',
                       logging: { level: 'debug2' },
+                      message_bus: { servers: ['nats://localhost:4222'] },
                       bits_service: { enabled: false } }
   }
   subject(:background_job_environment) { described_class.new(bg_config) }
