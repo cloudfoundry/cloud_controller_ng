006f6f2cc8b8d5e4c9633ccaa9dce4307176f764
t-seyama <t.seyama20@gmail.com>
2016-11-25 23:05:29 +0900

    Fix foreign key names for MySQL

--- db/migrations/20161012132630_add_private_stack.rb
+++ db/migrations/20161012132630_add_private_stack.rb
@@ -2,22 +2,22 @@ Sequel.migration do
   change do
     create_table :organizations_private_stacks do
       Integer :organization_id, null: false
-      foreign_key [:organization_id], :organizations, name: :fk_organization_id
+      foreign_key [:organization_id], :organizations, name: :fk_ops_organization_id
 
       Integer :private_stack_id, null: false
-      foreign_key [:private_stack_id], :stacks, name: :fk_private_stack_id
+      foreign_key [:private_stack_id], :stacks, name: :fk_ops_private_stack_id
 
-      index [:organization_id, :private_stack_id], unique: true, name: 'orgs_ps_ids'
+      index [:organization_id, :private_stack_id], unique: true, name: 'orgs_pstacks_ids'
     end
 
     create_table :spaces_private_stacks do
       Integer :space_id, null: false
-      foreign_key [:space_id], :spaces, name: :fk_space_id
+      foreign_key [:space_id], :spaces, name: :fk_sps_space_id
 
       Integer :private_stack_id, null: false
-      foreign_key [:private_stack_id], :stacks, name: :fk_private_stack_id
+      foreign_key [:private_stack_id], :stacks, name: :fk_sps_private_stack_id
 
-      index [:space_id, :private_stack_id], unique: true, name: 'spaces_ps_ids'
+      index [:space_id, :private_stack_id], unique: true, name: 'spaces_pstacks_ids'
     end
 
     alter_table :stacks do
