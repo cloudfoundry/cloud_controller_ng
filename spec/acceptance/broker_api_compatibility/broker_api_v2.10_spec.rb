922fef0c801c8638cd2749984c272beb3437c467
Julian Hjortshoj <julian.hjortshoj@emc.com>
2016-11-29 10:35:01 -0800

    add compatibility test for volume mounts
    [#130244039]
    
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- spec/acceptance/broker_api_compatibility/broker_api_v2.10_spec.rb
+++ spec/acceptance/broker_api_compatibility/broker_api_v2.10_spec.rb
@@ -12,7 +12,49 @@ RSpec.describe 'Service Broker API integration' do
       @broker = VCAP::CloudController::ServiceBroker.find guid: @broker_guid
     end
 
-    # NOTE: the only changes in 2.10 so far are for experimental volume mounts.  Since we do not guarantee backward
-    # compatibility on volume mounts, we won't add tests for that feature just yet.
+    describe 'Binding' do
+      let(:broker_response_status) { 200 }
+      let(:broker_response_body) do
+        {
+            'volume_mounts' => [{
+                 'device_type' => 'shared',
+                 'other' => 'stuff',
+                 'device' => { 'volume_id' => 'foo', 'mount_config' => { 'extra' => 'garbage' } },
+                 'mode' => 'rw',
+                 'container_dir' => '/var/vcap/data/foo',
+                 'driver' => 'mydriver'
+             }]
+        }.to_json
+      end
+      let(:app_guid) { @app_guid }
+      let(:service_instance_guid) { @service_instance_guid }
+      let(:catalog) { default_catalog(requires: ['volume_mount']) }
+
+      before do
+        provision_service
+        create_app
+      end
+
+      after do
+        delete_broker
+      end
+
+      describe 'service binding response with volume_mounts' do
+        before do
+          stub_request(:put, %r{/v2/service_instances/#{service_instance_guid}/service_bindings/#{guid_pattern}}).
+            to_return(status: broker_response_status, body: broker_response_body)
+        end
+
+        it 'displays the volume mount information to the user' do
+          post('/v2/service_bindings',
+               { app_guid: app_guid, service_instance_guid: service_instance_guid }.to_json,
+               json_headers(admin_headers))
+
+          parsed_body = MultiJson.load(last_response.body)
+
+          expect(parsed_body['entity']['volume_mounts']).to match_array([{ 'device_type' => 'shared', 'mode' => 'rw', 'container_dir' => '/var/vcap/data/foo' }])
+        end
+      end
+    end
   end
 end
