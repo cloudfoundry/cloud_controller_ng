5219f5cd3476e6cea4900fd27d4a5febfc30e474
Tim Hausler <thausler@pivotal.io>
2016-12-07 17:39:40 -0800

    Update spec helper and travis.yml for codeclimate gem bump
    [#133894135]
    
    Signed-off-by: Greg Cobb <gcobb@pivotal.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,12 @@
 require File.expand_path('../../config/boot', __FILE__)
 
 if ENV['CODECLIMATE_REPO_TOKEN'] && ENV['COVERAGE']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_filter '/vendor/'
+    add_filter '/docs/'
+  end
 end
 ENV['PB_IGNORE_DEPRECATIONS'] = 'true'
 
