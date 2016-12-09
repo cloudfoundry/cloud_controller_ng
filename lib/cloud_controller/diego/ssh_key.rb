0d882519df46f080e08916314e5799a282daeb8a
Luan Santos <lsantos@pivotal.io>
2016-12-09 10:07:58 -0800

    lazily create ssh keys to reduce cpu usage
    [#120643929]
    
    Signed-off-by: Michael Xu <mxu@pivotal.io>

--- lib/cloud_controller/diego/ssh_key.rb
+++ lib/cloud_controller/diego/ssh_key.rb
@@ -5,24 +5,30 @@ module VCAP
     module Diego
       class SSHKey
         def initialize(bits=1024)
-          @key = OpenSSL::PKey::RSA.new(bits)
+          @bits = bits
         end
 
         def private_key
-          @key.to_pem
+          key.to_pem
         end
 
         def authorized_key
           @auth_key ||= begin
-            type = @key.ssh_type
-            data = [@key.to_blob].pack('m0')
+            type = key.ssh_type
+            data = [key.to_blob].pack('m0')
 
             "#{type} #{data}"
           end
         end
 
         def fingerprint
-          @key.fingerprint
+          key.fingerprint
+        end
+
+        private
+
+        def key
+          @key ||= OpenSSL::PKey::RSA.new(@bits)
         end
       end
     end
