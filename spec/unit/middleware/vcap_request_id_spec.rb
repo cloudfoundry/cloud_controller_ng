a013a02521d020b8cb5669ff12339c9484cc8c4e
Aakash Shah <ashah@pivotal.io>
2017-03-13 10:44:12 -0700

    Refactor tests to use fake App to have inputs/outputs be more testable
    [#140969343]

--- spec/unit/middleware/vcap_request_id_spec.rb
+++ spec/unit/middleware/vcap_request_id_spec.rb
@@ -5,19 +5,25 @@ module CloudFoundry
   module Middleware
     RSpec.describe VcapRequestId do
       let(:middleware) { described_class.new(app) }
-      let(:app) { double(:app, call: app_response) }
+      let(:app) { FakeApp.new }
       let(:app_response) { [200, {}, 'a body'] }
       let(:uuid_regex) { '\w+-\w+-\w+-\w+-\w+' }
+
+      class FakeApp
+        attr_accessor :last_request_id, :last_env_input
+
+        def call(env)
+          @last_request_id = ::VCAP::Request.current_id
+          @last_env_input = env
+          [200, {}, 'a body']
+        end
+      end
 
       describe 'handling the request' do
         context 'setting the request_id in the logger' do
           it 'has assigned it before passing the request' do
-            expect(app).to receive(:call) do |_|
-              expect(::VCAP::Request.current_id).to match(/^specific-request-id::#{uuid_regex}$/)
-              app_response
-            end
-
             middleware.call('HTTP_X_VCAP_REQUEST_ID' => 'specific-request-id')
+            expect(app.last_request_id).to match(/^specific-request-id::#{uuid_regex}$/)
           end
 
           it 'nils it out after the request has been processed' do
@@ -29,36 +35,26 @@ module CloudFoundry
         context 'when HTTP_X_VCAP_REQUEST_ID is passed in from outside' do
           it 'includes it in cf.request_id and appends a uuid to ensure uniqueness' do
             middleware.call('HTTP_X_VCAP_REQUEST_ID' => 'request-id')
-
-            expect(app).to have_received(:call) do |env|
-              expect(env['cf.request_id']).to match(/^request-id::#{uuid_regex}$/)
-            end
+            expect(app.last_env_input['cf.request_id']).to match(/^request-id::#{uuid_regex}$/)
           end
 
           it 'accepts only alphanumeric request ids' do
             middleware.call('HTTP_X_VCAP_REQUEST_ID' => '; X-Hacked-Header: Stuff')
-
-            expect(app).to have_received(:call) do |env|
-              expect(env['cf.request_id']).to match(/^X-Hacked-HeaderStuff::#{uuid_regex}$/)
-            end
+            expect(app.last_env_input['cf.request_id']).to match(/^X-Hacked-HeaderStuff::#{uuid_regex}$/)
           end
 
           it 'accepts only 255 characters in the passed-in request id' do
             middleware.call('HTTP_X_VCAP_REQUEST_ID' => 'x' * 500)
 
             truncated_passed_id = 'x' * 255
-            expect(app).to have_received(:call) do |env|
-              expect(env['cf.request_id']).to match(/^#{truncated_passed_id}::#{uuid_regex}$/)
-            end
+            expect(app.last_env_input['cf.request_id']).to match(/^#{truncated_passed_id}::#{uuid_regex}$/)
           end
 
           context 'when HTTP_X_REQUEST_ID is also passed in from outside' do
             it 'preferes HTTP_X_VCAP_REQUEST_ID' do
               middleware.call('HTTP_X_VCAP_REQUEST_ID' => 'request-id', 'HTTP_X_REQUEST_ID' => 'not-vcap-request-id')
 
-              expect(app).to have_received(:call) do |env|
-                expect(env['cf.request_id']).to match(/^request-id::#{uuid_regex}$/)
-              end
+              expect(app.last_env_input['cf.request_id']).to match(/^request-id::#{uuid_regex}$/)
             end
           end
         end
@@ -67,18 +63,14 @@ module CloudFoundry
           it 'generates a uuid as the request_id' do
             middleware.call({})
 
-            expect(app).to have_received(:call) do |env|
-              expect(env['cf.request_id']).to match(/^#{uuid_regex}$/)
-            end
+            expect(app.last_env_input['cf.request_id']).to match(/^#{uuid_regex}$/)
           end
 
           context 'when HTTP_X_REQUEST_ID is passed in from outside' do
             it 'includes it in cf.request_id and appends a uuid to ensure uniqueness' do
               middleware.call('HTTP_X_REQUEST_ID' => 'not-vcap-request-id')
 
-              expect(app).to have_received(:call) do |env|
-                expect(env['cf.request_id']).to match(/^not-vcap-request-id::#{uuid_regex}$/)
-              end
+              expect(app.last_env_input['cf.request_id']).to match(/^not-vcap-request-id::#{uuid_regex}$/)
             end
           end
         end
