a247e0be2ccaa4eb6f5a40db2cafb49702cb22d3
Lyle Franklin <lfranklin@pivotal.io>
2017-03-15 13:43:14 -0700

    Fix failing app_event test
    [#141783519]
    
    Signed-off-by: Jason Stevens <jason.stevens@microsoft.com>

--- app/models/runtime/app_event.rb
+++ app/models/runtime/app_event.rb
@@ -14,7 +14,7 @@ module VCAP::CloudController
     end
 
     def self.user_visibility_filter(user)
-      { app: App.user_visible(user) }
+      { app: App.user_visible(user).order(nil) }
     end
   end
 end
