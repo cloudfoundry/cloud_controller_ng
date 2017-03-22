5473742bbf5b87375eaa11334baf8cba4ef20747
Michael Xu <mxu@pivotal.io>
2017-03-22 16:45:24 -0700

    Change delimiter in setup-local-cc to avoid escaping backslash
    Signed-off-by: Tim Hausler <thausler@pivotal.io>

--- scripts/setup-local-cc.sh
+++ scripts/setup-local-cc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 bosh ssh api_z1 0 <<'ENDSSH'
-  sudo sed -i -- 's/unix:\/var\/vcap\/sys\/run\/cloud_controller_ng\/cloud_controller\.sock/192\.168\.50\.1:9022/g' '/var/vcap/jobs/cloud_controller_ng/config/nginx.conf' && \
+  sudo sed -i -- 's#unix:/var/vcap/sys/run/cloud_controller_ng/cloud_controller\.sock#192\.168\.50\.1:9022#g' '/var/vcap/jobs/cloud_controller_ng/config/nginx.conf' && \
   sudo /var/vcap/bosh/bin/monit restart nginx_cc
 ENDSSH
 
