182ccaa9c1cb1409d437862d7c0e7c0af73720f4
Lyle Franklin <lfranklin@pivotal.io>
2017-03-22 10:43:13 -0700

    Add sleep in MS SQL script to avoid hangs in Travis
    Signed-off-by: Jason Stevens <jason.stevens@microsoft.com>

--- scripts/run-ms-sql-background.sh
+++ scripts/run-ms-sql-background.sh
@@ -3,5 +3,8 @@
 set -e
 
 echo "Starting MS SQL Server in the background..."
+
 docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password-123' -p 1433:1433 -d microsoft/mssql-server-linux
+sleep 10 # mssql CLI hangs if connection is attempted immediately after starting server
+
 echo "MS SQL Server is now running at localhost:1433"
