521a8530311d3e77de51cf529de783035566e617
Lyle Franklin <lfranklin@pivotal.io>
2017-03-24 15:39:23 -0700

    Write MS SQL logs to a file in case DB dies
    - Had unit tests fail saying the DB went away, but couldn't see the logs
      to see what happened
    
    [#141159813]

--- scripts/run-ms-sql-background.sh
+++ scripts/run-ms-sql-background.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+: ${LOG_FILE:=""}
+
 echo "Starting MS SQL Server in the background..."
 
-docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password-123' -p 1433:1433 -d microsoft/mssql-server-linux
+container_id="$(docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password-123' -p 1433:1433 -d microsoft/mssql-server-linux)"
+if [ -n "${LOG_FILE}" ]; then
+  echo "Streaming MS SQL logs to '${LOG_FILE}'..."
+  docker logs --follow "${container_id}" 2>&1 > "${LOG_FILE}" &
+fi
 sleep 10 # mssql CLI hangs if connection is attempted immediately after starting server
 
 echo "MS SQL Server is now running at localhost:1433"
