1b214040a1a0befb03992439a5b94b2bd53ecff0
Aakash Shah <ashah@pivotal.io>
2017-05-02 12:17:57 -0700

    Remove orphaned apps migration
    * We decided to back away from this as this can balloon and be fairly
    expensive of a migration.
    
    [#144624009]
    
    Signed-off-by: Michael Xu <mxu@pivotal.io>

--- db/migrations/20170502171127_remove_orphaned_apps.rb
+++
@@ -1,8 +0,0 @@
-Sequel.migration do
-  change do
-    transaction do
-      # remove orphaned apps
-      run 'DELETE FROM apps WHERE space_guid NOT IN (SELECT guid FROM spaces)'
-    end
-  end
-end
