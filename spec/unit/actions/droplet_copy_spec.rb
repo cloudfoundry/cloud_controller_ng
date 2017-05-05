396a1ea2a704df322464e83397de8fbd7b2bf911
Jen Spinney <jennifer.spinney@suse.com>
2017-05-05 10:14:01 -0700

    Fix test to compare against literals
    * Previously, many of the expects were comparing nil to nil.
    
    [#140458883]
    
    Signed-off-by: Nicolas Regez <nicolas.regez@swisscom.com>

--- spec/unit/actions/droplet_copy_spec.rb
+++ spec/unit/actions/droplet_copy_spec.rb
@@ -14,8 +14,16 @@ module VCAP::CloudController
         droplet_hash:          'abcdef',
         sha256_checksum:          'droplet-sha256-checksum',
         process_types:         { web: 'bundle exec rails s' },
+        buildpack_receipt_buildpack_guid: 'buildpack-guid',
+        buildpack_receipt_buildpack: 'buildpack',
+        buildpack_receipt_stack_name: 'stack',
         environment_variables: { 'THING' => 'STUFF' },
-        state:                 VCAP::CloudController::DropletModel::STAGED_STATE)
+        state:                 VCAP::CloudController::DropletModel::STAGED_STATE,
+        execution_metadata: 'execution_metadata',
+        staging_memory_in_mb: 128,
+        staging_disk_in_mb: 256,
+        docker_receipt_image: 'docker/image',
+       )
     end
     let(:user_audit_info) { UserAuditInfo.new(user_email: 'user-email', user_guid: 'user_guid') }
 
@@ -28,17 +36,17 @@ module VCAP::CloudController
         copied_droplet = DropletModel.last
 
         expect(copied_droplet.state).to eq DropletModel::COPYING_STATE
-        expect(copied_droplet.buildpack_receipt_buildpack_guid).to eq source_droplet.buildpack_receipt_buildpack_guid
         expect(copied_droplet.droplet_hash).to be nil
         expect(copied_droplet.sha256_checksum).to be nil
         expect(copied_droplet.environment_variables).to eq(nil)
         expect(copied_droplet.process_types).to eq({ 'web' => 'bundle exec rails s' })
-        expect(copied_droplet.buildpack_receipt_buildpack).to eq source_droplet.buildpack_receipt_buildpack
-        expect(copied_droplet.buildpack_receipt_stack_name).to eq source_droplet.buildpack_receipt_stack_name
-        expect(copied_droplet.execution_metadata).to eq source_droplet.execution_metadata
-        expect(copied_droplet.staging_memory_in_mb).to eq source_droplet.staging_memory_in_mb
-        expect(copied_droplet.staging_disk_in_mb).to eq source_droplet.staging_disk_in_mb
-        expect(copied_droplet.docker_receipt_image).to eq source_droplet.docker_receipt_image
+        expect(copied_droplet.buildpack_receipt_buildpack_guid).to eq 'buildpack-guid'
+        expect(copied_droplet.buildpack_receipt_buildpack).to eq 'buildpack'
+        expect(copied_droplet.buildpack_receipt_stack_name).to eq 'stack'
+        expect(copied_droplet.execution_metadata).to eq 'execution_metadata'
+        expect(copied_droplet.staging_memory_in_mb).to eq 128
+        expect(copied_droplet.staging_disk_in_mb).to eq 256
+        expect(copied_droplet.docker_receipt_image).to eq 'docker/image'
 
         expect(target_app.droplets).to include(copied_droplet)
       end
