84eb7c424398b359f96ea0c4c1eee25235cfd50e
Aakash Shah <ashah@pivotal.io>
2017-05-11 10:50:38 -0700

    Remove unused attr when scheduling frequent jobs
    [#143693647]
    Signed-off-by: Aakash Shah <ashah@pivotal.io>

--- lib/cloud_controller/clock/scheduler.rb
+++ lib/cloud_controller/clock/scheduler.rb
@@ -5,14 +5,14 @@ require 'cloud_controller/clock/job_timeout_calculator'
 module VCAP::CloudController
   class Scheduler
     CLEANUPS = [
-      { name: 'app_usage_events', job_name: 'app_usage_events_cleanup', class: Jobs::Runtime::AppUsageEventsCleanup, time: '18:00' },
-      { name: 'app_events', job_name: 'app_events_cleanup', class: Jobs::Runtime::AppEventsCleanup, time: '19:00' },
-      { name: 'audit_events', job_name: 'events_cleanup', class: Jobs::Runtime::EventsCleanup, time: '20:00' },
-      { name: 'failed_jobs', job_name: 'failed_jobs', class: Jobs::Runtime::FailedJobsCleanup, time: '21:00' },
-      { name: 'service_usage_events', job_name: 'service_usage_events_cleanup', class: Jobs::Services::ServiceUsageEventsCleanup, time: '22:00' },
-      { name: 'completed_tasks', job_name: 'prune_completed_tasks', class: Jobs::Runtime::PruneCompletedTasks, time: '23:00' },
-      { name: 'expired_blob_cleanup', job_name: 'expired_blob_cleanup', class: Jobs::Runtime::ExpiredBlobCleanup, time: '00:00' },
-      { name: 'expired_resource_cleanup', job_name: 'expired_resource_cleanup', class: Jobs::Runtime::ExpiredResourceCleanup, time: '00:30' },
+      { name: 'app_usage_events', class: Jobs::Runtime::AppUsageEventsCleanup, time: '18:00' },
+      { name: 'app_events', class: Jobs::Runtime::AppEventsCleanup, time: '19:00' },
+      { name: 'audit_events', class: Jobs::Runtime::EventsCleanup, time: '20:00' },
+      { name: 'failed_jobs', class: Jobs::Runtime::FailedJobsCleanup, time: '21:00' },
+      { name: 'service_usage_events', class: Jobs::Services::ServiceUsageEventsCleanup, time: '22:00' },
+      { name: 'completed_tasks', class: Jobs::Runtime::PruneCompletedTasks, time: '23:00' },
+      { name: 'expired_blob_cleanup', class: Jobs::Runtime::ExpiredBlobCleanup, time: '00:00' },
+      { name: 'expired_resource_cleanup', class: Jobs::Runtime::ExpiredResourceCleanup, time: '00:30' },
     ].freeze
 
     def initialize(config)
@@ -37,7 +37,7 @@ module VCAP::CloudController
       clock_opts = {
         name:     'diego_sync',
         interval: @config.dig(:diego_sync, :frequency_in_seconds),
-        timeout: job_timeout(:diego_sync),
+        timeout:  @timeout_calculator.calculate(:diego_sync),
       }
       @clock.schedule_frequent_inline_job(clock_opts) do
         Jobs::Diego::Sync.new
@@ -67,10 +67,6 @@ module VCAP::CloudController
           cutoff_age_in_days ? klass.new(cutoff_age_in_days) : klass.new
         end
       end
-    end
-
-    def job_timeout(job_name)
-      @timeout_calculator.calculate(job_name)
     end
   end
 end
