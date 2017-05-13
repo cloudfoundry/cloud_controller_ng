ade6f6880b675401be875dd99e5c8e56889ed997
Aakash Shah <ashah@pivotal.io>
2017-05-12 17:35:21 -0700

    Replace buildpack positioner with Sequel list plugin
    * This to prevent deadlocks with buildpack position management
    * Move positioning logic to controller instead of model hooks
    
    [#143674115]
    
    Signed-off-by: Nicolas Regez <nicolas.regez@swisscom.com>

--- spec/unit/models/runtime/buildpack_spec.rb
+++ spec/unit/models/runtime/buildpack_spec.rb
@@ -135,164 +135,9 @@ module VCAP::CloudController
       end
     end
 
-    describe '.at_last_position' do
+    describe '#update' do
       let!(:buildpacks) do
         Array.new(4) { |i| Buildpack.create(name: "name_#{100 - i}", position: i + 1) }
-      end
-
-      it 'gets the last position' do
-        expect(Buildpack.at_last_position).to eq buildpacks[3]
-      end
-
-      context 'no buildpacks in the database' do
-        let(:buildpacks) { nil }
-
-        it 'should return nil' do
-          expect(Buildpack.at_last_position).to be_nil
-        end
-      end
-    end
-
-    describe '.create' do
-      context 'with a specified position' do
-        it 'creates a buildpack entry at the lowest position' do
-          expect {
-            Buildpack.create(name: 'new_buildpack', key: 'abcdef', position: 5)
-          }.to change {
-            ordered_buildpacks
-          }.from([]).to([['new_buildpack', 1]])
-        end
-
-        it 'has a position 0 specified' do
-          bp = Buildpack.create(name: 'new_buildpack', key: 'abcdef', position: 0)
-          expect(bp.position).to(eql(1))
-        end
-      end
-
-      context 'without a specified position' do
-        it 'creates a buildpack entry at the lowest position' do
-          expect {
-            Buildpack.create(name: 'new_buildpack', key: 'abcdef')
-          }.to change {
-            ordered_buildpacks
-          }.from([]).to([['new_buildpack', 1]])
-        end
-      end
-
-      it "locks the buildpacks so we don't get duplicate positions", isolation: :truncation do
-        buildpacks_lock = double(:buildpacks_lock)
-        allow(Locking).to receive(:[]) { buildpacks_lock }
-        allow(Locking[name: 'buildpacks']).to receive(:lock!)
-        Buildpack.create(name: 'another_buildpack', key: 'abcdef')
-        expect(Locking[name: 'buildpacks']).to have_received(:lock!)
-      end
-
-      context 'when other buildpacks exist' do
-        let!(:buildpacks) do
-          Array.new(4) { |i| Buildpack.make(name: "name_#{100 - i}", position: i + 1) }
-        end
-
-        context 'with a specified position' do
-          it 'creates a buildpack at position 1 when less than 1' do
-            expect {
-              Buildpack.create(name: 'new_buildpack', key: 'abcdef', position: 0)
-            }.to change {
-              ordered_buildpacks
-            }.from(
-              [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4]]
-            ).to(
-              [['new_buildpack', 1], ['name_100', 2], ['name_99', 3], ['name_98', 4], ['name_97', 5]]
-            )
-          end
-
-          it 'creates a buildpack entry at the lowest position' do
-            expect {
-              Buildpack.create(name: 'new_buildpack', key: 'abcdef', position: 7)
-            }.to change {
-              ordered_buildpacks
-            }.from(
-              [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4]]
-            ).to(
-              [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4], ['new_buildpack', 5]]
-            )
-          end
-
-          it 'creates a buildpack entry and moves all other buildpacks' do
-            expect {
-              Buildpack.create(name: 'new_buildpack', key: 'abcdef', position: 2)
-            }.to change {
-              ordered_buildpacks
-            }.from(
-              [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4]]
-            ).to(
-              [['name_100', 1], ['new_buildpack', 2], ['name_99', 3], ['name_98', 4], ['name_97', 5]]
-            )
-          end
-
-          it 'allows an insert at the current last position' do
-            expect {
-              Buildpack.create(name: 'new_buildpack', key: 'abcdef', position: 4)
-            }.to change {
-              ordered_buildpacks
-            }.from(
-              [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4]]
-            ).to(
-              [['name_100', 1], ['name_99', 2], ['name_98', 3], ['new_buildpack', 4], ['name_97', 5]]
-            )
-          end
-        end
-
-        context 'without a specified position' do
-          it 'creates a buildpack entry at the lowest position' do
-            expect {
-              Buildpack.create(name: 'new_buildpack', key: 'abcdef')
-            }.to change {
-              ordered_buildpacks
-            }.from(
-              [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4]]
-            ).to(
-              [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4], ['new_buildpack', 5]]
-            )
-          end
-        end
-
-        context 'and called with a block' do
-          it 'orders based on position' do
-            expect {
-              Buildpack.create do |bp|
-                bp.set_all(name: 'new_buildpack', key: 'abcdef', position: 1)
-              end
-            }.to change {
-              ordered_buildpacks
-            }.from(
-              [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4]]
-            ).to(
-              [['new_buildpack', 1], ['name_100', 2], ['name_99', 3], ['name_98', 4], ['name_97', 5]]
-            )
-          end
-        end
-      end
-    end
-
-    describe '.update' do
-      let!(:buildpacks) do
-        Array.new(4) { |i| Buildpack.create(name: "name_#{100 - i}", position: i + 1) }
-      end
-
-      it "locks the buildpacks so we don't get duplicate positions" do
-        buildpacks_lock = double(:buildpacks_lock)
-        allow(Locking).to receive(:[]) { buildpacks_lock }
-        allow(Locking[name: 'buildpacks']).to receive(:lock!)
-        buildpacks.last.update position: 1
-        expect(Locking[name: 'buildpacks']).to have_received(:lock!)
-      end
-
-      it "does not update the position if it isn't specified" do
-        expect {
-          buildpacks.first.update(key: 'abcdef')
-        }.to_not change {
-          ordered_buildpacks
-        }
       end
 
       it 'does not modify the frozen hash provided by Sequel' do
@@ -300,77 +145,11 @@ module VCAP::CloudController
           buildpacks.first.update({ position: 2 }.freeze)
         }.not_to raise_error
       end
-
-      it 'shifts from the end to the beginning' do
-        expect {
-          buildpacks[3].update(position: 1)
-        }.to change {
-          ordered_buildpacks
-        }.from(
-          [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4]]
-        ).to(
-          [['name_97', 1], ['name_100', 2], ['name_99', 3], ['name_98', 4]]
-        )
-      end
-
-      it 'shifts in the middle' do
-        expect {
-          buildpacks[3].update(position: 2)
-        }.to change {
-          ordered_buildpacks
-        }.from(
-          [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4]]
-        ).to(
-          [['name_100', 1], ['name_97', 2], ['name_99', 3], ['name_98', 4]]
-        )
-      end
-
-      it 'shifts from the beginning to the end' do
-        expect {
-          buildpacks.first.update({ position: 4 })
-        }.to change {
-          ordered_buildpacks
-        }.from(
-          [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4]]
-        ).to(
-          [['name_99', 1], ['name_98', 2], ['name_97', 3], ['name_100', 4]]
-        )
-      end
-
-      context 'when updating past' do
-        context 'the beginning' do
-          it 'normalizes to the beginning of the list' do
-            expect {
-              buildpacks[1].update(position: 0)
-            }.to change {
-              ordered_buildpacks
-            }.from(
-              [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4]]
-            ).to(
-              [['name_99', 1], ['name_100', 2], ['name_98', 3], ['name_97', 4]]
-            )
-          end
-        end
-
-        context 'the end' do
-          it 'normalizes the position to a valid one' do
-            expect {
-              buildpacks[2].update(position: 5)
-            }.to change {
-              ordered_buildpacks
-            }.from(
-              [['name_100', 1], ['name_99', 2], ['name_98', 3], ['name_97', 4]]
-            ).to(
-              [['name_100', 1], ['name_99', 2], ['name_97', 3], ['name_98', 4]]
-            )
-          end
-        end
-      end
     end
 
-    describe 'destroy' do
-      let!(:buildpack1) { VCAP::CloudController::Buildpack.create({ name: 'first_buildpack', key: 'xyz', position: 5 }) }
-      let!(:buildpack2) { VCAP::CloudController::Buildpack.create({ name: 'second_buildpack', key: 'xyz', position: 10 }) }
+    describe '#destroy' do
+      let!(:buildpack1) { VCAP::CloudController::Buildpack.create({ name: 'first_buildpack', key: 'xyz', position: 1 }) }
+      let!(:buildpack2) { VCAP::CloudController::Buildpack.create({ name: 'second_buildpack', key: 'xyz', position: 2 }) }
 
       it 'removes the specified buildpack' do
         expect {
@@ -382,14 +161,6 @@ module VCAP::CloudController
         ).to(
           [['second_buildpack', 1]]
         )
-      end
-
-      it 'grabs a lock while destroying a buildpack', isolation: :truncation do
-        buildpacks_lock = double(:buildpacks_lock)
-        allow(Locking).to receive(:[]) { buildpacks_lock }
-        allow(Locking[name: 'buildpacks']).to receive(:lock!)
-        buildpack1.destroy
-        expect(Locking[name: 'buildpacks']).to have_received(:lock!)
       end
 
       it "doesn't shift when the last position is deleted" do
