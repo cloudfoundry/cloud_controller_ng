c46bde00bcb105fe16189fd58a512aa52e7f7cb1
Annie Sing <asing@pivotal.io>
2017-06-02 15:48:58 -0700

    Add debug comment to app fk on space_guid migration
    - This migration will fail if there are existing apps that have invalid
    space guids
    - Comment helps operators escape the failing migration by assigning the
    apps to spaces
    - Apps without spaces has only be observed in load testing environments.
    - Some environments ended up with the remove_orphaned_apps migration.
    Add empty migration file to enable these environments to upgrade
    
    [#146551809]
    Signed-off-by: Greg Cobb <gcobb@pivotal.io>

---
+++ db/migrations/20170502171127_remove_orphaned_apps.rb
@@ -0,0 +1,5 @@
+Sequel.migration do
+  change do
+    # intentionally left blank
+  end
+end
--- db/migrations/20170502181209_add_fk_apps_space_guid.rb
+++ db/migrations/20170502181209_add_fk_apps_space_guid.rb
@@ -1,3 +1,12 @@
+# If this migration fails, it is likely because the environment has an app without an associated space.
+# To resolve this issue, update each app's space_guid to the guid of an existing space.
+#
+# 1) Find the offending apps' guids: SELECT guid FROM apps WHERE space_guid NOT IN (SELECT guid FROM spaces);
+# 2) Get the guids for the spaces you want the apps to be in: SELECT guid FROM spaces WHERE name='<space_name>';
+# 3) Update each app's space_guid: UPDATE apps SET space_guid = '<space_guid>' WHERE guid='<app_guid>';
+# 4) You may now delete the app and/or space if you wish
+# 5) Proceed with the deploy as usual
+
 Sequel.migration do
   change do
     alter_table(:apps) do
