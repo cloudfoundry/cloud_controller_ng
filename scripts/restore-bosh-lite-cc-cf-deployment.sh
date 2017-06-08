560df4ddcbb671819b8aba311b975f791e3200fe
Annie Sing <asing@pivotal.io>
2017-06-07 17:04:42 -0700

    First pass at adapting run-local-cc for cf-deployment
    - Setup and Restore scripts are done
    - Still getting errors with run-local-cc script
    
    [#144478413]
    
    [ci skip]
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

---
+++ scripts/restore-bosh-lite-cc-cf-deployment.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu
+
+: "${BOSH_ENVIRONMENT:?}"
+: "${BOSH_CLIENT:?}"
+: "${BOSH_CLIENT_SECRET:?}"
+: "${BOSH_DEPLOYMENT:="cf"}"
+: "${BOSH_API_INSTANCE:="api/0"}"
+
+scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cc_dir="$( cd "${scripts_dir}/.." && pwd )"
+tmp_dir="${cc_dir}/tmp"
+
+echo "Resetting nginx to original socket..."
+bosh ssh "${BOSH_API_INSTANCE}" -c "\
+  sudo sed -i -- 's/192\.168\.50\.1:9022/unix:\/var\/vcap\/sys\/run\/cloud_controller_ng\/cloud_controller\.sock/g' '/var/vcap/jobs/cloud_controller_ng/config/nginx.conf' && \
+  sudo /var/vcap/bosh/bin/monit restart nginx_cc" > /dev/null
+
+echo "Removing internal hostnames from /etc/hosts..."
+sudo sed -i.bak "/.*\.service\.cf\.internal/d" /etc/hosts > /dev/null
+
+echo "Removing local config file..."
+rm -rf "${tmp_dir}/local-cc-config.yml"
+
+echo "Done"
