7ee5f7cd66a02b410cf39137b976739f44ac9a49
Alex Blease <ablease@pivotal.io>
2017-06-19 12:31:08 +0100

    Format nested schema errors
    Signed-off-by: Alex Blease <ablease@pivotal.io>

--- lib/services/service_brokers/validation_errors_formatter.rb
+++ lib/services/service_brokers/validation_errors_formatter.rb
@@ -1,6 +1,7 @@
 module VCAP::Services::ServiceBrokers
   class ValidationErrorsFormatter
     INDENT = '  '.freeze
+
     def format(validation_errors)
       message = "\n"
       validation_errors.messages.each { |e| message += "#{e}\n" }
@@ -9,19 +10,30 @@ module VCAP::Services::ServiceBrokers
 
         message += "Service #{service.name}\n"
         service_errors.messages.each do |error|
-          message += "#{INDENT}#{error}\n"
+          message += indent + "#{error}\n"
         end
 
         service_errors.nested_errors.each do |plan, plan_errors|
           next if plan_errors.empty?
 
-          message += "#{INDENT}Plan #{plan.name}\n"
+          message += indent + "Plan #{plan.name}\n"
           plan_errors.messages.each do |error|
-            message += "#{INDENT}#{INDENT}#{error}\n"
+            message += indent(2) + "#{error}\n"
+          end
+
+          plan_errors.nested_errors.each do |schema, schema_errors|
+            message += indent(2) + "Schemas\n"
+            schema_errors.messages.each do |error|
+              message += indent(3) + "#{error}\n"
+            end
           end
         end
       end
       message
+    end
+
+    def indent(num=1)
+      INDENT.to_s * num
     end
   end
 end
