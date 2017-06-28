b5ca4c487024c78dffeb8555595e9f1bb6b25f67
Sam Gunaratne <sgunaratne@pivotal.io>
2017-06-28 16:28:57 +0100

    Broker schemas must not contain external references
    [#145580733]
    
    Signed-off-by: Alex Blease <ablease@pivotal.io>

--- lib/services/service_brokers/v2/catalog_schemas.rb
+++ lib/services/service_brokers/v2/catalog_schemas.rb
@@ -1,3 +1,5 @@
+require 'json-schema'
+
 module VCAP::Services::ServiceBrokers::V2
   class CatalogSchemas
     attr_reader :errors, :create_instance
@@ -32,7 +34,24 @@ module VCAP::Services::ServiceBrokers::V2
         end
       end
 
+      validate_no_external_references(attrs)
       @create_instance = attrs
+    end
+
+    def validate_no_external_references(schema)
+      JSON::Validator.schema_reader = JSON::Schema::Reader.new(accept_uri: false, accept_file: false)
+
+      begin
+        JSON::Validator.validate!(schema, {})
+      rescue JSON::Schema::SchemaError => e
+        errors.add("Schema not valid. Custom meta schemas are not supported. #{e}")
+      rescue JSON::Schema::ReadRefused => e
+        errors.add("Schema not valid. No external references are allowed: #{e}")
+      rescue JSON::Schema::ValidationError
+        # We only care that there are no external references.
+      rescue => e
+        errors.add("Schema not valid. #{e}")
+      end
     end
   end
 end
