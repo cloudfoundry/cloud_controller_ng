fae25cb9c6b9a955dee846b84c15da9eb4c870b0
Tim Downey <tdowney@pivotal.io>
2017-07-14 08:54:06 -0700

    AddConstraintName cop now checks add_column methods
    - Updates AddConstraintName Rubocop cop to check add_column, column,
      String, and Integer Sequel methods to make sure that a constraint name
      is being specified if the column has an optional index or constraint
    - Many column adding methods in Sequel are metaprogrammed (e.g. String,
      Integer, etc.) so the cop does not cover all possibilities
    
    [#148173629]

--- spec/linters/migration/add_constraint_name.rb
+++ spec/linters/migration/add_constraint_name.rb
@@ -8,27 +8,78 @@ module RuboCop
           add_unique_constraint add_constraint add_foreign_key add_index add_primary_key add_full_text_index add_spatial_index
           unique_constraint constraint foreign_key index primary_key full_text_index spatial_index
         }.freeze
+        COLUMN_ADDING_METHODS = %i{
+          add_column column String Integer
+        }.freeze
 
         def on_block(node)
           node.each_descendant(:send) do |send_node|
-            next unless CONSTRAINT_METHODS.include?(method_name(send_node))
+            method = method_name(send_node)
+            next unless constraint_adding_method?(method) || column_adding_method?(method)
 
             opts = send_node.children.last
-            has_named_constraint = false
+            missing_named_constraint = true
 
-            if opts && opts.type == :hash
-              opts.each_node(:pair) do |pair|
-                if hash_key_type(pair) == :sym && hash_key_name(pair) == :name
-                  has_named_constraint = true
-                end
+            if opts
+              if constraint_adding_method?(method)
+                missing_named_constraint = add_constraint_missing_name?(opts)
+              elsif column_adding_method?(method)
+                missing_named_constraint = add_column_missing_name?(opts)
               end
             end
 
-            add_offense(send_node, :expression) unless has_named_constraint
+            add_offense(send_node, :expression) if missing_named_constraint
           end
         end
 
         private
+
+        def constraint_adding_method?(method)
+          CONSTRAINT_METHODS.include?(method)
+        end
+
+        def column_adding_method?(method)
+          COLUMN_ADDING_METHODS.include?(method)
+        end
+
+        def add_constraint_missing_name?(opts)
+          return true unless opts.type == :hash
+
+          opts.each_node(:pair) do |pair|
+            return false if hash_key_type(pair) == :sym && hash_key_name(pair) == :name
+          end
+
+          true
+        end
+
+        def add_column_missing_name?(opts)
+          return true if opts.type == :sym && %i{index primary_key unique}.include?(sym_opts_name(opts))
+
+          needs_named_index             = false
+          needs_named_primary_key       = false
+          needs_named_unique_constraint = false
+
+          opts.each_node(:pair) do |pair|
+            next unless hash_key_type(pair) == :sym
+            case hash_key_name(pair)
+            when :index then needs_named_index = true
+            when :primary_key then needs_named_primary_key = true
+            when :unique then needs_named_unique_constraint = true
+            end
+          end
+
+          opts.each_node(:pair) do |pair|
+            next unless hash_key_type(pair) == :sym
+
+            case hash_key_name(pair)
+            when :name then needs_named_index = false
+            when :primary_key_constraint_name then needs_named_primary_key = false
+            when :unique_constraint_name then needs_named_unique_constraint = false
+            end
+          end
+
+          [needs_named_index, needs_named_primary_key, needs_named_unique_constraint].any?
+        end
 
         def method_name(node)
           node.children[1]
@@ -40,6 +91,10 @@ module RuboCop
 
         def hash_key_name(pair)
           pair.children[0].children[0]
+        end
+
+        def sym_opts_name(opts)
+          opts.children[0]
         end
       end
     end
--- spec/linters/migration/add_constraint_name_spec.rb
+++ spec/linters/migration/add_constraint_name_spec.rb
@@ -31,6 +31,93 @@ RSpec.describe RuboCop::Cop::Migration::AddConstraintName do
     end
   end
 
+  RSpec.shared_examples 'a cop that validates explicit names are used when adding a column with an index' do |method_name|
+    context 'and the column is adding an index' do
+      it 'registers an offense if index is called without a name' do
+        inspect_source(cop, [
+          'create_table :jobs do',
+          "#{method_name} :foo, :index",
+          'end'
+        ])
+
+        expect(cop.offenses.size).to eq(1)
+        expect(cop.messages).to eq(['Please explicitly name your index or constraint.'])
+      end
+
+      it 'does not register an offense if index is called with a name' do
+        inspect_source(cop, [
+          'create_table :jobs do',
+          "#{method_name} :foo, index: {name: 'foo'}",
+          'end'
+        ])
+
+        expect(cop.offenses.size).to eq(0)
+        expect(cop.messages).to be_empty
+      end
+    end
+
+    context 'and the column is adding a unique constraint' do
+      it 'registers an offense if unique is called without a unique_constraint_name' do
+        inspect_source(cop, [
+          'create_table :jobs do',
+          "#{method_name} :foo, unique: true",
+          'end'
+        ])
+
+        expect(cop.offenses.size).to eq(1)
+        expect(cop.messages).to eq(['Please explicitly name your index or constraint.'])
+      end
+
+      it 'does not register an offense if unique is called with a unique_constraint_name' do
+        inspect_source(cop, [
+          'create_table :jobs do',
+          "#{method_name} :foo, unique: true, unique_constraint_name: 'something_real_unique'",
+          'end'
+        ])
+
+        expect(cop.offenses.size).to eq(0)
+        expect(cop.messages).to be_empty
+      end
+    end
+
+    context 'and the column is adding a primary_key constraint' do
+      it 'registers an offense if unique is called without a primary_key_constraint_name' do
+        inspect_source(cop, [
+          'create_table :jobs do',
+          "#{method_name} :foo, primary_key: true",
+          'end'
+        ])
+
+        expect(cop.offenses.size).to eq(1)
+        expect(cop.messages).to eq(['Please explicitly name your index or constraint.'])
+      end
+
+      it 'does not register an offense if primary_key is called with a primary_key_constraint_name' do
+        inspect_source(cop, [
+          'create_table :jobs do',
+          "#{method_name} :foo, primary_key: true, primary_key_constraint_name: 'something_real_unique'",
+          'end'
+        ])
+
+        expect(cop.offenses.size).to eq(0)
+        expect(cop.messages).to be_empty
+      end
+    end
+
+    context 'and the column is not adding any index or constraint' do
+      it 'does not register an offense' do
+        inspect_source(cop, [
+          'create_table :jobs do',
+          "#{method_name} :foo",
+          'end'
+        ])
+
+        expect(cop.offenses.size).to eq(0)
+        expect(cop.messages).to be_empty
+      end
+    end
+  end
+
   subject(:cop) { described_class.new(RuboCop::Config.new({})) }
 
   context 'when the method is add_unique_constraint' do
@@ -87,5 +174,21 @@ RSpec.describe RuboCop::Cop::Migration::AddConstraintName do
 
   context 'when the method is spatial_index' do
     it_behaves_like 'a cop that validates explicit names are added to the index', :spatial_index
+  end
+
+  context 'when the method is add_column' do
+    it_behaves_like 'a cop that validates explicit names are used when adding a column with an index', :add_column
+  end
+
+  context 'when the method is column' do
+    it_behaves_like 'a cop that validates explicit names are used when adding a column with an index', :column
+  end
+
+  context 'when the method is String' do
+    it_behaves_like 'a cop that validates explicit names are used when adding a column with an index', :String
+  end
+
+  context 'when the method is Integer' do
+    it_behaves_like 'a cop that validates explicit names are used when adding a column with an index', :Integer
   end
 end
