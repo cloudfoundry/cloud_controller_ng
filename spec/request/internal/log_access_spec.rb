206b9d514394106ea2871e0abf034bba69c9a85c
Lisa Cho <lcho@pivotal.io>
2017-07-20 16:19:15 -0700

    Fix linting errors
    Signed-off-by: Tim Downey <tdowney@pivotal.io>

--- spec/request/internal/log_access_spec.rb
+++ spec/request/internal/log_access_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 RSpec.describe 'Internal Log Access Endpoint' do
-  let(:user) {VCAP::CloudController::User.make}
-  let(:user_header) {headers_for(user)}
-  let(:space) {VCAP::CloudController::Space.make}
+  let(:user) { VCAP::CloudController::User.make }
+  let(:user_header) { headers_for(user) }
+  let(:space) { VCAP::CloudController::Space.make }
   let(:app_model) { VCAP::CloudController::AppModel.make(space: space) }
 
   before do
