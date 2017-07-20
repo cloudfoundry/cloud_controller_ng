130f6dd58276b392356ec9a116a954d2c0e4514a
Lisa Cho <lcho@pivotal.io>
2017-07-20 15:57:23 -0700

    Add mtls-secured endpoint for /internal/v4/log_access/:guid [finishes #148922709]
    Signed-off-by: Tim Downey <tdowney@pivotal.io>
    Signed-off-by: Lisa Cho <lcho@pivotal.io>
    Signed-off-by: Tim Downey <tdowney@pivotal.io>

--- app/controllers/internal/log_access_controller.rb
+++ app/controllers/internal/log_access_controller.rb
@@ -3,6 +3,7 @@ require 'fetchers/log_access_fetcher'
 module VCAP::CloudController
   class LogAccessController < RestController::BaseController
     get '/internal/log_access/:guid', :lookup
+    get '/internal/v4/log_access/:guid', :lookup
     def lookup(guid)
       check_read_permissions!
 
---
+++ spec/request/internal/log_access_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+RSpec.describe 'Internal Log Access Endpoint' do
+  let(:user) {VCAP::CloudController::User.make}
+  let(:user_header) {headers_for(user)}
+  let(:space) {VCAP::CloudController::Space.make}
+  let(:app_model) { VCAP::CloudController::AppModel.make(space: space) }
+
+  before do
+    space.organization.add_user(user)
+    space.add_developer(user)
+  end
+
+  describe 'GET /internal/log_access/:app_guid' do
+    context 'when the user has access to view the logs for the app' do
+      it 'returns 200' do
+        get "/internal/log_access/#{app_model.guid}", nil, user_header
+        expect(last_response.status).to eq(200)
+      end
+    end
+  end
+
+  describe 'GET /internal/v4/log_access/:app_guid' do
+    context 'when the user has access to view the logs for the app' do
+      it 'returns 200' do
+        get "/internal/v4/log_access/#{app_model.guid}", nil, user_header
+        expect(last_response.status).to eq(200)
+      end
+    end
+  end
+end
