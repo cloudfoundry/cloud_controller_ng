e06d71dfaa0bcb04dd3916f1199d33eb0fa74716
Lisa Cho <lcho@pivotal.io>
2017-07-21 12:01:12 -0700

    Update docker password limits
    - Reduce column size from text to 16k to conform to new rubocop
    - Reduce application limit to 1k. This was the size specified in the
    story and gives plenty of room for the additional space used when the
    password is encrypted
    
    [finishes #148468913 again]
    
    Signed-off-by: Lisa Cho <lcho@pivotal.io>

--- db/migrations/20170720233439_change_encrypted_docker_password_on_packages.rb
+++ db/migrations/20170720233439_change_encrypted_docker_password_on_packages.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   change do
     alter_table :packages do
-      set_column_type :encrypted_docker_password, String, text: true
+      set_column_type :encrypted_docker_password, String, size: 16_000
     end
   end
 end
