03c00d56a14f11b493a533d89c76269139a1199b
Matt Royal <mroyal@pivotal.io>
2017-08-04 12:16:07 -0700

    Further cleanup ProcessesController with before_action
    [#142818469]

--- app/controllers/v3/processes_controller.rb
+++ app/controllers/v3/processes_controller.rb
@@ -16,6 +16,9 @@ require 'controllers/v3/mixins/sub_resource'
 class ProcessesController < ApplicationController
   include SubResource
 
+  before_action :find_process_and_space, except: :index
+  before_action :ensure_can_write, only: %i(update terminate scale)
+
   def index
     message = ProcessesListMessage.from_params(subresource_query_params)
     invalid_param!(message.errors.full_messages) unless message.valid?
@@ -35,29 +38,22 @@ class ProcessesController < ApplicationController
   end
 
   def show
-    process, space = find_process_and_space
-    render status: :ok, json: Presenters::V3::ProcessPresenter.new(process, show_secrets: can_see_secrets?(space))
+    render status: :ok, json: Presenters::V3::ProcessPresenter.new(@process, show_secrets: can_see_secrets?(@space))
   end
 
   def update
     message = ProcessUpdateMessage.create_from_http_request(unmunged_body)
     unprocessable!(message.errors.full_messages) unless message.valid?
 
-    process, space = find_process_and_space
-    unauthorized! unless can_write?(space.guid)
+    ProcessUpdate.new(user_audit_info).update(@process, message)
 
-    ProcessUpdate.new(user_audit_info).update(process, message)
-
-    render status: :ok, json: Presenters::V3::ProcessPresenter.new(process)
+    render status: :ok, json: Presenters::V3::ProcessPresenter.new(@process)
   rescue ProcessUpdate::InvalidProcess => e
     unprocessable!(e.message)
   end
 
   def terminate
-    process, space = find_process_and_space
-    unauthorized! unless can_write?(space.guid)
-
-    ProcessTerminate.new(user_audit_info, process, params[:index].to_i).terminate
+    ProcessTerminate.new(user_audit_info, @process, params[:index].to_i).terminate
 
     head :no_content
   rescue ProcessTerminate::InstanceNotFound
@@ -70,35 +66,34 @@ class ProcessesController < ApplicationController
     message = ProcessScaleMessage.create_from_http_request(params[:body])
     unprocessable!(message.errors.full_messages) if message.invalid?
 
-    process, space = find_process_and_space
-    unauthorized! unless can_write?(space.guid)
+    ProcessScale.new(user_audit_info, @process, message).scale
 
-    ProcessScale.new(user_audit_info, process, message).scale
-
-    render status: :accepted, json: Presenters::V3::ProcessPresenter.new(process)
+    render status: :accepted, json: Presenters::V3::ProcessPresenter.new(@process)
   rescue ProcessScale::InvalidProcess => e
     unprocessable!(e.message)
   end
 
   def stats
-    process, _space = find_process_and_space
-    process_stats   = instances_reporters.stats_for_app(process)
+    process_stats = instances_reporters.stats_for_app(@process)
 
-    render status: :ok, json: Presenters::V3::ProcessStatsPresenter.new(process.type, process_stats)
+    render status: :ok, json: Presenters::V3::ProcessStatsPresenter.new(@process.type, process_stats)
   end
 
   private
 
   def find_process_and_space
     if app_nested?
-      process, app, space, org = ProcessFetcher.new.fetch_for_app_by_type(app_guid: params[:app_guid], process_type: params[:type])
-      app_not_found! unless app && can_read?(space.guid, org.guid)
-      process_not_found! unless process
+      @process, app, @space, org = ProcessFetcher.new.fetch_for_app_by_type(app_guid: params[:app_guid], process_type: params[:type])
+      app_not_found! unless app && can_read?(@space.guid, org.guid)
+      process_not_found! unless @process
     else
-      process, space, org = ProcessFetcher.new.fetch(process_guid: params[:process_guid])
-      process_not_found! unless process && can_read?(space.guid, org.guid)
+      @process, @space, org = ProcessFetcher.new.fetch(process_guid: params[:process_guid])
+      process_not_found! unless @process && can_read?(@space.guid, org.guid)
     end
-    [process, space]
+  end
+
+  def ensure_can_write
+    unauthorized! unless can_write?(@space.guid)
   end
 
   def process_not_found!
