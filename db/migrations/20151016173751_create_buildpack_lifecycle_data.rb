67e2fdbe908e36250f531056548f342e61f1dc8a
Lyle Franklin <lfranklin@pivotal.io>
2017-08-09 15:56:37 -0700

    Allow operator to configure SSL to CCDB
    - Adds two new properties:
      - `ccdb.ca_cert`, turns on SSL, verifies the DB certificate is signed
        by this CA
      - `ccdb.verify_ssl_hostname`, in addition to checking that CA is
        signed, verifies that CN in the database's certificate matches the
        hostname used to connect to the database
    - Had to bump sequel to avoid a bug where SSL options were not forwarded
      to postgres
    - Had to remove some outer transaction in migrations due to a bug around
      savepoints in mysql
      - https://github.com/jeremyevans/sequel/issues/1407
      - Sequel always runs postgres migrations in a transaction, so behavior is
        unchanged. MySQL does not support DDLs in transactions, so behavior is
        also unchanged.
    
    Signed-off-by: Tim Downey <tdowney@pivotal.io>
    
    [#132950039]
    
    Signed-off-by: Tim Downey <tdowney@pivotal.io>

--- db/migrations/20151016173751_create_buildpack_lifecycle_data.rb
+++ db/migrations/20151016173751_create_buildpack_lifecycle_data.rb
@@ -1,53 +1,53 @@
 Sequel.migration do
   up do
-    transaction do
-      alter_table(:v3_droplets) do
-        drop_foreign_key [:app_guid]
-      end
-      alter_table(:packages) do
-        drop_foreign_key [:app_guid]
-      end
-      alter_table(:apps) do
-        drop_foreign_key [:app_guid]
-        drop_index :app_guid
-      end
-      alter_table(:apps_v3_routes) do
-        drop_foreign_key :app_v3_id
-      end
+    alter_table(:v3_droplets) do
+      drop_foreign_key [:app_guid]
+    end
+    alter_table(:packages) do
+      drop_foreign_key [:app_guid]
+    end
+    alter_table(:apps) do
+      drop_foreign_key [:app_guid]
+      drop_index :app_guid
+    end
+    alter_table(:apps_v3_routes) do
+      drop_foreign_key :app_v3_id
+    end
 
+    transaction do
       run 'DELETE FROM apps_routes WHERE app_id IN (SELECT id FROM apps WHERE app_guid IS NOT NULL);'
       run 'DELETE FROM apps WHERE app_guid IS NOT NULL;'
-      self[:apps_v3_routes].truncate
-      self[:apps_v3].truncate
-      self[:v3_droplets].truncate
-      self[:packages].truncate
+    end
+    self[:apps_v3_routes].truncate
+    self[:apps_v3].truncate
+    self[:v3_droplets].truncate
+    self[:packages].truncate
 
-      alter_table(:apps_v3_routes) do
-        add_foreign_key :app_v3_id, :apps_v3
-      end
-      alter_table(:apps) do
-        add_index :app_guid
-        add_foreign_key [:app_guid], :apps_v3, key: :guid
-      end
-      alter_table(:packages) do
-        add_foreign_key [:app_guid], :apps_v3, key: :guid
-      end
-      alter_table(:v3_droplets) do
-        add_foreign_key [:app_guid], :apps_v3, key: :guid
-      end
+    alter_table(:apps_v3_routes) do
+      add_foreign_key :app_v3_id, :apps_v3
+    end
+    alter_table(:apps) do
+      add_index :app_guid
+      add_foreign_key [:app_guid], :apps_v3, key: :guid
+    end
+    alter_table(:packages) do
+      add_foreign_key [:app_guid], :apps_v3, key: :guid
+    end
+    alter_table(:v3_droplets) do
+      add_foreign_key [:app_guid], :apps_v3, key: :guid
+    end
 
-      create_table :buildpack_lifecycle_data do
-        VCAP::Migration.common(self, :buildpack_lifecycle_data)
+    create_table :buildpack_lifecycle_data do
+      VCAP::Migration.common(self, :buildpack_lifecycle_data)
 
-        String :app_guid
-        index :app_guid, name: :buildpack_lifecycle_data_app_guid
+      String :app_guid
+      index :app_guid, name: :buildpack_lifecycle_data_app_guid
 
-        String :droplet_guid
-        index :droplet_guid, name: :bp_lifecycle_data_droplet_guid
+      String :droplet_guid
+      index :droplet_guid, name: :bp_lifecycle_data_droplet_guid
 
-        String :stack
-        String :buildpack
-      end
+      String :stack
+      String :buildpack
     end
   end
 
