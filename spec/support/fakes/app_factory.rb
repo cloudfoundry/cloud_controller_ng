941d92a63638b75ed1be4119ad9af977ce81bd3e
Lisa Cho <lcho@pivotal.io>
2017-08-15 15:00:11 -0700

    Refactor AppFactory.make
    [#150018172]
    
    Signed-off-by: Matt Royal <mroyal@pivotal.io>

--- spec/support/fakes/app_factory.rb
+++ spec/support/fakes/app_factory.rb
@@ -1,20 +1,40 @@
 module VCAP
   module CloudController
     class AppFactory
-      def self.make(*args)
-        parent_app_attributes = args.extract_options!.symbolize_keys
-        attributes            = parent_app_attributes.slice!(:name, :space, :environment_json, :stack)
-        package_attributes    = attributes
-        attributes            = package_attributes.slice!(:docker_image, :docker_credentials)
+      APP_ATTRIBUTES     = %i(name space environment_json stack).freeze
+      PACKAGE_ATTRIBUTES = %i(docker_image docker_credentials).freeze
 
-        defaults   = { metadata: {} }
-        attributes = defaults.merge(attributes)
+      class << self
+        def make(*args)
+          options               = args.extract_options!.symbolize_keys
+          parent_app_attributes = options.slice(*APP_ATTRIBUTES)
+          package_attributes    = options.slice(*PACKAGE_ATTRIBUTES)
+          process_attributes    = options.except(*APP_ATTRIBUTES, *PACKAGE_ATTRIBUTES)
 
-        parent_app = attributes[:app]
-        unless parent_app
+          defaults           = { metadata: {} }
+          process_attributes = defaults.merge(process_attributes)
+
+          parent_app                = make_parent_app(package_attributes, parent_app_attributes, process_attributes)
+          process_attributes[:app]  = parent_app
+          process_attributes[:guid] = parent_app.guid if process_attributes[:type] == 'web' || process_attributes[:type].nil?
+
+          package = make_package(package_attributes, parent_app)
+
+          build   = BuildModel.make(app: parent_app, package: package)
+          droplet = DropletModel.make(app: parent_app, build: build, package: package)
+          parent_app.update(droplet_guid: droplet.guid)
+
+          VCAP::CloudController::ProcessModel.make(*args, process_attributes)
+        end
+
+        private
+
+        def make_parent_app(package_attributes, parent_app_attributes, process_attributes)
+          return process_attributes[:app] if process_attributes[:app]
+
           parent_app_blueprint_type = package_attributes[:docker_image].present? ? :docker : nil
-          if attributes.key?(:state)
-            parent_app_attributes[:desired_state] = attributes[:state]
+          if process_attributes.key?(:state)
+            parent_app_attributes[:desired_state] = process_attributes[:state]
           end
 
           if parent_app_attributes.any?
@@ -34,28 +54,21 @@ module VCAP
           else
             parent_app = VCAP::CloudController::AppModel.make(parent_app_blueprint_type, parent_app_attributes)
           end
-          attributes[:app] = parent_app
+          process_attributes[:app] = parent_app
+          parent_app
         end
-        attributes[:app] = parent_app
-        attributes[:guid] = parent_app.guid if attributes[:type] == 'web' || attributes[:type].nil?
 
-        args << attributes
-
-        package = if package_attributes.empty?
-                    VCAP::CloudController::PackageModel.make(app: parent_app, state: PackageModel::READY_STATE, package_hash: Sham.guid)
-                  else
-                    docker_credentials = package_attributes[:docker_credentials].nil? ? {} : package_attributes[:docker_credentials]
-                    VCAP::CloudController::PackageModel.make(:docker, app: parent_app,
-                                                                      docker_image: package_attributes[:docker_image],
-                                                                      docker_username: docker_credentials['username'],
-                                                                      docker_password: docker_credentials['password'])
-                  end
-
-        build = BuildModel.make(app: parent_app, package: package)
-        droplet = DropletModel.make(app: parent_app, build: build, package: package)
-        parent_app.update(droplet_guid: droplet.guid)
-
-        VCAP::CloudController::ProcessModel.make(*args)
+        def make_package(package_attributes, parent_app)
+          if package_attributes.empty?
+            VCAP::CloudController::PackageModel.make(app: parent_app, state: PackageModel::READY_STATE, package_hash: Sham.guid)
+          else
+            docker_credentials = package_attributes[:docker_credentials].nil? ? {} : package_attributes[:docker_credentials]
+            VCAP::CloudController::PackageModel.make(:docker, app: parent_app,
+                                                              docker_image:                                        package_attributes[:docker_image],
+                                                              docker_username:                                     docker_credentials['username'],
+                                                              docker_password:                                     docker_credentials['password'])
+          end
+        end
       end
     end
   end
