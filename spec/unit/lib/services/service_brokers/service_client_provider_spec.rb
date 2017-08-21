cc5c14e4bbf9ce5f442c5ebcc4052a0ecbd39a82
Luis Urraca <lurraca@pivotal.io>
2017-08-21 11:17:17 +0100

    Add ServiceClientProvider to conditionally instantiate Service Brokers clients
    [#150294263]
    
    Signed-off-by: Alberto Rios <arios@pivotal.io>

---
+++ spec/unit/lib/services/service_brokers/service_client_provider_spec.rb
@@ -0,0 +1,103 @@
+require 'spec_helper'
+
+RSpec.describe VCAP::Services::ServiceClientProvider do
+  describe '#provide' do
+    context 'service instances' do
+      context 'when the instance is a UserProvidedServiceInstance' do
+        let(:service_instance) { VCAP::CloudController::UserProvidedServiceInstance.make }
+
+        before do
+          allow(VCAP::Services::ServiceBrokers::UserProvided::Client).to receive(:new).and_call_original
+        end
+
+        it 'returns a client for a user provided service' do
+          VCAP::Services::ServiceClientProvider.provide(instance: service_instance)
+          expect(VCAP::Services::ServiceBrokers::UserProvided::Client).to have_received(:new)
+        end
+      end
+
+      context 'when the instance is a ManagedServiceInstance' do
+        let(:service_instance) { VCAP::CloudController::ManagedServiceInstance.make }
+        let(:expected_attrs) do
+          {
+            url: service_instance.service_broker.broker_url,
+            auth_username: service_instance.service_broker.auth_username,
+            auth_password: service_instance.service_broker.auth_password
+          }
+        end
+
+        before do
+          allow(VCAP::Services::ServiceBrokers::V2::Client).to receive(:new).and_call_original
+        end
+
+        it 'returns a service broker client' do
+          VCAP::Services::ServiceClientProvider.provide(instance: service_instance)
+          expect(VCAP::Services::ServiceBrokers::V2::Client).to have_received(:new).with(expected_attrs)
+        end
+      end
+    end
+
+    context 'service bindings' do
+      context 'when bound to a user provided service instance' do
+        let(:service_instance) { VCAP::CloudController::UserProvidedServiceInstance.make }
+        let(:binding) { VCAP::CloudController::ServiceBinding.make(service_instance: service_instance) }
+
+        before do
+          allow(VCAP::Services::ServiceBrokers::UserProvided::Client).to receive(:new).and_call_original
+        end
+
+        it 'returns a client for a user provided service' do
+          VCAP::Services::ServiceClientProvider.provide(binding: binding)
+          expect(VCAP::Services::ServiceBrokers::UserProvided::Client).to have_received(:new)
+        end
+      end
+
+      context 'when bound to a managed service instance' do
+        let(:binding) { VCAP::CloudController::ServiceBinding.make }
+        let(:expected_attrs) do
+          {
+            url: binding.service_instance.service_broker.broker_url,
+            auth_username: binding.service_instance.service_broker.auth_username,
+            auth_password: binding.service_instance.service_broker.auth_password
+          }
+        end
+
+        before do
+          allow(VCAP::Services::ServiceBrokers::V2::Client).to receive(:new).and_call_original
+        end
+
+        it 'return a service broker client' do
+          VCAP::Services::ServiceClientProvider.provide(binding: binding)
+          expect(VCAP::Services::ServiceBrokers::V2::Client).to have_received(:new).with(expected_attrs)
+        end
+      end
+    end
+
+    context 'service brokers' do
+      let(:broker) { VCAP::CloudController::ServiceBroker.make }
+      let(:expected_attrs) do
+        {
+          url: broker.broker_url,
+          auth_username: broker.auth_username,
+          auth_password: broker.auth_password
+        }
+      end
+
+      before do
+        allow(VCAP::Services::ServiceBrokers::V2::Client).to receive(:new).and_call_original
+      end
+
+      it 'returns a client for a broker' do
+        VCAP::Services::ServiceClientProvider.provide(broker: broker)
+        expect(VCAP::Services::ServiceBrokers::V2::Client).to have_received(:new).with(expected_attrs)
+      end
+    end
+
+    context 'when no binding or service instance' do
+      it 'returns nil' do
+        client = VCAP::Services::ServiceClientProvider.provide
+        expect(client).to be_nil
+      end
+    end
+  end
+end
