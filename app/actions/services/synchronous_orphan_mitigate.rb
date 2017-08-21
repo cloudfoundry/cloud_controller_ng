3d7101585507e43918b7ad389ded4808579e8eb0
Alberto Rios <arios@pivotal.io>
2017-08-21 15:33:56 +0100

    Refactored Asynch Orphan Mitigate to use ServiceClientProvider
    [#150294263]
    
    Signed-off-by: Luis Urraca <lurraca@pivotal.io>

--- app/actions/services/synchronous_orphan_mitigate.rb
+++ app/actions/services/synchronous_orphan_mitigate.rb
@@ -6,7 +6,8 @@ module VCAP::CloudController
 
     def attempt_deprovision_instance(service_instance)
       @logger.info "Attempting synchronous orphan mitigation for service instance #{service_instance.guid}"
-      service_instance.client.deprovision(service_instance)
+      client = VCAP::Services::ServiceClientProvider.provide(instance: service_instance)
+      client.deprovision(service_instance)
       @logger.info "Success deprovisioning orphaned service instance #{service_instance.guid}"
     rescue => e
       @logger.error "Unable to deprovision orphaned service instance #{service_instance.guid}: #{e}"
@@ -14,7 +15,8 @@ module VCAP::CloudController
 
     def attempt_delete_key(service_key)
       @logger.info "Attempting synchronous orphan mitigation for service key #{service_key.guid}"
-      service_key.client.unbind(service_key)
+      client = VCAP::Services::ServiceClientProvider.provide(binding: service_key)
+      client.unbind(service_key)
       @logger.info "Success deleting orphaned service key #{service_key.guid}"
     rescue => e
       @logger.error "Unable to delete orphaned service key #{service_key.guid}: #{e}"
@@ -22,7 +24,8 @@ module VCAP::CloudController
 
     def attempt_unbind(service_binding)
       @logger.info "Attempting synchronous orphan mitigation for service binding #{service_binding.guid}"
-      service_binding.client.unbind(service_binding)
+      client = VCAP::Services::ServiceClientProvider.provide(binding: service_binding)
+      client.unbind(service_binding)
       @logger.info "Success unbinding orphaned service binding #{service_binding.guid}"
     rescue => e
       @logger.error "Unable to delete orphaned service binding #{service_binding.guid}: #{e}"
