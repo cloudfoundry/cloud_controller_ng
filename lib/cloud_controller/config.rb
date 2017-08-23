3d028d58f0f9fa134982a04620355ea7b0ce8825
Tim Downey <tdowney@pivotal.io>
2017-08-23 09:39:57 -0700

    wip - passing cats
    Signed-off-by: Lisa Cho <lcho@pivotal.io>
    Signed-off-by: Aakash Shah <ashah@pivotal.io>
    Signed-off-by: Matt Royal <mroyal@pivotal.io>

--- lib/cloud_controller/config.rb
+++ lib/cloud_controller/config.rb
@@ -58,11 +58,6 @@ module VCAP::CloudController
           url: String
         },
 
-        :hm9000 => {
-          url: String,
-          internal_url: String
-        },
-
         optional(:dea_client) => {
           ca_file: String,
           cert_file: String,
@@ -121,8 +116,6 @@ module VCAP::CloudController
             password: String,
           }
         },
-
-        :cc_partition => String,
 
         optional(:default_account_capacity) => {
           memory: Integer,   #:default => 2048,
@@ -196,8 +189,6 @@ module VCAP::CloudController
         },
 
         :db_encryption_key => String,
-
-        optional(:flapping_crash_count_threshold) => Integer,
 
         optional(:varz_port) => Integer,
         optional(:varz_user) => String,
@@ -303,6 +294,8 @@ module VCAP::CloudController
         },
 
         optional(:perform_blob_cleanup) => bool,
+
+        optional(:allow_app_ssh_access) => bool,
       }
     end
 
