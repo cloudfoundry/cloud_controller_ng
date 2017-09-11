9fe077cc5335b9bbdf9a4ea1b734f6cc3c4b01f0
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-09-11 10:47:50 -0700

    Fix test description
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

--- spec/integration/perm_spec.rb
+++ spec/integration/perm_spec.rb
@@ -21,20 +21,18 @@ RSpec.describe 'Perm', type: :integration, skip: ENV.fetch('CF_RUN_PERM_SPECS') 
   describe 'PUT /v2/organizations/:guid/managers/:user_guid' do
     let(:org_manager) { VCAP::CloudController::User.make }
 
-    describe 'removing the last org manager' do
-      context 'as an admin' do
-        it 'is allowed' do
-          set_current_user_as_admin
+    context 'as an admin' do
+      it 'assigns the specified user to the org manager role' do
+        set_current_user_as_admin
 
-          expect(client.list_actor_roles(org_manager.guid)).to be_empty
+        expect(client.list_actor_roles(org_manager.guid)).to be_empty
 
-          put "/v2/organizations/#{org.guid}/managers/#{org_manager.guid}"
-          expect(last_response.status).to eq(201)
+        put "/v2/organizations/#{org.guid}/managers/#{org_manager.guid}"
+        expect(last_response.status).to eq(201)
 
-          roles = client.list_actor_roles(org_manager.guid)
-          expect(roles).not_to be_empty
-          expect(roles[0].name).to eq "org-manager-#{org.guid}"
-        end
+        roles = client.list_actor_roles(org_manager.guid)
+        expect(roles).not_to be_empty
+        expect(roles[0].name).to eq "org-manager-#{org.guid}"
       end
     end
   end
