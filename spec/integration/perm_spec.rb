20773bcaa9d0892cdb432d89573963b213c36744
Chris Hendrix <chendrix@pivotal.io>
2017-09-11 12:09:26 -0700

    Add tests for assigning all org roles in Perm
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- spec/integration/perm_spec.rb
+++ spec/integration/perm_spec.rb
@@ -36,4 +36,61 @@ RSpec.describe 'Perm', type: :integration, skip: ENV.fetch('CF_RUN_PERM_SPECS') 
       end
     end
   end
+
+  describe 'PUT /v2/organizations/:guid/auditors/:user_guid' do
+    let(:org_auditor) { VCAP::CloudController::User.make }
+
+    context 'as an admin' do
+      it 'assigns the specified user to the org auditor role' do
+        set_current_user_as_admin
+
+        expect(client.list_actor_roles(org_auditor.guid)).to be_empty
+
+        put "/v2/organizations/#{org.guid}/auditors/#{org_auditor.guid}"
+        expect(last_response.status).to eq(201)
+
+        roles = client.list_actor_roles(org_auditor.guid)
+        expect(roles).not_to be_empty
+        expect(roles[0].name).to eq "org-auditor-#{org.guid}"
+      end
+    end
+  end
+
+  describe 'PUT /v2/organizations/:guid/billing_managers/:user_guid' do
+    let(:org_billing_manager) { VCAP::CloudController::User.make }
+
+    context 'as an admin' do
+      it 'assigns the specified user to the org billing manager role' do
+        set_current_user_as_admin
+
+        expect(client.list_actor_roles(org_billing_manager.guid)).to be_empty
+
+        put "/v2/organizations/#{org.guid}/billing_managers/#{org_billing_manager.guid}"
+        expect(last_response.status).to eq(201)
+
+        roles = client.list_actor_roles(org_billing_manager.guid)
+        expect(roles).not_to be_empty
+        expect(roles[0].name).to eq "org-billing_manager-#{org.guid}"
+      end
+    end
+  end
+
+  describe 'PUT /v2/organizations/:guid/users/:user_guid' do
+    let(:org_user) { VCAP::CloudController::User.make }
+
+    context 'as an admin' do
+      it 'assigns the specified user to the org billing manager role' do
+        set_current_user_as_admin
+
+        expect(client.list_actor_roles(org_user.guid)).to be_empty
+
+        put "/v2/organizations/#{org.guid}/users/#{org_user.guid}"
+        expect(last_response.status).to eq(201)
+
+        roles = client.list_actor_roles(org_user.guid)
+        expect(roles).not_to be_empty
+        expect(roles[0].name).to eq "org-user-#{org.guid}"
+      end
+    end
+  end
 end
