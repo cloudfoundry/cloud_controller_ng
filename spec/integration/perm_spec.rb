fb1f57681cd4d34104fd6f8cd163547bbe48a2eb
Chris Hendrix <chendrix@pivotal.io>
2017-09-11 17:32:52 -0700

    Notify perm whenever space roles are assigned
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- spec/integration/perm_spec.rb
+++ spec/integration/perm_spec.rb
@@ -4,9 +4,8 @@ require 'perm'
 RSpec.describe 'Perm', type: :integration, skip: ENV.fetch('CF_RUN_PERM_SPECS') { 'false' } != 'true' do
   include ControllerHelpers
 
-  let(:org) { VCAP::CloudController::Organization.make }
   let(:assigner) { VCAP::CloudController::IsolationSegmentAssign.new }
-  let(:user_email) { VCAP::CloudController::Sham.email }
+  let(:assignee) { VCAP::CloudController::User.make }
 
   let(:perm_host) { ENV.fetch('PERM_RPC_HOST') { 'localhost:6283' } }
   let(:client) { CloudFoundry::Perm::V1::Client.new(perm_host) }
@@ -16,80 +15,139 @@ RSpec.describe 'Perm', type: :integration, skip: ENV.fetch('CF_RUN_PERM_SPECS') 
       enabled: true,
       host: perm_host
     }
+
+    allow_any_instance_of(VCAP::CloudController::UaaClient).to receive(:usernames_for_ids).with([assignee.guid]).and_return({ assignee.guid => assignee.username })
   end
 
-  describe 'PUT /v2/organizations/:guid/managers/:user_guid' do
-    let(:org_manager) { VCAP::CloudController::User.make }
+  describe 'assigning organization roles' do
+    let(:org) { VCAP::CloudController::Organization.make }
 
-    context 'as an admin' do
-      it 'assigns the specified user to the org manager role' do
-        set_current_user_as_admin
+    describe 'PUT /v2/organizations/:guid/managers/:user_guid' do
+      context 'as an admin' do
+        it 'assigns the specified user to the org manager role' do
+          set_current_user_as_admin
 
-        expect(client.list_actor_roles(org_manager.guid)).to be_empty
+          expect(client.list_actor_roles(assignee.guid)).to be_empty
 
-        put "/v2/organizations/#{org.guid}/managers/#{org_manager.guid}"
-        expect(last_response.status).to eq(201)
+          put "/v2/organizations/#{org.guid}/managers/#{assignee.guid}"
+          expect(last_response.status).to eq(201)
 
-        roles = client.list_actor_roles(org_manager.guid)
-        expect(roles).not_to be_empty
-        expect(roles[0].name).to eq "org-manager-#{org.guid}"
+          roles = client.list_actor_roles(assignee.guid)
+          expect(roles).not_to be_empty
+          expect(roles[0].name).to eq "org-manager-#{org.guid}"
+        end
+      end
+    end
+
+    describe 'PUT /v2/organizations/:guid/auditors/:user_guid' do
+      context 'as an admin' do
+        it 'assigns the specified user to the org auditor role' do
+          set_current_user_as_admin
+
+          expect(client.list_actor_roles(assignee.guid)).to be_empty
+
+          put "/v2/organizations/#{org.guid}/auditors/#{assignee.guid}"
+          expect(last_response.status).to eq(201)
+
+          roles = client.list_actor_roles(assignee.guid)
+          expect(roles).not_to be_empty
+          expect(roles[0].name).to eq "org-auditor-#{org.guid}"
+        end
+      end
+    end
+
+    describe 'PUT /v2/organizations/:guid/billing_managers/:user_guid' do
+      context 'as an admin' do
+        it 'assigns the specified user to the org billing manager role' do
+          set_current_user_as_admin
+
+          expect(client.list_actor_roles(assignee.guid)).to be_empty
+
+          put "/v2/organizations/#{org.guid}/billing_managers/#{assignee.guid}"
+          expect(last_response.status).to eq(201)
+
+          roles = client.list_actor_roles(assignee.guid)
+          expect(roles).not_to be_empty
+          expect(roles[0].name).to eq "org-billing_manager-#{org.guid}"
+        end
+      end
+    end
+
+    describe 'PUT /v2/organizations/:guid/users/:user_guid' do
+      context 'as an admin' do
+        it 'assigns the specified user to the org user role' do
+          set_current_user_as_admin
+
+          expect(client.list_actor_roles(assignee.guid)).to be_empty
+
+          put "/v2/organizations/#{org.guid}/users/#{assignee.guid}"
+          expect(last_response.status).to eq(201)
+
+          roles = client.list_actor_roles(assignee.guid)
+          expect(roles).not_to be_empty
+          expect(roles[0].name).to eq "org-user-#{org.guid}"
+        end
       end
     end
   end
 
-  describe 'PUT /v2/organizations/:guid/auditors/:user_guid' do
-    let(:org_auditor) { VCAP::CloudController::User.make }
+  describe 'assigning space roles' do
+    let(:org) { VCAP::CloudController::Organization.make(user_guids: [assignee.guid]) }
+    let(:space) {
+      VCAP::CloudController::Space.make(
+        organization: org,
+      )
+    }
 
-    context 'as an admin' do
-      it 'assigns the specified user to the org auditor role' do
-        set_current_user_as_admin
+    describe 'PUT /v2/spaces/:guid/managers/:user_guid' do
+      context 'as an admin' do
+        it 'assigns the specified user to the space manager role' do
+          set_current_user_as_admin
 
-        expect(client.list_actor_roles(org_auditor.guid)).to be_empty
+          expect(client.list_actor_roles(assignee.guid)).to be_empty
 
-        put "/v2/organizations/#{org.guid}/auditors/#{org_auditor.guid}"
-        expect(last_response.status).to eq(201)
+          put "/v2/spaces/#{space.guid}/managers/#{assignee.guid}"
+          expect(last_response.status).to eq(201)
 
-        roles = client.list_actor_roles(org_auditor.guid)
-        expect(roles).not_to be_empty
-        expect(roles[0].name).to eq "org-auditor-#{org.guid}"
+          roles = client.list_actor_roles(assignee.guid)
+          expect(roles).not_to be_empty
+          expect(roles[0].name).to eq "space-manager-#{space.guid}"
+        end
       end
     end
-  end
 
-  describe 'PUT /v2/organizations/:guid/billing_managers/:user_guid' do
-    let(:org_billing_manager) { VCAP::CloudController::User.make }
+    describe 'PUT /v2/spaces/:guid/auditors/:user_guid' do
+      context 'as an admin' do
+        it 'assigns the specified user to the space auditor role' do
+          set_current_user_as_admin
 
-    context 'as an admin' do
-      it 'assigns the specified user to the org billing manager role' do
-        set_current_user_as_admin
+          expect(client.list_actor_roles(assignee.guid)).to be_empty
 
-        expect(client.list_actor_roles(org_billing_manager.guid)).to be_empty
+          put "/v2/spaces/#{space.guid}/auditors/#{assignee.guid}"
+          expect(last_response.status).to eq(201)
 
-        put "/v2/organizations/#{org.guid}/billing_managers/#{org_billing_manager.guid}"
-        expect(last_response.status).to eq(201)
-
-        roles = client.list_actor_roles(org_billing_manager.guid)
-        expect(roles).not_to be_empty
-        expect(roles[0].name).to eq "org-billing_manager-#{org.guid}"
+          roles = client.list_actor_roles(assignee.guid)
+          expect(roles).not_to be_empty
+          expect(roles[0].name).to eq "space-auditor-#{space.guid}"
+        end
       end
     end
-  end
 
-  describe 'PUT /v2/organizations/:guid/users/:user_guid' do
-    let(:org_user) { VCAP::CloudController::User.make }
 
-    context 'as an admin' do
-      it 'assigns the specified user to the org billing manager role' do
-        set_current_user_as_admin
+    describe 'PUT /v2/spaces/:guid/developers/:user_guid' do
+      context 'as an admin' do
+        it 'assigns the specified user to the space developer role' do
+          set_current_user_as_admin
 
-        expect(client.list_actor_roles(org_user.guid)).to be_empty
+          expect(client.list_actor_roles(assignee.guid)).to be_empty
 
-        put "/v2/organizations/#{org.guid}/users/#{org_user.guid}"
-        expect(last_response.status).to eq(201)
+          put "/v2/spaces/#{space.guid}/developers/#{assignee.guid}"
+          expect(last_response.status).to eq(201), last_response.body
 
-        roles = client.list_actor_roles(org_user.guid)
-        expect(roles).not_to be_empty
-        expect(roles[0].name).to eq "org-user-#{org.guid}"
+          roles = client.list_actor_roles(assignee.guid)
+          expect(roles).not_to be_empty
+          expect(roles[0].name).to eq "space-developer-#{space.guid}"
+        end
       end
     end
   end
