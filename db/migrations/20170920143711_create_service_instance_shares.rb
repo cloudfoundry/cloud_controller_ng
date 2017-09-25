eac34356f50b277074c2b08d54fcaca48578c2fd
Alex Blease <ablease@pivotal.io>
2017-09-25 16:59:07 +0100

    Fix migration to include cascade on delete for foreign keys
    [#150801597]
    
    Signed-off-by: Jen Spinney <jennifer.spinney@suse.com>

--- db/migrations/20170920143711_create_service_instance_shares.rb
+++ db/migrations/20170920143711_create_service_instance_shares.rb
@@ -4,8 +4,8 @@ Sequel.migration do
       String :service_instance_guid, null: false, size: 255
       String :target_space_guid, null: false, size: 255
 
-      foreign_key [:service_instance_guid], :service_instances, key: :guid, name: :fk_service_instance_guid
-      foreign_key [:target_space_guid], :spaces, key: :guid, name: :fk_target_space_guid
+      foreign_key [:service_instance_guid], :service_instances, key: :guid, name: :fk_service_instance_guid, on_delete: :cascade
+      foreign_key [:target_space_guid], :spaces, key: :guid, name: :fk_target_space_guid, on_delete: :cascade
       primary_key [:service_instance_guid, :target_space_guid], name: :service_instance_target_space_pk
     end
   end
