22004e515121bc20b6656521234a784763b9591b
Lisa Cho <lcho@pivotal.io>
2017-10-16 16:59:53 -0700

    Use joins instead of subqueries for Processes#Index
    - Using several layers of nested subqueries is slow
    - [finishes #149198549]
    
    Signed-off-by: Matt Royal <mroyal@pivotal.io>

--- app/fetchers/process_list_fetcher.rb
+++ app/fetchers/process_list_fetcher.rb
@@ -5,32 +5,34 @@ module VCAP::CloudController
     end
 
     def fetch_all
-      filter(ProcessModel.dataset)
+      filter(process_dataset)
     end
 
     def fetch_for_spaces(space_guids:)
-      dataset = ProcessModel.dataset.where(space: Space.where(guid: space_guids))
-      filter(dataset)
+      filter(process_dataset.where(Sequel.qualify(:space, :guid) => space_guids))
     end
 
     def fetch_for_app
       app = AppModel.where(guid: @message.app_guid).eager(:space, :organization).all.first
       return nil unless app
-      dataset = app.processes_dataset
-      [app, filter(dataset)]
+      [app, filter(app.processes_dataset)]
     end
 
     private
+
+    def process_dataset
+      ProcessModel.dataset.eager_graph_with_options({ space: :organization }, join_type: :inner)
+    end
 
     def filter(dataset)
       dataset = dataset.where(type: @message.types) if @message.requested?(:types)
 
       if @message.requested?(:space_guids)
-        dataset = dataset.where(space: Space.where(guid: @message.space_guids))
+        dataset = dataset.where(Sequel.qualify(:space, :guid) => @message.space_guids)
       end
 
       if @message.requested?(:organization_guids)
-        dataset = dataset.where(space: Space.where(organization: Organization.where(guid: @message.organization_guids)))
+        dataset = dataset.where(Sequel.qualify(:organization, :guid) => @message.organization_guids)
       end
 
       if @message.requested?(:app_guids)
@@ -38,10 +40,10 @@ module VCAP::CloudController
       end
 
       if @message.requested?(:guids)
-        dataset = dataset.where(guid: @message.guids)
+        dataset = dataset.where(Sequel.qualify(:processes, :guid) => @message.guids)
       end
 
-      dataset.eager(:space)
+      dataset
     end
   end
 end
