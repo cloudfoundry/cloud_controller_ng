f7d61b5f1c9cc7606729be116b86f855ca3c8892
Annie Sing <asing@pivotal.io>
2017-11-06 14:21:20 -0800

    Change existing processes with health_check_timeout=0 to nil
    - Before adding this migration, when an app had health_check_timeout set
    to 0, it would error upon restart because we added a validation saying
    an app should never have a 0 second health_check_timeout. This migration
    tells cc to use the system wide default health_check_timeout in this
    case instead.
    
    [#124642011]

---
+++ db/migrations/20171106202032_change_processes_with_health_check_timeout_0_to_nil.rb
@@ -0,0 +1,9 @@
+Sequel.migration do
+  up do
+    transaction do
+      run <<-SQL
+        UPDATE processes SET health_check_timeout=NULL WHERE health_check_timeout='0';
+      SQL
+    end
+  end 
+end
