5bd1e4d6a4c7221d85410877f064f77abd0c267a
Greg Cobb <gcobb@pivotal.io>
2017-12-01 15:47:38 -0800

    Move SpaceCreate action into VCAP namespace

--- app/actions/space_create.rb
+++ app/actions/space_create.rb
@@ -1,23 +1,25 @@
-class SpaceCreate
-  class Error < ::StandardError
-  end
-
-  def create(org, message)
-    VCAP::CloudController::Space.create(name: message.name, organization: org)
-  rescue Sequel::ValidationFailed => e
-    validation_error!(e)
-  end
-
-  private
-
-  def validation_error!(error)
-    if error.errors.on([:organization_id, :name])&.include?(:unique)
-      error!('Name must be unique per organization')
+module VCAP::CloudController
+  class SpaceCreate
+    class Error < ::StandardError
     end
-    error!(error.message)
-  end
 
-  def error!(message)
-    raise Error.new(message)
+    def create(org, message)
+      VCAP::CloudController::Space.create(name: message.name, organization: org)
+    rescue Sequel::ValidationFailed => e
+      validation_error!(e)
+    end
+
+    private
+
+    def validation_error!(error)
+      if error.errors.on([:organization_id, :name])&.include?(:unique)
+        error!('Name must be unique per organization')
+      end
+      error!(error.message)
+    end
+
+    def error!(message)
+      raise Error.new(message)
+    end
   end
 end
--- spec/unit/actions/space_create_spec.rb
+++ spec/unit/actions/space_create_spec.rb
@@ -1,42 +1,44 @@
 require 'spec_helper'
 require 'actions/space_create'
 
-RSpec.describe SpaceCreate do
-  describe 'create' do
-    let(:org) { VCAP::CloudController::Organization.make }
-    it 'creates a space' do
-      message = VCAP::CloudController::SpaceCreateMessage.new(name: 'my-space')
-      space   = SpaceCreate.new.create(org, message)
+module VCAP::CloudController
+  RSpec.describe SpaceCreate do
+    describe 'create' do
+      let(:org) { VCAP::CloudController::Organization.make }
+      it 'creates a space' do
+        message = VCAP::CloudController::SpaceCreateMessage.new(name: 'my-space')
+        space = SpaceCreate.new.create(org, message)
 
-      expect(space.organization).to eq(org)
-      expect(space.name).to eq('my-space')
-    end
-
-    context 'when a model validation fails' do
-      it 'raises an error' do
-        errors = Sequel::Model::Errors.new
-        errors.add(:blork, 'is busted')
-        expect(VCAP::CloudController::Space).to receive(:create).
-          and_raise(Sequel::ValidationFailed.new(errors))
-
-        message = VCAP::CloudController::SpaceCreateMessage.new(name: 'foobar')
-        expect {
-          SpaceCreate.new.create(org, message)
-        }.to raise_error(SpaceCreate::Error, 'blork is busted')
+        expect(space.organization).to eq(org)
+        expect(space.name).to eq('my-space')
       end
 
-      context 'when it is a uniqueness error' do
-        let(:name) { 'Olsen' }
+      context 'when a model validation fails' do
+        it 'raises an error' do
+          errors = Sequel::Model::Errors.new
+          errors.add(:blork, 'is busted')
+          expect(VCAP::CloudController::Space).to receive(:create).
+            and_raise(Sequel::ValidationFailed.new(errors))
 
-        before do
-          VCAP::CloudController::Space.create(organization: org, name: name)
-        end
-
-        it 'raises a human-friendly error' do
-          message = VCAP::CloudController::SpaceCreateMessage.new(name: name)
+          message = VCAP::CloudController::SpaceCreateMessage.new(name: 'foobar')
           expect {
             SpaceCreate.new.create(org, message)
-          }.to raise_error(SpaceCreate::Error, 'Name must be unique per organization')
+          }.to raise_error(SpaceCreate::Error, 'blork is busted')
+        end
+
+        context 'when it is a uniqueness error' do
+          let(:name) { 'Olsen' }
+
+          before do
+            VCAP::CloudController::Space.create(organization: org, name: name)
+          end
+
+          it 'raises a human-friendly error' do
+            message = VCAP::CloudController::SpaceCreateMessage.new(name: name)
+            expect {
+              SpaceCreate.new.create(org, message)
+            }.to raise_error(SpaceCreate::Error, 'Name must be unique per organization')
+          end
         end
       end
     end
