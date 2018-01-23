5a43e17932581778186e1f2e0ca064d8aeb9b552
Eric Promislow <eric.promislow@suse.com>
2018-01-23 10:47:49 -0800

    Allow read-only admins to view logs/stats for apps
    [#151624354]
    
    Signed-off-by: Tim Downey <tdowney@pivotal.io>

--- spec/unit/controllers/internal/log_access_controller_spec.rb
+++ spec/unit/controllers/internal/log_access_controller_spec.rb
@@ -3,12 +3,10 @@ require 'fetchers/log_access_fetcher'
 
 module VCAP::CloudController
   RSpec.describe LogAccessController do
-    let(:user) { User.make }
-    let(:guid) { 'v2-or-v3-app-guid' }
-    let(:space_guids) { ['guid-1', 'guid-2'] }
-    let(:roles) { double(:roles) }
-    let(:membership) { double(:membership) }
-    let(:fetcher) { double(:fetcher, app_exists?: true, app_exists_by_space?: true) }
+    let(:app_model) { VCAP::CloudController::AppModel.make(enable_ssh: true) }
+    let(:space) { app_model.space }
+    let(:org) { space.organization }
+    let(:user) { VCAP::CloudController::User.make }
     let(:logger) { instance_double(Steno::Logger) }
 
     let(:log_access_controller) do
@@ -25,20 +23,16 @@ module VCAP::CloudController
 
     before do
       allow(logger).to receive(:debug)
-      allow(log_access_controller).to receive(:check_read_permissions!)
-      allow(LogAccessFetcher).to receive(:new).and_return(fetcher)
-      allow(log_access_controller).to receive(:membership).and_return(membership)
-      allow(log_access_controller).to receive(:current_user).and_return(user)
-      allow(VCAP::CloudController::Roles).to receive(:new).and_return(roles)
+      allow(LogAccessFetcher).to receive(:new).and_call_original
     end
 
     describe '#lookup' do
-      context 'when the user is unauthorized' do
+      context 'when the user does not have the cloud_controller.read scope' do
         it 'returns 403' do
           expect(log_access_controller).to receive(:check_read_permissions!).
             and_raise(CloudController::Errors::ApiError.new_from_details('NotAuthorized'))
           expect {
-            log_access_controller.lookup(guid)
+            log_access_controller.lookup(app_model.guid)
           }.to raise_error do |error|
             expect(error.name).to eq 'NotAuthorized'
             expect(error.response_code).to eq 403
@@ -47,55 +41,63 @@ module VCAP::CloudController
       end
 
       context 'permissions' do
-        context 'admin user' do
-          before do
-            allow(roles).to receive(:admin?).and_return(true)
-          end
-
-          it 'uses admin permissions' do
-            log_access_controller.lookup(guid)
-            expect(fetcher).to have_received(:app_exists?).with(guid)
-          end
+        let(:roles_to_http_responses) do
+          {
+            'admin' => 200,
+            'admin_read_only' => 200,
+            'global_auditor' => 404,
+            'space_developer' => 200,
+            'space_manager' => 200,
+            'space_auditor' => 200,
+            'org_manager' => 200,
+            'org_auditor' => 404,
+            'org_billing_manager' => 404,
+          }
         end
 
-        context 'non admin  user' do
-          before do
-            allow(roles).to receive(:admin?).and_return(false)
-            allow(membership).to receive(:space_guids_for_roles).with(
-              [Membership::SPACE_DEVELOPER,
-               Membership::SPACE_MANAGER,
-               Membership::SPACE_AUDITOR,
-               Membership::ORG_MANAGER],
-            ).and_return(space_guids)
-          end
+        roles = [
+            'admin',
+            'admin_read_only',
+            'global_auditor',
+            'space_developer',
+            'space_manager',
+            'space_auditor',
+            'org_manager',
+            'org_auditor',
+            'org_billing_manager',
+          ]
 
-          it 'checks for read permissions' do
-            log_access_controller.lookup(guid)
-            expect(log_access_controller).to have_received(:check_read_permissions!)
-            expect(fetcher).to have_received(:app_exists_by_space?).with(guid, space_guids)
+        roles.each do |role|
+          describe "as an #{role}" do
+            it 'returns the correct response status' do
+              expected_return_value = roles_to_http_responses[role]
+              set_current_user_as_role(role: role, org: org, space: space, user: user)
+              response_code = log_access_controller.lookup(app_model.guid)
+
+              expect(response_code).to eq(expected_return_value), "role #{role}: expected  #{expected_return_value}, got: #{response_code}"
+            end
           end
         end
       end
 
       context 'when the app does not exist' do
         before do
-          allow(roles).to receive(:admin?).and_return(true)
-          allow(fetcher).to receive(:app_exists?).with('some-guid').and_return(false)
+          set_current_user_as_admin
         end
 
         it 'returns 404' do
-          response_code = log_access_controller.lookup('some-guid')
+          response_code = log_access_controller.lookup('some-fake-guid')
           expect(response_code).to eq(404)
         end
       end
 
       context 'when the app is found' do
         before do
-          allow(roles).to receive(:admin?).and_return(true)
+          set_current_user_as_admin
         end
 
         it 'returns 200 when the app is found' do
-          response_code = log_access_controller.lookup(guid)
+          response_code = log_access_controller.lookup(app_model.guid)
           expect(response_code).to eq(200)
         end
       end
