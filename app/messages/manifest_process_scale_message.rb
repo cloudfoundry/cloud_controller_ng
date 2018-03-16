4312f8cec952f65fce374b694cbc7f4f758e2ecf
Tim Downey <tdowney@pivotal.io>
2018-03-16 11:46:11 -0700

    Simplify error messages for invalid Memory/Disk Quota amounts
    - When the user provides an invalid non-positive integer amount for
    memory or disk_quota in their manifest, we now will always return the
    same literal string error message
    - This allows us to remove some complicated logic from the
    ManifestProcessScaleMessage
    
    [#155665500]
    
    Signed-off-by: Sebastian Vidrio <svidrio@pivotal.io>

--- app/messages/manifest_process_scale_message.rb
+++ app/messages/manifest_process_scale_message.rb
@@ -3,57 +3,18 @@ require 'messages/base_message'
 module VCAP::CloudController
   class ManifestProcessScaleMessage < BaseMessage
     ALLOWED_KEYS = [:instances, :memory, :disk_quota].freeze
+    INVALID_MB_VALUE_ERROR = 'must be greater than 0MB'.freeze
+
     attr_accessor(*ALLOWED_KEYS)
 
     validates_with NoAdditionalKeysValidator
 
-    invalid_field_message = ->(object, data) do
-      if object.nil?
-        return invalid_field_message_with_nil_object(data)
-      end
-      case object.to_s.split('.')[-1]
-      when 'not_a_number'
-        return 'is not a number'
-      when 'greater_than'
-        return 'must be greater than 0MB'
-      when 'not_an_integer'
-        return 'must be an integer'
-      else
-        Steno.logger('ManifestProcessScaleMessage#invalid_field_message').warn("Unexpected error code of data:<#{data || 'nil'}>, object:<#{object}>")
-        return 'is not a number'
-      end
-    end
-
     validates :instances, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
-    validates :memory, numericality: { only_integer: true, greater_than: 0, message: invalid_field_message }, allow_nil: true
-    validates :disk_quota, numericality: { only_integer: true, greater_than: 0, message: invalid_field_message }, allow_nil: true
+    validates :memory, numericality: { only_integer: true, greater_than: 0, message: INVALID_MB_VALUE_ERROR }, allow_nil: true
+    validates :disk_quota, numericality: { only_integer: true, greater_than: 0, message: INVALID_MB_VALUE_ERROR }, allow_nil: true
 
     def self.create_from_http_request(body)
       ManifestProcessScaleMessage.new(body.deep_symbolize_keys)
-    end
-
-    # Sometimes ActiveModel can't resolve the error key against the class,
-    # so we have to examine the value alone.
-    # The object field is based on @base.class.lookup_ancestors[0].model_name.i18n_key,
-    # but if @base.class doesn't have a `i18n_scope` method, `object` is nil, so we
-    # have to analyze the data.
-    def self.invalid_field_message_with_nil_object(data)
-      value_as_string = data[:value].to_s
-      begin
-        Float(value_as_string)
-      rescue ArgumentError
-        return 'is not a number'
-      end
-      begin
-        value = Integer(value_as_string)
-      rescue ArgumentError
-        return 'must be an integer'
-      end
-      if value <= 0
-        return 'must be greater than 0MB'
-      end
-      Steno.logger('ManifestProcessScaleMessage#invalid_field_message').warn("Unexpected error with nil object, data:<#{data}>")
-      'is not a number'
     end
 
     private
--- spec/unit/messages/manifest_process_scale_message_spec.rb
+++ spec/unit/messages/manifest_process_scale_message_spec.rb
@@ -3,121 +3,129 @@ require 'messages/process_scale_message'
 
 module VCAP::CloudController
   RSpec.describe ManifestProcessScaleMessage do
-    context 'when unexpected keys are requested' do
-      let(:params) { { instances: 3, memory: 6, memory_in_mb: 2048 } } # memory_in_mb is unexpected unlike ProcessScaleMessage
-
-      it 'is not valid' do
-        message = ManifestProcessScaleMessage.new(params)
-
-        expect(message).not_to be_valid
-        expect(message.errors.full_messages[0]).to include("Unknown field(s): 'memory_in_mb'")
-      end
-    end
-
-    context 'when instances is not an number' do
-      let(:params) { { instances: 'silly string thing' } }
-
-      it 'is not valid' do
-        message = ManifestProcessScaleMessage.new(params)
-
-        expect(message).not_to be_valid
-        expect(message.errors.count).to eq(1)
-        expect(message.errors[:instances]).to include('is not a number')
-      end
-    end
-
-    context 'when instances is not an integer' do
-      let(:params) { { instances: 3.5 } }
-
-      it 'is not valid' do
-        message = ManifestProcessScaleMessage.new(params)
-
-        expect(message).not_to be_valid
-        expect(message.errors.count).to eq(1)
-        expect(message.errors[:instances]).to include('must be an integer')
-      end
-    end
-
-    context 'when instances is not a positive integer' do
-      let(:params) { { instances: -1 } }
-
-      it 'is not valid' do
-        message = ManifestProcessScaleMessage.new(params)
-
-        expect(message).not_to be_valid
-        expect(message.errors.count).to eq(1)
-        expect(message.errors[:instances]).to include('must be greater than or equal to 0')
-      end
-    end
-
-    context 'when memory is not a number' do
-      let(:params) { { memory: 'silly string thing' } }
-
-      it 'is not valid' do
-        message = ManifestProcessScaleMessage.new(params)
-
-        expect(message).not_to be_valid
-        expect(message.errors.count).to eq(1)
-        expect(message.errors.full_messages).to include('Memory is not a number')
-      end
-    end
-
-    context 'when memory is < 1' do
-      let(:params) { { memory: 0 } }
-
-      it 'is not valid' do
-        message = ManifestProcessScaleMessage.new(params)
-
-        expect(message).not_to be_valid
-        expect(message.errors.count).to eq(1)
-        expect(message.errors.full_messages).to include('Memory must be greater than 0MB')
-      end
-    end
-
-    context 'when memory is not an integer' do
-      let(:params) { { memory: 3.5 } }
-
-      it 'is not valid' do
-        message = ManifestProcessScaleMessage.new(params)
-
-        expect(message).not_to be_valid
-        expect(message.errors.count).to eq(1)
-        expect(message.errors.full_messages).to include('Memory must be an integer')
-      end
-
-      context 'when disk_quota is not an number' do
-        let(:params) { { disk_quota: 'silly string thing' } }
+    describe 'validations' do
+      context 'when unexpected keys are requested' do
+        let(:params) { { instances: 3, memory: 6, memory_in_mb: 2048 } } # memory_in_mb is unexpected unlike ProcessScaleMessage
 
         it 'is not valid' do
           message = ManifestProcessScaleMessage.new(params)
 
           expect(message).not_to be_valid
-          expect(message.errors.count).to eq(1)
-          expect(message.errors.full_messages).to include('Disk quota is not a number')
+          expect(message.errors.full_messages[0]).to include("Unknown field(s): 'memory_in_mb'")
         end
       end
 
-      context 'when disk_quota is < 1' do
-        let(:params) { { disk_quota: 0 } }
+      describe '#instances' do
+        context 'when instances is not an number' do
+          let(:params) { { instances: 'silly string thing' } }
 
-        it 'is not valid' do
-          message = ManifestProcessScaleMessage.new(params)
+          it 'is not valid' do
+            message = ManifestProcessScaleMessage.new(params)
 
-          expect(message).not_to be_valid
-          expect(message.errors.count).to eq(1)
-          expect(message.errors.full_messages).to include('Disk quota must be greater than 0MB')
+            expect(message).not_to be_valid
+            expect(message.errors.count).to eq(1)
+            expect(message.errors[:instances]).to include('is not a number')
+          end
+        end
+
+        context 'when instances is not an integer' do
+          let(:params) { { instances: 3.5 } }
+
+          it 'is not valid' do
+            message = ManifestProcessScaleMessage.new(params)
+
+            expect(message).not_to be_valid
+            expect(message.errors.count).to eq(1)
+            expect(message.errors[:instances]).to include('must be an integer')
+          end
+        end
+
+        context 'when instances is not a positive integer' do
+          let(:params) { { instances: -1 } }
+
+          it 'is not valid' do
+            message = ManifestProcessScaleMessage.new(params)
+
+            expect(message).not_to be_valid
+            expect(message.errors.count).to eq(1)
+            expect(message.errors[:instances]).to include('must be greater than or equal to 0')
+          end
         end
       end
 
-      context 'when disk_quota is not an integer' do
-        let(:params) { { disk_quota: 3.5 } }
+      describe '#memory' do
+        context 'when memory is not a number' do
+          let(:params) { { memory: 'silly string thing' } }
 
-        it 'is not valid' do
-          message = ManifestProcessScaleMessage.new(params)
+          it 'is not valid' do
+            message = ManifestProcessScaleMessage.new(params)
 
-          expect(message).not_to be_valid
-          expect(message.errors.count).to eq(1)
-          expect(message.errors.full_messages).to include('Disk quota must be an integer')
+            expect(message).not_to be_valid
+            expect(message.errors.count).to eq(1)
+            expect(message.errors.full_messages).to include('Memory must be greater than 0MB')
+          end
+        end
+
+        context 'when memory is < 1' do
+          let(:params) { { memory: 0 } }
+
+          it 'is not valid' do
+            message = ManifestProcessScaleMessage.new(params)
+
+            expect(message).not_to be_valid
+            expect(message.errors.count).to eq(1)
+            expect(message.errors.full_messages).to include('Memory must be greater than 0MB')
+          end
+        end
+
+        context 'when memory is not an integer' do
+          let(:params) { { memory: 3.5 } }
+
+          it 'is not valid' do
+            message = ManifestProcessScaleMessage.new(params)
+
+            expect(message).not_to be_valid
+            expect(message.errors.count).to eq(1)
+            expect(message.errors.full_messages).to include('Memory must be greater than 0MB')
+          end
+        end
+      end
+
+      describe '#disk_quota' do
+        context 'when disk_quota is not an number' do
+          let(:params) { { disk_quota: 'silly string thing' } }
+
+          it 'is not valid' do
+            message = ManifestProcessScaleMessage.new(params)
+
+            expect(message).not_to be_valid
+            expect(message.errors.count).to eq(1)
+            expect(message.errors.full_messages).to include('Disk quota must be greater than 0MB')
+          end
+        end
+
+        context 'when disk_quota is < 1' do
+          let(:params) { { disk_quota: 0 } }
+
+          it 'is not valid' do
+            message = ManifestProcessScaleMessage.new(params)
+
+            expect(message).not_to be_valid
+            expect(message.errors.count).to eq(1)
+            expect(message.errors.full_messages).to include('Disk quota must be greater than 0MB')
+          end
+        end
+
+        context 'when disk_quota is not an integer' do
+          let(:params) { { disk_quota: 3.5 } }
+
+          it 'is not valid' do
+            message = ManifestProcessScaleMessage.new(params)
+
+            expect(message).not_to be_valid
+            expect(message.errors.count).to eq(1)
+            expect(message.errors.full_messages).to include('Disk quota must be greater than 0MB')
+          end
         end
       end
 
@@ -130,8 +138,8 @@ module VCAP::CloudController
           expect(message).not_to be_valid
           expect(message.errors.count).to eq(2)
           expect(message.errors.full_messages).to match_array([
-            'Disk quota must be an integer',
-            'Memory is not a number'
+            'Disk quota must be greater than 0MB',
+            'Memory must be greater than 0MB'
           ])
         end
       end
@@ -154,32 +162,6 @@ module VCAP::CloudController
         message = ManifestProcessScaleMessage.create_from_http_request(body)
 
         expect(message.requested?(:instances)).to be_truthy
-      end
-    end
-
-    describe 'invalid_field_message_with_nil_object' do
-      let(:data) do { value: value } end
-      let(:result) { ManifestProcessScaleMessage.invalid_field_message_with_nil_object(data) }
-
-      context "when it's not a number" do
-        let(:value) { 'size-50 natty bo T-shirt' }
-        it 'is not a number' do
-          expect(result).to eq('is not a number')
-        end
-      end
-
-      context "when it's not an integer" do
-        let(:value) { '22.4' }
-        it 'is not a float' do
-          expect(result).to eq('must be an integer')
-        end
-      end
-
-      context "when it's negative" do
-        let(:value) { -22 }
-        it 'is negative' do
-          expect(result).to eq('must be greater than 0MB')
-        end
       end
     end
   end
