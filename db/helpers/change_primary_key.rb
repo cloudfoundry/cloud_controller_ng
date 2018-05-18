6e0b0081c89dd46993923499d56bd18ce40aaee1
Eric Promislow <eric.promislow@suse.com>
2018-05-17 18:27:23 -0700

    ... and add rubocop fixes
    [#157279663]

--- db/helpers/change_primary_key.rb
+++ db/helpers/change_primary_key.rb
@@ -5,10 +5,10 @@ require 'pry'
 # analyzing the schema.
 def has_primary_key(db, table, key)
   return db.primary_key(table) == key.to_s if db.respond_to?(:primary_key)
-  
-  column_info = db.schema(table).find { |column_info| column_info[0] == key }
-  return false if column_info.nil?
-  return column_info[1][:primary_key] == true
+
+  pk_column_info = db.schema(table).find { |column_info| column_info[0] == key }
+  return false if pk_column_info.nil?
+  pk_column_info[1][:primary_key] == true
 end
 
 def add_primary_key_to_table(table, key)
