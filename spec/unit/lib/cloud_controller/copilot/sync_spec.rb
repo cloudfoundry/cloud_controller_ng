e869c3168d33cf82f33b33a4ece2c7e431b0aef8
Zachary Gershman <zgershman@pivotal.io>
2018-05-21 14:33:40 -0700

    Send context paths to copilot
    - bumped cf-copilot gem to update copilot_client interface
    
    [#156145701]

--- spec/unit/lib/cloud_controller/copilot/sync_spec.rb
+++ spec/unit/lib/cloud_controller/copilot/sync_spec.rb
@@ -5,7 +5,7 @@ require 'cloud_controller/copilot/sync'
 module VCAP::CloudController
   RSpec.describe Copilot::Sync do
     describe '#sync' do
-      let(:route) { Route.make(domain: domain, host: 'some-host') }
+      let(:route) { Route.make(domain: domain, host: 'some-host', path: '/some/path') }
       let(:domain) { SharedDomain.make(name: 'example.org') }
       let!(:route_mapping) { RouteMappingModel.make(route: route) }
       let!(:web_process_model) { VCAP::CloudController::ProcessModel.make(type: 'web') }
@@ -21,6 +21,7 @@ module VCAP::CloudController
         expect(Copilot::Adapter).to have_received(:bulk_sync) do |args|
           expect(args[:routes].first.guid).to eq(route.guid)
           expect(args[:routes].first.fqdn).to eq('some-host.example.org')
+          expect(args[:routes].first.path).to eq('/some/path')
           expect(args[:route_mappings]).to eq([route_mapping])
           expect(args[:processes]).to eq([web_process_model])
         end
