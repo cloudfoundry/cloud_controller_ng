18516dc13a18241a08a0a77f0a7cb00918c33810
Greg Cobb <gcobb@pivotal.io>
2018-05-22 17:17:39 -0700

    Revert "Automatically create web processes on v3 api"
    This reverts commit 94ba4ac50792ab0537e1ccbdce2e33e332dfe026.
    
    Signed-off-by: Eric Promislow <eric.promislow@suse.com>

--- app/actions/process_create.rb
+++ app/actions/process_create.rb
@@ -7,15 +7,14 @@ module VCAP::CloudController
       @user_audit_info = user_audit_info
     end
 
-    def create(app, args)
-      type = args[:type]
-      attrs = args.merge({
+    def create(app, message)
+      attrs = message.merge({
         diego:             true,
-        instances:         args[:instances] || default_instance_count(type),
-        health_check_type: default_health_check_type(type),
+        instances:         message[:type] == ProcessTypes::WEB ? 1 : 0,
+        health_check_type: message[:type] == ProcessTypes::WEB ? 'port' : 'process',
         metadata:          {},
       })
-      attrs[:guid] = app.guid if type == ProcessTypes::WEB
+      attrs[:guid] = app.guid if message[:type] == ProcessTypes::WEB
 
       process = nil
       app.class.db.transaction do
@@ -24,16 +23,6 @@ module VCAP::CloudController
       end
 
       process
-    end
-
-    private
-
-    def default_health_check_type(type)
-      type == ProcessTypes::WEB ? 'port' : 'process'
-    end
-
-    def default_instance_count(type)
-      type == ProcessTypes::WEB ? 1 : 0
     end
   end
 end
--- spec/unit/actions/process_create_spec.rb
+++ spec/unit/actions/process_create_spec.rb
@@ -11,8 +11,7 @@ module VCAP::CloudController
       let(:message) do
         {
           type:    'web',
-          command: 'rackup',
-          instances: 42
+          command: 'rackup'
         }
       end
 
@@ -24,7 +23,6 @@ module VCAP::CloudController
         expect(app.processes.first.guid).to eq(process.guid)
         expect(process.type).to eq('web')
         expect(process.command).to eq('rackup')
-        expect(process.instances).to eq(42)
       end
 
       context 'if the command is nil' do
