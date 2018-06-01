595addbae4388e026745ade9152374b8bb2a54ee
Lisa Cho <lcho@pivotal.io>
2018-06-01 14:02:35 -0700

    Do not save the droplets' start commands on the process whether it is a new process or updating an existing process
    - This allows the process to always store the user's api and manifest-set start
    commands, while keeping the start commands from Procfiles and buildpacks
    on the droplet's process_types field
    - The command from Procfiles/buildpacks is already stored on the
    droplets so it is redundant to store it here also.
    
    - [#157756113]
    
    Signed-off-by: Davis W. Frank <dwfrank@pivotal.io>

--- spec/request/apps_spec.rb
+++ spec/request/apps_spec.rb
@@ -1302,9 +1302,7 @@ RSpec.describe 'Apps' do
       expect(update_event.metadata).to eq({
         'process_guid' => process_to_update.guid,
         'process_type' => 'web',
-        'request'      => {
-          'command' => 'PRIVATE DATA HIDDEN'
-        }
+        'request'      => {},
       })
     end
   end
