6720e2160d59a0addc20571d5eb2e39b09ae0fb8
Eli Wrenn <ewrenn@pivotal.io>
2018-07-17 11:56:21 -0700

    Prefactoring lib/cloud_controller/controllers.rb
    We want the following commit to be explicitly clear.
    
    [#158699580]
    
    Signed-off-by: Mike Kenyon <mkenyon@pivotal.io>

--- lib/cloud_controller/controllers.rb
+++ lib/cloud_controller/controllers.rb
@@ -1,6 +1,5 @@
 require 'cloud_controller/rest_controller'
 require 'controllers/v3/application_controller'
 
-Dir[File.expand_path('../../../app/controllers/**/*.rb', __FILE__)].each do |file|
-  require file
-end
+Dir[File.expand_path('../../../app/controllers/**/*.rb', __FILE__)].
+  each { |file| require file }
