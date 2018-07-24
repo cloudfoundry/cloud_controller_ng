b8cfd4071167a4334d3aacefb7a0f957c60b005a
Georgi Lozev <georgi.lozev@sap.com>
2018-07-24 13:43:26 +0300

    Rename binding response schema
    Rename the schema used in parse bind and unbind to have more generic
    name since it's used in both places and not only in the parse_bind
    method.
    
    Signed-off-by: Alex Blease <ablease@pivotal.io>
    Co-authored-by: Alex Blease <ablease@pivotal.io>

--- lib/services/service_brokers/v2/response_parser.rb
+++ lib/services/service_brokers/v2/response_parser.rb
@@ -47,7 +47,7 @@ module VCAP::Services
                     VolumeMountsValidator.new(opts[:service_guid],
                       SuccessValidator.new(state: 'succeeded')))))
             when 202
-              JsonSchemaValidator.new(@logger, async_bind_response_schema, SuccessValidator.new)
+              JsonSchemaValidator.new(@logger, async_binding_response_schema, SuccessValidator.new)
             when 409
               FailingValidator.new(Errors::ServiceBrokerConflict)
             when 422
@@ -74,7 +74,7 @@ module VCAP::Services
             when 201
               IgnoreDescriptionKeyFailingValidator.new(Errors::ServiceBrokerBadResponse)
             when 202
-              JsonSchemaValidator.new(@logger, async_bind_response_schema, SuccessValidator.new)
+              JsonSchemaValidator.new(@logger, async_binding_response_schema, SuccessValidator.new)
             when 204
               FailingValidator.new(Errors::ServiceBrokerBadResponse)
             when 410
@@ -213,7 +213,7 @@ module VCAP::Services
           parse_fetch_state(path, response)
         end
 
-        def async_bind_response_schema
+        def async_binding_response_schema
           {
             '$schema' => 'http://json-schema.org/draft-04/schema#',
             'type' => 'object',
