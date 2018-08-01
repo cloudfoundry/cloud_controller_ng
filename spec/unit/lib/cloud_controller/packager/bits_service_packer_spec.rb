73176a71ca4a648f31b5f32ab3523aa01ddbc984
Chris Selzo <cselzo@pivotal.io>
2018-08-01 10:05:49 -0700

    Revert "Merge pull request #1184 from petergtz/extend-cp-to-blobstore"
    This reverts commit 1a50d730ca7a9f4fa86d5a9a41f68257b25cce3d, reversing
    changes made to 6f7c877498ae347f500ce1093dc8b9300c4094af.

--- spec/unit/lib/cloud_controller/packager/bits_service_packer_spec.rb
+++ spec/unit/lib/cloud_controller/packager/bits_service_packer_spec.rb
@@ -10,37 +10,123 @@ module CloudController::Packager
     let(:cached_files_fingerprints) { [{ 'sha1' => 'abcde', 'fn' => 'lib.rb' }] }
 
     let(:package_blobstore) { double(:package_blobstore) }
+    let(:receipt) { [{ 'sha1' => '12345', 'fn' => 'app.rb' }] }
+    let(:package_file) { Tempfile.new('package') }
+    let(:resource_pool) { double(BitsService::ResourcePool) }
 
     before do
+      allow_any_instance_of(CloudController::DependencyLocator).to receive(:bits_service_resource_pool).
+        and_return(resource_pool)
       allow_any_instance_of(CloudController::DependencyLocator).to receive(:package_blobstore).
         and_return(package_blobstore)
+      allow(resource_pool).to receive(:upload_entries).
+        and_return(double(:response, code: 201, body: receipt.to_json))
+      allow(resource_pool).to receive(:bundles).
+        and_return(double(:response, code: 200, body: 'contents'))
       allow(package_blobstore).to receive(:cp_to_blobstore)
+      allow(Tempfile).to receive(:new).and_return(package_file)
     end
 
     describe '#send_package_to_blobstore' do
+      it 'uses the resource_pool to upload the zip file and pass cached fingerprints' do
+        expect(resource_pool).to receive(:bundles).
+          with(cached_files_fingerprints.to_json, uploaded_files_path)
+        packer.send_package_to_blobstore(blobstore_key, uploaded_files_path, cached_files_fingerprints)
+      end
+
       it 'uploads the package to the bits service' do
-        expect(package_blobstore).to receive(:cp_to_blobstore).
-          with(uploaded_files_path, blobstore_key, resources: cached_files_fingerprints)
+        expect(package_blobstore).to receive(:cp_to_blobstore) do |package_path, guid|
+          expect(File.read(package_path)).to eq('contents')
+          expect(guid).to eq(blobstore_key)
+        end.and_return(double(Net::HTTPCreated))
         packer.send_package_to_blobstore(blobstore_key, uploaded_files_path, cached_files_fingerprints)
       end
 
       it 'returns the uploaded file hash' do
-        expect(package_blobstore).to receive(:cp_to_blobstore).
-          with(uploaded_files_path, blobstore_key, resources: cached_files_fingerprints).
-          and_return({ sha1: 'abc', sha256: 'def' })
         result_hash = packer.send_package_to_blobstore(blobstore_key, uploaded_files_path, cached_files_fingerprints)
-        expect(result_hash).to eq({ sha1: 'abc', sha256: 'def' })
+        expect(result_hash).to eq({
+          sha1:   Digester.new.digest_file(package_file),
+          sha256: Digester.new(algorithm: Digest::SHA256).digest_file(package_file),
+        })
+      end
+
+      shared_examples 'a packaging failure' do
+        let(:expected_exception) { ::CloudController::Errors::ApiError }
+
+        it 'raises the exception' do
+          expect {
+            packer.send_package_to_blobstore(blobstore_key, uploaded_files_path, cached_files_fingerprints)
+          }.to raise_error(expected_exception)
+        end
+      end
+
+      context 'when no new bits are being uploaded' do
+        let(:uploaded_files_path) { nil }
+
+        it 'does not upload new entries to the bits service' do
+          expect(resource_pool).to_not receive(:upload_entries)
+          packer.send_package_to_blobstore(blobstore_key, uploaded_files_path, cached_files_fingerprints)
+        end
+
+        it 'downloads a bundle with the original fingerprints' do
+          expect(resource_pool).to receive(:bundles).with(cached_files_fingerprints.to_json, uploaded_files_path)
+          packer.send_package_to_blobstore(blobstore_key, uploaded_files_path, cached_files_fingerprints)
+        end
+
+        it 'uploads the package to the bits service' do
+          expect(package_blobstore).to receive(:cp_to_blobstore) do |package_path, guid|
+            expect(File.read(package_path)).to eq('contents')
+            expect(guid).to eq(blobstore_key)
+          end
+          packer.send_package_to_blobstore(blobstore_key, uploaded_files_path, cached_files_fingerprints)
+        end
+
+        it 'returns the correct package hash in the app' do
+          result_hash = packer.send_package_to_blobstore(blobstore_key, uploaded_files_path, cached_files_fingerprints)
+          expect(result_hash).to eq({
+            sha1:   Digester.new.digest_file(package_file),
+            sha256: Digester.new(algorithm: Digest::SHA256).digest_file(package_file),
+          })
+        end
+      end
+
+      context 'when `bundles` fails' do
+        before do
+          allow(resource_pool).to receive(:bundles).
+            and_raise(BitsService::Errors::UnexpectedResponseCode)
+        end
+
+        it_behaves_like 'a packaging failure'
+      end
+
+      context 'when writing the package to a temp file fails' do
+        let(:expected_exception) { StandardError.new('some error') }
+
+        before do
+          allow(Tempfile).to receive(:new).
+            and_raise(expected_exception)
+        end
+
+        it_behaves_like 'a packaging failure'
       end
 
       context 'when uploading the package to the bits service fails' do
         let(:expected_exception) { StandardError.new('some error') }
 
-        it 'raises the exception' do
+        before do
           allow(package_blobstore).to receive(:cp_to_blobstore).and_raise(expected_exception)
-          expect {
-            packer.send_package_to_blobstore(blobstore_key, uploaded_files_path, cached_files_fingerprints)
-          }.to raise_error(expected_exception)
         end
+
+        it_behaves_like 'a packaging failure'
+      end
+
+      context 'when the bits service has an internal error on bundles' do
+        before do
+          allow(resource_pool).to receive(:bundles).
+            and_raise(BitsService::Errors::UnexpectedResponseCode)
+        end
+
+        it_behaves_like 'a packaging failure'
       end
     end
   end
