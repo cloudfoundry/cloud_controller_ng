95b2f726caad0abeca0d5cf73bb5eca97a3f57a0
Charles Hansen <chansen@pivotal.io>
2018-11-09 10:51:04 -0800

    Remove revision_guid foreign key constraint on processes
    - We typically only put foreign key constraints on "child" tables
    pointing back to their parent resource. Revisions are a "sibling" to
    processes since they both belong to apps and may be created/removed
    independently of each other
    - It is safe to edit this migration in this way since it was never
    released
    
    [#161730474]
    
    Signed-off-by: Tim Downey <tdowney@pivotal.io>

--- db/migrations/20181107191728_add_revisions.rb
+++ db/migrations/20181107191728_add_revisions.rb
@@ -7,18 +7,8 @@ Sequel.migration do
       index [:app_guid], name: :fk_revision_app_guid_index
     end
 
-    collate_opts = { size: 255 }
-    if self.class.name.match?(/mysql/i)
-      collate_opts[:collate] = :utf8_bin
-    end
-
     alter_table(:processes) do
-      # rubocop:disable Migration/IncludeStringSize
-      add_column :revision_guid, String, collate_opts
-      # rubocop:enable Migration/IncludeStringSize
-
-      add_foreign_key [:revision_guid], :revisions, key: :guid, name: :fk_process_revision_guid
-      add_index [:revision_guid], name: :fk_process_revision_guid_index
+      add_column :revision_guid, String, size: 255, index: { name: :processes_revision_guid_index }
     end
   end
 end
