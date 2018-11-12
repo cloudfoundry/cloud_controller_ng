5f54464315d577f31f62f343a03a6e647cea0b67
Tim Downey <tdowney@pivotal.io>
2018-11-12 12:27:56 -0800

    Refactor yaml alias allowances
    Special thanks: zrob and gerg
    
    [Finishes #161832502]
    
    Signed-off-by: Chris Selzo <cselzo@pivotal.io>

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,9 @@ class Application < ::Rails::Application
 
   # For Rails 5 / Rack 2 - this is how to add a new parser
   original_parsers = ActionDispatch::Request.parameter_parsers
-  # The 4th argument to YAML.safe_load allows yaml aliases(anchors)
-  yaml_parser = lambda { |body| YAML.safe_load(body, [], [], true).with_indifferent_access }
+
+  allow_yaml_aliases = true
+  yaml_parser = lambda { |body| YAML.safe_load(body, [], [], allow_yaml_aliases).with_indifferent_access }
   new_parsers = original_parsers.merge({
     Mime::Type.lookup('application/x-yaml') => yaml_parser,
     Mime::Type.lookup('text/yaml') => yaml_parser,
