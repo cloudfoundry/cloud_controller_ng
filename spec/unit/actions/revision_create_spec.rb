2718def7f6b72155b4ec6e579fd895ed01d504ff
Chris Selzo <cselzo@pivotal.io>
2018-11-14 15:21:32 -0800

    Be explicit about created_at in revision_create_spec
    MySql time resolution is not fine grained enough to support rapid
    insertions having non-identical timestamps (Postgres is totes cool with
    it)
    
    [Finishes #161730559]
    
    Signed-off-by: Andres Medina <amedina@pivotal.io>

--- spec/unit/actions/revision_create_spec.rb
+++ spec/unit/actions/revision_create_spec.rb
@@ -17,33 +17,30 @@ module VCAP::CloudController
         it 'increments the version by 1' do
           subject.create(app)
           subject.create(app)
-          subject.create(app)
 
-          expect(RevisionModel.order_by(:created_at).map(&:version)).to eq([1, 2, 3])
+          expect(RevisionModel.map(&:version)).to eq([1, 2])
         end
 
         it 'rolls over to version 1 when we pass version 9999' do
-          RevisionModel.make(app: app, version: 1)
-          RevisionModel.make(app: app, version: 2)
+          RevisionModel.make(app: app, version: 1, created_at: 5.days.ago)
+          RevisionModel.make(app: app, version: 2, created_at: 4.days.ago)
           # ...
-          RevisionModel.make(app: app, version: 9998)
-          RevisionModel.make(app: app, version: 9999)
+          RevisionModel.make(app: app, version: 9998, created_at: 3.days.ago)
+          RevisionModel.make(app: app, version: 9999, created_at: 2.days.ago)
 
           subject.create(app)
           expect(RevisionModel.order_by(:created_at).map(&:version)).to eq([2, 9998, 9999, 1])
         end
 
         it 'replaces any existing revisions after rolling over' do
-          RevisionModel.make(app: app, version: 1)
-          RevisionModel.make(app: app, version: 2)
+          RevisionModel.make(app: app, version: 2, created_at: 4.days.ago)
           # ...
-          RevisionModel.make(app: app, version: 9998)
-          RevisionModel.make(app: app, version: 9999)
+          RevisionModel.make(app: app, version: 9998, created_at: 3.days.ago)
+          RevisionModel.make(app: app, version: 9999, created_at: 2.days.ago)
+          RevisionModel.make(app: app, version: 1, created_at: 1.days.ago)
 
           subject.create(app)
-          subject.create(app)
-          subject.create(app)
-          expect(RevisionModel.order_by(:created_at).map(&:version)).to eq([9998, 9999, 1, 2, 3])
+          expect(RevisionModel.order_by(:created_at).map(&:version)).to eq([9998, 9999, 1, 2])
         end
       end
     end
