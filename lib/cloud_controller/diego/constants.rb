9b8c21b092d5553ea717133550899c5bdc938c9d
Mona Mohebbi <mmohebbi@pivotal.io>
2018-12-18 15:37:17 -0800

    Rename new constant
    - Not just used for staging
    
    [Finishes #162394048]
    
    Signed-off-by: Chris Selzo <cselzo@pivotal.io>

--- lib/cloud_controller/diego/constants.rb
+++ lib/cloud_controller/diego/constants.rb
@@ -1,12 +1,13 @@
 module VCAP::CloudController
   module Diego
     STAGING_DOMAIN                   = 'cf-app-staging'.freeze
-    STAGING_LEGACY_DOWNLOAD_USER     = 'vcap'.freeze
     STAGING_TRUSTED_SYSTEM_CERT_PATH = '/etc/cf-system-certificates'.freeze
     STAGING_LOG_SOURCE               = 'STG'.freeze
     STAGING_DEFAULT_LANG             = 'en_US.UTF-8'.freeze
     STAGING_RESULT_FILE              = '/tmp/result.json'.freeze
     STAGING_TASK_CPU_WEIGHT          = 50
+
+    LEGACY_DOWNLOAD_USER = 'vcap'.freeze
 
     RUNNING_TRUSTED_SYSTEM_CERT_PATH = '/etc/cf-system-certificates'.freeze
     DEFAULT_FILE_DESCRIPTOR_LIMIT    = 1024
