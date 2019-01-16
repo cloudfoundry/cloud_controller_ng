78821bc85298ad0bf9046ee12b25e2c9b1da642e
Andres Medina <amedina@pivotal.io>
2019-01-16 11:07:41 -0800

    Fix rubocop
    Signed-off-by: Eric Promislow <eric.promislow@suse.com>

--- lib/tasks/rake_config.rb
+++ lib/tasks/rake_config.rb
@@ -9,17 +9,18 @@ class RakeConfig
     end
 
     def config
-      VCAP::CloudController::Config.load_from_file(get_config_file, context: context)
+      VCAP::CloudController::Config.load_from_file(config_file, context: context)
     end
 
     private
 
-    def get_config_file
+    def config_file
       if ENV['CLOUD_CONTROLLER_NG_CONFIG']
         return ENV['CLOUD_CONTROLLER_NG_CONFIG']
       end
+
       [File.expand_path('../../config/cloud_controller.yml', __dir__),
-        '/var/vcap/jobs/cloud_controller_ng/config/cloud_controller_ng.yml'].find { |candidate| candidate && File.exists?(candidate) }
+       '/var/vcap/jobs/cloud_controller_ng/config/cloud_controller_ng.yml'].find { |candidate| candidate && File.exist?(candidate) }
     end
   end
 end
