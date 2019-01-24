1f0bd238fb275baef05686d26427b90624df80d6
Andres Medina <amedina@pivotal.io>
2019-01-24 12:07:16 -0800

    appease rubocop for env var lengths
    Signed-off-by: Connor Braa <cbraa@pivotal.io>

--- db/migrations/20190123184851_add_environment_variables_to_revisions.rb
+++ db/migrations/20190123184851_add_environment_variables_to_revisions.rb
@@ -1,8 +1,8 @@
 Sequel.migration do
   change do
     alter_table(:revisions) do
-      add_column :encrypted_environment_variables, String, text: true
-      add_column :salt, String
+      add_column :encrypted_environment_variables, String, size: 16_000
+      add_column :salt, String, size: 255
       add_column :encryption_key_label, String, size: 255
     end
   end
