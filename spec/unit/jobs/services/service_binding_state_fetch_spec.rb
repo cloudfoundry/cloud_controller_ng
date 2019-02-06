87732f117d9275d4763589af89aecd4029cfcd68
Henry Stanley <hstanley@pivotal.io>
2019-02-06 17:48:57 +0100

    Reduce duplication between ServiceBindingStateFetch and ServiceInstanceStateFetch
    [finishes #163691035]
    [PR]
    
    Co-authored-by: Oleksii Fedorov <ofedorov@pivotal.io>

--- spec/unit/jobs/services/service_binding_state_fetch_spec.rb
+++ spec/unit/jobs/services/service_binding_state_fetch_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'jobs/services/service_binding_state_fetch'
+require_relative 'shared/when_broker_returns_retry_after_header'
 
 module VCAP::CloudController
   module Jobs
@@ -524,10 +525,41 @@ module VCAP::CloudController
             end
           end
 
-          context 'when calling last operation responds with an error' do
+          context 'when calling last operation responds with an error HttpResponseError' do
             before do
               response = VCAP::Services::ServiceBrokers::V2::HttpResponse.new(code: 412, body: {})
               err = HttpResponseError.new('oops', 'uri', 'GET', response)
+              allow(client).to receive(:fetch_service_binding_last_operation).and_raise(err)
+
+              run_job(job)
+            end
+
+            it 'should enqueue another fetch job' do
+              expect(Delayed::Job.count).to eq 1
+            end
+
+            it 'maintains the service binding last operation details' do
+              service_binding.reload
+              expect(service_binding.last_operation.state).to eq('in progress')
+            end
+
+            context 'and the max poll duration has been reached' do
+              before do
+                Timecop.travel(Time.now + max_duration.minutes + 1.minute) do
+                  # executes job but does not enqueue another job
+                  execute_all_jobs(expected_successes: 1, expected_failures: 0)
+                end
+              end
+
+              it 'should not enqueue another fetch job' do
+                expect(Delayed::Job.count).to eq 0
+              end
+            end
+          end
+
+          context 'when calling last operation responds with an error HttpResponseError' do
+            before do
+              err = HttpRequestError.new('oops', 'uri', 'GET', RuntimeError.new)
               allow(client).to receive(:fetch_service_binding_last_operation).and_raise(err)
 
               run_job(job)
@@ -646,76 +678,7 @@ module VCAP::CloudController
             end
           end
 
-          context 'when brokers return Retry-After header' do
-            let(:state) { 'in progress' }
-            let(:default_polling_interval) { VCAP::CloudController::Config.config.get(:broker_client_default_async_poll_interval_seconds) }
-            let(:last_operation_response) { { last_operation: { state: state, description: description }, retry_after: broker_polling_interval } }
-
-            before do
-              allow(client).to receive(:fetch_service_binding_last_operation).and_return(last_operation_response)
-            end
-
-            context 'when the broker returns interval' do
-              context 'when the interval is greater than the default configuration' do
-                let(:broker_polling_interval) { default_polling_interval * 2 }
-
-                it 'the polling interval should be the one broker returned' do
-                  Timecop.freeze(Time.now)
-                  first_run_time = Time.now
-
-                  Jobs::Enqueuer.new(job, { queue: 'cc-generic', run_at: first_run_time }).enqueue
-                  execute_all_jobs(expected_successes: 1, expected_failures: 0)
-                  expect(Delayed::Job.count).to eq(1)
-
-                  run_time_default_interval = first_run_time + default_polling_interval.seconds + 1.second
-                  Timecop.travel(run_time_default_interval) do
-                    execute_all_jobs(expected_successes: 0, expected_failures: 0)
-                  end
-
-                  run_time_broker_interval = first_run_time + broker_polling_interval.seconds + 1.second
-                  Timecop.travel(run_time_broker_interval) do
-                    execute_all_jobs(expected_successes: 1, expected_failures: 0)
-                  end
-                end
-              end
-
-              context 'when the interval is less than the default configuration' do
-                let(:broker_polling_interval) { default_polling_interval / 2 }
-
-                it 'the polling interval should be the default specified in the configuration' do
-                  Timecop.freeze(Time.now)
-                  first_run_time = Time.now
-
-                  Jobs::Enqueuer.new(job, { queue: 'cc-generic', run_at: first_run_time }).enqueue
-                  execute_all_jobs(expected_successes: 1, expected_failures: 0)
-                  expect(Delayed::Job.count).to eq(1)
-
-                  run_time_default_interval = first_run_time + default_polling_interval.seconds + 1.second
-                  Timecop.travel(run_time_default_interval) do
-                    execute_all_jobs(expected_successes: 1, expected_failures: 0)
-                  end
-                end
-              end
-
-              context 'when the interval is greater than the max value (24 hours)' do
-                let(:broker_polling_interval) { 24.hours.seconds + 1.minutes }
-
-                it 'the polling interval should not exceed the max' do
-                  Timecop.freeze(Time.now)
-                  first_run_time = Time.now
-
-                  Jobs::Enqueuer.new(job, { queue: 'cc-generic', run_at: first_run_time }).enqueue
-                  execute_all_jobs(expected_successes: 1, expected_failures: 0)
-                  expect(Delayed::Job.count).to eq(1)
-
-                  run_time_max_interval = first_run_time + 24.hours + 1.second
-                  Timecop.travel(run_time_max_interval) do
-                    execute_all_jobs(expected_successes: 1, expected_failures: 0)
-                  end
-                end
-              end
-            end
-          end
+          include_examples 'when brokers return Retry-After header', :fetch_service_binding_last_operation
         end
       end
     end
