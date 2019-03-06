20c00be72140dda7968ed8e288eb723d8f1c0264
Mona Mohebbi <mmohebbi@pivotal.io>
2019-03-05 16:43:54 -0800

    Fix rubocop errors on migration
    Co-authored-by: Andres Medina <amedina@pivotal.io>
    Co-authored-by: Mona Mohebbi <mmohebbi@pivotal.io>

--- db/migrations/20190302003850_add_revision_description_column.rb
+++ db/migrations/20190302003850_add_revision_description_column.rb
@@ -3,6 +3,7 @@ Sequel.migration do
     alter_table(:revisions) do
       # rubocop:disable Migration/IncludeStringSize
       add_column :description, String, text: true, default: 'N/A', null: false
+      # rubocop:enable Migration/IncludeStringSize
     end
   end
 end
