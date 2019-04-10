535d040834544432f30b17f1c91544de6edb4b96
Tim Downey <tdowney@pivotal.io>
2019-04-10 11:45:34 -0700

    Additional refactoring
    Co-authored-by: Connor Braa <cbraa@pivotal.io>
    Co-authored-by: Tim Downey <tdowney@pivotal.io>

--- lib/cloud_controller/security/security_context_configurer.rb
+++ lib/cloud_controller/security/security_context_configurer.rb
@@ -17,15 +17,33 @@ module VCAP::CloudController
       end
 
       private
+      UUID_REGEX = /\A[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}\z/i.freeze
 
       def decode_token(header_token)
         token_information = @token_decoder.decode_token(header_token)
         return nil if token_information.nil? || token_information.empty?
 
         if !token_information['user_id'] && token_information['client_id']
+          if is_shadowing_user?(token_information['client_id'])
+            logger.error("Invalid token client_id: #{token_information['client_id']}")
+            raise VCAP::CloudController::UaaTokenDecoder::BadToken
+          end
+
           token_information['user_id'] = token_information['client_id']
         end
         token_information
+      end
+
+      def is_user_in_uaadb?(id)
+        CloudController::DependencyLocator.instance.uaa_client.usernames_for_ids(Array(id)).present?
+      end
+
+      def is_uuid_shaped?(id)
+        return true if id =~ UUID_REGEX
+      end
+
+      def is_shadowing_user?(client_id)
+        is_uuid_shaped?(client_id) && is_user_in_uaadb?(client_id)
       end
 
       def user_from_token(token)
