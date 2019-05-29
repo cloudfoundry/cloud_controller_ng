2068e578204205439c73a55dfe0113c16a6c4bbb
Joseph Palermo <jpalermo@pivotal.io>
2019-05-29 10:20:17 -0700

    Add mTLS to opi clients
    Signed-off-by: Kalai Wei <kawei@pivotal.io>
    Co-authored-by: Kalai Wei <kawei@pivotal.io>

--- lib/cloud_controller/opi/base_client.rb
+++ lib/cloud_controller/opi/base_client.rb
@@ -3,6 +3,14 @@ module OPI
     def initialize(config)
       @config = config
       @client = HTTPClient.new(base_url: url)
+      opi_config = config.get(:opi)
+      client_cert_file = opi_config[:client_cert_file]
+      client_key_file = opi_config[:client_key_file]
+      ca_file = opi_config[:ca_file]
+      if client_cert_file && client_key_file && ca_file
+        client.ssl_config.add_trust_ca(ca_file)
+        client.ssl_config.set_client_cert_file(client_cert_file, client_key_file)
+      end
     end
 
     private
