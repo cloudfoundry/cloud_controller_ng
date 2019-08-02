133486e37c8a66883f8cad0da6fcaeefad0feda3
Seth Boyles <sboyles@pivotal.io>
2019-08-02 11:57:52 -0700

    Catch error thrown if Diego Process Sync creates a desired lrp process
    * fixes race condition seen in ci
    Co-authored-by: Seth Boyles <sboyles@pivotal.io>
    Co-authored-by: Renee Chu <rchu@pivotal.io>
    
    [finishes #167495930]

--- spec/unit/lib/cloud_controller/diego/desire_app_handler_spec.rb
+++ spec/unit/lib/cloud_controller/diego/desire_app_handler_spec.rb
@@ -35,6 +35,22 @@ module VCAP::CloudController
             expect(client).not_to have_received(:desire_app)
           end
         end
+
+        context 'race condition when the Diego::ProcessesSync runs and creates a process ahead of the DesireAppHandler' do
+          let(:desired_lrp_update) { double(:desired_lrp_update) }
+          let(:get_app_response) {nil }
+
+          before do
+            allow(client).to receive(:update_app)
+            allow(client).to receive(:desire_app).and_raise (CloudController::Errors::ApiError.new_from_details('RunnerError', 'the requested resource already exists'))
+          end
+
+          it 'catches the error and updates the app' do
+            DesireAppHandler.create_or_update_app(process, client)
+            expect(client).to have_received(:update_app)
+            expect(client).to have_received(:get_app).exactly(2).times
+          end
+        end
       end
     end
   end
