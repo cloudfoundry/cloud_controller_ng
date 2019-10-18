c1cc5f389209e9d307566a8413eb51e7cab5bf5f
Tim Downey <tdowney@pivotal.io>
2019-10-18 13:21:00 -0700

    add ability to include domains on /v3/routes and /v3/routes/:guid
    Addresses issue: https://github.com/cloudfoundry/cloud_controller_ng/issues/1470
    
    [169074981](https://www.pivotaltracker.com/story/show/169074981)
    
    Authored-by: Tim Downey <tdowney@pivotal.io>

--- spec/unit/messages/route_show_message_spec.rb
+++ spec/unit/messages/route_show_message_spec.rb
@@ -5,26 +5,35 @@ module VCAP::CloudController
   RSpec.describe RouteShowMessage do
     describe 'fields' do
       it 'accepts a string guid param' do
-        message = RouteShowMessage.new({ 'guid' => 'some-guid' })
+        message = RouteShowMessage.from_params({ 'guid' => 'some-guid' })
         expect(message).to be_valid
       end
 
       it 'does not accept empty params' do
-        message = RouteShowMessage.new({})
+        message = RouteShowMessage.from_params({})
         expect(message).not_to be_valid
         expect(message.errors[:guid]).to include("can't be blank")
       end
 
       it 'does not accept guid params of incorrect type' do
-        message = RouteShowMessage.new({ 'guid' => 123 })
+        message = RouteShowMessage.from_params({ 'guid' => 123 })
         expect(message).not_to be_valid
         expect(message.errors[:guid]).to include('must be a string')
       end
 
       it 'does not accept any other params' do
-        message = RouteShowMessage.new({ 'foobar' => 'pants' })
+        message = RouteShowMessage.from_params({ 'foobar' => 'pants' })
         expect(message).not_to be_valid
         expect(message.errors[:base]).to include("Unknown query parameter(s): 'foobar'")
+      end
+    end
+
+    describe 'includes' do
+      it 'only allows domains to be included' do
+        message = RouteShowMessage.from_params({ 'guid' => 'some-guid', 'include' => 'domain' })
+        expect(message).to be_valid
+        message = RouteShowMessage.from_params({ 'guid' => 'some-guid', 'include' => 'kube' })
+        expect(message).not_to be_valid
       end
     end
   end
