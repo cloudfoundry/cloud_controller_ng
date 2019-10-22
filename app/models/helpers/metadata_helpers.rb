6b9c3e06133510c94140f04e8442975de016167c
Eric Promislow <eric.promislow@suse.com>
2019-10-22 18:06:16 +0200

    Improving label query processing: remove obsolete comments
    [#168488761](https://www.pivotaltracker.com/story/show/168488761)
    
    Authored-by: Eric Promislow <eric.promislow@suse.com>

--- app/models/helpers/metadata_helpers.rb
+++ app/models/helpers/metadata_helpers.rb
@@ -1,24 +1,6 @@
 module VCAP::CloudController
   class MetadataHelpers
     KEY_SEPARATOR = '/'.freeze
-    # REQUIREMENT_SPLITTER = /(?:\(.*?\)|[^,])+/.freeze
-    # KEY_CHARACTERS = %r{[\w\-\.\_\/]+}.freeze
-    #
-    # IN_PATTERN = /\A(?<key>.*?) in \((?<values>.*)\)\z/.freeze                     # foo in (bar,baz)
-    # NOT_IN_PATTERN = /\A(?<key>.*?) notin \((?<values>.*)\)\z/.freeze              # funky notin (uptown,downtown)
-    # EQUAL_PATTERN = /\A(?<key>#{KEY_CHARACTERS})(==?)(?<values>.*)\z/.freeze       # foo=bar or foo==bar
-    # NOT_EQUAL_PATTERN = /\A(?<key>#{KEY_CHARACTERS})(!=)(?<values>.*)\z/.freeze    # foo!=bar
-    # EXISTS_PATTERN = /^\A(?<key>#{KEY_CHARACTERS})(?<values>)\z/.freeze            # foo
-    # NOT_EXISTS_PATTERN = /\A!(?<key>#{KEY_CHARACTERS})(?<values>)\z/.freeze        # !foo
-    #
-    # REQUIREMENT_OPERATOR_PAIRS = [
-    #   { pattern: IN_PATTERN, operator: :in },
-    #   { pattern: NOT_IN_PATTERN, operator: :notin },
-    #   { pattern: EQUAL_PATTERN, operator: :equal },
-    #   { pattern: NOT_EQUAL_PATTERN, operator: :not_equal },
-    #   { pattern: EXISTS_PATTERN, operator: :exists }, # foo
-    #   { pattern: NOT_EXISTS_PATTERN, operator: :not_exists },
-    # ].freeze
 
     class << self
       def extract_prefix(metadata_key)
