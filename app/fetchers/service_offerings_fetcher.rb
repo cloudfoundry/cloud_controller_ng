36318e18ffe70a3dae16f5da10dd6f83e59a6e83
Joao Lebre <jlebre@pivotal.io>
2019-10-31 13:54:53 +0000

    Complete service offering fetcher for different usecases
    [#169341752]
    
    Co-authored-by: Oleksii Fedorov <ofedorov@pivotal.io>

--- app/fetchers/service_offerings_fetcher.rb
+++ app/fetchers/service_offerings_fetcher.rb
@@ -2,17 +2,27 @@ module VCAP::CloudController
   class ServiceOfferingsFetcher
     class << self
       def fetch_one(guid, org_guids: nil)
-        return nil if org_guids == []
         return Service.find(guid: guid) unless org_guids
 
         guids = Service.dataset.
                 join(:service_plans, service_id: :id).
-                join(:service_plan_visibilities, service_plan_id: :id).
-                join(:organizations, id: :organization_id).
-                where(
-                  Sequel[:organizations][:guid] => org_guids,
-                  Sequel[:services][:guid] => guid
-                ).
+                left_join(:service_plan_visibilities, service_plan_id: :id).
+                left_join(:organizations, id: :organization_id).
+                where do
+                  (Sequel[:services][:guid] =~ guid) &
+                    ((Sequel[:service_plans][:public] =~ true) | (Sequel[:organizations][:guid] =~ org_guids))
+                end.
+                select(Sequel[:services][:guid]).
+                all.
+                map(&:guid)
+
+        Service.find(guid: guids.first)
+      end
+
+      def fetch_one_anonymously(guid)
+        guids = Service.dataset.
+                join(:service_plans, service_id: :id).
+                where { (Sequel[:services][:guid] =~ guid) & (Sequel[:service_plans][:public] =~ true) }.
                 select(Sequel[:services][:guid]).
                 all.
                 map(&:guid)
--- spec/unit/fetchers/service_offerings_fetcher_spec.rb
+++ spec/unit/fetchers/service_offerings_fetcher_spec.rb
@@ -26,7 +26,7 @@ module VCAP::CloudController
     end
 
     describe '#fetch_one(guid, org_guids:)' do
-      let(:plan) { ServicePlan.make }
+      let(:plan) { ServicePlan.make(public: false) }
       let(:offering) { plan.service }
       let!(:visibility) { ServicePlanVisibility.make(service_plan: plan, organization: org_2) }
 
@@ -49,6 +49,55 @@ module VCAP::CloudController
 
         it 'returns nil' do
           expect(ServiceOfferingsFetcher.fetch_one(offering_3.guid, org_guids: org_guids)).to be_nil
+        end
+      end
+
+      context 'when the specified plan is public' do
+        let(:plan) { ServicePlan.make(public: true) }
+        let(:org_guids) { [org_1.guid, org_3.guid] }
+
+        it 'returns the service offering even if org guids is empty' do
+          expect(ServiceOfferingsFetcher.fetch_one(offering.guid, org_guids: [])).to eq(offering)
+        end
+
+        it 'returns the service offering even if org guids does not include where it is enabled' do
+          expect(ServiceOfferingsFetcher.fetch_one(offering.guid, org_guids: org_guids)).to eq(offering)
+        end
+      end
+
+      context 'when there are no service plan visibilites' do
+        let(:plan) { ServicePlan.make(public: true) }
+        let(:offering) { plan.service }
+        let(:visibility) {}
+
+        it 'returns the service offering' do
+          expect(ServiceOfferingsFetcher.fetch_one(offering.guid, org_guids: [])).to eq(offering)
+        end
+      end
+    end
+
+    describe '#fetch_one_anonymously(guid)' do
+      let(:plan) { ServicePlan.make(public: true) }
+      let(:offering) { plan.service }
+
+      context 'when the specified GUID does not match any existing offerings' do
+        it 'returns nil' do
+          expect(ServiceOfferingsFetcher.fetch_one_anonymously('does-not-exist')).to be_nil
+        end
+      end
+
+      context 'when the specified GUID matches an existing offering' do
+        it 'returns the service offering' do
+          expect(ServiceOfferingsFetcher.fetch_one_anonymously(offering.guid)).to eq(offering)
+        end
+      end
+
+      context 'when the specified GUID matches a non-public service offering' do
+        let(:private_plan) { ServicePlan.make(public: false) }
+        let(:private_offering) { private_plan.service }
+
+        it 'returns nil' do
+          expect(ServiceOfferingsFetcher.fetch_one_anonymously(private_offering.guid)).to be_nil
         end
       end
     end
