6162d45db20bb58fad9090422a4358ae216c20b0
George Blue <blgm@users.noreply.github.com>
2019-11-22 11:36:50 +0000

    V3 service_offerings visibility for space-scoped offerings (#1500)
    - service offerings from space-scoped can be seen
    - split the ServiceOfferingsFetcher so that plan visibility is handled
    with a new ServicePlanVisibilityFetcher
    - now respects the `hide_marketplace_from_unauthenticated_users` feature flag
    
    [more details in story #169482815](https://www.pivotaltracker.com/story/show/169482815)
    
    [finishes #169894646](https://www.pivotaltracker.com/story/show/169894646)

--- app/controllers/v3/service_offerings_controller.rb
+++ app/controllers/v3/service_offerings_controller.rb
@@ -1,37 +1,52 @@
 require 'fetchers/service_offerings_fetcher'
+require 'fetchers/service_plan_visibility_fetcher'
 require 'presenters/v3/service_offering_presenter'
 
 class ServiceOfferingsController < ApplicationController
   def show
     guid = hashed_params[:guid]
 
-    offering = if !current_user
-                 ServiceOfferingsFetcher.fetch_one_anonymously(guid)
-               elsif permission_queryer.can_read_globally?
-                 ServiceOfferingsFetcher.fetch_one(guid)
-               else
-                 ServiceOfferingsFetcher.fetch_one(guid, org_guids: permission_queryer.readable_org_guids)
-               end
+    not_authenticated! if !current_user && VCAP::CloudController::FeatureFlag.enabled?(:hide_marketplace_from_unauthenticated_users)
 
+    offering, space, public = ServiceOfferingsFetcher.fetch(guid)
     service_offering_not_found! if offering.nil?
 
-    presenter = Presenters::V3::ServiceOfferingPresenter.new(offering)
-    render status: :ok, json: presenter.to_json
+    if permission_queryer.can_read_globally? || public || visible_space_scoped?(space) || visible_in_readable_orgs?(offering)
+      presenter = Presenters::V3::ServiceOfferingPresenter.new(offering)
+      render status: :ok, json: presenter.to_json
+    else
+      service_offering_not_found!
+    end
   end
 
   def enforce_authentication?
-    return false if action_name == 'show'
-
-    super
+    action_name == 'show' ? false : super
   end
 
   def enforce_read_scope?
-    return false if action_name == 'show'
+    action_name == 'show' ? false : super
+  end
 
-    super
+  private
+
+  def visible_in_readable_orgs?(offering)
+    return false if !current_user
+
+    ServicePlanVisibilityFetcher.service_plans_visible_in_orgs?(offering.service_plans.map(&:guid), permission_queryer.readable_org_guids)
+  end
+
+  def visible_space_scoped?(space)
+    return false if !current_user
+    return false if !space
+
+    space.has_member?(current_user)
   end
 
   def service_offering_not_found!
     resource_not_found!(:service_offering)
+  end
+
+  def not_authenticated!
+    raise CloudController::Errors::NotAuthenticated
   end
 end
--- app/fetchers/service_offerings_fetcher.rb
+++ app/fetchers/service_offerings_fetcher.rb
@@ -1,33 +1,14 @@
 module VCAP::CloudController
   class ServiceOfferingsFetcher
     class << self
-      def fetch_one(guid, org_guids: nil)
-        return Service.find(guid: guid) unless org_guids
+      def fetch(service_offering_guid)
+        service_offering = Service.where(guid: service_offering_guid).eager(:service_plans, :service_broker).first
+        return [nil, nil, false] if service_offering.nil?
 
-        guids = Service.dataset.
-                join(:service_plans, service_id: :id).
-                left_join(:service_plan_visibilities, service_plan_id: :id).
-                left_join(:organizations, id: :organization_id).
-                where do
-                  (Sequel[:services][:guid] =~ guid) &
-                    ((Sequel[:service_plans][:public] =~ true) | (Sequel[:organizations][:guid] =~ org_guids))
-                end.
-                select(Sequel[:services][:guid]).
-                all.
-                map(&:guid)
+        public = service_offering.service_plans.any?(&:public)
+        space = service_offering.service_broker.space
 
-        Service.find(guid: guids.first)
-      end
-
-      def fetch_one_anonymously(guid)
-        guids = Service.dataset.
-                join(:service_plans, service_id: :id).
-                where { (Sequel[:services][:guid] =~ guid) & (Sequel[:service_plans][:public] =~ true) }.
-                select(Sequel[:services][:guid]).
-                all.
-                map(&:guid)
-
-        Service.find(guid: guids.first)
+        [service_offering, space, public]
       end
     end
   end
