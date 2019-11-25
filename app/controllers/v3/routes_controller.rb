d9f39bbe17e9def7b5c68e857916445187252b26
Tim Downey <tdowney@pivotal.io>
2019-11-25 12:31:45 -0800

    add ability to include domains on /v3/routes and /v3/routes/:guid
    Addresses issue: https://github.com/cloudfoundry/cloud_controller_ng/issues/1470
    
    [169074981](https://www.pivotaltracker.com/story/show/169074981)
    
    Authored-by: Tim Downey <tdowney@pivotal.io>

--- app/controllers/v3/routes_controller.rb
+++ app/controllers/v3/routes_controller.rb
@@ -4,6 +4,7 @@ require 'messages/route_show_message'
 require 'messages/route_update_message'
 require 'messages/route_update_destinations_message'
 require 'actions/update_route_destinations'
+require 'decorators/include_route_domain_decorator'
 require 'presenters/v3/route_presenter'
 require 'presenters/v3/route_destinations_presenter'
 require 'presenters/v3/paginated_list_presenter'
@@ -24,22 +25,32 @@ class RoutesController < ApplicationController
       eager_loaded_associations: Presenters::V3::RoutePresenter.associated_resources
     )
 
+    decorators = []
+    decorators << IncludeRouteDomainDecorator if IncludeRouteDomainDecorator.match?(message.include)
+
     render status: :ok, json: Presenters::V3::PaginatedListPresenter.new(
       presenter: Presenters::V3::RoutePresenter,
       paginated_result: SequelPaginator.new.get_page(dataset, message.try(:pagination_options)),
       path: '/v3/routes',
       message: message,
+      decorators: decorators,
     )
   end
 
   def show
-    message = RouteShowMessage.new({ guid: hashed_params['guid'] })
+    message = RouteShowMessage.from_params(query_params.merge(guid: hashed_params[:guid]))
     unprocessable!(message.errors.full_messages) unless message.valid?
 
     route = Route.find(guid: message.guid)
     route_not_found! unless route && permission_queryer.can_read_route?(route.space.guid, route.organization.guid)
 
-    render status: :ok, json: Presenters::V3::RoutePresenter.new(route)
+    decorators = []
+    decorators << IncludeRouteDomainDecorator if IncludeRouteDomainDecorator.match?(message.include)
+
+    render status: :ok, json: Presenters::V3::RoutePresenter.new(
+      route,
+      decorators: decorators,
+    )
   end
 
   def create
@@ -79,7 +90,7 @@ class RoutesController < ApplicationController
   end
 
   def destroy
-    message = RouteShowMessage.new({ guid: hashed_params['guid'] })
+    message = RouteShowMessage.from_params({ guid: hashed_params['guid'] })
     unprocessable!(message.errors.full_messages) unless message.valid?
 
     route = Route.find(guid: message.guid)
@@ -95,7 +106,7 @@ class RoutesController < ApplicationController
   end
 
   def index_destinations
-    message = RouteShowMessage.new({ guid: hashed_params['guid'] })
+    message = RouteShowMessage.from_params({ guid: hashed_params['guid'] })
     unprocessable!(message.errors.full_messages) unless message.valid?
 
     route = Route.find(guid: message.guid)
