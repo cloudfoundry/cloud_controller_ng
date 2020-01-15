7fc7700e9d6191bfc541e7d174da7a1114acb3da
Marcela Campo <mcampo@pivotal.io>
2020-01-15 16:51:07 +0000

    Update service_offering object
    [finishes #170701141]
    [finishes #170644025]
    [PR]
    
    Co-authored-by: George Blue <gblue@pivotal.io>

--- spec/unit/presenters/v3/service_offering_presenter_spec.rb
+++ spec/unit/presenters/v3/service_offering_presenter_spec.rb
@@ -6,14 +6,17 @@ RSpec.describe VCAP::CloudController::Presenters::V3::ServiceOfferingPresenter d
   let(:guid) { 'some-offering-guid' }
   let(:name) { 'some-offering-name' }
   let(:description) { 'some offering description' }
-  let(:available) { true }
-  let(:bindable) { true }
-  let(:metadata) { '{"foo": "bar"}' }
+  let(:available) { false }
+  let(:bindable) { false }
+  let(:metadata) { '{"foo": "bar", "baz": {"answer": 42}' }
   let(:id) { 'broker-id' }
   let(:tags) { %w(foo bar) }
   let(:requires) { %w(syslog_drain route_forwarding volume_mount) }
-  let(:updateable) { true }
+  let(:updateable) { false }
   let(:service_broker) { VCAP::CloudController::ServiceBroker.make }
+  let(:instances_retrievable) { false }
+  let(:bindings_retrievable) { false }
+  let(:created_at) { Time.now.round(0) - 100 }
 
   let(:service_offering) do
     VCAP::CloudController::Service.make(
@@ -28,27 +31,41 @@ RSpec.describe VCAP::CloudController::Presenters::V3::ServiceOfferingPresenter d
       requires: requires,
       plan_updateable: updateable,
       service_broker: service_broker,
+      instances_retrievable: instances_retrievable,
+      bindings_retrievable: bindings_retrievable,
+      created_at: created_at
     )
   end
 
   describe '#to_hash' do
-    let(:result) { described_class.new(service_offering).to_hash }
+    let(:result) { described_class.new(service_offering).to_hash.deep_symbolize_keys }
 
     it 'presents the service offering as JSON' do
-      expect(result).to eq({
+      expect(result).to match({
         'guid': guid,
         'name': name,
         'description': description,
         'available': available,
-        'bindable': bindable,
-        'broker_service_offering_metadata': metadata,
-        'broker_service_offering_id': id,
         'tags': tags,
         'requires': requires,
-        'created_at': service_offering.created_at,
+        'created_at': created_at,
         'updated_at': service_offering.updated_at,
-        'plan_updateable': updateable,
         'shareable': false,
+        'broker_catalog': {
+          'id': id,
+          'metadata': {
+            'foo': 'bar',
+            'baz': {
+              'answer': 42,
+            }
+          },
+          'features': {
+            'plan_updateable': updateable,
+            'bindable': bindable,
+            'instances_retrievable': instances_retrievable,
+            'bindings_retrievable': bindings_retrievable
+          }
+        },
         'links': {
           'self': {
             'href': "#{link_prefix}/v3/service_offerings/#{guid}"
@@ -71,19 +88,81 @@ RSpec.describe VCAP::CloudController::Presenters::V3::ServiceOfferingPresenter d
       })
     end
 
-    context 'when the broker metadata contains a `shareable` field' do
+    context 'when `available` is true' do
+      let(:available) { true }
+
+      it 'displays `true``' do
+        expect(result.dig(:available)).to be(true)
+      end
+    end
+
+    context 'when `bindable` is true' do
+      let(:bindable) { true }
+
+      it 'displays `true``' do
+        expect(result.dig(:broker_catalog, :features, :bindable)).to be(true)
+      end
+    end
+
+    context 'when `updateable` is true' do
+      let(:updateable) { true }
+
+      it 'displays `true``' do
+        expect(result.dig(:broker_catalog, :features, :plan_updateable)).to be(true)
+      end
+    end
+
+    context 'when `instances_retrievable` is true' do
+      let(:instances_retrievable) { true }
+
+      it 'displays `true``' do
+        expect(result.dig(:broker_catalog, :features, :instances_retrievable)).to be(true)
+      end
+    end
+
+    context 'when `bindings_retrievable` is true' do
+      let(:bindings_retrievable) { true }
+
+      it 'displays `true``' do
+        expect(result.dig(:broker_catalog, :features, :bindings_retrievable)).to be(true)
+      end
+    end
+
+    context 'when `shareable` is true' do
       let(:metadata) { '{"shareable": true}' }
 
-      it 'reads it' do
+      it 'displays `true``' do
         expect(result[:shareable]).to be true
       end
     end
 
+    context 'when `shareable` is non-boolean' do
+      let(:metadata) { '{"shareable": "invalid value"}' }
+
+      it 'displays `false``' do
+        expect(result[:shareable]).to be false
+      end
+    end
+
+    context 'when `shareable` is explicitly false' do
+      let(:metadata) { '{"shareable": false}' }
+
+      it 'displays `false``' do
+        expect(result[:shareable]).to be false
+      end
+    end
+
+    # Note that the metadata is saved as a serialized JSON string, so it should always
+    # be possible to parse it.
     context 'when the broker metadata cannot be parsed' do
       let(:metadata) { 'this will cause a JSON parse error' }
 
       it 'defaults `shareable` to false' do
         expect(result[:shareable]).to be false
+      end
+
+      it 'defaults `metadata` to nil' do
+        expect(result.dig(:broker_catalog, :metadata)).to be_nil
       end
     end
   end
