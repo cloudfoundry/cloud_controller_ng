75380f4c46ad9f936a4963826aabdd486645413c
Marcela Campo <mcampo@pivotal.io>
2020-02-12 11:13:02 +0000

    Refactor how allowed keys are specified
    [#171131664]

--- app/messages/service_plans_list_message.rb
+++ app/messages/service_plans_list_message.rb
@@ -3,22 +3,25 @@ require 'messages/validators/label_selector_requirement_validator'
 
 module VCAP::CloudController
   class ServicePlansListMessage < MetadataListMessage
-    register_allowed_keys [
-      :names,
-      :available,
-      :space_guids,
-      :organization_guids,
-      :service_broker_guids,
-      :service_broker_names,
-      :service_offering_guids,
-      :broker_catalog_ids,
+    @@array_keys = [:names,
+                    :space_guids,
+                    :organization_guids,
+                    :service_broker_guids,
+                    :service_broker_names,
+                    :service_offering_guids,
+                    :broker_catalog_ids,
     ]
+    @@single_keys = [
+      :available,
+    ]
+
+    register_allowed_keys(@@single_keys + @@array_keys)
 
     validates_with NoAdditionalParamsValidator
     validates :available, inclusion: { in: %w(true false), message: "only accepts values 'true' or 'false'" }, allow_nil: true
 
     def self.from_params(params)
-      super(params, %w(names space_guids organization_guids service_broker_guids service_broker_names service_offering_guids broker_catalog_ids))
+      super(params, @@array_keys.map(&:to_s))
     end
 
     def available?
