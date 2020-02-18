6108f57d4a99b4ca9b7da4f864040b2dd3879827
Reid Mitchell <rmitchell@pivotal.io>
2020-02-18 14:24:15 -0800

    add missing file for previous commit
    [finishes #168655063]
    
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>
    Co-authored-by: Merric de Launey <mdelauney@pivotal.io>

---
+++ app/presenters/v3/security_group_presenter.rb
@@ -0,0 +1,29 @@
+require 'presenters/v3/base_presenter'
+
+module VCAP::CloudController::Presenters::V3
+  class SecurityGroupPresenter < BasePresenter
+    def to_hash
+      {
+        guid: security_group.guid,
+        created_at: security_group.created_at,
+        updated_at: security_group.updated_at,
+        name: security_group.name,
+        links: build_links,
+      }
+    end
+
+    private
+
+    def security_group
+      @resource
+    end
+
+    def build_links
+      url_builder = VCAP::CloudController::Presenters::ApiUrlBuilder.new
+
+      {
+        self: { href: url_builder.build_url(path: "/v3/security_groups/#{security_group.guid}") },
+      }
+    end
+  end
+end
