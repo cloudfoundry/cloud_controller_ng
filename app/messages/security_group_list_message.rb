842f92fce810ea0909c6ca63abc52eaa813ba983
Teal Stannard <tstannard@pivotal.io>
2020-02-28 15:52:50 -0800

    v3: Client can filter security groups
    Valid filter keys are:
    - globally_enabled_running
    - globally_enabled_staging
    - staging_space_guids
    - running_space_guids
    - names
    - guids
    
    [#168655161]
    
    Co-authored-by: Teal Stannard <tstannard@pivotal.io>
    Co-authored-by: Belinda Liu <bliu@pivotal.io>

--- app/messages/security_group_list_message.rb
+++ app/messages/security_group_list_message.rb
@@ -2,9 +2,16 @@ require 'messages/list_message'
 
 module VCAP::CloudController
   class SecurityGroupListMessage < ListMessage
-    register_allowed_keys []
+    register_allowed_keys [:guids, :names, :running_space_guids, :staging_space_guids, :globally_enabled_running, :globally_enabled_staging]
 
     validates_with NoAdditionalParamsValidator
+
+    validates :guids, array: true, allow_nil: true
+    validates :names, array: true, allow_nil: true
+    validates :running_space_guids, array: true, allow_nil: true
+    validates :staging_space_guids, array: true, allow_nil: true
+    validates :globally_enabled_running, array: true, allow_nil: true
+    validates :globally_enabled_staging, array: true, allow_nil: true
 
     def self.from_params(params)
       super(params, %w())
