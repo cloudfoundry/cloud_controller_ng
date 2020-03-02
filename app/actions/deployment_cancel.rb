128916a94b5eb6345f5b08a92ca4772f8665adaa
Greg Cobb <gcobb@pivotal.io>
2020-03-02 15:15:18 -0800

    Inline valid_state? method in DeploymentCancel
    - In response to PR feedback
    
    [#1513]
    
    Authored-by: Greg Cobb <gcobb@pivotal.io>

--- app/actions/deployment_cancel.rb
+++ app/actions/deployment_cancel.rb
@@ -11,7 +11,7 @@ module VCAP::CloudController
       def cancel(deployment:, user_audit_info:)
         deployment.db.transaction do
           deployment.lock!
-          reject_invalid_state!(deployment) unless valid_state?(deployment)
+          reject_invalid_state!(deployment) unless deployment.cancelable?
 
           begin
             AppAssignDroplet.new(user_audit_info).assign(deployment.app, deployment.previous_droplet)
@@ -28,10 +28,6 @@ module VCAP::CloudController
       end
 
       private
-
-      def valid_state?(deployment)
-        deployment.cancelable?
-      end
 
       def reject_invalid_state!(deployment)
         raise InvalidStatus.new("Cannot cancel a deployment with status: #{deployment.status_value} and reason: #{deployment.status_reason}")
