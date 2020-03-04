7d12d5bf4e0854dc9b08896397db59c738795289
Jaskanwal Pawar <jpawar@pivotal.io>
2020-03-04 14:34:21 -0800

    Make max duraction configurable on worker schema again
    Co-authored-by: Jaskanwal Pawar <jpawar@pivotal.io>
    Co-authored-by: Renee Chu <rchu@pivotal.io>

--- lib/cloud_controller/config_schemas/worker_schema.rb
+++ lib/cloud_controller/config_schemas/worker_schema.rb
@@ -36,7 +36,7 @@ module VCAP::CloudController
           stacks_file: String,
           newrelic_enabled: bool,
 
-          max_migration_duration_in_minutes: Integer,
+          optional(:max_migration_duration_in_minutes) => Integer,
           db: {
             optional(:database) => Hash, # db connection hash for sequel
             max_connections: Integer, # max connections in the connection pool
