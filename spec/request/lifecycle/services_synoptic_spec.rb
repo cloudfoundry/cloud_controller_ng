ec3252adfb0da5335eb36f591856adaa57569d87
Marcela Campo <mcampo@pivotal.io>
2020-03-06 15:01:21 +0000

    Add synoptic lifecycle tests for service plans
    [#171049657](https://www.pivotaltracker.com/story/show/171049657)

--- spec/request/lifecycle/services_synoptic_spec.rb
+++ spec/request/lifecycle/services_synoptic_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'V3 services synoptic' do
     expect(last_response).to have_status_code(202)
     execute_all_jobs(expected_successes: 1, expected_failures: 0)
 
-    get '/v3/service_offerings', {}, admin_headers
+    get '/v3/service_offerings', nil, admin_headers
     expect(last_response).to have_status_code(200)
     expect(parsed_response['resources'][0]).to include(
       'name' => 'service_name-1'
@@ -26,9 +26,39 @@ RSpec.describe 'V3 services synoptic' do
       'name' => 'route_volume_service_name-2'
     )
 
-    # TODO: enable service access once those endpoints are written
-    # TODO: view service plans those endpoints are written
+    get '/v3/service_plans', nil, admin_headers
+    expect(last_response).to have_status_code(200)
+    expect(parsed_response['resources'][0]).to include(
+      'name' => 'plan_name-1'
+    )
+    expect(parsed_response['resources'][1]).to include(
+      'name' => 'plan_name-2'
+    )
+
+    plan_guid = parsed_response['resources'][0]['guid']
+
+    get '/v3/service_plans', nil, {}
+    expect(last_response).to have_status_code(200)
+    expect(parsed_response['resources']).to have(0).elements
+
+    post "v3/service_plans/#{plan_guid}/visibility", visibility_request.to_json, admin_headers
+    expect(last_response).to have_status_code(200)
+
+    get '/v3/service_plans', nil, {}
+    expect(last_response).to have_status_code(200)
+    expect(parsed_response['resources']).to have(1).elements
+
+    get "v3/service_plans/#{plan_guid}/visibility", nil, admin_headers
+    expect(last_response).to have_status_code(200)
+    expect(parsed_response['type']).to eq('public')
+
     # TODO: create a service instance once those endpoints are written
+  end
+
+  let(:visibility_request) do
+    {
+        type: 'public'
+    }
   end
 
   let(:create_service_broker_request_body) do
