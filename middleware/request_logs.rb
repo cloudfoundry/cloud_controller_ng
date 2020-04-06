882e651f57b119cb4b3488818c7902dfdaf8be6b
Florian Braun <FloThinksPi@users.noreply.github.com>
2020-04-06 10:55:03 +0200

    Add config-flag for GDPR compliance and changed logging of ip adresses
    IP Adresses now get their last octet cut of and replaced by 0 if the
    new config option logging.gdpr_compliant is true. E.g 192.168.1.80
    will be logged as 192.168.1.0 which provides sufficent anonymization
    to comply with european data protection regulations.
    
    If the flag is false or not present, CC will have the old behaviour
    which means logging the full ips.
    
    Signed-off-by: Marc Misoch <47423110+MMisoch@users.noreply.github.com>
    
    Add unittest for ip anonymization in request logs
    
    Signed-off-by: Marc Misoch <47423110+MMisoch@users.noreply.github.com>
    
    Fix rubocop issues for request_logs(_specs).rb
    
    Signed-off-by: Marc Misoch <47423110+MMisoch@users.noreply.github.com>
    
    Make ip anonymization ipv6 capable and use stdlib to validate adresses
    
    We ditched any additional libary as the stdlib is sufficient and we dont
    want to introduce new dependencies as this is for now just needed once.
    
    Signed-off-by: Marc Misoch <47423110+MMisoch@users.noreply.github.com>
    
    Add more test for ipv6 behaviour and print ipv6 canonicaly by default
    
    Signed-off-by: Marc Misoch <47423110+MMisoch@users.noreply.github.com>
    
    Renamed config flag to anonymize_ips to reflect correct behaviour
    
    Signed-off-by: Marc Misoch <47423110+MMisoch@users.noreply.github.com>
    
    Pull out anonymize ip logic in own function
    
    Signed-off-by: Marc Misoch <47423110+MMisoch@users.noreply.github.com>

--- middleware/request_logs.rb
+++ middleware/request_logs.rb
@@ -1,3 +1,5 @@
+require 'ipaddr'
+
 module CloudFoundry
   module Middleware
     class RequestLogs
@@ -6,17 +8,31 @@ module CloudFoundry
         @logger = logger
       end
 
+      def anonymize_ip(request_ip)
+        # Remove last octet of ip if EU GDPR compliance is needed
+        ip = IPAddr.new(request_ip) rescue nil
+        if VCAP::CloudController::Config.config.get(:logging, :anonymize_ips) && ip.nil?.!
+           if ip.ipv4?
+             ip.to_string.split('.')[0...-1].join('.') + '.0'
+           else
+             ip.to_string.split(':')[0...-5].join(':') + ':0000:0000:0000:0000:0000'
+           end
+         else
+           ip.to_string rescue request_ip
+         end
+      end
+
       def call(env)
         request = ActionDispatch::Request.new(env)
 
         @logger.info(
           sprintf('Started %<method>s "%<path>s" for user: %<user>s, ip: %<ip>s with vcap-request-id: %<request_id>s at %<at>s',
-            method: request.request_method,
-            path: request.filtered_path,
-            user: env['cf.user_guid'],
-            ip: request.ip,
-            request_id: env['cf.request_id'],
-            at: Time.now.utc)
+                  method: request.request_method,
+                  path: request.filtered_path,
+                  user: env['cf.user_guid'],
+                  ip: anonymize_ip(request.ip),
+                  request_id: env['cf.request_id'],
+                  at: Time.now.utc)
         )
 
         status, headers, body = @app.call(env)
