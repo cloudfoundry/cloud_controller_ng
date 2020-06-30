98b3ed7fef7aedcda5c0d22a83e1a34e9d9d63d9
Matt Royal <mroyal@pivotal.io>
2020-06-30 10:46:17 -0700

    /v3/buildpacks returns Cloud Native Buildpacks
    - Add KpackBuildpackListFetcher to translate K8s CRs into buildpack
      objects
    - Add request spec
    - Add new Kubeclient for interacting with experimental.kpack.pivotal.io api
    - NOTE: Filtering does not work for CNBs
    
    [#169924422]
    
    Co-authored-by: Matt Royal <mroyal@pivotal.io>
    Co-authored-by: Sannidhi Jalukar <sjalukar@pivotal.io>

--- lib/cloud_controller/dependency_locator.rb
+++ lib/cloud_controller/dependency_locator.rb
@@ -370,14 +370,23 @@ module CloudController
 
     def kpack_client
       kubernetes_config = VCAP::CloudController::Config.config.get(:kubernetes)
-      kube_client = Kubernetes::KubeClientBuilder.build({
+      service_account_token = File.open(kubernetes_config[:service_account][:token_file]).read
+      ca_crt = File.open(kubernetes_config[:ca_file]).read
+      build_kube_client = Kubernetes::KubeClientBuilder.build({
         api_group_url: "#{kubernetes_config[:host_url]}/apis/build.pivotal.io",
         version: 'v1alpha1',
-        service_account_token: File.open(kubernetes_config[:service_account][:token_file]).read,
-        ca_crt: File.open(kubernetes_config[:ca_file]).read
+        service_account_token: service_account_token,
+        ca_crt: ca_crt,
       })
 
-      Kubernetes::KpackClient.new(kube_client)
+      kpack_kube_client = Kubernetes::KubeClientBuilder.build({
+        api_group_url: "#{kubernetes_config[:host_url]}/apis/experimental.kpack.pivotal.io",
+        version: 'v1alpha1',
+        service_account_token: service_account_token,
+        ca_crt: ca_crt,
+      })
+
+      Kubernetes::KpackClient.new(build_kube_client: build_kube_client, kpack_kube_client: kpack_kube_client)
     end
 
     def route_crd_client
