b8eb81f92b523036e575ffd7da69123d4cf5981b
Piyali Banerjee <pbanerjee@pivotal.io>
2020-08-04 15:36:39 -0700

    Allow droplet image to be updated.
    - Allow droplet image to be updated if droplet has a docker lifecycle
    and if the droplet has already been staged. This lays the groundwork for
    the image controller to be able to patch a droplet with a rebased image reference.
    
    [#172542372]
    Co-authored-by: Piyali Banerjee <pbanerjee@pivotal.io>
    Co-authored-by: Nat Bennett <nbennett@pivotal.io>

--- app/actions/droplet_update.rb
+++ app/actions/droplet_update.rb
@@ -1,11 +1,19 @@
 module VCAP::CloudController
   class DropletUpdate
-    class Error < ::StandardError
-    end
+    class Error < ::StandardError; end
+    class InvalidDroplet < StandardError; end
 
     def update(droplet, message)
       droplet.db.transaction do
         droplet.lock!
+
+        if message.requested?(:image)
+          raise InvalidDroplet.new('Droplet image cannot be updated during staging') if droplet.staging?
+          raise InvalidDroplet.new('Images can only be updated for docker droplets') unless droplet.docker?
+
+          droplet.docker_receipt_image = message.image
+        end
+
         LabelsUpdate.update(droplet, message.labels, DropletLabelModel)
         AnnotationsUpdate.update(droplet, message.annotations, DropletAnnotationModel)
         droplet.save
