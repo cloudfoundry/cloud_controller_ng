874360af5faee9f71be027a409cb9dd284db6832
toby lorne <toby@toby.codes>
2020-08-13 23:13:07 +0100

    revisions: #show_environment_variables audit event
    Revisions environment variables can contain sensitive information,
    access to sensitive information should be audited
    
    Signed-off-by: toby lorne <toby@toby.codes>

--- app/repositories/revision_event_repository.rb
+++ app/repositories/revision_event_repository.rb
@@ -22,6 +22,26 @@ module VCAP::CloudController
           organization_guid: app.space.organization_guid,
         )
       end
+
+      def self.record_show_environment_variables(revision, app, user_audit_info)
+        Event.create(
+          type:              'audit.app.revision.environment_variables.show',
+          actor:             user_audit_info.user_guid,
+          actor_type:        'user',
+          actor_name:        user_audit_info.user_email,
+          actor_username:    user_audit_info.user_name,
+          actee:             app.guid,
+          actee_type:        'app',
+          actee_name:        app.name,
+          timestamp:         Sequel::CURRENT_TIMESTAMP,
+          metadata:          {
+            revision_guid: revision.guid,
+            revision_version: revision.version
+          },
+          space_guid:        app.space_guid,
+          organization_guid: app.space.organization_guid,
+        )
+      end
     end
   end
 end
