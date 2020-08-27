f121c28d4227123167be95f7ac8360e371269134
Brian Butz <bbutz@pivotal.io>
2020-08-27 17:03:46 +0100

    v3(services): Refactor service_binding_read action for clarity
    [#173529395](https://www.pivotaltracker.com/story/show/173529395)
    
    Signed-off-by: Urse Searle <usearle@pivotal.io>

--- app/actions/service_binding_read.rb
+++ app/actions/service_binding_read.rb
@@ -6,18 +6,25 @@ module VCAP::CloudController
     end
 
     def fetch_parameters(service_binding)
+      ensure_parameters_are_retrievable(service_binding)
+      retrieve_parameters(service_binding)
+    end
+
+    private
+
+    def ensure_parameters_are_retrievable(service_binding)
       unless binding_retrievable?(service_binding)
         raise NotSupportedError.new
       end
 
       raise_if_binding_locked(service_binding)
+    end
 
+    def retrieve_parameters(service_binding)
       client = VCAP::Services::ServiceClientProvider.provide(instance: service_binding.service_instance)
       response = client.fetch_service_binding(service_binding)
       response.fetch(:parameters, {})
     end
-
-    private
 
     def binding_retrievable?(service_binding)
       service_binding.service_instance.managed_instance? && service_binding.service.bindings_retrievable
