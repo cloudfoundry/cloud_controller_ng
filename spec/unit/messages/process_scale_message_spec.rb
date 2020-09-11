256c714f868ccd571110ba2b28060fb0e68919de
Merric de Launey <mdelauney@pivotal.io>
2020-09-11 23:42:43 +0000

    v3: Scaling process fields will not return a 500 when values exceed DB limit
    [#174455697]
    
    Authored-by: Merric de Launey <mdelauney@pivotal.io>

--- spec/unit/messages/process_scale_message_spec.rb
+++ spec/unit/messages/process_scale_message_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'messages/process_scale_message'
+require 'messages/base_message'
 
 module VCAP::CloudController
   RSpec.describe ProcessScaleMessage do
@@ -50,6 +51,18 @@ module VCAP::CloudController
       end
     end
 
+    context 'when instances is > the max value allowed in the database' do
+      let(:params) { { instances: BaseMessage::MAX_DB_INT + 1 } }
+
+      it 'is not valid' do
+        message = ProcessScaleMessage.new(params)
+
+        expect(message).not_to be_valid
+        expect(message.errors.count).to eq(1)
+        expect(message.errors[:instances]).to include('must be less than or equal to 2147483647')
+      end
+    end
+
     context 'when memory_in_mb is not an number' do
       let(:params) { { memory_in_mb: 'silly string thing' } }
 
@@ -75,14 +88,14 @@ module VCAP::CloudController
     end
 
     context 'when memory_in_mb is > the max value allowed in the database' do
-      let(:params) { { memory_in_mb: 0 } }
+      let(:params) { { memory_in_mb: BaseMessage::MAX_DB_INT + 1 } }
 
       it 'is not valid' do
         message = ProcessScaleMessage.new(params)
 
         expect(message).not_to be_valid
         expect(message.errors.count).to eq(1)
-        expect(message.errors[:memory_in_mb]).to include('must be greater than 0')
+        expect(message.errors[:memory_in_mb]).to include('must be less than or equal to 2147483647')
       end
     end
 
@@ -96,41 +109,53 @@ module VCAP::CloudController
         expect(message.errors.count).to eq(1)
         expect(message.errors[:memory_in_mb]).to include('must be an integer')
       end
+    end
 
-      context 'when disk_in_mb is not an number' do
-        let(:params) { { disk_in_mb: 'silly string thing' } }
+    context 'when disk_in_mb is not an number' do
+      let(:params) { { disk_in_mb: 'silly string thing' } }
 
-        it 'is not valid' do
-          message = ProcessScaleMessage.new(params)
+      it 'is not valid' do
+        message = ProcessScaleMessage.new(params)
 
-          expect(message).not_to be_valid
-          expect(message.errors.count).to eq(1)
-          expect(message.errors[:disk_in_mb]).to include('is not a number')
-        end
+        expect(message).not_to be_valid
+        expect(message.errors.count).to eq(1)
+        expect(message.errors[:disk_in_mb]).to include('is not a number')
       end
+    end
 
-      context 'when disk_in_mb is < 1' do
-        let(:params) { { disk_in_mb: 0 } }
+    context 'when disk_in_mb is < 1' do
+      let(:params) { { disk_in_mb: 0 } }
 
-        it 'is not valid' do
-          message = ProcessScaleMessage.new(params)
+      it 'is not valid' do
+        message = ProcessScaleMessage.new(params)
 
-          expect(message).not_to be_valid
-          expect(message.errors.count).to eq(1)
-          expect(message.errors[:disk_in_mb]).to include('must be greater than 0')
-        end
+        expect(message).not_to be_valid
+        expect(message.errors.count).to eq(1)
+        expect(message.errors[:disk_in_mb]).to include('must be greater than 0')
       end
+    end
 
-      context 'when disk_in_mb is not an integer' do
-        let(:params) { { disk_in_mb: 3.5 } }
+    context 'when disk_in_mb is > the max value allowed in the database' do
+      let(:params) { { disk_in_mb: BaseMessage::MAX_DB_INT + 1 } }
 
-        it 'is not valid' do
-          message = ProcessScaleMessage.new(params)
+      it 'is not valid' do
+        message = ProcessScaleMessage.new(params)
 
-          expect(message).not_to be_valid
-          expect(message.errors.count).to eq(1)
-          expect(message.errors[:disk_in_mb]).to include('must be an integer')
-        end
+        expect(message).not_to be_valid
+        expect(message.errors.count).to eq(1)
+        expect(message.errors[:disk_in_mb]).to include('must be less than or equal to 2147483647')
+      end
+    end
+
+    context 'when disk_in_mb is not an integer' do
+      let(:params) { { disk_in_mb: 3.5 } }
+
+      it 'is not valid' do
+        message = ProcessScaleMessage.new(params)
+
+        expect(message).not_to be_valid
+        expect(message.errors.count).to eq(1)
+        expect(message.errors[:disk_in_mb]).to include('must be an integer')
       end
     end
   end
