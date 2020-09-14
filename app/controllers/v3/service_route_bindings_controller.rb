c23cd9be48920a27f4dd7a44ef24e9cba8ba0aeb
Brian Butz <bbutz@pivotal.io>
2020-09-14 09:51:56 +0100

    v3(services): Include missing require

--- app/controllers/v3/service_route_bindings_controller.rb
+++ app/controllers/v3/service_route_bindings_controller.rb
@@ -5,6 +5,7 @@ require 'actions/service_route_binding_create'
 require 'jobs/v3/create_route_binding_job'
 require 'presenters/v3/service_route_binding_presenter'
 require 'fetchers/route_binding_list_fetcher'
+require 'decorators/include_binding_service_instance_decorator'
 require 'decorators/include_binding_route_decorator'
 
 class ServiceRouteBindingsController < ApplicationController
