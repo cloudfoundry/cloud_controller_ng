22c875fc4ce2a0725011dca7114743897ea88b42
Brian Butz <bbutz@pivotal.io>
2020-09-18 13:45:29 +0100

    Only load db_spec_helper if spec_helper hasn't loaded already

--- spec/db_spec_helper.rb
+++ spec/db_spec_helper.rb
@@ -1,30 +1,32 @@
-require 'lightweight_spec_helper'
+unless defined?(SPEC_HELPER_LOADED)
+  require 'lightweight_spec_helper'
 
-require 'rspec/collection_matchers'
+  require 'rspec/collection_matchers'
 
-require 'rails'
-require 'support/bootstrap/spec_bootstrap'
-require 'support/database_isolation'
-require 'sequel_plugins/sequel_plugins'
+  require 'rails'
+  require 'support/bootstrap/spec_bootstrap'
+  require 'support/database_isolation'
+  require 'sequel_plugins/sequel_plugins'
 
-require 'machinist/sequel'
-require 'machinist/object'
+  require 'machinist/sequel'
+  require 'machinist/object'
 
-VCAP::CloudController::SpecBootstrap.init(recreate_tables: false)
+  VCAP::CloudController::SpecBootstrap.init(recreate_tables: false)
 
-require 'support/fakes/blueprints'
+  require 'support/fakes/blueprints'
 
-RSpec.configure do |rspec_config|
-  rspec_config.before :suite do
-    VCAP::CloudController::SpecBootstrap.seed
-  end
+  RSpec.configure do |rspec_config|
+    rspec_config.before :suite do
+      VCAP::CloudController::SpecBootstrap.seed
+    end
 
-  rspec_config.around :each do |example|
-    # DatabaseIsolation requires the api config context
-    TestConfig.context = :api
-    TestConfig.reset
+    rspec_config.around :each do |example|
+      # DatabaseIsolation requires the api config context
+      TestConfig.context = :api
+      TestConfig.reset
 
-    isolation = DatabaseIsolation.choose(example.metadata[:isolation], TestConfig.config_instance, DbConfig.new.connection)
-    isolation.cleanly { example.run }
+      isolation = DatabaseIsolation.choose(example.metadata[:isolation], TestConfig.config_instance, DbConfig.new.connection)
+      isolation.cleanly { example.run }
+    end
   end
 end
