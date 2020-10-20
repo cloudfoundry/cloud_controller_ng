d05176f3ee9ae7bf3f0b747825eb9f2ddf060445
Felisia Martini <fmartini@pivotal.io>
2020-10-20 17:15:13 +0100

    Added check that we are not performing orphan mitigation on failed bind request

--- spec/support/shared_examples/request/bindings_orphan_mitigation.rb
+++ spec/support/shared_examples/request/bindings_orphan_mitigation.rb
@@ -1,7 +1,7 @@
 RSpec.shared_examples 'create binding orphan mitigation' do
   let(:job) { VCAP::CloudController::PollableJobModel.last }
-  let(:response_body) { '{}' }
-  let(:broker_status_code) { 200 }
+  let(:bind_response_body) { '{}' }
+  let(:broker_bind_status_code) { 200 }
 
   before do
     stub_request(:delete, bind_url).
@@ -13,7 +13,7 @@ RSpec.shared_examples 'create binding orphan mitigation' do
 
     stub_request(:put, bind_url).
       with(query: { accepts_incomplete: true },
-        body: client_body).to_return(status: broker_status_code, body: response_body)
+        body: client_body).to_return(status: broker_bind_status_code, body: bind_response_body)
   end
 
   context 'when it is not performed' do
@@ -25,7 +25,7 @@ RSpec.shared_examples 'create binding orphan mitigation' do
 
     [200, 201, 202].each do |code|
       context "response is #{code}" do
-        let(:broker_status_code) { code }
+        let(:broker_bind_status_code) { code }
 
         it 'does not perform orphan mitigation' do
           execute_all_jobs(expected_successes: 1, expected_failures: 0)
@@ -35,8 +35,8 @@ RSpec.shared_examples 'create binding orphan mitigation' do
     end
 
     context 'response is 200 with malformed response' do
-      let(:broker_status_code) { 200 }
-      let(:response_body) { nil }
+      let(:broker_bind_status_code) { 200 }
+      let(:bind_response_body) { nil }
 
       it 'does not perform orphan mitigation' do
         execute_all_jobs(expected_successes: 0, expected_failures: 1)
@@ -45,13 +45,27 @@ RSpec.shared_examples 'create binding orphan mitigation' do
         assert_no_orphan_mitigation_performed(plan_id, offering_id)
       end
     end
+
+    context 'response for last operation is 200 state failed' do
+      let(:broker_bind_status_code) { 202 }
+      before do
+        stub_request(:get, "#{bind_url}/last_operation").
+          with({ query: { plan_id: plan_id, service_id: offering_id } }).
+          to_return(status: 200, body: '{"state": "failed"}')
+      end
+
+      it 'does not perform orphan mitigation' do
+        execute_all_jobs(expected_successes: 0, expected_failures: 1)
+        assert_no_orphan_mitigation_performed(plan_id, offering_id)
+      end
+    end
   end
 
   context 'broker returns a 2xx code' do
     [201, 202].each do |code|
       context "response is #{code} with malformed response" do
-        let(:broker_status_code) { code }
-        let(:response_body) { nil }
+        let(:broker_bind_status_code) { code }
+        let(:bind_response_body) { nil }
 
         it 'performs orphan mitigation' do
           execute_all_jobs(expected_successes: 1, expected_failures: 1)
@@ -64,8 +78,8 @@ RSpec.shared_examples 'create binding orphan mitigation' do
 
     [203, 204, 205, 206, 206, 208, 226].each do |code|
       context "response is #{code}" do
-        let(:broker_status_code) { code }
-        let(:response_body) { '{}' }
+        let(:broker_bind_status_code) { code }
+        let(:bind_response_body) { '{}' }
 
         it 'performs orphan mitigation' do
           execute_all_jobs(expected_successes: 1, expected_failures: 1)
@@ -78,8 +92,8 @@ RSpec.shared_examples 'create binding orphan mitigation' do
   context 'broker returns a 4xx code' do
     [400, 401, 408, 409, *411..431].each do |code|
       context "response is #{code}" do
-        let(:broker_status_code) { code }
-        let(:response_body) { '{ "error": "ConcurrencyError", "description": "some description" }' }
+        let(:broker_bind_status_code) { code }
+        let(:bind_response_body) { '{ "error": "ConcurrencyError", "description": "some description" }' }
 
         it 'updates the binding and job' do
           execute_all_jobs(expected_successes: 0, expected_failures: 1)
@@ -104,7 +118,7 @@ RSpec.shared_examples 'create binding orphan mitigation' do
   context 'broker returns a 5xx code' do
     [*500..511].each do |code|
       context "response is #{code}" do
-        let(:broker_status_code) { code }
+        let(:broker_bind_status_code) { code }
 
         it 'does orphan mitigation and fails the job' do
           execute_all_jobs(expected_successes: 1, expected_failures: 1)
