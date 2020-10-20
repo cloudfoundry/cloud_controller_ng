a67575aad497c8294789b9c865c7f7a1e1d240f5
Felisia Martini <fmartini@pivotal.io>
2020-10-20 17:33:07 +0100

    Rearrange test order
    Rearrange to explicitly show the cases when orphan mittigation should and should not be performed

--- spec/support/shared_examples/request/bindings_orphan_mitigation.rb
+++ spec/support/shared_examples/request/bindings_orphan_mitigation.rb
@@ -16,20 +16,22 @@ RSpec.shared_examples 'create binding orphan mitigation' do
         body: client_body).to_return(status: broker_bind_status_code, body: bind_response_body)
   end
 
-  context 'when it is not performed' do
+  context 'should not be performed' do
     before do
       stub_request(:get, "#{bind_url}/last_operation").
         with({ query: { plan_id: plan_id, service_id: offering_id } }).
         to_return(status: 200, body: '{"state": "in progress"}')
     end
 
-    [200, 201, 202].each do |code|
-      context "response is #{code}" do
-        let(:broker_bind_status_code) { code }
+    context 'broker returns valid 200, 201, 202' do
+      [200, 201, 202].each do |code|
+        context "response is #{code}" do
+          let(:broker_bind_status_code) { code }
 
-        it 'does not perform orphan mitigation' do
-          execute_all_jobs(expected_successes: 1, expected_failures: 0)
-          assert_no_orphan_mitigation_performed(plan_id, offering_id)
+          it 'succeeds the job and does not perform orphan mitigation' do
+            execute_all_jobs(expected_successes: 1, expected_failures: 0)
+            assert_no_orphan_mitigation_performed(plan_id, offering_id)
+          end
         end
       end
     end
@@ -38,7 +40,7 @@ RSpec.shared_examples 'create binding orphan mitigation' do
       let(:broker_bind_status_code) { 200 }
       let(:bind_response_body) { nil }
 
-      it 'does not perform orphan mitigation' do
+      it 'fails the job and does not perform orphan mitigation' do
         execute_all_jobs(expected_successes: 0, expected_failures: 1)
 
         assert_failed_job(binding, job)
@@ -54,96 +56,90 @@ RSpec.shared_examples 'create binding orphan mitigation' do
           to_return(status: 200, body: '{"state": "failed"}')
       end
 
-      it 'does not perform orphan mitigation' do
+      it 'fails the job and does not perform orphan mitigation' do
         execute_all_jobs(expected_successes: 0, expected_failures: 1)
+
+        assert_failed_job(binding, job)
         assert_no_orphan_mitigation_performed(plan_id, offering_id)
       end
     end
-  end
 
-  context 'broker returns a 2xx code' do
-    [201, 202].each do |code|
-      context "response is #{code} with malformed response" do
-        let(:broker_bind_status_code) { code }
-        let(:bind_response_body) { nil }
+    context 'broker returns a 4xx code' do
+      [400, 401, 408, 409, *411..431].each do |code|
+        context "response is #{code}" do
+          let(:broker_bind_status_code) { code }
+          let(:bind_response_body) { '{ "error": "ConcurrencyError", "description": "some description" }' }
 
-        it 'performs orphan mitigation' do
-          execute_all_jobs(expected_successes: 1, expected_failures: 1)
+          it 'fails the job and updates the binding and job' do
+            execute_all_jobs(expected_successes: 0, expected_failures: 1)
 
-          assert_failed_job(binding, job)
-          assert_orphan_mitigation_performed(plan_id, offering_id)
-        end
-      end
-    end
-
-    [203, 204, 205, 206, 206, 208, 226].each do |code|
-      context "response is #{code}" do
-        let(:broker_bind_status_code) { code }
-        let(:bind_response_body) { '{}' }
-
-        it 'performs orphan mitigation' do
-          execute_all_jobs(expected_successes: 1, expected_failures: 1)
-          assert_orphan_mitigation_performed(plan_id, offering_id)
+            assert_failed_job(binding, job)
+            assert_no_orphan_mitigation_performed(plan_id, offering_id)
+          end
         end
       end
     end
   end
 
-  context 'broker returns a 4xx code' do
-    [400, 401, 408, 409, *411..431].each do |code|
-      context "response is #{code}" do
-        let(:broker_bind_status_code) { code }
-        let(:bind_response_body) { '{ "error": "ConcurrencyError", "description": "some description" }' }
+  context 'should be performed' do
+    context 'broker returns a 2xx code' do
+      [201, 202].each do |code|
+        context "response is #{code} with malformed response" do
+          let(:broker_bind_status_code) { code }
+          let(:bind_response_body) { nil }
 
-        it 'updates the binding and job' do
-          execute_all_jobs(expected_successes: 0, expected_failures: 1)
+          it 'performs orphan mitigation' do
+            execute_all_jobs(expected_successes: 1, expected_failures: 1)
 
-          assert_failed_job(binding, job)
+            assert_failed_job(binding, job)
+            assert_orphan_mitigation_performed(plan_id, offering_id)
+          end
+        end
+      end
 
-          expect(
-            a_request(:delete, bind_url).
-              with(
-                query: {
-                  accepts_incomplete: true,
-                  plan_id: plan_id,
-                  service_id: offering_id,
-                },
-              )
-          ).not_to have_been_made
+      [203, 204, 205, 206, 206, 208, 226].each do |code|
+        context "response is #{code}" do
+          let(:broker_bind_status_code) { code }
+          let(:bind_response_body) { '{}' }
+
+          it 'performs orphan mitigation' do
+            execute_all_jobs(expected_successes: 1, expected_failures: 1)
+            assert_orphan_mitigation_performed(plan_id, offering_id)
+          end
         end
       end
     end
-  end
 
-  context 'broker returns a 5xx code' do
-    [*500..511].each do |code|
-      context "response is #{code}" do
-        let(:broker_bind_status_code) { code }
+    context 'broker returns a 5xx code' do
+      [*500..511].each do |code|
+        context "response is #{code}" do
+          let(:broker_bind_status_code) { code }
 
-        it 'does orphan mitigation and fails the job' do
-          execute_all_jobs(expected_successes: 1, expected_failures: 1)
+          it 'does orphan mitigation and fails the job' do
+            execute_all_jobs(expected_successes: 1, expected_failures: 1)
 
-          assert_failed_job(binding, job)
-          assert_orphan_mitigation_performed(plan_id, offering_id)
+            assert_failed_job(binding, job)
+            assert_orphan_mitigation_performed(plan_id, offering_id)
+          end
         end
       end
     end
-  end
 
-  context 'timeout' do
-    before do
-      stub_request(:put, bind_url).
-        with(query: {
-          accepts_incomplete: true,
-        },
-          body: client_body).to_timeout
-    end
+    context 'timeout' do
+      before do
+        stub_request(:put, bind_url).
+          with(query: {
+            accepts_incomplete: true,
+          },
+            body: client_body).to_timeout
+      end
 
-    it 'does orphan mitigation and fails the job' do
-      execute_all_jobs(expected_successes: 1, expected_failures: 1)
+      it 'does orphan mitigation and fails the job' do
+        execute_all_jobs(expected_successes: 1, expected_failures: 1)
 
-      assert_failed_job(binding, job)
-      assert_orphan_mitigation_performed(plan_id, offering_id)
+        assert_failed_job(binding, job)
+        assert_orphan_mitigation_performed(plan_id, offering_id)
+      end
     end
   end
 end
@@ -160,7 +156,7 @@ def assert_orphan_mitigation_performed(plan_id, offering_id)
 end
 
 def assert_no_orphan_mitigation_performed(plan_id, offering_id)
-  expect(delete_request(plan_id, offering_id)).to_not have_been_made.once
+  expect(delete_request(plan_id, offering_id)).to_not have_been_made
 end
 
 def delete_request(plan_id, offering_id)
