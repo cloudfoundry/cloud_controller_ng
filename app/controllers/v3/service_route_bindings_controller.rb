9e84e0c1494e0926d209963e60290bdcdf37995a
Derik Evangelista <devangelista@pivotal.io>
2020-10-30 17:09:53 +0000

    v3(route-bindings): update metadata
    [#174252441](https://www.pivotaltracker.com/story/show/174252441)

--- app/controllers/v3/service_route_bindings_controller.rb
+++ app/controllers/v3/service_route_bindings_controller.rb
@@ -13,7 +13,7 @@ require 'decorators/include_binding_route_decorator'
 require 'cloud_controller/paging/sequel_paginator'
 
 class ServiceRouteBindingsController < ApplicationController
-  before_action :set_route_binding, only: [:show, :parameters, :destroy]
+  before_action :set_route_binding, except: [:create]
 
   def index
     message = list_message
@@ -91,6 +91,19 @@ class ServiceRouteBindingsController < ApplicationController
     bad_request!('this service does not support fetching route bindings parameters.')
   rescue LockCheck::ServiceBindingLockedError
     unprocessable!('There is an operation in progress for the service route binding.')
+  end
+
+  def update
+    route_binding_not_found! unless @route_binding.present? && can_read_space?(@route_binding.route.space)
+    unauthorized! unless can_write_space?(@route_binding.route.space)
+
+    unprocessable!('The service route binding is being deleted') if delete_in_progress?(@route_binding)
+
+    message = MetadataUpdateMessage.new(hashed_params[:body])
+    unprocessable!(message.errors.full_messages) unless message.valid?
+
+    updated_route_binding = TransactionalMetadataUpdate.update(@route_binding, message)
+    render status: :ok, json: Presenters::V3::ServiceRouteBindingPresenter.new(updated_route_binding)
   end
 
   private
@@ -226,5 +239,9 @@ class ServiceRouteBindingsController < ApplicationController
 
   def set_route_binding
     @route_binding = RouteBinding.first(guid: hashed_params[:guid])
+  end
+
+  def delete_in_progress?(binding)
+    binding.operation_in_progress? && binding.last_operation.type == 'delete'
   end
 end
