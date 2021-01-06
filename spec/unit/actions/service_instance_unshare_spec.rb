bdc12eadccc369cabf70d847b3033e618c71e6d8
George Blue <gblue@pivotal.io>
2021-01-06 16:46:47 +0000

    v3(services): use v3 code path for unshare service
    - when a service instance is unshared, associated bindings are deleted
    - this now uses the v3 service instance unbind code
    
    [#173680675](https://www.pivotaltracker.com/story/show/173680675)

--- spec/unit/actions/service_instance_unshare_spec.rb
+++ spec/unit/actions/service_instance_unshare_spec.rb
@@ -40,15 +40,15 @@ module VCAP::CloudController
 
       context 'when bindings exist in the target space' do
         let(:app) { AppModel.make(space: target_space, name: 'myapp') }
-        let(:delete_binding_action) { instance_double(ServiceBindingDelete) }
+        let(:delete_binding_action) { instance_double(V3::ServiceCredentialBindingDelete) }
         let(:service_binding) { ServiceBinding.make(app: app, service_instance: service_instance) }
 
         before do
-          allow(ServiceBindingDelete).to receive(:new).with(user_audit_info, accepts_incomplete) { delete_binding_action }
+          allow(V3::ServiceCredentialBindingDelete).to receive(:new).with(:credential, user_audit_info) { delete_binding_action }
         end
 
         it 'deletes bindings and unshares' do
-          allow(delete_binding_action).to receive(:delete).with([service_binding]).and_return([[], []])
+          allow(delete_binding_action).to receive(:delete).with(service_binding).and_return({ finished: true })
 
           service_instance_unshare.unshare(service_instance, target_space, user_audit_info)
           expect(service_instance.shared_spaces).to be_empty
@@ -57,7 +57,7 @@ module VCAP::CloudController
         context 'when an unbind fails' do
           it 'does not unshare' do
             err = StandardError.new('oops')
-            allow(delete_binding_action).to receive(:delete).with([service_binding]).and_return([[err], []])
+            allow(delete_binding_action).to receive(:delete).with(service_binding).and_raise(err)
 
             expect { service_instance_unshare.unshare(service_instance, target_space, user_audit_info) }.to raise_error(VCAP::CloudController::ServiceInstanceUnshare::Error)
             expect(service_instance.shared_spaces).to_not be_empty
@@ -65,34 +65,39 @@ module VCAP::CloudController
         end
 
         context 'when bindings have delete operations in progress' do
-          let(:app_1) { AppModel.make(space: target_space, name: 'myapp1') }
-          let(:app_2) { AppModel.make(space: target_space, name: 'myapp2') }
-          let(:app_3) { AppModel.make(space: target_space, name: 'myapp3') }
-          let(:service_binding_1) { ServiceBinding.make(app: app_1, service_instance: service_instance) }
-          let(:service_binding_2) { ServiceBinding.make(app: app_2, service_instance: service_instance) }
-          let(:service_binding_3) { ServiceBinding.make(app: app_3, service_instance: service_instance) }
+          let!(:app_1) { AppModel.make(space: target_space, name: 'myapp1') }
+          let!(:app_2) { AppModel.make(space: target_space, name: 'myapp2') }
+          let!(:app_3) { AppModel.make(space: target_space, name: 'myapp3') }
+          let!(:service_binding_1) { ServiceBinding.make(app: app_1, service_instance: service_instance) }
+          let!(:service_binding_2) { ServiceBinding.make(app: app_2, service_instance: service_instance) }
+          let!(:service_binding_3) { ServiceBinding.make(app: app_3, service_instance: service_instance) }
 
           before do
-            service_binding_1.service_binding_operation = ServiceBindingOperation.make(state: 'in progress', type: 'delete')
-            service_binding_3.service_binding_operation = ServiceBindingOperation.make(state: 'in progress', type: 'delete')
-
-            allow(ServiceBindingDelete).to receive(:new) { delete_binding_action }
-            allow(delete_binding_action).to receive(:delete).and_return([[], []])
+            allow(delete_binding_action).to receive(:delete).and_return({ finished: false })
           end
 
-          it 'unshares without deleting the binding' do
+          it 'fails to unshare' do
             expect { service_instance_unshare.unshare(service_instance, target_space, user_audit_info) }.
               to raise_error(VCAP::CloudController::ServiceInstanceUnshare::Error) do |err|
               expect(err.message).to include("\n\tThe binding between an application and service instance #{service_instance.name}" \
                                              " in space #{target_space.name} is being deleted asynchronously.")
             end
+
+            expect(service_instance.shared_spaces).to include(target_space)
+          end
+
+          it 'enqueues jobs to poll the delete operations' do
+            expect { service_instance_unshare.unshare(service_instance, target_space, user_audit_info) }.
+              to raise_error(VCAP::CloudController::ServiceInstanceUnshare::Error)
+
+            expect(VCAP::CloudController::PollableJobModel.count).to eq(3)
           end
         end
 
         context 'when a binding has operation in progress and the delete action fails' do
           before do
             err = StandardError.new('some-error')
-            allow(delete_binding_action).to receive(:delete).with([service_binding]).and_return([err])
+            allow(delete_binding_action).to receive(:delete).with(service_binding).and_raise(err)
 
             service_binding.service_binding_operation = ServiceBindingOperation.make(state: 'in progress', type: 'delete')
           end
