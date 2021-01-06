25a570e6e858c1960c30d0b9082607de1fd12c09
Felisia Martini <fmartini@pivotal.io>
2021-01-06 12:12:15 +0000

    Correct ServiceBindingLabel model relationship

--- app/models/services/service_binding_label_model.rb
+++ app/models/services/service_binding_label_model.rb
@@ -1,6 +1,6 @@
 module VCAP::CloudController
   class ServiceBindingLabelModel < Sequel::Model(:service_binding_labels)
-    many_to_one :route_binding,
+    many_to_one :service_binding,
       primary_key: :guid,
       key: :resource_guid,
       without_guid_generation: true
