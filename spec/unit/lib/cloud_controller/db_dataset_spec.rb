a88236a7c64de7448f60dd3d2563ec0abf602c85
andy-paine <andy.paine@engineerbetter.com>
2021-06-15 09:15:09 +0100

    Add tests for threshold logging
    Ensure that extra logs will only be emitted when user opts in by
    providing threshold

--- spec/unit/lib/cloud_controller/db_dataset_spec.rb
+++ spec/unit/lib/cloud_controller/db_dataset_spec.rb
@@ -5,18 +5,52 @@ module VCAP::CloudController
     describe '#post_load', type: :model do
       let(:logs) { StringIO.new }
       let(:logger) { Logger.new(logs) }
-      let(:db) { Route.db }
+      let(:db) { Space.db }
 
       before do
-        db.opts.merge!({ log_db_queries: true, query_size_log_threshold: 1 })
         db.loggers << logger
       end
 
-      it 'log the number of returned rows' do
-        space = Space.make
-        Route.make(space: space)
-        Route.dataset.eager(:space).all
-        expect(logs.string).to match 'Loaded 1 records for query SELECT \* FROM "routes"'
+      describe 'with logging enabled' do
+        before do
+          db.opts.merge!({ log_db_queries: true, query_size_log_threshold: 5 })
+        end
+
+        it 'logs the number of returned rows over threshold' do
+          10.times { Space.make }
+          Space.all
+          expect(logs.string).to match 'Loaded 10 records for query SELECT \* FROM "spaces"'
+        end
+
+        it 'does not log the number of returned rows under threshold' do
+          2.times { Space.make }
+          Space.all
+          expect(logs.string).to_not match 'Loaded'
+        end
+      end
+
+      describe 'with no threshold set' do
+        before do
+          db.opts.merge!({ log_db_queries: true, query_size_log_threshold: nil })
+        end
+
+        it 'does not log' do
+          2.times { Space.make }
+          Space.all
+          expect(logs.string).to_not match 'Loaded'
+        end
+      end
+
+      describe 'with db query logging not enabled' do
+        before do
+          db.opts.merge!({ log_db_queries: false, query_size_log_threshold: 1 })
+        end
+
+        it 'does not log the number of returned rows under threshold' do
+          2.times { Space.make }
+          Space.all
+          expect(logs.string).to_not match 'Loaded'
+        end
       end
     end
   end
