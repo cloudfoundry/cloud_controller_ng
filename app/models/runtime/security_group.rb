038f813bc5fb4d017ac6535d326286bb1dd0d70d
Maria Shaldybin <mshaldibina@pivotal.io>
2021-07-14 16:52:46 -0700

    Space application supporter can get and list security groups (#2341)
    * Space application supporter can get and list security groups
    
    Allowed access:
    * GET /v3/security_groups/:guid - Can see globally–enabled security
    groups or groups associated with a space they can see
    * GET /v3/security_groups - Can see globally–enabled security groups or
    groups associated with a space they can see
    * GET /v3/spaces/:guid/running_security_groups
    * GET /v3/spaces/:guid/staging_security_groups
    
    Closes [#2230](https://github.com/cloudfoundry/cloud_controller_ng/issues/2230)
    
    Co-authored-by: Matthew Kocher <mkocher@vmware.com>
    Co-authored-by: Maria Shaldybin <mariash@vmware.com>

--- app/models/runtime/security_group.rb
+++ app/models/runtime/security_group.rb
@@ -37,10 +37,12 @@ module VCAP::CloudController
 
       Sequel.or([
         [:spaces, user.spaces_dataset],
+        [:spaces, user.application_supported_spaces_dataset],
         [:spaces, user.managed_spaces_dataset],
         [:spaces, user.audited_spaces_dataset],
         [:spaces, managed_organizations_spaces_dataset],
         [:staging_spaces, user.spaces_dataset],
+        [:staging_spaces, user.application_supported_spaces_dataset],
         [:staging_spaces, user.managed_spaces_dataset],
         [:staging_spaces, user.audited_spaces_dataset],
         [:staging_spaces, managed_organizations_spaces_dataset],
