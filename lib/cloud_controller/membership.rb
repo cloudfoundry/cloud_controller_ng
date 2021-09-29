aafd800c9973aec1854151bc7ecb651351677c6d
Philipp Thun <philipp.thun@sap.com>
2021-09-29 16:12:20 +0200

    Reduce complexity of the [space|org]_guids_for_roles_subquery
    - Check if the list of given roles contains a space/org role and omit
      the corresponding subquery if not.
    - Combine the subqueries in a "SELECT x WHERE y OR z" query that can
      deal with y or z being 'nil'.

--- lib/cloud_controller/membership.rb
+++ lib/cloud_controller/membership.rb
@@ -35,14 +35,18 @@ module VCAP::CloudController
     end
 
     def org_guids_for_roles_subquery(roles)
-      org_role_dataset = OrganizationRole.where(type: org_roles(roles), user_id: @user.id)
-      org_guid_dataset = Organization.where(id: org_role_dataset.select(:organization_id)).select(:guid)
+      ids_for_org_roles_dataset, ids_for_space_roles_dataset = nil
 
-      space_role_dataset = SpaceRole.where(type: space_roles(roles), user_id: @user.id)
-      spaces_dataset = Space.where(id: space_role_dataset.select(:space_id))
-      org_guids_for_space_roles_dataset = Organization.where(id: spaces_dataset.select(:organization_id)).select(:guid)
+      if org_role?(roles)
+        ids_for_org_roles_dataset = OrganizationRole.where(type: org_roles(roles), user_id: @user.id).select(:organization_id)
+      end
 
-      org_guids_for_space_roles_dataset.union(org_guid_dataset)
+      if space_role?(roles)
+        space_roles_dataset = SpaceRole.where(type: space_roles(roles), user_id: @user.id).select(:space_id)
+        ids_for_space_roles_dataset = Space.where(id: space_roles_dataset).select(:organization_id)
+      end
+
+      Organization.where(id: ids_for_org_roles_dataset).or(id: ids_for_space_roles_dataset).select(:guid)
     end
 
     def space_guids_for_roles(roles)
@@ -50,13 +54,17 @@ module VCAP::CloudController
     end
 
     def space_guids_for_roles_subquery(roles)
-      space_role_dataset = SpaceRole.where(type: space_roles(roles), user_id: @user.id)
-      space_guid_dataset = Space.where(id: space_role_dataset.select(:space_id)).select(:guid)
+      ids_for_space_roles_dataset, org_ids_for_org_roles_dataset = nil
 
-      org_role_dataset = OrganizationRole.where(type: org_roles(roles), user_id: @user.id)
-      space_guids_for_org_roles_dataset = Space.where(organization_id: org_role_dataset.select(:organization_id)).select(:guid)
+      if space_role?(roles)
+        ids_for_space_roles_dataset = SpaceRole.where(type: space_roles(roles), user_id: @user.id).select(:space_id)
+      end
 
-      space_guids_for_org_roles_dataset.union(space_guid_dataset)
+      if org_role?(roles)
+        org_ids_for_org_roles_dataset = OrganizationRole.where(type: org_roles(roles), user_id: @user.id).select(:organization_id)
+      end
+
+      Space.where(id: ids_for_space_roles_dataset).or(organization_id: org_ids_for_org_roles_dataset).select(:guid)
     end
 
     private
