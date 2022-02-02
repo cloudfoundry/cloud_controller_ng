197a975ca4671d5a7d38d8c8e65a3c7a4f7e4a1f
Merric de Launey <mdelauney@pivotal.io>
2022-02-02 23:10:12 +0000

    Revert "Merge pull request #2478 from cloudfoundry/dependabot/bundler/fog-aws-3.12.0"
    This reverts commit 5ee5e06382675a852e4511f269f4f60f4a3ed0f1, reversing
    changes made to 719f170d4602ceaf96590205943e92e67ad59b73.
    
    azure tests are currently failing, reverting this commit to unblock
    releasing

--- lib/cloud_controller/blobstore/fog/providers.rb
+++ lib/cloud_controller/blobstore/fog/providers.rb
@@ -1,19 +1,6 @@
-require 'fog/core'
-
-# This can be removed once fog-google fixes
-# https://github.com/fog/fog-google/issues/421, which will allow us to upgrade
-# fog-core to 2.2.4.
-original = Fog::Logger[:deprecation]
-Fog::Logger[:deprecation] = nil
-
 require 'fog/aliyun'
-# Hack until https://github.com/fog/fog-aliyun/pull/155 is merged and released
-Fog::Aliyun::Compute = Fog::Compute::Aliyun
-
 require 'fog/aws'
 require 'fog/local'
 require 'fog/google'
 require 'fog/azurerm'
 require 'fog/openstack'
-
-Fog::Logger[:deprecation] = original
