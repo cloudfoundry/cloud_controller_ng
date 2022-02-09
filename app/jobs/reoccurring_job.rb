c41e3c31339872613eab182658b29c457792880d
Sven Krieger <37476281+svkrieger@users.noreply.github.com>
2022-02-09 13:14:25 +0100

    Implement exponential backoff for SB operations
    Added a config parameter to the cloud_controller which let's users configure whether an exponential backoff, and with which rate, shall be used for service fetch operations.
    Defaults to 1.0, which means there is no exponential backoff.
    
    Co-authored-by: Aftab Alam <81828613+iaftab-alam@users.noreply.github.com>

--- app/jobs/reoccurring_job.rb
+++ app/jobs/reoccurring_job.rb
@@ -3,7 +3,7 @@ require 'jobs/cc_job'
 module VCAP::CloudController
   module Jobs
     class ReoccurringJob < VCAP::CloudController::Jobs::CCJob
-      attr_reader :finished, :start_time
+      attr_reader :finished, :start_time, :retry_number
 
       def success(current_delayed_job)
         pollable_job = PollableJobModel.find_by_delayed_job(current_delayed_job)
@@ -49,6 +49,7 @@ module VCAP::CloudController
       def initialize
         @start_time = Time.now
         @finished = false
+        @retry_number = 0
       end
 
       def default_maximum_duration_seconds
@@ -59,8 +60,16 @@ module VCAP::CloudController
         Config.config.get(:broker_client_default_async_poll_interval_seconds)
       end
 
+      def default_polling_exponential_backoff
+        Config.config.get(:broker_client_async_poll_exponential_backoff_rate)
+      end
+
+      def next_execution_in
+        polling_interval_seconds * default_polling_exponential_backoff**retry_number
+      end
+
       def next_enqueue_would_exceed_maximum_duration?
-        Time.now + polling_interval_seconds > start_time + maximum_duration_seconds
+        Time.now + next_execution_in > start_time + maximum_duration_seconds
       end
 
       def finish
@@ -75,9 +84,10 @@ module VCAP::CloudController
       def enqueue_next_job(pollable_job)
         opts = {
           queue: Jobs::Queues.generic,
-          run_at: Delayed::Job.db_time_now + polling_interval_seconds
+          run_at: Delayed::Job.db_time_now + next_execution_in
         }
 
+        @retry_number += 1
         Jobs::Enqueuer.new(self, opts).enqueue_pollable(existing_guid: pollable_job.guid)
       end
     end
