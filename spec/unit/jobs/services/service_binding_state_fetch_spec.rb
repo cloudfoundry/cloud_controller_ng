66f8016537cac5833d75725ffcb46ed6d36dba6b
Sven Krieger <37476281+svkrieger@users.noreply.github.com>
2022-02-14 17:00:20 +0100

    Add unit tests for exponential backoff(v2 api)
    Co-authored-by: Aftab Alam <81828613+iaftab-alam@users.noreply.github.com>

--- spec/unit/jobs/services/service_binding_state_fetch_spec.rb
+++ spec/unit/jobs/services/service_binding_state_fetch_spec.rb
@@ -419,6 +419,66 @@ module VCAP::CloudController
               end
             end
 
+            context 'when the last_operation state is in progress and exponential backoff is not set to default' do
+              let(:polling_interval) { 60 }
+
+              it 'calculates the polling intervals based on the default interval and the exponential backoff rate' do
+                TestConfig.config[:broker_client_async_poll_exponential_backoff_rate] = 2.0
+                enqueued_time = 0
+
+                Timecop.freeze do
+                  run_job(job)
+                  enqueued_time = Time.now
+                end
+
+                [60, 180, 420, 900, 1860].each do |seconds|
+                  Timecop.freeze((seconds - 1).seconds.after(enqueued_time)) do
+                    execute_all_jobs(expected_successes: 0, expected_failures: 0)
+                  end
+
+                  Timecop.freeze((seconds + 1).seconds.after(enqueued_time)) do
+                    execute_all_jobs(expected_successes: 1, expected_failures: 0)
+                  end
+                end
+              end
+
+              it 'calculates the polling intervals based on the configured interval and the exponential backoff rate' do
+                TestConfig.config[:broker_client_async_poll_exponential_backoff_rate] = 2.0
+                TestConfig.config[:broker_client_default_async_poll_interval_seconds] = 10
+
+                enqueued_time = 0
+
+                Timecop.freeze do
+                  run_job(job)
+                  enqueued_time = Time.now
+                end
+
+                [10, 30, 70, 150, 310].each do |seconds|
+                  Timecop.freeze((seconds - 1).seconds.after(enqueued_time)) do
+                    execute_all_jobs(expected_successes: 0, expected_failures: 0)
+                  end
+
+                  Timecop.freeze((seconds + 1).seconds.after(enqueued_time)) do
+                    execute_all_jobs(expected_successes: 1, expected_failures: 0)
+                  end
+                end
+              end
+
+              it 'takes the exponential backoff into account when checking whether the next run would exceed the maximum duration' do
+                TestConfig.config[:broker_client_async_poll_exponential_backoff_rate] = 1.3
+                TestConfig.config[:broker_client_max_async_poll_duration_minutes] = 60
+
+                job.retry_number = 10
+                Timecop.freeze(Time.now + 3384.321.ceil.seconds) do
+                  run_job(job)
+                  service_binding.reload
+
+                  expect(service_binding.last_operation.state).to eq('failed')
+                  expect(service_binding.last_operation.description).to eq('Service Broker failed to create binding within the required time.')
+                end
+              end
+            end
+
             context 'when the last_operation state is failed' do
               let(:state) { 'failed' }
               let(:description) { 'something went wrong' }
