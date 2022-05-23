ce6e84d0dd3d28f8eecf9d458d2ef365a0d663fb
MerricdeLauney <mdelauney@pivotal.io>
2022-05-23 14:13:01 -0700

    Update route_unshare_spec.rb

--- spec/unit/actions/route_unshare_spec.rb
+++ spec/unit/actions/route_unshare_spec.rb
@@ -10,7 +10,7 @@ module VCAP::CloudController
     let(:target_space2) { Space.make }
     let(:user_audit_info) { UserAuditInfo.new(user_guid: 'user-guid-1', user_email: 'user@email.com') }
 
-    describe '#delete' do
+    describe '#unshare' do
       let(:target_space3) { Space.make }
       before do
         allow_any_instance_of(Repositories::RouteEventRepository).to receive(:record_route_unshare)
@@ -27,7 +27,7 @@ module VCAP::CloudController
         expect(target_space2.routes_shared_from_other_spaces[0]).to eq route
         expect(target_space3.routes_shared_from_other_spaces[0]).to eq route
 
-        modified_route = route_unshare.delete(route, target_space2, user_audit_info)
+        modified_route = route_unshare.unshare(route, target_space2, user_audit_info)
         # The target_space variable will not refresh its routes anymore after we added a call to grab the shared routes with the space to the
         # Validation of shared routes. If we just reload the objects we can get the new version of thh spaces shared routes.
         target_space1.reload
@@ -48,7 +48,7 @@ module VCAP::CloudController
         expect_any_instance_of(Repositories::RouteEventRepository).to receive(:record_route_unshare).with(
           route, user_audit_info, target_space2.guid)
 
-        route_unshare.delete(route, target_space2, user_audit_info)
+        route_unshare.unshare(route, target_space2, user_audit_info)
       end
 
       context 'when unsharing a space fails' do
@@ -60,7 +60,7 @@ module VCAP::CloudController
           expect(route.shared_spaces.length).to eq 3
 
           expect {
-            route_unshare.delete(route, target_space1, user_audit_info)
+            route_unshare.unshare(route, target_space1, user_audit_info)
           }.to raise_error('db failure')
 
           route.reload
@@ -71,7 +71,7 @@ module VCAP::CloudController
       context 'when attempting to unshare the owning space' do
         it 'does not permit you to unshare that space' do
           expect {
-            route_unshare.delete(route, route.space, user_audit_info)
+            route_unshare.unshare(route, route.space, user_audit_info)
           }.to raise_error(VCAP::CloudController::RouteUnshare::Error,
                            "Unable to unshare route '#{route.uri}' from space '#{route.space.guid}'. Routes cannot be removed from the space that owns them.")
 
