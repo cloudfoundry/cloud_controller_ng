54444da60be0a2d74f78a6293cf92948feb9e39a
M. Oleske <moleske@pivotal.io>
2022-06-22 11:13:58 -0700

    Update missed tests from table rename

--- spec/migrations/20220601183937_create_asg_latest_updates_spec.rb
+++ spec/migrations/20220601183937_create_asg_latest_updates_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe 'create singleton timestamp of last update to asgs', isolation: :
   let(:tmp_migrations_dir) { Dir.mktmpdir }
 
   it 'creates the asg update table' do
-    expect(db.table_exists?(:asg_latest_updates)).to be_truthy
+    expect(db.table_exists?(:asg_timestamps)).to be_truthy
   end
 
   it 'only allows one updated timestamp row' do
-    db[:asg_latest_updates].insert({ last_update: DateTime.now })
+    db[:asg_timestamps].insert({ last_update: DateTime.now })
     expect {
-      db[:asg_latest_updates].insert({ last_update: DateTime.now })
+      db[:asg_timestamps].insert({ last_update: DateTime.now })
     }.to raise_error(Exception)
   end
 end
