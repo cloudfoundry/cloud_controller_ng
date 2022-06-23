7cb7e88b54dc046de79851345c839cfbfe780c46
M. Oleske <moleske@pivotal.io>
2022-06-23 16:02:51 -0700

    Use Sequel instead of custom raw SQL
    - this allows bundle exec rake to work with mysql
    - it loses database constraints, which are now only enforced by the renew function in AsgLatestUpdate
    - would be nice to keep the database constraint, but we couldn't figure out why the sequel migration didn't like the CREATE UNIQUE INDEX line for mysql (it was fine with postgres)
    
    Co-authored-by: Michael Oleske <moleske@pivotal.io>
    Co-authored-by: David Alvarado <alvaradoda@vmware.com>

--- db/migrations/20220601183937_create_asg_lastest_updates.rb
+++ db/migrations/20220601183937_create_asg_lastest_updates.rb
@@ -1,27 +1,8 @@
 Sequel.migration do
-  up do
-    if database_type == :postgres
-      run <<-SQL
-        CREATE TABLE asg_timestamps (
-          id INT PRIMARY KEY DEFAULT 1,
-          last_update TIMESTAMP
-        );
-        CREATE UNIQUE INDEX asg_change_singleton ON asg_timestamps ((true));
-      SQL
-    else
-      run <<-SQL
-          CREATE TABLE asg_timestamps (
-            id INT PRIMARY KEY DEFAULT 1,
-            last_update TIMESTAMP,
-            unique_value ENUM('lockme') NOT NULL DEFAULT 'lockme'
-          );
-
-          CREATE UNIQUE INDEX asg_change_singleton ON asg_timestamps (unique_value);
-      SQL
+  change do
+    create_table :asg_timestamps do
+      primary_key name: :id
+      Timestamp :last_update
     end
-  end
-
-  down do
-    drop_table(:asg_timestamps)
   end
 end
