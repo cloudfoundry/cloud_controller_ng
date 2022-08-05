6dbd10a1da5fb2e5d071c7e8e099c88a06a39db5
Merric de Launey <mdelauney@pivotal.io>
2022-08-05 20:56:32 +0000

    Wrap space guid within data field in PATCH v3/routes/:guid/relationships/space
    This change ensures the endpoint adheres to the v3 api style guide
    https://github.com/cloudfoundry/cloud_controller_ng/blob/main/docs/v3_style_guide.md
    
    [#182668597]
    
    Authored-by: Merric de Launey <mdelauney@pivotal.io>

--- app/messages/route_transfer_owner_message.rb
+++ app/messages/route_transfer_owner_message.rb
@@ -2,9 +2,28 @@ require 'messages/base_message'
 
 module VCAP::CloudController
   class RouteTransferOwnerMessage < BaseMessage
-    register_allowed_keys [:guid]
+    register_allowed_keys [:data]
 
     validates_with NoAdditionalKeysValidator
-    validates :guid, presence: true, string: true, allow_nil: false, allow_blank: false
+    validates :data, presence: true, hash: true, allow_nil: false
+    validate :data_content
+
+    def space_guid
+      HashUtils.dig(data, :guid)
+    end
+
+    def data_content
+      return if data.nil?
+
+      errors.add(:data, 'can only accept one key') unless data.keys.length == 1
+      errors.add(:data, "can only accept key 'guid'") unless data.key?(:guid)
+      if space_guid && !space_guid.is_a?(String)
+        errors.add(:data, "#{space_guid} must be a string")
+        return
+      end
+      if space_guid && space_guid.empty?
+        errors.add(:data, "guid can't be blank")
+      end
+    end
   end
 end
