ae97601d21bdb752f9203adf4a521722878ce982
Joseph Palermo <jpalermo@pivotal.io>
2022-10-05 15:41:41 -0700

    Increase the webdav receive_timeout to 120 seconds (#3010)
    We were hitting the 60 second default timeout when uploading a large app. This timeout is used after the PUT to the
    blobstore when the httpclient is trying to read the headers for the response. Since the blobstore is busy
    processing the large upload, it was taking longer than 60 seconds to start the response.
    
    Currently not configurable but should be easy to thread it through the config if that becomes needed later.
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>
    Co-authored-by: Merric de Launey <mdelauney@pivotal.io>

--- lib/cloud_controller/blobstore/webdav/http_client_provider.rb
+++ lib/cloud_controller/blobstore/webdav/http_client_provider.rb
@@ -1,9 +1,10 @@
 module CloudController
   module Blobstore
     class HTTPClientProvider
-      def self.provide(ca_cert_path: nil, connect_timeout: nil)
+      def self.provide(ca_cert_path: nil, connect_timeout: nil, receive_timeout: nil)
         client = HTTPClient.new
         client.connect_timeout = connect_timeout if connect_timeout
+        client.receive_timeout = receive_timeout if receive_timeout
         client.ssl_config.verify_mode = VCAP::CloudController::Config.config.get(:skip_cert_verify) ? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER
         client.ssl_config.set_default_paths
 
