f5a14f94bf0c4418fad5fd52cd97f54742b68d78
Will Gant <william.gant@sap.com>
2022-10-27 17:48:48 +0100

    Cut unneeded query

--- app/controllers/v3/service_instances_controller.rb
+++ app/controllers/v3/service_instances_controller.rb
@@ -198,7 +198,7 @@ class ServiceInstancesV3Controller < ApplicationController
   def credentials
     service_instance = UserProvidedServiceInstance.first(guid: hashed_params[:guid])
     service_instance_not_found! unless service_instance && can_read_service_instance?(service_instance)
-    unauthorized! unless permission_queryer.can_read_secrets_in_space?(service_instance.space.id, service_instance.space.organization_id)
+    unauthorized! unless permission_queryer.can_read_secrets_in_space?(service_instance.space_id, service_instance.space.organization_id)
 
     render status: :ok, json: (service_instance.credentials || {})
   end
