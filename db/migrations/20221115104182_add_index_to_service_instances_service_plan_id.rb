a669eafa0aae9bf0006948844af0e2059df3e2e0
Will Gant <william.gant@sap.com>
2022-11-23 14:30:15 +0000

    Use shorter index name

--- db/migrations/20221115104182_add_index_to_service_instances_service_plan_id.rb
+++ db/migrations/20221115104182_add_index_to_service_instances_service_plan_id.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   change do
     alter_table :service_instances do
-      add_index :service_plan_id, name: :service_instances_service_plan_id_index
+      add_index :service_plan_id, name: :si_service_plan_id_index
     end
   end
 end
