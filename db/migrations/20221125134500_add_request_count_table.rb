abb7dc9e50b78fe67e4de0d1cd338149000ed875
johha <johannes.haass@sap.com>
2022-11-25 14:22:34 +0100

    Re-add db table `request_counts`
    While updating capi from version 1.139.0 to 1.140.0 it can happen that
    not yet updated cc-workers will fail during a restart with error `relation "request_counts" does not exist`.
    To avoid this we need to create the table again.
    In a future release it can be removed again.
    
    See https://github.com/cloudfoundry/cloud_controller_ng/issues/3075
    
    Co-authored-by: Florian Braun <5863788+FloThinksPi@users.noreply.github.com>

---
+++ db/migrations/20221125134500_add_request_count_table.rb
@@ -0,0 +1,13 @@
+Sequel.migration do
+  change do
+    create_table? :request_counts do
+      primary_key :id
+
+      String :user_guid
+      index :user_guid
+
+      Integer :count, default: 0
+      Time :valid_until
+    end
+  end
+end
