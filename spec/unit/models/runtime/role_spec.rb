2258522dc36c195feb5e526ce99abecc0d48e981
Philipp Thun <philipp.thun@sap.com>
2023-01-25 01:37:55 +0100

    Optimize Role model/dataset (#3068)
    The Role model is not backed by a dedicated table, but a dataset that is
    a UNION of eight individual roles tables. When filtering for roles, this
    was done by adding WHERE conditions to the overall dataset, which
    resulted in slow query executions. A more efficient approach is to move
    the WHERE conditions into the individual datasets per role.
    
    This commit customizes the Role model/dataset. The creation of the
    UNIONed dataset is moved into a dedicated class (RoleDataset) that
    supports building a dataset with filters (i.e. WHERE conditions) per
    role (i.e. individual table). The 'where' method of the Role dataset is
    overridden and uses Dataset.from ([1]) to change the dataset's source.
    It replaces the source with a new dataset built with filters per role.
    
    As 'where' calls can be chained, the filters need to be kept in the
    dataset instance (cache) and the source dataset needs to be built again
    on each 'where' invocation with all the filters, i.e. previous and new
    WHERE conditions.
    
    Furthermore fully qualified identifiers (table name and column name)
    have to be adapted; as the WHERE conditions are applied on the
    individual roles tables, no table name prefix is required and thus
    removed from the identifiers.
    
    Some filters can now be handled in a more efficient way. When filtering
    for a specific role type, the corresponding dataset gets a
    'WHERE 1 = 1', whereas all the other datasets get a 'WHERE 1 = 0',
    meaning they never return any rows. Something similar is done when
    filtering for organizations or spaces; the opposite role types get a
    'WHERE 1 = 0' as they don't have the requested 'id' field.
    
    As there might be filters that still should be applied to the overall
    dataset, 'where' invocations with a block (aka. virtual row block) are
    treated as before, i.e. applied at the end.
    
    Drawbacks:
    - The current implementation of the custom Role model/dataset is rather
      strict with regards to allowed filter expressions. This means that if
      some functionality is added (e.g. new or different filter), the custom
      Role model/dataset needs to be adapted. This was done by intention to
      ensure that every code path is tested.
    - To limit the number of different filter expressions, invocations of
      'first(cond)' and 'find(cond)' on the custom Role model/dataset have
      been replaced with 'where(cond).first' which is semantically
      identical.
    
    [1] https://sequel.jeremyevans.net/rdoc/classes/Sequel/Dataset.html#method-i-from

--- spec/unit/models/runtime/role_spec.rb
+++ spec/unit/models/runtime/role_spec.rb
@@ -5,12 +5,15 @@ module VCAP::CloudController
     it { is_expected.to have_timestamp_columns }
 
     context 'when there are roles' do
-      let!(:organization_user) { OrganizationUser.make }
-      let!(:organization_manager) { OrganizationManager.make }
+      let(:user) { User.make }
+      let(:org) { Organization.make }
+      let(:space) { Space.make }
+      let!(:organization_user) { OrganizationUser.make(user: user, organization: org) }
+      let!(:organization_manager) { OrganizationManager.make(organization: org) }
       let!(:organization_billing_manager) { OrganizationBillingManager.make }
       let!(:organization_auditor) { OrganizationAuditor.make }
-      let!(:space_developer) { SpaceDeveloper.make }
-      let!(:space_auditor) { SpaceAuditor.make }
+      let!(:space_developer) { SpaceDeveloper.make(user: user, space: space) }
+      let!(:space_auditor) { SpaceAuditor.make(space: space) }
       let!(:space_manager) { SpaceManager.make }
       let!(:space_supporter) { SpaceSupporter.make }
 
@@ -27,6 +30,35 @@ module VCAP::CloudController
         expect(roles[VCAP::CloudController::RoleTypes::SPACE_AUDITOR]).to be_a_guid
         expect(roles[VCAP::CloudController::RoleTypes::SPACE_MANAGER]).to be_a_guid
         expect(roles[VCAP::CloudController::RoleTypes::SPACE_SUPPORTER]).to be_a_guid
+      end
+
+      context 'optimized SQL queries' do
+        before do
+          OrganizationUser.make
+        end
+
+        it 'works for different filters' do
+          expect {
+            expect(Role.where(type: VCAP::CloudController::RoleTypes::ORGANIZATION_USER).count).to eq(2)
+            expect(Role.where(guid: organization_manager.guid).count).to eq(1)
+            expect(Role.where(user_id: user.id).count).to eq(2)
+            expect(Role.where(organization_id: org.id).count).to eq(2)
+            expect(Role.where(space_id: space.id).count).to eq(2)
+          }.to have_queried_db_times(/((\bwhere\b).*?){8}/i, 5) # SQL statement has eight WHERE conditions, one per UNIONed table.
+        end
+
+        it 'works for combined filters' do
+          expect {
+            expect(Role.where(type: VCAP::CloudController::RoleTypes::ORGANIZATION_USER).where(organization_id: org.id).count).to eq(1)
+            expect(Role.where(user_id: user.id).where(space_id: space.id).count).to eq(1)
+          }.to have_queried_db_times(/((\bwhere\b).*?){8}/i, 2) # SQL statement has eight WHERE conditions, one per UNIONed table.
+        end
+
+        it 'works for filters applied with table name prefix' do
+          expect {
+            expect(Role.where(t1__guid: organization_billing_manager.guid).count).to eq(1)
+          }.to have_queried_db_times(/((\bwhere\b).*?){8}/i, 1) # SQL statement has eight WHERE conditions, one per UNIONed table.
+        end
       end
     end
   end
