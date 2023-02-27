42837b1c7dc9bfdc92222ba987d1009a857fa330
Philipp Thun <philipp.thun@sap.com>
2023-02-27 11:55:09 +0100

    Correctly re-raise exceptions in HTTPClient's create_socket method
    The RuboCop auto-corrected code was wrong (NoMethodError). Added a test
    to ensure that the rescue block is working as expected.

--- spec/unit/lib/http/httpclient_spec.rb
+++ spec/unit/lib/http/httpclient_spec.rb
@@ -1,9 +1,20 @@
-require 'httpclient'
+require 'http/httpclient'
 
 RSpec.describe HTTPClient do
   describe 'version' do
     it 'should not be updated' do
       expect(HTTPClient::VERSION).to eq('2.8.3'), 'revisit monkey patch in lib/http/httpclient.rb'
+    end
+  end
+
+  describe 're-raising errors' do
+    let(:client) { double(socket_connect_timeout: nil) }
+
+    it 'adds host and port to the error message' do
+      allow(TCPSocket).to receive(:new).and_raise(Errno::ECONNREFUSED)
+
+      session = HTTPClient::Session.new(client, nil, nil, nil)
+      expect { session.create_socket('host', 123) }.to raise_error(SystemCallError, /(host:123)/)
     end
   end
 end
