012cd8766b4bd72adb85d46dacd61e7249dabb0f
M. Oleske <moleske@pivotal.io>
2023-04-20 16:06:28 -0700

    Use lazy setting constant so we don't always run the query
    - from Philipp's comment https://github.com/cloudfoundry/cloud_controller_ng/pull/3256#issuecomment-1514843307

--- lib/sequel/extensions/requires_unique_column_names_in_subquery.rb
+++ lib/sequel/extensions/requires_unique_column_names_in_subquery.rb
@@ -1,13 +1,19 @@
 module Sequel::UniqueColumnSubquery
   def requires_unique_column_names_in_subquery_select_list?
-    begin
-      self.db.fetch('SELECT * FROM (SELECT 1 AS a, 1 AS a) AS t1').all
-      unique_names_in_subquery = false
-    rescue Sequel::DatabaseError
-      unique_names_in_subquery = true
+    unless Sequel::UniqueColumnSubquery.const_defined?(:UNIQUE_NAMES_IN_SUBQUERY)
+      Sequel::UniqueColumnSubquery.const_set(:UNIQUE_NAMES_IN_SUBQUERY, unique_names_in_subquery?)
     end
 
-    unique_names_in_subquery
+    Sequel::UniqueColumnSubquery.const_get(:UNIQUE_NAMES_IN_SUBQUERY)
+  end
+
+  private
+
+  def unique_names_in_subquery?
+    self.db.fetch('SELECT * FROM (SELECT 1 AS a, 1 AS a) AS t1').all
+    false
+  rescue Sequel::DatabaseError
+    true
   end
 end
 
