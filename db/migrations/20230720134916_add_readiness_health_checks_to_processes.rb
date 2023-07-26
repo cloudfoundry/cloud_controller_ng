6aaff7b327211654bfe1586117b64c66c4ceccc0
Maria Shaldybin <mariash@vmware.com>
2023-07-26 18:00:26 +0000

    correctly default to process for readiness_health_check_type
    Signed-off-by: Amelia Downs <adowns@vmware.com>

--- db/migrations/20230720134916_add_readiness_health_checks_to_processes.rb
+++ db/migrations/20230720134916_add_readiness_health_checks_to_processes.rb
@@ -3,7 +3,7 @@ Sequel.migration do
     alter_table :processes do
       add_column :readiness_health_check_http_endpoint, String, text: true
       add_column :readiness_health_check_invocation_timeout, :integer, null: true, default: nil
-      add_column :readiness_health_check_type, String, text: true, default: 'process'
+      add_column :readiness_health_check_type, String, default: 'process'
     end
   end
 end
