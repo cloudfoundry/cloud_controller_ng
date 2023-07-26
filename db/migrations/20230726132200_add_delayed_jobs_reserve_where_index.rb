bee7a8ae62b43a3656b30cf65459f4a5fe663985
Philipp Thun <philipp.thun@sap.com>
2023-07-26 13:33:08 +0200

    Add delayed_jobs_reserve_where index
    With PR #3324 [1] the 'delayed_jobs_reserve' index was changed by adding the field 'priority' which is used in the ORDER BY clause. This change re-adds the previous index, that only contains fields used in the WHERE clause of the query. Although PostgreSQL could always use the new index, there seem to be situations where the query planner decides for a sequential table scan (theory: if the number of entries is rather low).
    
    [1] https://github.com/cloudfoundry/cloud_controller_ng/pull/3324
    
    Co-authored-by: Dimitar Velinov <dimitar.velinov01@sap.com>

---
+++ db/migrations/20230726132200_add_delayed_jobs_reserve_where_index.rb
@@ -0,0 +1,9 @@
+Sequel.migration do
+  up do
+    add_index :delayed_jobs, [:queue, :locked_at, :locked_by, :failed_at, :run_at], name: :delayed_jobs_reserve_where
+  end
+
+  down do
+    drop_index :delayed_jobs, nil, name: :delayed_jobs_reserve_where
+  end
+end
