470cef34f3a2a4a7c88b9800cdd6755670f2374a
Geoff Franks <gfranks@vmware.com>
2023-10-26 15:21:10 +0000

    Add mysql vs postgres tests for increased before/after testing of migration
    Signed-off-by: Amelia Downs <adowns@vmware.com>
    Co-authored-by: Amelia Downs <adowns@vmware.com>

--- spec/migrations/20231016094900_microsecond_timestamp_msql_asg_update_spec.rb
+++ spec/migrations/20231016094900_microsecond_timestamp_msql_asg_update_spec.rb
@@ -9,34 +9,39 @@ RSpec.describe 'migration to enable microsecond precision on asg last updated ta
         def supports_timestamp_usecs?
           true
         end
-
-        def timestamp_precision
-          6
-        end
       end
     end
   end
 
   describe 'asg_timestamps table' do
     it 'the last_update column handles sub-second time' do
-      initial_time = DateTime.new(2010)
+      dt_without_fractional_seconds = DateTime.new(2001, 2, 3, 4, 5, 6)
+      dt_with_fractional_seconds = dt_without_fractional_seconds.advance(seconds: 0.123456)
 
-      ds.insert(id: 1, last_update: initial_time)
+      ds.insert(id: 1, last_update: dt_with_fractional_seconds)
       t1 = ds.first(id: 1)
-      expect(t1[:last_update].to_datetime.rfc3339(6)).to eq(initial_time.rfc3339(6))
+
+      if db.database_type == :mysql
+        expect(t1[:last_update].to_datetime.subsec).to eq(0)
+        expect(t1[:last_update].to_datetime.rfc3339(6)).to eq(dt_without_fractional_seconds.rfc3339(6))
+      else
+        expect(t1[:last_update].to_datetime.subsec).not_to eq(0)
+        expect(t1[:last_update].to_datetime.rfc3339(6)).to eq(dt_with_fractional_seconds.rfc3339(6))
+      end
 
       # Change TIMESTAMP to TIMESTAMP(6)
       expect { Sequel::Migrator.run(db, migration_to_test, allow_missing_migration_files: true) }.not_to raise_error
 
       # the migration shouldn't add accuracy to previously inserted values
       t1_post_migration = ds.first(id: 1)
-      expect(t1_post_migration[:last_update].to_datetime.rfc3339(6)).to eq(initial_time.rfc3339(6))
+      expect(t1_post_migration[:last_update].to_datetime.rfc3339(6)).to eq(t1[:last_update].to_datetime.rfc3339(6))
 
       # but new data should retain microsecond accuracy
-      post_migration_time = DateTime.now
-      ds.insert(id: 2, last_update: post_migration_time)
+      ds.insert(id: 2, last_update: dt_with_fractional_seconds)
       t2 = ds.first(id: 2)
-      expect(t2[:last_update].to_datetime.rfc3339(6)).to eq(post_migration_time.rfc3339(6))
+
+      expect(t2[:last_update].to_datetime.subsec).not_to eq(0)
+      expect(t2[:last_update].to_datetime.rfc3339(6)).to eq(dt_with_fractional_seconds.rfc3339(6))
     end
   end
 end
