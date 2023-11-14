702b1534e22384e775ecfa6578560224d62a3c71
Sven Krieger <37476281+svkrieger@users.noreply.github.com>
2023-11-14 15:29:21 +0100

    Use constants instead of methods

--- lib/cloud_controller/metrics/prometheus_updater.rb
+++ lib/cloud_controller/metrics/prometheus_updater.rb
@@ -2,11 +2,39 @@ require 'prometheus/client'
 
 module VCAP::CloudController::Metrics
   class PrometheusUpdater
+    DURATION_BUCKETS = [5, 10, 30, 60, 300, 600, 890].freeze
+
+    METRICS = [
+      { type: :gauge, name: :cc_job_queues_length_total, docstring: 'Job queues length of worker processes', labels: [:queue] },
+      { type: :gauge, name: :cc_failed_jobs_total, docstring: 'Number of failed jobs of worker processes', labels: [:queue] },
+      { type: :counter, name: :cc_staging_requested_total, docstring: 'Number of staging requests' },
+      { type: :histogram, name: :cc_staging_succeeded_duration_seconds, docstring: 'Durations of successful staging events', buckets: DURATION_BUCKETS },
+      { type: :histogram, name: :cc_staging_failed_duration_seconds, docstring: 'Durations of failed staging events', buckets: DURATION_BUCKETS },
+      { type: :gauge, name: :cc_requests_outstanding_total, docstring: 'Requests outstanding' },
+      { type: :counter, name: :cc_requests_completed_total, docstring: 'Requests completed' },
+      { type: :gauge, name: :cc_thread_info_thread_count, docstring: 'Thread count' },
+      { type: :gauge, name: :cc_thread_info_event_machine_connection_count, docstring: 'Event Machine connection count' },
+      { type: :gauge, name: :cc_thread_info_event_machine_threadqueue_size, docstring: 'EventMachine thread queue size' },
+      { type: :gauge, name: :cc_thread_info_event_machine_threadqueue_num_waiting, docstring: 'EventMachine num waiting in thread' },
+      { type: :gauge, name: :cc_thread_info_event_machine_resultqueue_size, docstring: 'EventMachine queue size' },
+      { type: :gauge, name: :cc_thread_info_event_machine_resultqueue_num_waiting, docstring: 'EventMachine requests waiting in queue' },
+      { type: :gauge, name: :cc_vitals_started_at, docstring: 'CloudController Vitals: started_at' },
+      { type: :gauge, name: :cc_vitals_mem_bytes, docstring: 'CloudController Vitals: mem_bytes' },
+      { type: :gauge, name: :cc_vitals_cpu_load_avg, docstring: 'CloudController Vitals: cpu_load_avg' },
+      { type: :gauge, name: :cc_vitals_mem_used_bytes, docstring: 'CloudController Vitals: mem_used_bytes' },
+      { type: :gauge, name: :cc_vitals_mem_free_bytes, docstring: 'CloudController Vitals: mem_free_bytes' },
+      { type: :gauge, name: :cc_vitals_num_cores, docstring: 'CloudController Vitals: num_cores' },
+      { type: :gauge, name: :cc_running_tasks_total, docstring: 'Total running tasks' },
+      { type: :gauge, name: :cc_running_tasks_memory_bytes, docstring: 'Total memory consumed by running tasks' },
+      { type: :gauge, name: :cc_users_total, docstring: 'Number of users' },
+      { type: :gauge, name: :cc_deployments_in_progress_total, docstring: 'Number of in progress deployments' }
+    ].freeze
+
     def initialize(registry=Prometheus::Client.registry)
       @registry = registry
 
       # Register all metrics, to initialize them for discoverability
-      metrics.map do |metric|
+      METRICS.map do |metric|
         register_metric(metric[:type], metric[:name], metric[:docstring], labels: metric[:labels] || {}, buckets: metric[:buckets] || {}) unless @registry.exist?(metric[:name])
       end
     end
@@ -102,38 +130,6 @@ module VCAP::CloudController::Metrics
     end
 
     private
-
-    def metrics
-      [
-        { type: :gauge, name: :cc_job_queues_length_total, docstring: 'Job queues length of worker processes', labels: [:queue] },
-        { type: :gauge, name: :cc_failed_jobs_total, docstring: 'Number of failed jobs of worker processes', labels: [:queue] },
-        { type: :counter, name: :cc_staging_requested_total, docstring: 'Number of staging requests' },
-        { type: :histogram, name: :cc_staging_succeeded_duration_seconds, docstring: 'Durations of successful staging events', buckets: duration_buckets },
-        { type: :histogram, name: :cc_staging_failed_duration_seconds, docstring: 'Durations of failed staging events', buckets: duration_buckets },
-        { type: :gauge, name: :cc_requests_outstanding_total, docstring: 'Requests outstanding' },
-        { type: :counter, name: :cc_requests_completed_total, docstring: 'Requests completed' },
-        { type: :gauge, name: :cc_thread_info_thread_count, docstring: 'Thread count' },
-        { type: :gauge, name: :cc_thread_info_event_machine_connection_count, docstring: 'Event Machine connection count' },
-        { type: :gauge, name: :cc_thread_info_event_machine_threadqueue_size, docstring: 'EventMachine thread queue size' },
-        { type: :gauge, name: :cc_thread_info_event_machine_threadqueue_num_waiting, docstring: 'EventMachine num waiting in thread' },
-        { type: :gauge, name: :cc_thread_info_event_machine_resultqueue_size, docstring: 'EventMachine queue size' },
-        { type: :gauge, name: :cc_thread_info_event_machine_resultqueue_num_waiting, docstring: 'EventMachine requests waiting in queue' },
-        { type: :gauge, name: :cc_vitals_started_at, docstring: 'CloudController Vitals: started_at' },
-        { type: :gauge, name: :cc_vitals_mem_bytes, docstring: 'CloudController Vitals: mem_bytes' },
-        { type: :gauge, name: :cc_vitals_cpu_load_avg, docstring: 'CloudController Vitals: cpu_load_avg' },
-        { type: :gauge, name: :cc_vitals_mem_used_bytes, docstring: 'CloudController Vitals: mem_used_bytes' },
-        { type: :gauge, name: :cc_vitals_mem_free_bytes, docstring: 'CloudController Vitals: mem_free_bytes' },
-        { type: :gauge, name: :cc_vitals_num_cores, docstring: 'CloudController Vitals: num_cores' },
-        { type: :gauge, name: :cc_running_tasks_total, docstring: 'Total running tasks' },
-        { type: :gauge, name: :cc_running_tasks_memory_bytes, docstring: 'Total memory consumed by running tasks' },
-        { type: :gauge, name: :cc_users_total, docstring: 'Number of users' },
-        { type: :gauge, name: :cc_deployments_in_progress_total, docstring: 'Number of in progress deployments' }
-      ]
-    end
-
-    def duration_buckets
-      [5, 10, 30, 60, 300, 600, 890]
-    end
 
     def nanoseconds_to_seconds(time_ns)
       (time_ns / 1e9).to_f
