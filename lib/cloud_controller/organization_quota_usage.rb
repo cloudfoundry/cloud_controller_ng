c70e1c5dfc9292f4456b4ce115924fc1bb30bd45
Katharina Przybill <30441792+kathap@users.noreply.github.com>
2023-11-17 14:52:23 +0100

    Enhance /v3/organizations/:guid/usage_summary
    It would be nice to get org usage info for (nearly) all resources that are restricted by an org quota plan, beside memory in mb and started instances, which are already implemented.
    
    In the course of the change running_and_pending_tasks_count was refactored from nested selects to joins. Joins are generally more efficient than nested selects, especially when dealing with large datasets.
    
    Co-authored-by: Philipp Thun <philipp.thun@sap.com>

---
+++ lib/cloud_controller/organization_quota_usage.rb
@@ -0,0 +1,34 @@
+module VCAP::CloudController
+  class OrganizationQuotaUsage
+    def initialize(organization)
+      @organization = organization
+    end
+
+    def routes
+      OrganizationRoutes.new(@organization).count
+    end
+
+    def service_instances
+      @organization.managed_service_instances_dataset.count
+    end
+
+    def private_domains
+      @organization.owned_private_domains_dataset.count
+    end
+
+    def service_keys
+      VCAP::CloudController::ServiceKey.dataset.join(:service_instances, id: :service_instance_id).
+        join(:spaces, id: :space_id).
+        where(spaces__organization_id: @organization.id).
+        count
+    end
+
+    def reserved_route_ports
+      OrganizationReservedRoutePorts.new(@organization).count
+    end
+
+    def app_tasks
+      @organization.running_and_pending_tasks_count
+    end
+  end
+end
