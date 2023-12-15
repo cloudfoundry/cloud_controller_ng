54ae36479e953fc8d0e38fdea4586e601ca8e765
Michael Oleske <moleske@vmware.com>
2023-12-15 22:10:00 +0000

    Don't do this
    - remove rfc822 and copy pasta only the regex thing we were using
    - remember to manually edit the cookie_validation.rb in the gem
      cookiejar

--- lib/sequel_plugins/vcap_validations.rb
+++ lib/sequel_plugins/vcap_validations.rb
@@ -1,5 +1,21 @@
 module Sequel::Plugins::VcapValidations
   module InstanceMethods
+    QTEXT = '[^\\x0d\\x22\\x5c\\x80-\\xff]'.freeze
+    DTEXT = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]'.freeze
+    ATOM = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-' \
+           '\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+'.freeze
+    QUOTED_PAIR = '\\x5c[\\x00-\\x7f]'.freeze
+    DOMAIN_LITERAL = "\\x5b(?:#{DTEXT}|#{QUOTED_PAIR})*\\x5d".freeze
+    QUOTED_STRING = "\\x22(?:#{QTEXT}|#{QUOTED_PAIR})*\\x22".freeze
+    DOMAIN_REF = ATOM
+    SUB_DOMAIN = "(?:#{DOMAIN_REF}|#{DOMAIN_LITERAL})".freeze
+    WORD = "(?:#{ATOM}|#{QUOTED_STRING})".freeze
+    DOMAIN = "#{SUB_DOMAIN}(?:\\x2e#{SUB_DOMAIN})*".freeze
+    LOCAL_PART = "#{WORD}(?:\\x2e#{WORD})*".freeze
+    ADDR_SPEC = "#{LOCAL_PART}\\x40#{DOMAIN}".freeze
+
+    EMAIL_REGEXP_WHOLE = Regexp.new("\\A#{ADDR_SPEC}\\z", Regexp::NOENCODING)
+    EMAIL_REGEXP_PART = Regexp.new(ADDR_SPEC.to_s, Regexp::NOENCODING)
     # Validates that an attribute is a valid http or https url
     #
     # @param [Symbol] The attribute to validate
@@ -13,7 +29,7 @@ module Sequel::Plugins::VcapValidations
     #
     # @param [Symbol] The attribute to validate
     def validates_email(attr)
-      validates_format(RFC822::EMAIL_REGEXP_WHOLE, attr, message: :email) if send(attr)
+      validates_format(EMAIL_REGEXP_WHOLE, attr, message: :email) if send(attr)
     end
   end
 end
--- lib/vcap/rest_api/message.rb
+++ lib/vcap/rest_api/message.rb
@@ -1,5 +1,4 @@
 require 'vcap/json_message'
-require 'rfc822'
 
 module VCAP
   module RestAPI
@@ -58,7 +57,23 @@ module VCAP
 
       URL = UrlDecorator.new(URI::DEFAULT_PARSER.make_regexp(%w[http https]))
       HTTPS_URL = HttpsUrlDecorator.new(URI::DEFAULT_PARSER.make_regexp('https'))
-      EMAIL = EmailDecorator.new(RFC822::EMAIL_REGEXP_WHOLE)
+      QTEXT = '[^\\x0d\\x22\\x5c\\x80-\\xff]'.freeze
+      DTEXT = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]'.freeze
+      ATOM = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-' \
+             '\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+'.freeze
+      QUOTED_PAIR = '\\x5c[\\x00-\\x7f]'.freeze
+      DOMAIN_LITERAL = "\\x5b(?:#{DTEXT}|#{QUOTED_PAIR})*\\x5d".freeze
+      QUOTED_STRING = "\\x22(?:#{QTEXT}|#{QUOTED_PAIR})*\\x22".freeze
+      DOMAIN_REF = ATOM
+      SUB_DOMAIN = "(?:#{DOMAIN_REF}|#{DOMAIN_LITERAL})".freeze
+      WORD = "(?:#{ATOM}|#{QUOTED_STRING})".freeze
+      DOMAIN = "#{SUB_DOMAIN}(?:\\x2e#{SUB_DOMAIN})*".freeze
+      LOCAL_PART = "#{WORD}(?:\\x2e#{WORD})*".freeze
+      ADDR_SPEC = "#{LOCAL_PART}\\x40#{DOMAIN}".freeze
+
+      EMAIL_REGEXP_WHOLE = Regexp.new("\\A#{ADDR_SPEC}\\z", Regexp::NOENCODING)
+      EMAIL_REGEXP_PART = Regexp.new(ADDR_SPEC.to_s, Regexp::NOENCODING)
+      EMAIL = EmailDecorator.new(EMAIL_REGEXP_WHOLE)
       GIT_URL = GitUrlDecorator.new(URI::DEFAULT_PARSER.make_regexp(%w[http https git]))
 
       # The block will be evaluated in the context of the schema validator used
