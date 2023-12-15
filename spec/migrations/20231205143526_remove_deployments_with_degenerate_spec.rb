4cbb0c11cab3b015a77087b02ae8b15ab670dcfd
Wei Quan <w.quan@sap.com>
2023-12-15 15:36:35 +0100

    Use SQL instead of CC model in test

--- spec/migrations/20231205143526_remove_deployments_with_degenerate_spec.rb
+++ spec/migrations/20231205143526_remove_deployments_with_degenerate_spec.rb
@@ -2,31 +2,20 @@ require 'spec_helper'
 require 'migrations/helpers/migration_shared_context'
 
 RSpec.describe 'migration to clean up degenerate records from deployments records', isolation: :truncation do
-  let(:tmp_migrations_dir) { Dir.mktmpdir }
-
-  before do
-    FileUtils.cp(
-      File.join(DBMigrator::SEQUEL_MIGRATIONS, '20231205143526_remove_deployments_with_degenerate.rb'),
-      tmp_migrations_dir
-    )
+  include_context 'migration' do
+    let(:migration_filename) { '20231205143526_remove_deployments_with_degenerate.rb' }
   end
 
-  let(:app) { VCAP::CloudController::AppModel.make }
-
-  context 'when a table has status reason DEGENERATE' do
-    let!(:deployment_with_status_reason_degenerate) do
-      VCAP::CloudController::DeploymentModel.create(
-        guid: 'with-status-reason-degenerate',
-        app: app,
+  describe 'deployments table' do
+    it 'degenerate record is removed from deployments' do
+      db[:deployments].insert(
+        guid: 'bommel',
         original_web_process_instance_count: 1,
         status_reason: 'DEGENERATE'
       )
-    end
 
-    it 'degenerate record is removed from deployments' do
-      Sequel::Migrator.run(VCAP::CloudController::DeploymentModel.db, tmp_migrations_dir, table: :my_fake_table)
-      deployment = VCAP::CloudController::DeploymentModel.where(status_reason: 'DEGENERATE').first
-
+      expect { Sequel::Migrator.run(db, migration_to_test, allow_missing_migration_files: true) }.not_to raise_error
+      deployment = db[:deployments].first(status_reason: 'DEGENERATE')
       expect(deployment).to be_nil
     end
   end
