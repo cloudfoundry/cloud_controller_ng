f747656179d1447377e0f135ef67e8369dd496b8
Greg Cobb <gcobb@vmware.com>
2023-12-29 17:12:35 -0800

    Upgrade to Rails 7
    - Zeitwerk is now the default autoloader, so configuring it is not
      needed

--- spec/unit/lib/delayed_job/delayed_job_spec.rb
+++ spec/unit/lib/delayed_job/delayed_job_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe 'delayed_job' do
   describe 'version' do
     it 'is not updated' do
-      expect(Gem.loaded_specs['delayed_job'].version).to eq('4.1.9'), 'revisit monkey patch in lib/delayed_job/quit_trap.rb'
+      expect(Gem.loaded_specs['delayed_job'].version).to eq('4.1.11'), 'revisit monkey patch in lib/delayed_job/quit_trap.rb'
     end
   end
 end
