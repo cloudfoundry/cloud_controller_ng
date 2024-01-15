340aa3ee859a386fdbc1d72394f762edde7a9f9d
Wei Quan <64415962+WeiQuan0605@users.noreply.github.com>
2024-01-15 13:36:43 +0100

    Fix regression of clean up degenerate deployments (#3596)
    * If a foreign key constraint exists, first delete the child records, then delete the parent records.
    
    Co-authored-by: Philipp Thun <philipp.thun@sap.com>

--- db/migrations/20231205143526_remove_deployments_with_degenerate.rb
+++ db/migrations/20231205143526_remove_deployments_with_degenerate.rb
@@ -1,7 +1,14 @@
 Sequel.migration do
   up do
     degenerate_records = self[:deployments].where(status_reason: 'DEGENERATE')
-    degenerate_records.delete
+
+    if degenerate_records.count > 0
+      guids_dataset = degenerate_records.select(:guid)
+      self[:deployment_processes].where(deployment_guid: guids_dataset).delete
+      self[:deployment_labels].where(resource_guid: guids_dataset).delete
+      self[:deployment_annotations].where(resource_guid: guids_dataset).delete
+      degenerate_records.delete
+    end
   end
 
   down do
