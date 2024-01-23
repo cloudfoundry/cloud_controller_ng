03a78d0c0944a393f8ae0e6d5e782229978ea7ba
Philipp Thun <philipp.thun@sap.com>
2024-01-23 13:48:11 +0100

    Use idigest in UserResetInterval

--- middleware/mixins/user_reset_interval.rb
+++ middleware/mixins/user_reset_interval.rb
@@ -5,7 +5,7 @@ module CloudFoundry
     module UserResetInterval
       def next_expires_in(user_guid, reset_interval_in_minutes)
         interval = reset_interval_in_minutes.minutes.to_i
-        offset = Digest::XXH64.hexdigest(user_guid).remainder(interval)
+        offset = Digest::XXH64.idigest(user_guid).remainder(interval)
 
         interval - (Time.now.to_i - offset).remainder(interval)
       end
