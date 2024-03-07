9776ff256f34c599942326bfb7211a0ab5344c24
Ben Fuller <ben.fuller@broadcom.com>
2024-03-07 10:41:38 -0700

    show user what the corrupted-env-variables are in the most recoverable format

--- lib/cloud_controller/serializer.rb
+++ lib/cloud_controller/serializer.rb
@@ -12,7 +12,7 @@ module VCAP::CloudController::Serializer
         rescue MultiJson::ParseError
           logger ||= Steno.logger('cc.serializer')
           logger.error("Failed to deserialize #{guid}")
-          nil
+          { 'corrupted-env': string }
         end
       end
       alias_method "#{accessor_method_name}_without_serialization", accessor_method_name
