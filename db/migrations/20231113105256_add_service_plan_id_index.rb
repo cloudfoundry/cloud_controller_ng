102c22f573f422e07062e22e4e38393bcd9853ab
Philipp Thun <philipp.thun@sap.com>
2024-04-09 10:16:16 +0200

    Fix add|drop_index syntax (#3723)
    Wrong syntax:
      options: %i[if_exists concurrently]
    
    Correct syntax:
      if_exists: true, concurrently: true
    
    As this changes already released migrations, it is only relevant for
    foundations where these have not been applied yet (e.g. due to skipping
    the latest CAPI release).
    
    Furthermore 'copy-and-paste' errors will be prevented when new
    migrations are created.
    
    The 'migration_shared_context' has been enhanced by spying on
    add|drop_index invocations, checking for valid options.

--- db/migrations/20231113105256_add_service_plan_id_index.rb
+++ db/migrations/20231113105256_add_service_plan_id_index.rb
@@ -1,9 +1,11 @@
 Sequel.migration do
+  no_transaction # to use the 'concurrently' option
+
   up do
-    add_index :service_plan_visibilities, :service_plan_id, name: :spv_service_plan_id_index, options: [:concurrently] if database_type == :postgres
+    add_index :service_plan_visibilities, :service_plan_id, name: :spv_service_plan_id_index, concurrently: true if database_type == :postgres
   end
 
   down do
-    drop_index :service_plan_visibilities, nil, name: :spv_service_plan_id_index, options: [:concurrently] if database_type == :postgres
+    drop_index :service_plan_visibilities, nil, name: :spv_service_plan_id_index, concurrently: true if database_type == :postgres
   end
 end
--- db/migrations/20240219113000_add_routes_space_id_index.rb
+++ db/migrations/20240219113000_add_routes_space_id_index.rb
@@ -1,9 +1,11 @@
 Sequel.migration do
+  no_transaction # to use the 'concurrently' option
+
   up do
-    add_index :routes, :space_id, name: :routes_space_id_index, options: %i[if_not_exists concurrently] if database_type == :postgres
+    add_index :routes, :space_id, name: :routes_space_id_index, if_not_exists: true, concurrently: true if database_type == :postgres
   end
 
   down do
-    drop_index :routes, :space_id, name: :routes_space_id_index, options: %i[if_exists concurrently] if database_type == :postgres
+    drop_index :routes, :space_id, name: :routes_space_id_index, if_exists: true, concurrently: true if database_type == :postgres
   end
 end
