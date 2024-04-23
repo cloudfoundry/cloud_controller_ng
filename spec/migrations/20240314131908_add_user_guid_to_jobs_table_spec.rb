53a9ad092e1cea6b964cf95d2187fe65eea12728
Philipp Thun <philipp.thun@sap.com>
2024-04-23 16:21:41 +0200

    Adapt to changed migration-shared-context

--- spec/migrations/20240314131908_add_user_guid_to_jobs_table_spec.rb
+++ spec/migrations/20240314131908_add_user_guid_to_jobs_table_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe 'migration to add user_guid column to jobs table and add an index
   describe 'jobs table' do
     it 'adds a column `user_guid`' do
       expect(db[:jobs].columns).not_to include(:user_guid)
-      expect { Sequel::Migrator.run(db, migration_to_test, allow_missing_migration_files: true) }.not_to raise_error
+      expect { Sequel::Migrator.run(db, migrations_path, target: current_migration_index, allow_missing_migration_files: true) }.not_to raise_error
       expect(db[:jobs].columns).to include(:user_guid)
     end
 
     it 'adds an index on the user_guid column' do
       expect(db.indexes(:jobs)).not_to include(:jobs_user_guid_index)
-      expect { Sequel::Migrator.run(db, migration_to_test, allow_missing_migration_files: true) }.not_to raise_error
+      expect { Sequel::Migrator.run(db, migrations_path, target: current_migration_index, allow_missing_migration_files: true) }.not_to raise_error
       expect(db.indexes(:jobs)).to include(:jobs_user_guid_index)
     end
 
@@ -27,13 +27,13 @@ RSpec.describe 'migration to add user_guid column to jobs table and add an index
 
         it 'does not fail' do
           expect(db[:jobs].columns).to include(:user_guid)
-          expect { Sequel::Migrator.run(db, migration_to_test, allow_missing_migration_files: true) }.not_to raise_error
+          expect { Sequel::Migrator.run(db, migrations_path, target: current_migration_index, allow_missing_migration_files: true) }.not_to raise_error
         end
 
         it 'continues to create the index' do
           expect(db[:jobs].columns).to include(:user_guid)
           expect(db.indexes(:jobs)).not_to include(:jobs_user_guid_index)
-          expect { Sequel::Migrator.run(db, migration_to_test, allow_missing_migration_files: true) }.not_to raise_error
+          expect { Sequel::Migrator.run(db, migrations_path, target: current_migration_index, allow_missing_migration_files: true) }.not_to raise_error
           expect(db.indexes(:jobs)).to include(:jobs_user_guid_index)
         end
       end
@@ -46,7 +46,7 @@ RSpec.describe 'migration to add user_guid column to jobs table and add an index
 
         it 'does not fail' do
           expect(db.indexes(:jobs)).to include(:jobs_user_guid_index)
-          expect { Sequel::Migrator.run(db, migration_to_test, allow_missing_migration_files: true) }.not_to raise_error
+          expect { Sequel::Migrator.run(db, migrations_path, target: current_migration_index, allow_missing_migration_files: true) }.not_to raise_error
         end
       end
     end
@@ -54,27 +54,27 @@ RSpec.describe 'migration to add user_guid column to jobs table and add an index
     describe 'idempotency of down' do
       context 'index does not exist' do
         before do
-          Sequel::Migrator.run(db, migration_to_test, allow_missing_migration_files: true)
+          Sequel::Migrator.run(db, migrations_path, target: current_migration_index, allow_missing_migration_files: true)
           db.drop_index :jobs, :user_guid, name: :jobs_user_guid_index
         end
 
         it 'does not fail' do
           expect(db[:jobs].columns).to include(:user_guid)
           expect(db.indexes(:jobs)).not_to include(:jobs_user_guid_index)
-          expect { Sequel::Migrator.run(db, migration_to_test, allow_missing_migration_files: true, target: 0) }.not_to raise_error
+          expect { Sequel::Migrator.run(db, migrations_path, target: current_migration_index - 1, allow_missing_migration_files: true) }.not_to raise_error
         end
 
         it 'continues to remove the `user_guid` column' do
           expect(db[:jobs].columns).to include(:user_guid)
           expect(db.indexes(:jobs)).not_to include(:jobs_user_guid_index)
-          expect { Sequel::Migrator.run(db, migration_to_test, allow_missing_migration_files: true, target: 0) }.not_to raise_error
+          expect { Sequel::Migrator.run(db, migrations_path, target: current_migration_index - 1, allow_missing_migration_files: true) }.not_to raise_error
           expect(db[:jobs].columns).not_to include(:user_guid)
         end
       end
 
       context 'index and column do not exist' do
         before do
-          Sequel::Migrator.run(db, migration_to_test, allow_missing_migration_files: true)
+          Sequel::Migrator.run(db, migrations_path, target: current_migration_index, allow_missing_migration_files: true)
           db.drop_index :jobs, :user_guid, name: :jobs_user_guid_index
           db.drop_column :jobs, :user_guid
         end
@@ -82,7 +82,7 @@ RSpec.describe 'migration to add user_guid column to jobs table and add an index
         it 'does not fail' do
           expect(db[:jobs].columns).not_to include(:user_guid)
           expect(db.indexes(:jobs)).not_to include(:jobs_user_guid_index)
-          expect { Sequel::Migrator.run(db, migration_to_test, allow_missing_migration_files: true, target: 0) }.not_to raise_error
+          expect { Sequel::Migrator.run(db, migrations_path, target: current_migration_index - 1, allow_missing_migration_files: true) }.not_to raise_error
         end
       end
     end
