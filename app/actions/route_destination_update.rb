13f67275814d5d187fcaac118e8c815cc5e6e377
Tim Downey <tdowney@vmware.com>
2024-05-08 10:42:57 -0600

    Use ProcessRouteHandler directly from RouteDestinationUpdate

--- app/actions/route_destination_update.rb
+++ app/actions/route_destination_update.rb
@@ -15,9 +15,8 @@ module VCAP::CloudController
           destination.save
         end
 
-        runners = CloudController::DependencyLocator.instance.runners
         destination.processes.each do |process|
-          notify_backend_of_route_update(process, runners)
+          ProcessRouteHandler.new(process).notify_backend_of_route_update
         end
 
         destination
@@ -25,22 +24,12 @@ module VCAP::CloudController
 
       private
 
-      def notify_backend_of_route_update(process, runners)
-        runners.runner_for_process(process).update_routes if process.staged? && process.started?
-      rescue Diego::Runner::CannotCommunicateWithDiegoError => e
-        logger.error("failed communicating with diego backend: #{e.message}")
-      end
-
       def validate_protocol_matches_route!(destination, message)
         if destination.route&.protocol == 'tcp'
           raise Error.new("Destination protocol must be 'tcp' if the parent route's protocol is 'tcp'") unless message.protocol == 'tcp'
         elsif message.protocol == 'tcp'
           raise Error.new("Destination protocol must be 'http1' or 'http2' if the parent route's protocol is 'http'")
         end
-      end
-
-      def logger
-        @logger ||= Steno.logger('cc.route_destination_update')
       end
     end
   end
