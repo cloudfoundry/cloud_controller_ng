a3ee88b0b3441be9e779333665182ba6e8ba031b
Johannes Haass <johannes.haass@sap.com>
2024-07-08 15:34:50 +0200

    Add thread based delayed workers
    Co-authored-by: Johannes Haass <johannes.haass@sap.com>

--- lib/cloud_controller/config_schemas/base/api_schema.rb
+++ lib/cloud_controller/config_schemas/base/api_schema.rb
@@ -345,7 +345,8 @@ module VCAP::CloudController
               optional(:app_usage_events_cleanup) => { timeout_in_seconds: Integer },
               optional(:blobstore_delete) => { timeout_in_seconds: Integer },
               optional(:diego_sync) => { timeout_in_seconds: Integer },
-              optional(:priorities) => Hash
+              optional(:priorities) => Hash,
+              optional(:threads) => Integer
             },
 
             # perm settings no longer have any effect but are preserved here
