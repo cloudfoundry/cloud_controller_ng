1e146800b5370f22a644f59c2f12a0c985b86849
Ralf Pannemans <ralf.pannemans@sap.com>
2024-07-12 17:24:33 +0200

    WIP: Restructured buildpack model
    Co-authored-by: Nicolas Bender <nicolas.bender@sap.com>

--- app/models/runtime/buildpack.rb
+++ app/models/runtime/buildpack.rb
@@ -1,6 +1,11 @@
 module VCAP::CloudController
   class Buildpack < Sequel::Model
     plugin :list
+    plugin :single_table_inheritance, :lifecycle,
+      model_map: {
+        VCAP::CloudController::Lifecycles::BUILDPACK => "VCAP::CloudController::ClassicBuildpack",
+        VCAP::CloudController::Lifecycles::CNB => "VCAP::CloudController::CNBBuildpack"
+      }
 
     export_attributes :name, :stack, :position, :enabled, :locked, :filename
     import_attributes :name, :stack, :position, :enabled, :locked, :filename, :key
@@ -15,21 +20,6 @@ module VCAP::CloudController
 
     add_association_dependencies labels: :destroy
     add_association_dependencies annotations: :destroy
-
-    def self.list_admin_buildpacks(stack_name=nil)
-      scoped = exclude(key: nil).exclude(key: '')
-      if stack_name.present?
-        scoped = scoped.filter(Sequel.or([
-          [:stack, stack_name],
-          [:stack, nil]
-        ]))
-      end
-      scoped.order(:position).all
-    end
-
-    def self.at_last_position
-      where(position: max(:position)).first
-    end
 
     def self.user_visibility_filter(_user)
       full_dataset_filter
