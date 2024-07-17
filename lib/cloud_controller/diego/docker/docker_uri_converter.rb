d9c8f9902bf94849bb4b67bbf2f793ddcfef655f
Nicolas Bender <nicolas.bender@sap.com>
2024-07-17 15:45:22 +0200

    Move sha256 part to the image path for Docker LRP rootfs
    Co-authored-by: Pavel Busko <pavel.busko@sap.com>

--- lib/cloud_controller/diego/docker/docker_uri_converter.rb
+++ lib/cloud_controller/diego/docker/docker_uri_converter.rb
@@ -6,6 +6,12 @@ module VCAP::CloudController
       raise UriUtils::InvalidDockerURI.new "Docker URI [#{docker_uri}] should not contain scheme" if docker_uri.include? '://'
 
       host, path, tag = UriUtils.parse_docker_uri(docker_uri)
+
+      if !tag.nil? && tag.start_with?('@sha256:')
+        path = "#{path}@sha256"
+        tag.slice!('@sha256:')
+      end
+
       Addressable::URI.new(scheme: 'docker', host: host, path: path, fragment: tag).to_s
     end
   end
