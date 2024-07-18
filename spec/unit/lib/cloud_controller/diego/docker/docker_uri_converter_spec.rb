e5fb425e93b980f9bd95e39df89ff4b983f95cc1
Pavel Busko <pavel.busko@sap.com>
2024-07-18 09:36:32 +0200

    Move @sha256 part to the image path for Docker LRP rootfs (#3889)
    Co-authored-by: Nicolas Bender <nicolas.bender@sap.com>

--- spec/unit/lib/cloud_controller/diego/docker/docker_uri_converter_spec.rb
+++ spec/unit/lib/cloud_controller/diego/docker/docker_uri_converter_spec.rb
@@ -38,6 +38,14 @@ module VCAP::CloudController
           expect(converter.convert(image_url)).to eq('docker:///user/image#tag')
         end
       end
+
+      context('and a user/image@sha256:9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08') do
+        let(:image_url) { 'user/image@sha256:9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08' }
+
+        it 'builds the correct rootFS path' do
+          expect(converter.convert(image_url)).to eq('docker:///user/image@sha256#9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08')
+        end
+      end
     end
 
     context('and the docker image url has host:port') do
