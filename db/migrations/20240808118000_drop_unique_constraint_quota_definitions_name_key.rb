6edeb7d6efe9670136f2fa82e2ed4b31f8201201
Katharina Przybill <30441792+kathap@users.noreply.github.com>
2024-09-03 15:12:54 +0200

    test withput mysql5.7 support

--- db/migrations/20240808118000_drop_unique_constraint_quota_definitions_name_key.rb
+++ db/migrations/20240808118000_drop_unique_constraint_quota_definitions_name_key.rb
@@ -13,12 +13,6 @@ Sequel.migration do
 
   down do
     if self.class.name.match?(/mysql/i)
-      # mysql 5 is not so smart as mysql 8, prevent Mysql2::Error: Duplicate key name 'name'
-      alter_table :quota_definitions do
-        # rubocop:disable Sequel/ConcurrentIndex
-        drop_index :name, name: :name if @db.indexes(:quota_definitions).include?(:name)
-        # rubocop:enable Sequel/ConcurrentIndex
-      end
       alter_table :quota_definitions do
         add_unique_constraint :name, name: :name, if_not_exists: true
       end
