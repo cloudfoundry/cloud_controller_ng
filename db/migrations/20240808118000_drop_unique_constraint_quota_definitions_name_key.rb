555981544eb22d5aef457898932336019379e0db
Katharina Przybill <30441792+kathap@users.noreply.github.com>
2024-09-04 09:40:43 +0200

    try backwards tests

--- db/migrations/20240808118000_drop_unique_constraint_quota_definitions_name_key.rb
+++ db/migrations/20240808118000_drop_unique_constraint_quota_definitions_name_key.rb
@@ -1,10 +1,6 @@
 Sequel.migration do
   up do
-    if self.class.name.match?(/mysql/i)
-      alter_table :quota_definitions do
-        drop_constraint :name, if_exists: true
-      end
-    elsif self.class.name.match?(/postgres/i)
+    if self.class.name.match?(/postgres/i)
       alter_table :quota_definitions do
         drop_constraint :quota_definitions_name_key, if_exists: true
       end
@@ -12,17 +8,7 @@ Sequel.migration do
   end
 
   down do
-    if self.class.name.match?(/mysql/i)
-      # mysql 5 is not so smart as mysql 8, prevent Mysql2::Error: Duplicate key name 'name'
-      alter_table :quota_definitions do
-        # rubocop:disable Sequel/ConcurrentIndex
-        drop_index :name, name: :name if @db.indexes(:quota_definitions).include?(:name)
-        # rubocop:enable Sequel/ConcurrentIndex
-      end
-      alter_table :quota_definitions do
-        add_unique_constraint :name, name: :name, if_not_exists: true
-      end
-    elsif self.class.name.match?(/postgres/i)
+    if self.class.name.match?(/postgres/i)
       alter_table :quota_definitions do
         add_unique_constraint :name, name: :quota_definitions_name_key, if_not_exists: true
       end
