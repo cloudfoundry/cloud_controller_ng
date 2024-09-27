ddd0857db885d6a7099e826615e0e21fcaa03ee1
Philipp Thun <philipp.thun@sap.com>
2024-09-27 14:19:50 +0200

    Adapt AppFeaturesController and AppFeatureUpdate

--- app/actions/app_feature_update.rb
+++ app/actions/app_feature_update.rb
@@ -6,6 +6,8 @@ module VCAP::CloudController
         app.update(enable_ssh: message.enabled)
       when 'revisions'
         app.update(revisions_enabled: message.enabled)
+      when 'file-based-service-bindings'
+        app.update(file_based_service_bindings_enabled: message.enabled)
       end
     end
   end
