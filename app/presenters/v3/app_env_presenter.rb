f701088c5163a2c226cddaae1c028e41ed8dad47
Philipp Thun <philipp.thun@sap.com>
2024-10-01 15:23:08 +0200

    Adapt system env presenter
    When the app feature 'file-based service bindings' is enabled,
    SERVICE_BINDING_ROOT is returned instead of VCAP_SERVICES.
    
    For an app using file-based service bindings the '/env' endpoint (i.e.
    GET /v3/apps/:guid/env) returns the following:
    
      {
        ...
        "system_env_json": {
          "SERVICE_BINDING_ROOT": "/etc/cf-service-bindings"
        },
        ...
      }
    
    The file runtime_environment/system_env_presenter_spec.rb has been
    deleted because all the tests it contained are also present in
    system_environment/system_env_presenter_spec.rb.

--- app/presenters/v3/app_env_presenter.rb
+++ app/presenters/v3/app_env_presenter.rb
@@ -25,7 +25,7 @@ module VCAP::CloudController
             environment_variables: app.environment_variables,
             staging_env_json: EnvironmentVariableGroup.staging.environment_json,
             running_env_json: EnvironmentVariableGroup.running.environment_json,
-            system_env_json: redact_hash(SystemEnvPresenter.new(app.service_bindings).system_env),
+            system_env_json: redact_hash(SystemEnvPresenter.new(app).system_env),
             application_env_json: vcap_application
           }
         end
