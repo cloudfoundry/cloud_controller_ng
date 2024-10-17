eb4d99515f8f32c1cdf9c0d97d3e0aa1302419ca
Johannes Haass <johannes.haass@sap.com>
2024-10-17 15:54:18 +0200

    Fix protoc installation

--- lib/diego/bbs/models/desired_lrp_requests_pb.rb
+++ lib/diego/bbs/models/desired_lrp_requests_pb.rb
@@ -25,10 +25,6 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
     optional :error, :message, 1, "diego.bbs.models.Error"
     repeated :desired_lrp_scheduling_infos, :message, 2, "diego.bbs.models.DesiredLRPSchedulingInfo"
   end
-  add_message "diego.bbs.models.DesiredLRPSchedulingInfoByProcessGuidResponse" do
-    optional :error, :message, 1, "diego.bbs.models.Error"
-    optional :desired_lrp_scheduling_info, :message, 2, "diego.bbs.models.DesiredLRPSchedulingInfo"
-  end
   add_message "diego.bbs.models.DesiredLRPByProcessGuidRequest" do
     optional :process_guid, :string, 1
   end
@@ -52,7 +48,6 @@ module Diego
       DesiredLRPsRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("diego.bbs.models.DesiredLRPsRequest").msgclass
       DesiredLRPResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("diego.bbs.models.DesiredLRPResponse").msgclass
       DesiredLRPSchedulingInfosResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("diego.bbs.models.DesiredLRPSchedulingInfosResponse").msgclass
-      DesiredLRPSchedulingInfoByProcessGuidResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("diego.bbs.models.DesiredLRPSchedulingInfoByProcessGuidResponse").msgclass
       DesiredLRPByProcessGuidRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("diego.bbs.models.DesiredLRPByProcessGuidRequest").msgclass
       DesireLRPRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("diego.bbs.models.DesireLRPRequest").msgclass
       UpdateDesiredLRPRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("diego.bbs.models.UpdateDesiredLRPRequest").msgclass
