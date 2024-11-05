d5cb6939a520d5b4d71cb645f28dec739ffd3e26
Sven Krieger <37476281+svkrieger@users.noreply.github.com>
2024-11-05 08:45:27 +0100

    Enable UAA shadow user creation for POST /v3/users

--- app/actions/user_create.rb
+++ app/actions/user_create.rb
@@ -4,10 +4,21 @@ module VCAP::CloudController
     end
 
     def create(message:)
-      user = User.create(guid: message.guid)
+      begin
+        shadow_user = User.create_uaa_shadow_user(message.username, message.origin) if message.username && message.origin
+      rescue CF::UAA::TargetError => e
+        raise e unless e.info['error'] == 'scim_resource_already_exists'
+
+        existing_guid = e.info['user_id']
+      end
+
+      user_guid = existing_guid || shadow_user['id'] || message.guid
+
+      user = User.create(guid: user_guid)
       User.db.transaction do
         MetadataUpdate.update(user, message)
       end
+
       user
     rescue Sequel::ValidationFailed => e
       validation_error!(message, e)
@@ -16,7 +27,11 @@ module VCAP::CloudController
     private
 
     def validation_error!(message, error)
-      error!("User with guid '#{message.guid}' already exists.") if error.errors.on(:guid)&.any? { |e| [:unique].include?(e) }
+      error!("User with guid '#{message.guid}' already exists.") if message.guid && error.errors.on(:guid)&.any? { |e| [:unique].include?(e) }
+
+      if !message.guid && error.errors.on(:guid)&.any? { |e| [:unique].include?(e) }
+        error!("User with username '#{message.username}' and origin '#{message.origin}' already exists.")
+      end
 
       error!(error.message)
     end
