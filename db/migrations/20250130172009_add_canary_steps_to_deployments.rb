1066ad27e577453ba832290da469265b51edb4ae
Sam Gunaratne <sgunaratne@vmware.com>
2025-02-03 14:41:58 -0700

    wip canary step model and actions

---
+++ db/migrations/20250130172009_add_canary_steps_to_deployments.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  up do
+    alter_table(:deployments) do
+      # TODO: what size?
+      add_column :canary_steps, String, size: 4096
+      add_column :canary_current_step, :integer
+    end
+  end
+  down do
+    alter_table(:deployments) do
+      drop_column :canary_steps
+      drop_column :canary_current_step
+    end
+  end
+end
