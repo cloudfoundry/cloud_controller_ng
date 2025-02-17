eed3d7bab9589f0763b8feb8b2e514e3dc665779
Katharina Przybill <30441792+kathap@users.noreply.github.com>
2025-02-17 13:06:19 +0100

    Better error message and support logs for /v3/service_offerings and plans (#4213)
    * Better error message and support logs for /v3/service_offerings and plans

--- spec/request/service_instances_spec.rb
+++ spec/request/service_instances_spec.rb
@@ -1260,7 +1260,10 @@ RSpec.describe 'V3 service instances' do
             expect(last_response).to have_status_code(422)
             expect(parsed_response['errors']).to include(
               include({
-                        'detail' => 'Invalid service plan. Ensure that the service plan exists, is available, and you have access to it.',
+                        'detail' => 'Invalid service plan. This could be due to a space-scoped broker which is offering the service plan ' \
+                                    "'#{service_plan.name}' with guid '#{service_plan.guid}' in another space or that the plan " \
+                                    'is not enabled in this organization. Ensure that the service plan is visible in your current space ' \
+                                    "'#{space.name}' with guid '#{space.guid}'.",
                         'title' => 'CF-UnprocessableEntity',
                         'code' => 10_008
                       })
@@ -1276,7 +1279,9 @@ RSpec.describe 'V3 service instances' do
             expect(last_response).to have_status_code(422)
             expect(parsed_response['errors']).to include(
               include({
-                        'detail' => 'Invalid service plan. Ensure that the service plan exists, is available, and you have access to it.',
+                        'detail' => "Invalid service plan. The service plan '#{service_plan.name}' with guid '#{service_plan.guid}' " \
+                                    "has been removed from the service broker's catalog. " \
+                                    'It is not possible to create new service instances using this plan.',
                         'title' => 'CF-UnprocessableEntity',
                         'code' => 10_008
                       })
@@ -2404,6 +2409,26 @@ RSpec.describe 'V3 service instances' do
           end
         end
 
+        context 'not enabled in that org' do
+          let(:service_plan) { VCAP::CloudController::ServicePlan.make(public: false, active: true) }
+          let(:service_plan_guid) { service_plan.guid }
+
+          it 'fails saying the plan is invalid' do
+            api_call.call(admin_headers)
+            expect(last_response).to have_status_code(422)
+            expect(parsed_response['errors']).to include(
+              include({
+                        'detail' => 'Invalid service plan. This could be due to a space-scoped broker which is offering the service plan ' \
+                                    "'#{service_plan.name}' with guid '#{service_plan.guid}' in another space or that the plan " \
+                                    'is not enabled in this organization. Ensure that the service plan is visible in your current space ' \
+                                    "'#{space.name}' with guid '#{space.guid}'.",
+                        'title' => 'CF-UnprocessableEntity',
+                        'code' => 10_008
+                      })
+            )
+          end
+        end
+
         context 'not available' do
           let(:service_plan) { VCAP::CloudController::ServicePlan.make(public: true, active: false) }
           let(:service_plan_guid) { service_plan.guid }
@@ -2413,7 +2438,9 @@ RSpec.describe 'V3 service instances' do
             expect(last_response).to have_status_code(422)
             expect(parsed_response['errors']).to include(
               include({
-                        'detail' => 'Invalid service plan. Ensure that the service plan exists, is available, and you have access to it.',
+                        'detail' => "Invalid service plan. The service plan '#{service_plan.name}' with guid '#{service_plan.guid}' " \
+                                    "has been removed from the service broker's catalog. " \
+                                    'It is not possible to create new service instances using this plan.',
                         'title' => 'CF-UnprocessableEntity',
                         'code' => 10_008
                       })
