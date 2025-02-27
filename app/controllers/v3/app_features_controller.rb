83c3db5e72631c5b504938207fdd7499c8d6da48
Sven Krieger <37476281+svkrieger@users.noreply.github.com>
2025-02-27 12:39:24 +0100

    Introduce 'file-based-vcap-services' app feature

--- app/controllers/v3/app_features_controller.rb
+++ app/controllers/v3/app_features_controller.rb
@@ -3,6 +3,7 @@ require 'controllers/v3/mixins/app_sub_resource'
 require 'presenters/v3/app_ssh_feature_presenter'
 require 'presenters/v3/app_revisions_feature_presenter'
 require 'presenters/v3/app_service_binding_k8s_feature_presenter'
+require 'presenters/v3/app_file_based_vcap_services_feature_presenter'
 require 'presenters/v3/app_ssh_status_presenter'
 require 'actions/app_feature_update'
 
@@ -12,8 +13,9 @@ class AppFeaturesController < ApplicationController
   SSH_FEATURE = 'ssh'.freeze
   REVISIONS_FEATURE = 'revisions'.freeze
   SERVICE_BINDING_K8S_FEATURE = 'service-binding-k8s'.freeze
+  FILE_BASED_VCAP_SERVICES_FEATURE = 'file-based-vcap-services'.freeze
 
-  TRUSTED_APP_FEATURES = [SSH_FEATURE, SERVICE_BINDING_K8S_FEATURE].freeze
+  TRUSTED_APP_FEATURES = [SSH_FEATURE, SERVICE_BINDING_K8S_FEATURE, FILE_BASED_VCAP_SERVICES_FEATURE].freeze
   UNTRUSTED_APP_FEATURES = [REVISIONS_FEATURE].freeze
   APP_FEATURES = (TRUSTED_APP_FEATURES + UNTRUSTED_APP_FEATURES).freeze
 
@@ -53,6 +55,10 @@ class AppFeaturesController < ApplicationController
     message = VCAP::CloudController::AppFeatureUpdateMessage.new(hashed_params['body'])
     unprocessable!(message.errors.full_messages) unless message.valid?
 
+    if message.enabled && both_service_binding_features_enabled?(app, name)
+      unprocessable!("'file-based-vcap-services' and 'service-binding-k8s' features cannot be enabled at the same time.")
+    end
+
     AppFeatureUpdate.update(hashed_params[:name], app, message)
     render status: :ok, json: feature_presenter_for(hashed_params[:name], app)
   end
@@ -83,7 +89,8 @@ class AppFeaturesController < ApplicationController
     presenters = {
       SSH_FEATURE => Presenters::V3::AppSshFeaturePresenter,
       REVISIONS_FEATURE => Presenters::V3::AppRevisionsFeaturePresenter,
-      SERVICE_BINDING_K8S_FEATURE => Presenters::V3::AppServiceBindingK8sFeaturePresenter
+      SERVICE_BINDING_K8S_FEATURE => Presenters::V3::AppServiceBindingK8sFeaturePresenter,
+      FILE_BASED_VCAP_SERVICES_FEATURE => Presenters::V3::AppFileBasedVcapServicesFeaturePresenter
     }
     presenters[feature_name].new(app)
   end
@@ -92,7 +99,16 @@ class AppFeaturesController < ApplicationController
     [
       Presenters::V3::AppSshFeaturePresenter.new(app),
       Presenters::V3::AppRevisionsFeaturePresenter.new(app),
-      Presenters::V3::AppServiceBindingK8sFeaturePresenter.new(app)
+      Presenters::V3::AppServiceBindingK8sFeaturePresenter.new(app),
+      Presenters::V3::AppFileBasedVcapServicesFeaturePresenter.new(app)
     ]
+  end
+
+  def both_service_binding_features_enabled?(app, feature_name)
+    if feature_name == 'file-based-vcap-services'
+      app.service_binding_k8s_enabled
+    elsif feature_name == 'service-binding-k8s'
+      app.file_based_vcap_services_enabled
+    end
   end
 end
