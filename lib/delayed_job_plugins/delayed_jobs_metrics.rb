b8b233da3fc4d38690065bf79b366a8094f97ee6
D059372 <johannes.haass@sap.com>
2025-04-09 14:18:20 +0200

    Add pickup and duration time metrics for processed jobs
    This change adds a new delayed job plugin which generates the following
    metrics after a cc-worker processed a job.
    - cc_job_pickup_delay_seconds: the difference of `run_at` and `locked_at`
    - cc_job_duration_seconds: Time difference of `locked_at` and current time.
    
    Both metrics are labeled with the name of the worker and the queue of the job to allow further aggregation etc.

---
+++ lib/delayed_job_plugins/delayed_jobs_metrics.rb
@@ -0,0 +1,21 @@
+module DelayedJobMetrics
+  class Plugin < Delayed::Plugin
+    def self.prometheus
+      @prometheus ||= CloudController::DependencyLocator.instance.cc_worker_prometheus_updater
+    end
+
+    callbacks do |lifecycle|
+      lifecycle.after(:perform) do |worker, job|
+        labels = { queue: job.queue, worker: worker.name }
+
+        job_pickup_delay = job.locked_at && job.run_at ? job.locked_at - job.run_at : nil
+        prometheus.update_histogram_metric(:cc_job_pickup_delay_seconds, job_pickup_delay, labels:) if job_pickup_delay
+
+        job_duration = job.locked_at ? Time.now.utc - job.locked_at : nil
+        prometheus.update_histogram_metric(:cc_job_duration_seconds, job_duration, labels:) if job_duration
+      end
+    end
+  end
+end
+
+Delayed::Worker.plugins << DelayedJobMetrics::Plugin
---
+++ spec/unit/lib/delayed_job_plugins/delayed_jobs_metrics_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+RSpec.describe DelayedJobMetrics::Plugin do
+  let(:prometheus) { instance_double(VCAP::CloudController::Metrics::PrometheusUpdater) }
+  let(:worker) { instance_double(Delayed::Worker, name: 'test_worker') }
+
+  before do
+    allow(CloudController::DependencyLocator.instance).to receive(:cc_worker_prometheus_updater).and_return(prometheus)
+    allow(prometheus).to receive(:update_histogram_metric)
+  end
+
+  it 'processes a job and updates Prometheus metrics with simulated time delay' do
+    Timecop.freeze(Time.now) do
+      events_cleanup_job = VCAP::CloudController::Jobs::Runtime::EventsCleanup.new(10_000)
+      VCAP::CloudController::Jobs::Enqueuer.new({ queue: VCAP::CloudController::Jobs::Queues.generic }).enqueue(events_cleanup_job)
+
+      events_cleanup_job = Delayed::Job.last
+      expect(events_cleanup_job).not_to be_nil
+
+      Timecop.travel(10.seconds) do
+        worker = Delayed::Worker.new
+        worker.name = 'test_worker'
+        worker.work_off(1)
+
+        expect(prometheus).to have_received(:update_histogram_metric).with(
+          :cc_job_pickup_delay_seconds,
+          be_within(0.5).of(10.0),
+          labels: { queue: VCAP::CloudController::Jobs::Queues.generic, worker: 'test_worker' }
+        ).once
+
+        expect(prometheus).to have_received(:update_histogram_metric).with(
+          :cc_job_duration_seconds,
+          kind_of(Numeric),
+          labels: { queue: VCAP::CloudController::Jobs::Queues.generic, worker: 'test_worker' }
+        ).once
+      end
+    end
+  end
+end
