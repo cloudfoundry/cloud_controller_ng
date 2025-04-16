9d652488ff99ec321989f6d3b77eb1378c38f896
Clemens Hoffmann <clemens.hoffmann@sap.com>
2025-04-16 14:03:06 +0200

    Notify diego outside of transaction

--- app/actions/route_update.rb
+++ app/actions/route_update.rb
@@ -2,14 +2,15 @@ module VCAP::CloudController
   class RouteUpdate
     def update(route:, message:)
       Route.db.transaction do
-        if message.requested?(:options)
-          route.options = route.options.symbolize_keys.merge(message.options).compact
-          route.apps.each do |process|
-            ProcessRouteHandler.new(process).notify_backend_of_route_update
-          end
-        end
+        route.options = route.options.symbolize_keys.merge(message.options).compact if message.requested?(:options)
         route.save
         MetadataUpdate.update(route, message)
+      end
+
+      if message.requested?(:options)
+        route.apps.each do |process|
+          ProcessRouteHandler.new(process).notify_backend_of_route_update
+        end
       end
       route
     end
