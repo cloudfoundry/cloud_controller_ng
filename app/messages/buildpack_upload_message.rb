908035d0aea10056f4f2c4abc08aeb1bfef86b45
Tom Kennedy <tom.kennedy@broadcom.com>
2025-04-24 10:32:00 -0400

    Add support for uploading buildpacks packaged as cnb (tar) files
    - This is a follow on to https://github.com/cloudfoundry/cloud_controller_ng/pull/3898 which only supports uploading CNBs as zip or tgz whereas they are released in cnb format
    
    Signed-off-by: Tom Kennedy <tom.kennedy@broadcom.com>

--- app/messages/buildpack_upload_message.rb
+++ app/messages/buildpack_upload_message.rb
@@ -3,6 +3,7 @@ require 'messages/base_message'
 module VCAP::CloudController
   GZIP_MIME = Regexp.new("\x1F\x8B\x08".force_encoding('binary'))
   ZIP_MIME = Regexp.new("PK\x03\x04".force_encoding('binary'))
+  CNB_MIME = Regexp.new("\x75\x73\x74\x61\x72\x00\x30\x30".force_encoding('binary'))
 
   class BuildpackUploadMessage < BaseMessage
     class MissingFilePathError < StandardError; end
@@ -53,7 +54,10 @@ module VCAP::CloudController
 
       return if mime_bits =~ /^#{VCAP::CloudController::GZIP_MIME}/ || mime_bits =~ /^#{VCAP::CloudController::ZIP_MIME}/
 
-      errors.add(:base, "#{bits_name} is not a zip or gzip archive")
+      mime_bits_at_offset = File.read(bits_path, 8, 257)
+      return if mime_bits_at_offset =~  /^#{VCAP::CloudController::CNB_MIME}/
+
+      errors.add(:base, "#{bits_name} is not a zip or gzip archive or cnb file")
     end
 
     def missing_file_path
