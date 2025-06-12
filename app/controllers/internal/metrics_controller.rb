32866b6a73466136f240415c91bd30a5a8f522fe
Sven Krieger <37476281+svkrieger@users.noreply.github.com>
2025-06-12 15:24:10 +0200

    Use separate webserver for metrics (#4377)

--- app/controllers/internal/metrics_controller.rb
+++ app/controllers/internal/metrics_controller.rb
@@ -3,6 +3,8 @@ require 'prometheus/client/formats/text'
 
 module VCAP::CloudController
   module Internal
+    # This controller is only used when the webserver is not Puma
+    # When using Puma, the metrics are served by a separate webserver in the main process
     class MetricsController < RestController::BaseController
       allow_unauthenticated_access
       get '/internal/v4/metrics', :index
