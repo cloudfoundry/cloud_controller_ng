6d37276dfa149168db8b465a69b47c665f02c8ff
Philipp Thun <philipp.thun@sap.com>
2025-07-29 15:19:25 +0200

    Bigint Migration for Multiple Tables (Step 1)
    - dedicated migration file for delayed_jobs, jobs, app_usage_events and
      service_usage_events
    - migration spec per table

---
+++ db/migrations/20250729143100_bigint_migration_service_usage_events_step1.rb
@@ -0,0 +1,32 @@
+require 'database/bigint_migration'
+
+table = :service_usage_events
+
+Sequel.migration do
+  up do
+    if database_type == :postgres && !VCAP::BigintMigration.opt_out?
+      if VCAP::BigintMigration.table_empty?(self, table)
+        VCAP::BigintMigration.change_pk_to_bigint(self, table)
+      else
+        VCAP::BigintMigration.add_bigint_column(self, table)
+        VCAP::BigintMigration.create_trigger_function(self, table)
+      end
+    end
+  end
+
+  down do
+    if database_type == :postgres
+      # There is no guarantee that the table is still empty - which was the condition for simply switching the id
+      # column's type to bigint. We nevertheless want to revert the type to integer as this is the opposite procedure of
+      # the up migration. In case there is a lot of data in the table at this moment in time, this change might be
+      # problematic, e.g. take a longer time.
+      #
+      # Ideally this down migration SHOULD NEVER BE EXECUTED IN A PRODUCTION SYSTEM! (It's there for proper integration
+      # testing of the bigint migration steps.)
+      VCAP::BigintMigration.revert_pk_to_integer(self, table)
+
+      VCAP::BigintMigration.drop_trigger_function(self, table)
+      VCAP::BigintMigration.drop_bigint_column(self, table)
+    end
+  end
+end
