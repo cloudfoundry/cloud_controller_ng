42b01f38a4ba5437c6d3abee529160c6487e7508
Clemens Hoffmann <clemens.hoffmann@sap.com>
2025-12-17 16:42:58 +0100

    Validate the route merged with the route update from the message.

--- app/actions/route_update.rb
+++ app/actions/route_update.rb
@@ -2,7 +2,24 @@ module VCAP::CloudController
   class RouteUpdate
     def update(route:, message:)
       Route.db.transaction do
-        route.options = route.options.symbolize_keys.merge(message.options).compact if message.requested?(:options)
+        if message.requested?(:options)
+          # Merge existing options with new options from message
+          existing_options = route.options&.deep_symbolize_keys || {}
+          merged_options = existing_options.merge(message.options).compact
+
+          # Remove hash-specific options if switching to non-hash loadbalancing
+          if merged_options[:loadbalancing] && merged_options[:loadbalancing] != 'hash'
+            merged_options.delete(:hash_header)
+            merged_options.delete(:hash_balance)
+          end
+
+          # Validate the merged options
+          validate_route_options!(merged_options)
+
+          # Set the options on the route
+          route.options = merged_options
+        end
+
         route.save
         MetadataUpdate.update(route, message)
       end
@@ -13,6 +30,26 @@ module VCAP::CloudController
         end
       end
       route
+    end
+
+    private
+
+    def validate_route_options!(options)
+      return if options.blank?
+
+      loadbalancing = options[:loadbalancing]
+      return if loadbalancing != 'hash'
+
+      hash_header = options[:hash_header]
+
+      if hash_header.blank?
+        error!('Hash header must be present when loadbalancing is set to hash')
+      end
+
+    end
+
+    def error!(message)
+      raise Error.new(message)
     end
   end
 end
