82d586fa2e48f6de3420316ac90b46303397a04d
Greg Cobb <gcobb@vmware.com>
2026-01-27 00:13:23 -0800

    Remove Spork preloader
    - Spork is old and unsupported
    - I wasn't able to get it working with modern ruby
    - Spring is the modern implementation of the same concept

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,28 +3,5 @@ require 'rubygems'
 require 'mock_redis'
 require 'spec_helper_helper'
 
-begin
-  require 'spork'
-  # uncomment the following line to use spork with the debugger
-  # require 'spork/ext/ruby-debug'
-
-  run_spork = !`ps | grep spork | grep -v grep`.empty?
-rescue LoadError
-  run_spork = false
-end
-
-if run_spork
-  Spork.prefork do
-    # Loading more in this block will cause your tests to run faster. However,
-    # if you change any configuration or code from libraries loaded here, you'll
-    # need to restart spork for it to take effect.
-    SpecHelperHelper.init
-  end
-  Spork.each_run do
-    # This code will be run each time you run your specs.
-    SpecHelperHelper.each_run
-  end
-else
-  SpecHelperHelper.init
-  SpecHelperHelper.each_run
-end
+SpecHelperHelper.init
+SpecHelperHelper.each_run
