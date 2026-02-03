6864feead1846c1467be74b44c5ebe90c0ad0b70
Rashed Kamal <rashed.kamal@broadcom.com>
2026-02-03 18:02:10 -0500

    Build a hash when updating stack state and state_reason
    Signed-off-by: Rashed Kamal <rashed.kamal@broadcom.com>

--- app/actions/stack_update.rb
+++ app/actions/stack_update.rb
@@ -12,8 +12,11 @@ module VCAP::CloudController
 
     def update(stack, message)
       stack.db.transaction do
-        stack.update(state: message.state) if message.requested?(:state)
-        stack.update(state_reason: message.state_reason) if message.requested?(:state_reason)
+        stack_updates = {}
+        stack_updates[:state] = message.state if message.requested?(:state)
+        stack_updates[:state_reason] = message.state_reason if message.requested?(:state_reason)
+        stack.update(stack_updates) if stack_updates.any?
+
         MetadataUpdate.update(stack, message)
         Repositories::StackEventRepository.new.record_stack_update(stack, @user_audit_info, message.audit_hash)
       end
