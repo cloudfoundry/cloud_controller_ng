f0f6c66220a35d59019098d4657efb88f780417a
rkoster <hi@rkoster.dev>
2026-02-20 08:34:18 +0000

    style: fix rubocop offenses in byte_quantity files
    - Use single-quoted strings
    - Use do/end for multi-line blocks
    - Use sprintf instead of String#%
    - Use self-assignment shorthand (*=)
    - Use unless instead of if with negation
    - Use nil? predicate instead of == nil
    - Remove redundant return statements
    - Remove redundant self
    - Add underscores to large numeric literals
    - Provide exception object to raise

--- lib/cloud_controller/byte_quantity.rb
+++ lib/cloud_controller/byte_quantity.rb
@@ -14,7 +14,7 @@ module VCAP
       BYTESPATTERN = /^(-?\d+(?:\.\d+)?)([KMGT]i?B?|B)$/i
 
       class InvalidByteQuantityError < RuntimeError
-        def initialize(msg="byte quantity must be a positive integer with a unit of measurement like M, MB, MiB, G, GiB, or GB")
+        def initialize(msg='byte quantity must be a positive integer with a unit of measurement like M, MB, MiB, G, GiB, or GB')
           super
         end
       end
@@ -29,32 +29,30 @@ module VCAP
       # The unit that results in the smallest number greater than or equal to 1 is
       # always chosen.
       def self.byte_size(bytes)
-        if !bytes.is_a? Numeric
-          raise TypeError, "must be an integer or float"
-        end
+        raise TypeError.new('must be an integer or float') unless bytes.is_a? Numeric
 
         case
         when bytes >= TERABYTE
-          unit = "T"
+          unit = 'T'
           value = bytes / TERABYTE
         when bytes >= GIGABYTE
-          unit = "G"
+          unit = 'G'
           value = bytes / GIGABYTE
         when bytes >= MEGABYTE
-          unit = "M"
+          unit = 'M'
           value = bytes / MEGABYTE
         when bytes >= KILOBYTE
-          unit = "K"
+          unit = 'K'
           value = bytes / KILOBYTE
         when bytes >= BYTE
-          unit = "B"
+          unit = 'B'
           value = bytes
         else
-          return "0"
+          return '0'
         end
 
-        value = "%g" % ("%.1f" % value)
-        return value << unit
+        value = sprintf('%g', sprintf('%.1f', value))
+        value << unit
       end
 
       # Parses a string formatted by bytes_size as bytes. Note binary-prefixed and
@@ -65,31 +63,29 @@ module VCAP
       # * TB = T = TiB = 1024 * G
       def self.to_bytes(bytes)
         matches = BYTESPATTERN.match(bytes.strip)
-        if matches == nil
-          raise InvalidByteQuantityError
-        end
+        raise InvalidByteQuantityError if matches.nil?
 
         value = Float(matches[1])
 
         case matches[2][0].capitalize
-        when "T"
-          value = value * TERABYTE
-        when "G"
-          value = value * GIGABYTE
-        when "M"
-          value = value * MEGABYTE
-        when "K"
-          value = value * KILOBYTE
+        when 'T'
+          value *= TERABYTE
+        when 'G'
+          value *= GIGABYTE
+        when 'M'
+          value *= MEGABYTE
+        when 'K'
+          value *= KILOBYTE
         end
 
-        return value.to_i
+        value.to_i
       rescue TypeError
         raise InvalidByteQuantityError
       end
 
       # Parses a string formatted by byte_size as megabytes.
       def self.to_megabytes(bytes)
-        (self.to_bytes(bytes) / MEGABYTE).to_i
+        (to_bytes(bytes) / MEGABYTE).to_i
       end
     end
   end
--- spec/unit/lib/cloud_controller/byte_quantity_spec.rb
+++ spec/unit/lib/cloud_controller/byte_quantity_spec.rb
@@ -8,70 +8,70 @@ require 'cloud_controller/byte_quantity'
 
 module VCAP::CloudController
   RSpec.describe ByteQuantity do
-    describe "#byte_size" do
-      it "prints in the largest possible unit" do
-        expect(ByteQuantity.byte_size(10 * ByteQuantity::TERABYTE)).to eq("10T")
-        expect(ByteQuantity.byte_size(10.5 * ByteQuantity::TERABYTE)).to eq("10.5T")
+    describe '#byte_size' do
+      it 'prints in the largest possible unit' do
+        expect(ByteQuantity.byte_size(10 * ByteQuantity::TERABYTE)).to eq('10T')
+        expect(ByteQuantity.byte_size(10.5 * ByteQuantity::TERABYTE)).to eq('10.5T')
 
-        expect(ByteQuantity.byte_size(10 * ByteQuantity::GIGABYTE)).to eq("10G")
-        expect(ByteQuantity.byte_size(10.5 * ByteQuantity::GIGABYTE)).to eq("10.5G")
+        expect(ByteQuantity.byte_size(10 * ByteQuantity::GIGABYTE)).to eq('10G')
+        expect(ByteQuantity.byte_size(10.5 * ByteQuantity::GIGABYTE)).to eq('10.5G')
 
-        expect(ByteQuantity.byte_size(100 * ByteQuantity::MEGABYTE)).to eq("100M")
-        expect(ByteQuantity.byte_size(100.5 * ByteQuantity::MEGABYTE)).to eq("100.5M")
+        expect(ByteQuantity.byte_size(100 * ByteQuantity::MEGABYTE)).to eq('100M')
+        expect(ByteQuantity.byte_size(100.5 * ByteQuantity::MEGABYTE)).to eq('100.5M')
 
-        expect(ByteQuantity.byte_size(100 * ByteQuantity::KILOBYTE)).to eq("100K")
-        expect(ByteQuantity.byte_size(100.5 * ByteQuantity::KILOBYTE)).to eq("100.5K")
+        expect(ByteQuantity.byte_size(100 * ByteQuantity::KILOBYTE)).to eq('100K')
+        expect(ByteQuantity.byte_size(100.5 * ByteQuantity::KILOBYTE)).to eq('100.5K')
 
-        expect(ByteQuantity.byte_size(1)).to eq("1B")
+        expect(ByteQuantity.byte_size(1)).to eq('1B')
       end
 
       it "prints '0' for zero bytes" do
-        expect(ByteQuantity.byte_size(0)).to eq("0")
+        expect(ByteQuantity.byte_size(0)).to eq('0')
       end
 
-      it "raises a type error on non-number values" do
-        expect {
-          ByteQuantity.byte_size("something else")
-        }.to raise_error(TypeError, "must be an integer or float")
+      it 'raises a type error on non-number values' do
+        expect do
+          ByteQuantity.byte_size('something else')
+        end.to raise_error(TypeError, 'must be an integer or float')
       end
     end
 
-    describe "#to_bytes" do
-      it "parses byte amounts with short units (e.g. M, G)" do
-        expect(ByteQuantity.to_bytes("5B")).to eq(5)
-        expect(ByteQuantity.to_bytes("5K")).to eq(5 * ByteQuantity::KILOBYTE)
-        expect(ByteQuantity.to_bytes("5M")).to eq(5 * ByteQuantity::MEGABYTE)
-        expect(ByteQuantity.to_bytes("5G")).to eq(5 * ByteQuantity::GIGABYTE)
-        expect(ByteQuantity.to_bytes("5T")).to eq(5 * ByteQuantity::TERABYTE)
+    describe '#to_bytes' do
+      it 'parses byte amounts with short units (e.g. M, G)' do
+        expect(ByteQuantity.to_bytes('5B')).to eq(5)
+        expect(ByteQuantity.to_bytes('5K')).to eq(5 * ByteQuantity::KILOBYTE)
+        expect(ByteQuantity.to_bytes('5M')).to eq(5 * ByteQuantity::MEGABYTE)
+        expect(ByteQuantity.to_bytes('5G')).to eq(5 * ByteQuantity::GIGABYTE)
+        expect(ByteQuantity.to_bytes('5T')).to eq(5 * ByteQuantity::TERABYTE)
       end
 
-      it "parses byte amounts that are float (e.g. 5.3KB)" do
-        expect(ByteQuantity.to_bytes("13.5KB")).to eq(13824)
-        expect(ByteQuantity.to_bytes("4.5KB")).to eq(4608)
-        expect(ByteQuantity.to_bytes("2.55KB")).to eq(2611)
+      it 'parses byte amounts that are float (e.g. 5.3KB)' do
+        expect(ByteQuantity.to_bytes('13.5KB')).to eq(13_824)
+        expect(ByteQuantity.to_bytes('4.5KB')).to eq(4608)
+        expect(ByteQuantity.to_bytes('2.55KB')).to eq(2611)
       end
 
-      it "parses byte amounts with long units (e.g MB, GB)" do
-        expect(ByteQuantity.to_bytes("5MB")).to eq(5 * ByteQuantity::MEGABYTE)
-        expect(ByteQuantity.to_bytes("5mb")).to eq(5 * ByteQuantity::MEGABYTE)
-        expect(ByteQuantity.to_bytes("2GB")).to eq(2 * ByteQuantity::GIGABYTE)
-        expect(ByteQuantity.to_bytes("3TB")).to eq(3 * ByteQuantity::TERABYTE)
+      it 'parses byte amounts with long units (e.g MB, GB)' do
+        expect(ByteQuantity.to_bytes('5MB')).to eq(5 * ByteQuantity::MEGABYTE)
+        expect(ByteQuantity.to_bytes('5mb')).to eq(5 * ByteQuantity::MEGABYTE)
+        expect(ByteQuantity.to_bytes('2GB')).to eq(2 * ByteQuantity::GIGABYTE)
+        expect(ByteQuantity.to_bytes('3TB')).to eq(3 * ByteQuantity::TERABYTE)
       end
 
-      it "parses byte amounts with long binary units (e.g MiB, GiB)" do
-        expect(ByteQuantity.to_bytes("5MiB")).to eq(5 * ByteQuantity::MEGABYTE)
-        expect(ByteQuantity.to_bytes("5mib")).to eq(5 * ByteQuantity::MEGABYTE)
-        expect(ByteQuantity.to_bytes("2GiB")).to eq(2 * ByteQuantity::GIGABYTE)
-        expect(ByteQuantity.to_bytes("3TiB")).to eq(3 * ByteQuantity::TERABYTE)
+      it 'parses byte amounts with long binary units (e.g MiB, GiB)' do
+        expect(ByteQuantity.to_bytes('5MiB')).to eq(5 * ByteQuantity::MEGABYTE)
+        expect(ByteQuantity.to_bytes('5mib')).to eq(5 * ByteQuantity::MEGABYTE)
+        expect(ByteQuantity.to_bytes('2GiB')).to eq(2 * ByteQuantity::GIGABYTE)
+        expect(ByteQuantity.to_bytes('3TiB')).to eq(3 * ByteQuantity::TERABYTE)
       end
 
-      it "allows whitespace before and after the value" do
+      it 'allows whitespace before and after the value' do
         expect(ByteQuantity.to_bytes("\t\n\r 5MB ")).to eq(5 * ByteQuantity::MEGABYTE)
       end
 
       context 'when the byte amount is 0' do
         it 'returns 0 bytes' do
-          expect(ByteQuantity.to_bytes("0TB")).to eq(0)
+          expect(ByteQuantity.to_bytes('0TB')).to eq(0)
         end
       end
 
@@ -81,54 +81,54 @@ module VCAP::CloudController
         end
       end
 
-      context "raises an error" do
-        it "when the unit is missing" do
-          expect {
-            ByteQuantity.to_bytes("5")
-          }.to raise_error(ByteQuantity::InvalidByteQuantityError)
+      context 'when it raises an error' do
+        it 'raises when the unit is missing' do
+          expect do
+            ByteQuantity.to_bytes('5')
+          end.to raise_error(ByteQuantity::InvalidByteQuantityError)
         end
 
-        it "when the unit is unrecognized" do
-          expect {
-            ByteQuantity.to_bytes("5MBB")
-          }.to raise_error(ByteQuantity::InvalidByteQuantityError)
+        it 'raises when the unit is unrecognized' do
+          expect do
+            ByteQuantity.to_bytes('5MBB')
+          end.to raise_error(ByteQuantity::InvalidByteQuantityError)
 
-          expect {
-            ByteQuantity.to_bytes("5BB")
-          }.to raise_error(ByteQuantity::InvalidByteQuantityError)
+          expect do
+            ByteQuantity.to_bytes('5BB')
+          end.to raise_error(ByteQuantity::InvalidByteQuantityError)
         end
       end
     end
 
-    describe "#to_megabytes" do
-      it "parses byte amounts with short units (e.g. M, G)" do
-        expect(ByteQuantity.to_megabytes("5B")).to eq(0)
-        expect(ByteQuantity.to_megabytes("5K")).to eq(0)
-        expect(ByteQuantity.to_megabytes("5M")).to eq(5)
-        expect(ByteQuantity.to_megabytes("5m")).to eq(5)
-        expect(ByteQuantity.to_megabytes("5G")).to eq(5120)
-        expect(ByteQuantity.to_megabytes("5T")).to eq(5242880)
+    describe '#to_megabytes' do
+      it 'parses byte amounts with short units (e.g. M, G)' do
+        expect(ByteQuantity.to_megabytes('5B')).to eq(0)
+        expect(ByteQuantity.to_megabytes('5K')).to eq(0)
+        expect(ByteQuantity.to_megabytes('5M')).to eq(5)
+        expect(ByteQuantity.to_megabytes('5m')).to eq(5)
+        expect(ByteQuantity.to_megabytes('5G')).to eq(5120)
+        expect(ByteQuantity.to_megabytes('5T')).to eq(5_242_880)
       end
 
-      it "parses byte amounts with long units (e.g MB, GB)" do
-        expect(ByteQuantity.to_megabytes("5B")).to eq(0)
-        expect(ByteQuantity.to_megabytes("5KB")).to eq(0)
-        expect(ByteQuantity.to_megabytes("5MB")).to eq(5)
-        expect(ByteQuantity.to_megabytes("5mb")).to eq(5)
-        expect(ByteQuantity.to_megabytes("5GB")).to eq(5120)
-        expect(ByteQuantity.to_megabytes("5TB")).to eq(5242880)
+      it 'parses byte amounts with long units (e.g MB, GB)' do
+        expect(ByteQuantity.to_megabytes('5B')).to eq(0)
+        expect(ByteQuantity.to_megabytes('5KB')).to eq(0)
+        expect(ByteQuantity.to_megabytes('5MB')).to eq(5)
+        expect(ByteQuantity.to_megabytes('5mb')).to eq(5)
+        expect(ByteQuantity.to_megabytes('5GB')).to eq(5120)
+        expect(ByteQuantity.to_megabytes('5TB')).to eq(5_242_880)
       end
 
-      it "parses byte amounts with long binary units (e.g MiB, GiB)" do
-        expect(ByteQuantity.to_megabytes("5B")).to eq(0)
-        expect(ByteQuantity.to_megabytes("5KiB")).to eq(0)
-        expect(ByteQuantity.to_megabytes("5MiB")).to eq(5)
-        expect(ByteQuantity.to_megabytes("5mib")).to eq(5)
-        expect(ByteQuantity.to_megabytes("5GiB")).to eq(5120)
-        expect(ByteQuantity.to_megabytes("5TiB")).to eq(5242880)
+      it 'parses byte amounts with long binary units (e.g MiB, GiB)' do
+        expect(ByteQuantity.to_megabytes('5B')).to eq(0)
+        expect(ByteQuantity.to_megabytes('5KiB')).to eq(0)
+        expect(ByteQuantity.to_megabytes('5MiB')).to eq(5)
+        expect(ByteQuantity.to_megabytes('5mib')).to eq(5)
+        expect(ByteQuantity.to_megabytes('5GiB')).to eq(5120)
+        expect(ByteQuantity.to_megabytes('5TiB')).to eq(5_242_880)
       end
 
-      it "allows whitespace before and after the value" do
+      it 'allows whitespace before and after the value' do
         expect(ByteQuantity.to_megabytes("\t\n\r 5MB ")).to eq(5)
       end
 
@@ -144,21 +144,21 @@ module VCAP::CloudController
         end
       end
 
-      context "raises an error" do
-        it "when the unit is missing" do
-          expect {
-            ByteQuantity.to_megabytes("5")
-          }.to raise_error(ByteQuantity::InvalidByteQuantityError)
+      context 'when it raises an error' do
+        it 'raises when the unit is missing' do
+          expect do
+            ByteQuantity.to_megabytes('5')
+          end.to raise_error(ByteQuantity::InvalidByteQuantityError)
         end
 
-        it "when the unit is unrecognized" do
-          expect {
-            ByteQuantity.to_megabytes("5MBB")
-          }.to raise_error(ByteQuantity::InvalidByteQuantityError)
+        it 'raises when the unit is unrecognized' do
+          expect do
+            ByteQuantity.to_megabytes('5MBB')
+          end.to raise_error(ByteQuantity::InvalidByteQuantityError)
 
-          expect {
-            ByteQuantity.to_megabytes("5BB")
-          }.to raise_error(ByteQuantity::InvalidByteQuantityError)
+          expect do
+            ByteQuantity.to_megabytes('5BB')
+          end.to raise_error(ByteQuantity::InvalidByteQuantityError)
         end
       end
     end
