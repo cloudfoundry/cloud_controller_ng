25394158578fe6c584b1aa115bc4ccca23068b6e
Wei Quan <w.quan@sap.com>
2026-09-09 11:38:49 +0200

    Use zero-downtime constraint pattern for lifecycle_type NOT NULL migration

--- db/migrations/20260825120100_make_lifecycle_type_non_nullable.rb
+++ db/migrations/20260825120100_make_lifecycle_type_non_nullable.rb
@@ -1,13 +1,33 @@
 Sequel.migration do
+  no_transaction
+
   up do
-    alter_table(:apps)     { set_column_not_null :lifecycle_type }
-    alter_table(:droplets) { set_column_not_null :lifecycle_type }
-    alter_table(:builds)   { set_column_not_null :lifecycle_type }
+    %i[apps droplets builds].each do |table|
+      if database_type == :postgres
+        transaction do
+          alter_table(table) do
+            add_constraint({ name: :"#{table}_lifecycle_type_not_null", not_valid: true }) do
+              Sequel.lit('lifecycle_type IS NOT NULL')
+            end
+          end
+        end
+
+        VCAP::Migration.with_concurrent_timeout(self) do
+          run("ALTER TABLE #{table} VALIDATE CONSTRAINT #{table}_lifecycle_type_not_null")
+        end
+      else
+        alter_table(table) { set_column_not_null :lifecycle_type }
+      end
+    end
   end
 
   down do
-    alter_table(:apps)     { set_column_allow_null :lifecycle_type }
-    alter_table(:droplets) { set_column_allow_null :lifecycle_type }
-    alter_table(:builds)   { set_column_allow_null :lifecycle_type }
+    %i[apps droplets builds].each do |table|
+      if database_type == :postgres
+        alter_table(table) { drop_constraint(:"#{table}_lifecycle_type_not_null") }
+      else
+        alter_table(table) { set_column_allow_null :lifecycle_type }
+      end
+    end
   end
 end
