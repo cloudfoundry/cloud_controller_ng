306869db7f352804a2fa20476c7e0bd5c9442dc8
Wei Quan <w.quan@sap.com>
2026-09-25 10:02:52 +0200

    Add key method to Blob base clas

--- lib/cloud_controller/blobstore/blob.rb
+++ lib/cloud_controller/blobstore/blob.rb
@@ -2,6 +2,10 @@ module CloudController
   module Blobstore
     class Blob
       CACHE_ATTRIBUTES = %i[etag last_modified created_at content_length].freeze
+
+      def key
+        raise NotImplementedError
+      end
     end
   end
 end
